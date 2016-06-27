342fab4760b29277290844852448a5a7f2f1717b
Howard Bushouse <bushouse@stsci.edu>
2016-06-27 09:03:44 -0400

    Updated pipeline init to include new B7 version of calwebb_sloper

--- jwst/pipeline/__init__.py
+++ jwst/pipeline/__init__.py
@@ -5,5 +5,6 @@ from .calwebb_dark import DarkPipeline
 from .calwebb_image2 import Image2Pipeline
 from .calwebb_image3 import Image3Pipeline
 from .calwebb_sloper import SloperPipeline
+from .calwebb_sloper_b7 import SloperPipelineB7
 from .calwebb_spec2 import Spec2Pipeline
 from .linear_pipeline import TestLinearPipeline
