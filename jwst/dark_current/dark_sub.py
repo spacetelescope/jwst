fa2600649056a7d515c23a1a48888fa434297d58
Jane Morrison <morrison@as.arizona.edu>
2016-07-21 14:11:07 -0400

    Updated Dark Subtraction to use MIRI integration Dependent Darks

--- jwst/dark_current/dark_sub.py
+++ jwst/dark_current/dark_sub.py
@@ -37,12 +37,14 @@ def do_correction(input_model, dark_model, dark_output=None):
     """
 
     # Save some data params for easy use later
+    detector = input_model.meta.instrument.detector
     sci_nints = input_model.data.shape[0]
     sci_ngroups = input_model.data.shape[1]
     sci_nframes = input_model.meta.exposure.nframes
     sci_groupgap = input_model.meta.exposure.groupgap
     drk_nframes = dark_model.meta.exposure.nframes
     drk_groupgap = dark_model.meta.exposure.groupgap
+
 
     log.debug('Dark sub using nints=%d, ngroups=%d, nframes=%d, groupgap=%d',
                sci_nints, sci_ngroups, sci_nframes, sci_groupgap)
@@ -71,7 +73,14 @@ def do_correction(input_model, dark_model, dark_output=None):
 
         # Create a frame-averaged version of the dark data to match
         # the nframes and groupgap settings of the science data
-        averaged_dark = average_dark_frames(dark_model, sci_ngroups,
+        # If the data is MIRI data - then the darks are integration dependent and we average them
+        # with a seperate routine
+
+        if detector[:3] == 'MIR':
+            averaged_dark = average_MIRIdark_frames(dark_model, sci_nints, sci_ngroups,
+                                                    sci_nframes, sci_groupgap)
+        else:
+            averaged_dark = average_dark_frames(dark_model, sci_ngroups,
                                             sci_nframes, sci_groupgap)
 
         # Save the frame-averaged dark data that was just created,
@@ -94,6 +103,7 @@ def average_dark_frames(input_dark, ngroups, nframes, groupgap):
     """
     Averages the individual frames of data in a dark reference
     file to match the group structure of a science data set.
+    This routine is not used for MIRI (see average_MIRIdark_frames)
 
     Parameters
     ----------
@@ -157,6 +167,93 @@ def average_dark_frames(input_dark, ngroups, nframes, groupgap):
     return avg_dark
 
 
+def average_MIRIdark_frames(input_dark, nints, ngroups, nframes, groupgap):
+    """
+    Averages the individual frames of data in a dark reference
+    file to match the group structure of a science data set.
+    MIRI needs a seperate routine because the darks are integration dependent. 
+    We need an average dark for each dark integration 
+
+    Parameters
+    ----------
+    input_dark: dark data model
+        the input dark data
+
+    nints: int
+        number of integrations in the science data 
+
+    ngroups: int
+        number of groups in the science data set
+
+    nframes: int
+        number of frames per group in the science data set
+
+    groupgap: int
+        number of frames skipped between groups in the science data set
+
+    Returns
+    -------
+    avg_dark: dark data model
+        New dark object with averaged frames
+
+    """
+
+    # Create a model for the averaged dark data
+    dint = input_dark.data.shape[0]
+    dny = input_dark.data.shape[2]
+    dnx = input_dark.data.shape[3]
+    avg_dark = datamodels.DarkMIRIModel((dint, ngroups, dny, dnx))
+    avg_dark.update(input_dark)
+
+    # Do a direct copy of the 2-d DQ array into the new dark
+    avg_dark.dq = input_dark.dq
+
+
+    # check if the number of integrations in dark reference file
+    # is less than science data, if so then we only need to find the
+    # average for num_ints integrations (if science data only has
+    # 1 integration then there is no need to average the second integration
+    # of the dark refefence file) 
+    num_ints = dint
+    if(dint > nints):
+        num_ints = nints
+
+    # Loop over valid integrations in dark reference file
+    # Loop over the groups of the input science data, copying or
+    # averaging the dark frames to match the group structure
+
+    for it in range(num_ints):
+        start = 0
+        for group in range(ngroups):
+            end = start + nframes
+
+        # If there's only 1 frame per group, just copy the dark frames
+            if nframes == 1:
+                log.debug('copy dark frame %d', start)
+                avg_dark.data[it,group] = input_dark.data[it,start]
+                avg_dark.err[it,group] = input_dark.err[it,start]
+
+        # Otherwise average nframes into a new group: take the mean of
+        # the SCI arrays and the quadratic sum of the ERR arrays.
+            else:
+                log.debug('average dark frames %d to %d', start + 1, end)
+                avg_dark.data[it,group] = input_dark.data[it,start:end].mean(axis=0)
+                avg_dark.err[it,group] = np.sqrt(np.add.reduce(
+                        input_dark.err[it,start:end]**2, axis=0)) / (end - start)
+
+        # Skip over unused frames
+            start = end + groupgap
+
+    
+    # Reset some metadata values for the averaged dark
+    avg_dark.meta.exposure.nframes = nframes
+    avg_dark.meta.exposure.ngroups = ngroups
+    avg_dark.meta.exposure.groupgap = groupgap
+
+    
+    return avg_dark
+
+
 def subtract_dark(input, dark):
     """
     Subtracts dark current data from science arrays, combines
@@ -178,6 +275,8 @@ def subtract_dark(input, dark):
 
     """
 
+    dark_ints = dark.data.shape[0] -1
+
     log.debug("subtract_dark: nints=%d, ngroups=%d, size=%d,%d",
               input.meta.exposure.nints, input.meta.exposure.ngroups,
               input.data.shape[-1], input.data.shape[-2])
@@ -186,21 +285,21 @@ def subtract_dark(input, dark):
     # Create output as a copy of the input science data model
     output = input.copy()
 
+    
     # combine the science and dark DQ arrays
     detector = input.meta.instrument.detector
     if(detector[:3] == 'MIR'):
-           output.pixeldq = np.bitwise_or(input.pixeldq, dark.dq[0,0,:,:])
+           output.pixeldq = np.bitwise_or(input.pixeldq, np.bitwise_or(dark.dq[0,0,:,:], dark.dq[1,0,:,:]))
     else:
            output.pixeldq = np.bitwise_or(input.pixeldq, dark.dq)
-
 
 
     # loop over all integrations and groups in input science data
     for i in range(input.data.shape[0]):
         if(detector[:3] =='MIR'):
 
-            if(i == 0): dark_int = dark.data[0]
-            if(i > 0): dark_int = dark.data[1]
+            if(i <= dark_ints): dark_int = dark.data[i]
+            if(i > dark_ints): dark_int = dark.data[dark_ints]
 
         for j in range(input.data.shape[1]):
             # subtract the SCI arrays
