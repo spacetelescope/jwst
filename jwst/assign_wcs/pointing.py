f78101099442b4c56441176e550af7a9ae9ddc39
Nadia Dencheva <nadia.dencheva@gmail.com>
2016-07-25 17:47:53 -0400

    add pointing transform to NIRSPEC modes

--- jwst/assign_wcs/pointing.py
+++ jwst/assign_wcs/pointing.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, unicode_literals, division,
                         print_function)
-
+import numpy as np
 from ..datamodels import fits_support
 from gwcs import utils as gwutils
 
@@ -13,4 +13,33 @@ def create_fitswcs_transform(input_model):
     hdu = fits_support.get_hdu(ff._hdulist, "PRIMARY")
     header = hdu.header
     transform = gwutils.make_fitswcs_transform(header)
+    return transform
+
+
+def fitswcs_transform_from_model(input_model):
+    """
+    Create a fits wcs from a datamodel.meta.wcsinfo.
+    """
+    wcsinfo = {}
+    wcsaxes = input_model._instance['meta']['wcsinfo']['wcsaxes']
+    wcsinfo['WCSAXES'] = wcsaxes
+    for key in ['CTYPE', 'CRPIX', 'CRVAL', 'CDELT', 'CUNIT']:
+        val = []
+        for ax in range(1, wcsaxes + 1):
+            try:
+                val.append(input_model._instance['meta']['wcsinfo'][(key + "{0}".format(ax)).lower()])
+            except KeyError:
+                pass
+        wcsinfo[key.upper()] = val
+
+    pc = np.zeros((wcsaxes, 2))
+
+    for i in range(1, wcsaxes + 1):
+        for j in range(1, 3):
+            pc[i-1, j-1] = input_model._instance['meta']['wcsinfo']['pc{0}_{1}'.format(i, j)]
+    wcsinfo['PC'] = pc
+    # getting "coordinates" from the schema is currently broken.
+    wcsinfo['RADESYS'] = 'ICRS' #input_model._instance['meta']['coordinates']['reference_frame']
+    wcsinfo['has_cd'] = False
+    transform = gwutils.make_fitswcs_transform(wcsinfo)
     return transform
