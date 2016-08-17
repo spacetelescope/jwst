f1bd59c8a5ac6a48c462ca84672e0d5400aff7fe
Howard Bushouse <bushouse@stsci.edu>
2016-08-16 21:38:59 -0400

    Update coron StackRefs step inputs

--- jwst/coron/stack_refs.py
+++ jwst/coron/stack_refs.py
@@ -12,32 +12,42 @@ import logging
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-def make_cube(input_table):
+def make_cube(input_models):
     """
-    make_cube: Stack individual coronagraphic PSF reference images into a
-    jwst.datamodels.CubeModel, for use in LOCI/KLIP processing.
+    make_cube: Stack all of the integrations from multiple PSF
+    reference exposures into a single CubeModel, for use in the
+    coronagraphic alignment and PSF-subtraction steps.
     """
 
     # Get the number of input images
-    num_refs = len(input_table.input_filenames)
+    num_refs = len(input_models)
 
-    # Get the size of the first input image
-    img = datamodels.ImageModel(input_table.input_filenames[0])
-    nrows, ncols = img.data.shape
-    img.close()
+    # Loop over all the inputs to find the total number of integrations
+    nrows_ref, ncols_ref = input_models[0].shape[-2:]
+    nints = 0
+    for i in range(num_refs):
+        nints += input_models[i].shape[0]
+        nrows, ncols = input_models[i].shape[-2:]
+        if (nrows != nrows_ref) or (ncols != ncols_ref):
+            raise ValueError('All PSF exposures must have the same x/y dimensions!')
 
-    # Create an empty cube array of the appropriate dimensions
-    cube = np.zeros((num_refs, nrows, ncols), dtype=np.float32)
+    # Create empty output data arrays of the appropriate dimensions
+    outdata = np.zeros((nints, nrows, ncols), dtype=np.float32)
+    outerr = outdata.copy()
+    outdq = np.zeros((nints, nrows, ncols), dtype=np.uint32)
 
-    # Loop over the input images, copying only the science data array
-    # into the cube
-    for i, name in enumerate(input_table.input_filenames):
-        log.info('Adding member %s', name)
-        img = datamodels.ImageModel(name)
-        cube[i] = img.data
-        img.close()
+    # Loop over the input images, copying the data arrays
+    # into the output arrays
+    nint = 0
+    for i in range(num_refs):
+        log.info(' Adding psf member %d to output stack', i+1)
+        for j in range(input_models[i].shape[0]):
+            outdata[nint] = input_models[i].data[j]
+            outerr[nint] = input_models[i].err[j]
+            outdq[nint] = input_models[i].dq[j]
+            nint += 1
 
     # Create the ouput Cube model
-    output_model = datamodels.CubeModel(data=cube)
+    output_model = datamodels.CubeModel(data=outdata, err=outerr, dq=outdq)
 
     return output_model
