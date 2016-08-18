fba5c01d6596183f909e0b0055b1a906307eaa52
J.D.Eisenhamer <eisenhamer@stsci.edu>
2016-08-18 16:00:02 -0400

    Add test for model roundtrip to and from files

--- jwst/exp_to_source/tests/test_exp_to_source.py
+++ jwst/exp_to_source/tests/test_exp_to_source.py
@@ -1,18 +1,26 @@
 from glob import iglob
 import os
+import pytest
+import tempfile
 
-from ...datamodels import MultiSlitModel
+from ...datamodels import (MultiExposureModel, MultiSlitModel)
 from ..exp_to_source import exp_to_source
 
 INPUT_FILES = 'data/jwst_nod*_cal.fits'
 
 
-def test_exp_to_source():
+@pytest.fixture
+def run_exp_to_source(scope='module'):
     inputs = [
         MultiSlitModel(f)
         for f in iglob(t_path(INPUT_FILES))
     ]
     outputs = exp_to_source(inputs)
+    return inputs, outputs
+
+
+def test_model_structure(run_exp_to_source):
+    inputs, outputs = run_exp_to_source
     assert len(outputs) == 5
     assert len(outputs[next(iter(outputs))].exposures) == 3
     for in_idx, in_model in enumerate(inputs):
@@ -22,6 +30,24 @@ def test_exp_to_source():
             assert len(exposure.meta._instance) == len(in_model.meta._instance)
             assert exposure.meta.filename == in_model.meta.filename
             assert outputs[slit.name].meta.filename != in_model.meta.filename
+
+
+def test_model_roundtrip(run_exp_to_source):
+    inputs, outputs = run_exp_to_source
+    files = []
+    with tempfile.TemporaryDirectory() as path:
+        for output in outputs:
+            file_path = os.path.join(path, output) + '.fits'
+            outputs[output].save(file_path)
+            files.append(file_path)
+        for file_path in files:
+            multiexposure_model = MultiExposureModel(file_path)
+            assert len(multiexposure_model.exposures) == 3
+            exp_files = set()
+            for exposure in multiexposure_model.exposures:
+                exp_files.add(exposure.meta.filename)
+            assert len(exp_files) == len(multiexposure_model.exposures)
+            assert multiexposure_model.meta.filename not in exp_files
 
 
 def t_path(partial_path):
