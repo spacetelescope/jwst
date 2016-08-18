0eac928d0d993aeb97930074b43fa293de761f95
J.D.Eisenhamer <eisenhamer@stsci.edu>
2016-08-18 10:18:14 -0400

    Add tests
    These were not included before due to issues
    of filesizes, which have been dealt with.

--- jwst/exp_to_source/tests/test_exp_to_source.py
+++ jwst/exp_to_source/tests/test_exp_to_source.py
@@ -1,15 +1,30 @@
-from jwst.datamodels import MultiSlitModel
+from glob import iglob
+import os
 
+from ...datamodels import MultiSlitModel
 from ..exp_to_source import exp_to_source
 
-INPUT_FILE = '/Users/eisenham/Documents/ssbdev/testdata/jwst_data/NIRSpec/MSA/NRSMOS-MODEL-23_NRS1_SloperPipeline_assign_wcs_extract_2d.fits'
+INPUT_FILES = 'data/jwst_nod*_cal.fits'
 
 
 def test_exp_to_source():
-    inputs = [MultiSlitModel(INPUT_FILE),
-              MultiSlitModel(INPUT_FILE)]
+    inputs = [
+        MultiSlitModel(f)
+        for f in iglob(t_path(INPUT_FILES))
+    ]
     outputs = exp_to_source(inputs)
-    assert len(outputs) == 15
-    assert len(outputs[outputs.keys()[0]].slits) == 2
-    assert (inputs[0].slits[0].data == outputs['S1001'].slits[0].data).all()
-    assert (inputs[1].slits[14].data == outputs['S1015'].slits[1].data).all()
+    assert len(outputs) == 5
+    assert len(outputs[next(iter(outputs))].exposures) == 3
+    for in_idx, in_model in enumerate(inputs):
+        for slit in in_model.slits:
+            exposure = outputs[slit.name].exposures[in_idx]
+            assert (exposure.data == slit.data).all()
+            assert len(exposure.meta._instance) == len(in_model.meta._instance)
+            assert exposure.meta.filename == in_model.meta.filename
+            assert outputs[slit.name].meta.filename != in_model.meta.filename
+
+
+def t_path(partial_path):
+    """Construction the full path for test files"""
+    test_dir = os.path.dirname(__file__)
+    return os.path.join(test_dir, partial_path)
