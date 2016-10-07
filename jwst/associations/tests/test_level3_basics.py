28b4bf5b89cc1f387be6be7dd28d623edab47a38
J.D.Eisenhamer <eisenhamer@stsci.edu>
2016-10-06 22:38:36 -0400

    Implement new attribute "asn_id"
    Resolves #322

--- jwst/associations/tests/test_level3_basics.py
+++ jwst/associations/tests/test_level3_basics.py
@@ -1,57 +1,24 @@
 """Test general Level 3 rules environment"""
 from __future__ import absolute_import
 
-import re
-
-from . import helpers
 from .helpers import (combine_pools, t_path)
 
 from .. import (AssociationRegistry, generate)
 
 
-# Level 3 product name templates
-L3_PRODUCT_NAME = (
-    'jw(?P<program>\d{5})'
-    '-(?P<acid>[a-z]\d{3,4})'
-    '_(?P<target>t\d{3})'
-    '_(?P<instrument>.+?)'
-    '_(?P<opt_elem>.+)'
-)
-
-
-class TestLevel3Environment(object):
-
-    def test_l35_productname(self):
-        rules = AssociationRegistry()
-        pool = combine_pools(t_path('data/pool_002_image_miri.csv'))
-        asns, orphaned = generate(pool, rules)
-        asn = asns[0]
-        match = re.match(L3_PRODUCT_NAME, asn['products'][0]['name'])
-        yield helpers.not_none, match
-        matches = match.groupdict()
-        yield helpers.check_equal, matches['program'], '99009'
-        yield helpers.check_equal, matches['acid'], 'a3001'
-        yield helpers.check_equal, matches['target'], 't001'
-        yield helpers.check_equal, matches['instrument'], 'miri'
-        yield helpers.check_equal, matches['opt_elem'], 'f560w'
-
-    def test_l3_productname(self):
-        global_constraints = {}
-        global_constraints['asn_candidate_ids'] = {
-            'value': '.+o001.+',
-            'inputs': ['ASN_CANDIDATE'],
-            'force_unique': True,
-            'is_acid': True,
-        }
-        rules = AssociationRegistry(global_constraints=global_constraints)
-        pool = combine_pools(t_path('data/pool_002_image_miri.csv'))
-        asns, orphaned = generate(pool, rules)
-        asn = asns[0]
-        match = re.match(L3_PRODUCT_NAME, asn['products'][0]['name'])
-        yield helpers.not_none, match
-        matches = match.groupdict()
-        yield helpers.check_equal, matches['program'], '99009'
-        yield helpers.check_equal, matches['acid'], 'o001'
-        yield helpers.check_equal, matches['target'], 't001'
-        yield helpers.check_equal, matches['instrument'], 'miri'
-        yield helpers.check_equal, matches['opt_elem'], 'f560w'
+def test_meta():
+    rules = AssociationRegistry()
+    pool = combine_pools(t_path('data/pool_002_image_miri.csv'))
+    asns, orphaned = generate(pool, rules)
+    assert len(asns) > 0
+    asn = asns[0]
+    data = asn.data
+    assert data['program'] == '99009'
+    assert data['target'] == '1'
+    assert data['asn_type'] == 'image'
+    assert data['asn_id'] == 'a3001'
+    assert data['asn_pool'] == 'pool_002_image_miri'
+    assert data['asn_rule'] == 'Asn_Image'
+    assert data['degraded_status'] == 'No known degraded exposures in association.'
+    assert data['version_id'] is None
+    assert data['constraints'] is not None
