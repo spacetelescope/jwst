3970811edc5a7977f712879f3470ef4996c32547
Nadia Dencheva <nadia.astropy@gmail.com>
2016-10-31 16:11:08 -0400

    Revert "Moved wavelength unit conversion earlier"

--- jwst/assign_wcs/nirspec.py
+++ jwst/assign_wcs/nirspec.py
@@ -520,10 +520,6 @@ def gwa_to_ifuslit(slits, disperser, wrange, order, reference_files):
     ymax = .55
     agreq = AngleFromGratingEquation(disperser['groove_density'], order, name='alpha_from_greq')
     lgreq = WavelengthFromGratingEquation(disperser['groove_density'], order, name='lambda_from_greq')
-    # The wavelength units up to this point are
-    # meters as required by the pipeline but the desired output wavelength units is microns.
-    # So we are going to Scale the spectral units by 1e6 (meters -> microns)
-    lgreq = lgreq | Scale(1e6)
     collimator2gwa = collimator_to_gwa(reference_files, disperser)
     mask = mask_slit(ymin, ymax)
 
@@ -578,10 +574,6 @@ def gwa_to_slit(open_slits, disperser, wrange, order, reference_files):
     """
     agreq = AngleFromGratingEquation(disperser['groove_density'], order, name='alpha_from_greq')
     lgreq = WavelengthFromGratingEquation(disperser['groove_density'], order, name='lambda_from_greq')
-    # The wavelength units up to this point are
-    # meters as required by the pipeline but the desired output wavelength units is microns.
-    # So we are going to Scale the spectral units by 1e6 (meters -> microns)
-    lgreq = lgreq | Scale(1e6)
     collimator2gwa = collimator_to_gwa(reference_files, disperser)
 
     msa = AsdfFile.open(reference_files['msa'])
@@ -897,9 +889,11 @@ def oteip_to_v23(reference_files):
         ote = f.tree['model'].copy()
     fore2ote_mapping = Identity(3, name='fore2ote_mapping')
     fore2ote_mapping.inverse = Mapping((0, 1, 2, 2))
-    # Create the transform to v2/v3/lambda.
+    # Create the transform to v2/v3/lambda.  The wavelength units up to this point are
+    # meters as required by the pipeline but the desired output wavelength units is microns.
+    # So we are going to Scale the spectral units by 1e6 (meters -> microns)
     # The spatial units are currently in deg. Convertin to arcsec.
-    oteip_to_xyan = fore2ote_mapping | (ote & Scale(1))
+    oteip_to_xyan = fore2ote_mapping | (ote & Scale(1e6))
     # Add a shift for the aperture.
     oteip2v23 = oteip_to_xyan | Identity(1) & (Shift(468 / 3600) | Scale(-1)) & Identity(1)
     return oteip2v23
