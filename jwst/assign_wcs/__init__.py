c881c3fa9c4352b3dfa1946babdee4b1509bafe9
Nadia Dencheva <nadia.dencheva@gmail.com>
2016-11-15 15:09:31 -0500

    remove old code [skip ci]

--- jwst/assign_wcs/__init__.py
+++ jwst/assign_wcs/__init__.py
@@ -3,27 +3,3 @@ from .assign_wcs_step import AssignWcsStep
 
 __version__ = '0.7.0'
 
-
-'''
-def test(verbose=False) :
-    import os, pytest
-
-    # get the pandokia plugin if it is available (it will only
-    # do anything if we are run from pandokia).
-    try :
-        import pandokia.helpers.pytest_plugin as pytest_plugin
-    except ImportError :
-        pytest_plugin = None
-
-    if pytest_plugin :
-        addplugins = [pytest_plugin]
-    else :
-        addplugins = None
-
-    from . import tests
-    dir = os.path.dirname(tests.__file__)
-
-    args=[dir]
-
-    return pytest.main(args)
-'''
