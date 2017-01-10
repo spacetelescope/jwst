70701d2bebdc34ae0195705852b722563a7d0c03
Bernie Simon <bsimon@stsci.edu>
2017-01-10 13:45:01 -0500

    Add tests for datamodels.open
    I added the test I had written for the generic open function to the
    datamodels test suite in test_open.py. I also created a data
    subdirectory in tests and added my test files.

--- jwst/datamodels/tests/test_open.py
+++ jwst/datamodels/tests/test_open.py
@@ -1,12 +1,31 @@
+from __future__ import absolute_import, unicode_literals, division, print_function
+
 """
 Test datamodel.open
 """
 
 import os
+import os.path
+import numpy as np
+from astropy.io import fits
 
 from .. import (DataModel, ModelContainer)
 from ..util import open
 
+from jwst import datamodels
+import jwst.datamodels.image
+import jwst.datamodels.cube
+import jwst.datamodels.reference
+
+from jwst.datamodels.reference import ReferenceFileModel, ReferenceImageModel, \
+                                ReferenceCubeModel, ReferenceQuadModel 
+from jwst.datamodels.flat import FlatModel
+from jwst.datamodels.mask import MaskModel
+from jwst.datamodels.photom import NircamPhotomModel
+from jwst.datamodels.gain import GainModel
+from jwst.datamodels.readnoise import ReadnoiseModel
+
+ROOT_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 def test_open_fits():
     """Test opening a model from a FITS file"""
@@ -23,6 +42,74 @@ def test_open_association():
     m = open(asn_file)
     assert isinstance(m, ModelContainer)
 
+def test_open_shape():
+    init = (200, 200)
+    model = jwst.datamodels.open(init)
+    assert type(model) == jwst.datamodels.image.ImageModel
+    model.close()
+
+def test_open_illegal():
+    init = 5
+    try:
+        model = jwst.datamodels.open(init)
+    except ValueError:
+        fail = 1
+    else:
+        fail = 0
+    assert fail == 1
+
+def test_open_hdulist():
+    hdulist = fits.HDUList()
+    data = np.empty((50, 50), dtype=np.float32)
+    primary = fits.PrimaryHDU()
+    hdulist.append(primary)
+    science = fits.ImageHDU(data=data, name='SCI')
+    hdulist.append(science)
+
+    model = jwst.datamodels.open(hdulist)
+    assert type(model) == jwst.datamodels.image.ImageModel
+    model.close()
+
+def test_open_image():
+    base_name = 'jwst_image.fits'
+    image_name = os.path.join(ROOT_DIR, base_name)
+    model = jwst.datamodels.open(image_name)
+    assert type(model) == jwst.datamodels.image.ImageModel
+    model.close()
+
+
+def test_open_reference_files():
+    files = {'nircam_flat.fits' : FlatModel,
+             'nircam_mask.fits' : MaskModel,
+             'nircam_photom.fits' : NircamPhotomModel,
+             'nircam_gain.fits' : GainModel,
+             'nircam_readnoise.fits' : ReadnoiseModel}
+    
+    for base_name, klass in files.items():
+        file = os.path.join(ROOT_DIR, base_name)
+        model = jwst.datamodels.open(file)
+        if model.shape:
+            ndim = len(model.shape)
+        else:
+            ndim = 0
+            
+        if ndim == 0:
+            my_klass = ReferenceFileModel
+        elif ndim == 2:
+            my_klass = ReferenceImageModel
+        elif ndim == 3:
+            my_klass = ReferenceCubeModel
+        elif ndim == 4:
+            my_klass = ReferenceQuadModel
+        else:
+            my_class = None
+            
+        assert isinstance(model, my_klass)
+        model.close()
+        
+        model = klass(file)
+        assert isinstance(model, klass)
+        model.close()
 
 # Utilities
 def t_path(partial_path):
