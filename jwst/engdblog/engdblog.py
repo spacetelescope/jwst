6a78261eef3970827105996f21b907fb5039b152
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-01-24 14:22:47 -0500

    Initial implementation of new step EngDBLogStep
    Resolves #600

---
+++ jwst/engdblog/engdblog.py
@@ -0,0 +1,83 @@
+
+from ..lib.engdb_tools import ENGDB_Service
+from ..stpipe import Step
+
+
+class EngDBLogStep(Step):
+    """Pipeline step to retreive selected engineering mnemonic values
+
+    Notes
+    -----
+    This is primarily developed to satisfy the following DMS requirements.
+    However, it has been developed in hopes that it may provide useful
+    diagnostics and/or as an example of step writing and engineering DB
+    access.
+
+    DMS-256 Calibration software shall access the DMS engineering database
+        in order to derive the engineering information needed for science
+        instrument calibration.
+
+    DMS-458 The Calibration software shall retrieve relevant engineering
+        data from the Engineering Database.
+    """
+
+    spec = """
+    stime = string(default='2016-01-01') # Start time
+    etime = string(default='2016-01-31') # End time
+    verbosity = option('initial', 'all', default='initial') # How much to report.
+    """
+
+    def process(self, mnemonics):
+        """
+        Step processing
+
+        Parameters
+        ----------
+        mnemonics: [str (, ...)]
+            The list of mnemonics to retrieve
+
+        Returns
+        -------
+        {mnemonic: values[, ...]}
+            `dict` of the specified mnemonics and their values
+        """
+
+        result = {}
+        stime = self.stime
+        etime = self.etime
+        verbosity = self.verbosity
+        edb = ENGDB_Service()
+
+        for mnemonic in mnemonics:
+            try:
+                values = edb.get_values(mnemonic, stime, etime)
+            except:
+                self.log.info(
+                    '{} has no entries in time range {}:{}'.format(
+                        mnemonic, stime, etime
+                    )
+                )
+                continue
+            if verbosity == 'initial':
+                result[mnemonic] = values[0]
+                self.log.info(
+                    '{}[{}:{}] = {}'.format(
+                        mnemonic,
+                        stime,
+                        etime,
+                        values[0]
+                    )
+                )
+            elif verbosity == 'all':
+                result[mnemonic] = values
+                self.log.info(
+                    '{}[{}:{}] = {}'.format(
+                        mnemonic,
+                        stime,
+                        etime,
+                        values
+                    )
+                )
+                next
+
+        return result
