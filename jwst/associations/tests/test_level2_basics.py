22a1c299e1e9234439c9fbdef57d632d52414a33
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-04-14 08:05:54 -0400

    Setup tests and test data for product-version of Level2
    WIP #639, #735

--- jwst/associations/tests/test_level2_basics.py
+++ jwst/associations/tests/test_level2_basics.py
@@ -14,51 +14,64 @@ from .. import (
     load_asn
 )
 
+NONSSCIENCE = ['BACKGROUND']
 
-@pytest.mark.xfail(reason='Temporarily removed Level2 associations')
-def test_level2_schema():
+
+def from_level2_schema():
     with open(t_path('data/asn_level2.json')) as asn_file:
         asn = load_asn(asn_file)
-    assert len(asn['members']) == 4
-    member = asn['members'][0]
-    assert member['expname'] == 'test_lrs1_rate.fits'
-    assert member['exptype'] == 'SCIENCE'
-    assert isinstance(member['bkgexps'], list)
-    bkg = member['bkgexps'][0]
-    assert bkg['expname'] == 'test_lrsbkg_rate.fits'
+    return [asn]
 
 
-def test_level2_image():
+def from_level2_image():
     """Test creation of a Level 2 image association"""
     rules = registry_level2_only()
     pool = combine_pools(t_path('data/pool_002_image_miri.csv'))
     asns, orphaned = generate(pool, rules)
-    assert len(asns) == 8
-    len(orphaned) == 0
-    asn = asns[0]
-    assert asn['asn_rule'] == 'Asn_Lv2Image'
-    assert asn['asn_type'] == 'image2'
-    assert len(asn['members']) == 1
-    member = asn['members'][0]
-    base_keys = {'expname', 'exptype'}
-    assert base_keys.issubset(member.keys())
-    assert member['expname'] == 'jw_00001_rate.fits'
-    assert member['exptype'] == 'SCIENCE'
+    return asns
 
 
-def test_level2_spec():
+def from_level2_spec():
     """Test creation of a Level 2 spectral association"""
     rules = registry_level2_only()
     pool = combine_pools(t_path('data/pool_007_spec_miri.csv'))
     asns, orphaned = generate(pool, rules)
-    assert len(asns) == 11
-    len(orphaned) == 0
-    asn = asns[0]
-    assert asn['asn_rule'] == 'Asn_Lv2Spec'
-    assert asn['asn_type'] == 'spec2'
-    assert len(asn['members']) == 1
-    member = asn['members'][0]
-    base_keys = {'expname', 'exptype'}
-    assert base_keys.issubset(member.keys())
-    assert member['expname'] == 'jw_00001_rate.fits'
-    assert member['exptype'] == 'SCIENCE'
+    return asns
+
+
+@pytest.mark.parametrize(
+    'asns, n_asns, n_products, asn_type, asn_rule',
+    [
+        (from_level2_schema(), 1, 6, 'image2', 'Asn_Lv2Image'),
+        (from_level2_image(), 1, 8, 'image2', 'Asn_Lv2Image'),
+        (from_level2_spec(), 1, 11, 'spec2', 'Asn_Lv2Spec'),
+    ]
+)
+def test_level2_basics(asns, n_asns, n_products, asn_type, asn_rule):
+    assert len(asns) == n_asns
+    for asn in asns:
+        assert asn['asn_type'] == asn_type
+        assert asn['asn_rule'] == asn_rule
+        products = asn['products']
+        assert len(products) == n_products
+        for product in products:
+            members = product['members']
+            assert len(members) > 0
+            science = [
+                member
+                for member in members
+                if member['exptype'] == 'SCIENCE'
+            ]
+            assert len(science) == 1
+            if len(members) > 1:
+                nonscience = [
+                    member
+                    for member in members
+                    if member['exptype'] != 'SCIENCE'
+                ]
+                assert len(nonscience) > 0
+                exptypes = set(
+                    member['exptype']
+                    for member in nonscience
+                )
+                assert exptypes.issubset(NONSSCIENCE)
