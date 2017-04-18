1f98066b32193dd4de3f1c2d26c1c55703331fbc
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-04-18 16:37:31 -0400

    Setup test for refactor of calXXX2 processing and associations
    WIP #825, #750

---
+++ jwst/pipeline/tests/test_calwebb_spec2.py
@@ -0,0 +1,75 @@
+"""Test calwebb_spec2"""
+
+from contextlib import contextmanager
+from os import path
+import pytest
+
+from ...associations.asn_from_list import asn_from_list
+from ...associations.lib.rules_level2_base import DMSLevel2bBase
+from ...datamodels import open as dm_open
+from ..calwebb_spec2 import Spec2Pipeline
+
+
+def abspath(filepath):
+    """Get the absolute file path"""
+    return path.abspath(path.expanduser(path.expandvars(filepath)))
+
+
+DATAPATH = abspath(
+    '$DEVDIR/testdata/jwst_data/dev/build7.1/spec2_test'
+)
+EXPFILE = 'jw00035001001_01101_00001_mirimage_rate.fits'
+
+
+# Skip if the data is not available
+pytestmark = pytest.mark.skipif(
+    not path.exists(DATAPATH),
+    reason='Test data not accessible'
+)
+
+
+@contextmanager
+def chdir(path):
+    """A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
+
+    """
+    prev_cwd = path.chdir()
+    try:
+        yield
+    finally:
+        prev_cwd.chdir()
+
+
+def test_asn(tmpdir):
+    exppath = path.join(DATAPATH, EXPFILE)
+    expcal = EXPFILE.replace('_rate', '_cal')
+    lv2_meta = {
+        'program': 'test',
+        'target': 'test',
+        'asn_pool': 'test',
+    }
+    asn = asn_from_list([exppath], rule=DMSLevel2bBase, meta=lv2_meta)
+    asn_file, serialized = asn.dump()
+    with tmpdir.join(asn_file).open('w') as fp:
+        fp.write(serialized)
+    with tmpdir.as_cwd():
+        Spec2Pipeline.call(asn_file)
+        assert path.isfile(expcal)
+
+
+@pytest.mark.xfail(reason='Not yet implemented')
+def test_datamodel(tmpdir):
+    model = dm_open(path.join(DATAPATH, EXPFILE))
+    expcal = EXPFILE.replace('_rate', '_cal')
+    with tmpdir.as_cwd():
+        Spec2Pipeline.call(model)
+        assert path.isfile(expcal)
+
+
+def test_file(tmpdir):
+    exppath = path.join(DATAPATH, EXPFILE)
+    expcal = EXPFILE.replace('_rate', '_cal')
+    with tmpdir.as_cwd():
+        Spec2Pipeline.call(exppath)
+        assert path.isfile(expcal)
