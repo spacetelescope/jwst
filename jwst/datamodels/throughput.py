a410d2c6e1fbccdb4558d071b2418e7ae7ea051c
Dave Grumm <grumm@stsci.edu>
2017-05-10 14:24:54 -0400

    Removed core schema, added referencefile schema to throughput schema; corrected typo in throughput.py. See ticket #861.

--- jwst/datamodels/throughput.py
+++ jwst/datamodels/throughput.py
@@ -13,7 +13,7 @@ class ThroughputModel(model_base.DataModel):
     schema_url = "throughput.schema.yaml"
 
     def __init__(self, init=None, filter_table=None, **kwargs):
-        super(ThroughModel, self).__init__(init=init, **kwargs)
+        super(ThroughputModel, self).__init__(init=init, **kwargs)
 
         if filter_table is not None:
             self.filter_table = filter_table
