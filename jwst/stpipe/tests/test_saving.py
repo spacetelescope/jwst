89e9865bbc11aabd3b18c8d16894bab78e3b3352
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-05-11 15:46:59 -0400

    Tests for saving default and saving with output_dir specified
    WIP #678

--- jwst/stpipe/tests/test_saving.py
+++ jwst/stpipe/tests/test_saving.py
@@ -31,23 +31,26 @@ def mk_tmp_dirs():
         os.chdir(old_path)
 
 
-def test_save_model(mk_tmp_dirs):
+def test_save_step_default(mk_tmp_dirs):
+    """Default save should be current working directory"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
     orig_filename = join(dirname(__file__), 'data', 'flat.fits')
     temp_filename = join(tmp_data_path, 'flat_FOO.fits')
     shutil.copyfile(orig_filename, temp_filename)
 
     args = [
-        'jwst.stpipe.tests.steps.SaveStep',
+        'jwst.stpipe.tests.steps.StepWithModel',
         temp_filename
     ]
 
     Step.from_cmdline(args)
-    fname = join(tmp_data_path, 'flat_FOO_SaveStep.fits')
+
+    fname = 'flat_FOO_SaveStep.fits'
     assert isfile(fname)
 
 
-def test_save_with_config(mk_tmp_dirs):
+def test_save_pipeline_default(mk_tmp_dirs):
+    """Default save should be current working directory"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
     step_fn = 'save_pipeline.cfg'
     data_fn = 'flat.fits'
@@ -64,6 +67,52 @@ def test_save_with_config(mk_tmp_dirs):
     args = [
         step_fn_path,
         tmp_data_fn_path,
+        '--steps.savestep.skip=False'
+    ]
+
+    Step.from_cmdline(args)
+
+    output_pipeline_fn_path = data_name + '_SavePipeline' + data_ext
+    output_stepsave_fn_path = data_name[:-1] + '_processed' + data_ext
+    assert isfile(output_pipeline_fn_path)
+    assert isfile(output_stepsave_fn_path)
+
+
+def test_save_step_specified(mk_tmp_dirs):
+    """Save to specified folder"""
+    tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
+    orig_filename = join(dirname(__file__), 'data', 'flat.fits')
+
+    args = [
+        'jwst.stpipe.tests.steps.StepWithModel',
+        orig_filename,
+        '--output_dir='+tmp_config_path
+    ]
+
+    Step.from_cmdline(args)
+
+    output_fn_path = join(tmp_data_path, 'flat_FOO_SaveStep.fits')
+    assert isfile(output_fn_path)
+
+
+def test_save_pipeline_specified(mk_tmp_dirs):
+    """Save to specified folder"""
+    tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
+    step_fn = 'save_pipeline.cfg'
+    data_fn = 'flat.fits'
+    data_name, data_ext = splitext(data_fn)
+
+    step_fn_path = join(dirname(__file__), 'steps', step_fn)
+    data_fn_path = join(dirname(__file__), 'data', data_fn)
+
+    tmp_step_fn_path = join(tmp_config_path, step_fn)
+    shutil.copy(step_fn_path, tmp_step_fn_path)
+
+    args = [
+        step_fn_path,
+        data_fn_path,
+        '--steps.savestep.skip=False',
+        '--output_dir='+tmp_data_path
     ]
 
     Step.from_cmdline(args)
@@ -72,6 +121,9 @@ def test_save_with_config(mk_tmp_dirs):
         tmp_data_path,
         data_name + '_SavePipeline' + data_ext
     )
-    output_stepsave_fn_path = data_name[:-1] + '_processed' + data_ext
+    output_stepsave_fn_path = join(
+        tmp_data_path,
+        data_name[:-1] + '_processed' + data_ext
+    )
     assert isfile(output_pipeline_fn_path)
-    assert not isfile(output_stepsave_fn_path)
+    assert isfile(output_stepsave_fn_path)
