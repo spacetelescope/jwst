2708a8adba47dc3f0b89ab59b90b8adf18a574d4
Howard Bushouse <bushouse@stsci.edu>
2017-05-11 09:29:53 -0400

    Include MIRI REFOUT data array

--- jwst/datamodels/level1b.py
+++ jwst/datamodels/level1b.py
@@ -21,18 +21,24 @@ class Level1bModel(model_base.DataModel):
     zeroframe : numpy array
         The zero-frame data
 
+    refout : numpy array
+        The MIRI reference output data
+
     group : table
         The group parameters table
 
     """
     schema_url = "level1b.schema.yaml"
 
-    def __init__(self, init=None, data=None, zeroframe=None, group=None,
-        **kwargs):
+    def __init__(self, init=None, data=None, refout=None, zeroframe=None,
+                 group=None, **kwargs):
         super(Level1bModel, self).__init__(init=init, **kwargs)
 
         if data is not None:
             self.data = data
+
+        if refout is not None:
+            self.refout = refout
 
         if zeroframe is not None:
             self.zeroframe = zeroframe
