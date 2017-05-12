85751ec9a3d142990de4a602a8aecbcf8763155f
David Law <dlaw@varda.stsci.edu>
2017-05-12 10:03:52 -0400

    Reverting imager bounding box change

--- jwst/assign_wcs/miri.py
+++ jwst/assign_wcs/miri.py
@@ -69,9 +69,9 @@ def imaging(input_model, reference_files):
     try:
         bb = distortion.bounding_box
     except NotImplementedError:
-        shape = 1032,1024
+        shape = input_model.data.shape
         # Note: Since bounding_box is attached to the model here it's in reverse order.
-        distortion.bounding_box = ((-0.5, shape[1] - 0.5), (3.5, shape[0] - 4.5))
+        distortion.bounding_box = ((-0.5, shape[0] - 0.5), (3.5, shape[1] - 4.5))
 
     # Create the pipeline
     pipeline = [(detector, distortion),
