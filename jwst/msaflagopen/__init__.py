df235141f26f55fd10f331222674eb4ac46a29d5
Robert Jedrzejewski <rij@stsci.edu>
2017-05-18 13:53:38 -0400

    WIP: Remove svn version hooks, etc, and add __version__ string

--- jwst/msaflagopen/__init__.py
+++ jwst/msaflagopen/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 from .msaflagopen_step import MSAFlagOpenStep
 
-from .version import (__version__, __svn_revision__, __svn_full_info__, __setup_datetime__)
+__version__ = '0.7.0'
 
