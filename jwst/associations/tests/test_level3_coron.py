22eff3c4b59033faf58665ce7ebca91c5f47b6ea
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-08-30 14:09:47 -0400

    Add test for coronographic associations
    WIP #311

---
+++ jwst/associations/tests/test_level3_coron.py
@@ -0,0 +1,23 @@
+"""Test Coronograpy rules
+
+Coronograpy rules produce associations meant to be
+processed by `calwebb_coron3`
+"""
+from __future__ import absolute_import
+
+from .helpers import BasePoolRule, PoolParams, t_path
+
+
+class TestLevel3Coron(BasePoolRule):
+
+    pools = [
+        PoolParams(
+            path=t_path('data/pool_013_coron_nircam.csv'),
+            n_asns=4,
+            n_orphaned=1
+        ),
+    ]
+
+    valid_rules = [
+        'Asn_Coron',
+    ]
