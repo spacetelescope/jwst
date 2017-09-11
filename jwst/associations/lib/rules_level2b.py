bc5c2016189a95c3b196a645c7416596a5c9bc4f
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-09-11 17:41:31 -0400

    Add image-mode special exposures also
    WIP #639, #735

--- jwst/associations/lib/rules_level2b.py
+++ jwst/associations/lib/rules_level2b.py
@@ -23,6 +23,21 @@ class Asn_Lv2Image(
     """Level2b Image"""
 
 
+class Asn_Lv2ImageSpecial(
+        AsnMixin_Lv2Special,
+        AsnMixin_Lv2Singleton,
+        AsnMixin_Lv2Image,
+        AsnMixin_Lv2Mode,
+):
+    """Level2b Image that are marked special
+
+    Image exposures that are marked as backgrounds, imprints, etc.,
+    still get 2b processing just as normal science. However, no other
+    exposures should get included into the association.
+
+    """
+
+
 class Asn_Lv2Spec(
         AsnMixin_Lv2Singleton,
         AsnMixin_Lv2Spec,
