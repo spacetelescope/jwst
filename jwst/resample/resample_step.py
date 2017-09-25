1ef670d520a7b9dbcad407721d3ac2ff222701ba
James Davies <jdavies@stsci.edu>
2017-09-25 14:10:25 -0400

    Simplify single input to ResampleStep

--- jwst/resample/resample_step.py
+++ jwst/resample/resample_step.py
@@ -8,13 +8,12 @@ from . import resample
 
 class ResampleStep(Step):
     """
-    ResampleStep: Uses the drizzle process to resample (geometric correction)
-    a single 2D image or resample and combine a set of 2D images specified as
-    an association.
+    ResampleStep: Resample input data onto a regular grid using the
+    drizzle algorithm.
 
     Parameters
     -----------
-    input : str or model
+    input : DataModel or Association
         Single filename for either a single image or an association table.
     """
 
@@ -31,30 +30,21 @@ class ResampleStep(Step):
 
     def process(self, input):
 
-        input_models = datamodels.open(input)
-        # If single input, insert into a ModelContainer
-        if not isinstance(input_models, datamodels.ModelContainer):
-            s = datamodels.ModelContainer()
-            s.append(input_models)
-            s.assign_group_ids()
-            s.meta.resample.output = s.group_names[0].replace('_resamp', '_drz')
-            self.input_models = s
-        else:
-            self.input_models = input_models
+        input = datamodels.open(input)
 
-        # identify what reference file has been associated with these inputs
-        try:
-            ref_filename = self.get_reference_file(self.input_models[0],
-                self.reference_file_types[0])
-            self.log.info('{} reffile is {}'.format(self.reference_file_types[0],
-                ref_filename))
-        except:
-            self.log.error('{} reffile is not found.'.format(
-                self.reference_file_types[0]))
+        # If single input, wrap in a ModelContainer
+        if not isinstance(input, datamodels.ModelContainer):
+            input_models = datamodels.ModelContainer([input])
+            input_models.meta.resample.output = input.meta.filename
+            self.blendheaders = False
+        else:
+            input_models = input
+
+        for reftype in self.reference_file_types:
+            ref_filename = self.get_reference_file(input_models[0], reftype)
 
         # Call the resampling routine
-        resamp = resample.ResampleData(self.input_models,
-            ref_filename=ref_filename,
+        resamp = resample.ResampleData(input_models, ref_filename=ref_filename,
             single=self.single, wht_type=self.wht_type, pixfrac=self.pixfrac,
             kernel=self.kernel, fillval=self.fillval, good_bits=self.good_bits,
             blendheaders=self.blendheaders)
