7ba35a8da156d6afc774a5ef62b3d07f5dba9fa0
Bernie Simon <bsimon@stsci.edu>
2017-09-25 15:38:42 -0400

    Modify registry initialization so it is only invoked oned
    The __init__ method for datamodels can be call more than once. But
    this astropy.io.registry code throws an error if it is. So code was
    added to check if the __init__ method has been called previously and
    only do the registry initialization if it has not been called
    previously.

--- jwst/datamodels/__init__.py
+++ jwst/datamodels/__init__.py
@@ -139,15 +139,20 @@ __all__ = [
     'TrapDensityModel', 'TrapParsModel', 'TrapsFilledModel',
     'WavelengthrangeModel', 'WaveCorrModel']
 
+# Initialize the astropy.io registry,
+# but only the first time this module is called
+
+try:
+    _defined_models
+except NameError:
+    with registry.delay_doc_updates(DataModel):
+        registry.register_reader('datamodel', DataModel, ndmodel.read)
+        registry.register_writer('datamodel', DataModel, ndmodel.write)
+        registry.register_identifier('datamodel', DataModel, ndmodel.identify)
+
 _all_models = __all__[1:]
 _local_dict = dict(locals())
 _defined_models = { k: _local_dict[k] for k in _all_models }
-
-# Initialize the astropy.io registry
-with registry.delay_doc_updates(DataModel):
-    registry.register_reader('datamodel', DataModel, ndmodel.read)
-    registry.register_writer('datamodel', DataModel, ndmodel.write)
-    registry.register_identifier('datamodel', DataModel, ndmodel.identify)
 
 
 '''
