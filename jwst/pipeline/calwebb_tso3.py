spacetelescope/jwst

commit 89b9f0f832356a741d22995aa9c2aec21feddff4

Populate asn info correctly in final output (#1231)
- fix bug populating asn pool name in final output
- reorder some logging messages
- rename variables for clarity

jwst/pipeline/calwebb_tso3.py
@@ -50,17 +50,17 @@ class Tso3Pipeline(Pipeline):
 
         Parameters
         ----------
-        input: Level3 Association, or ModelContainer
+        input: Level3 Association, json format
             The exposures to process
         """
 
-        self.log.info('Starting calwebb_tso3 ...')
-        input_asn = datamodels.open(input)
-        self.output_basename = input_asn.meta.asn_table.products[0].name
+        self.log.info('Starting calwebb_tso3...')
+        input_models = datamodels.open(input)
+        self.output_basename = input_models.meta.asn_table.products[0].name
 
         input_exptype = None
         # Input may consist of multiple exposures, so loop over each of them
-        for cube in input_asn:
+        for cube in input_models:
             if input_exptype is None:
                 input_exptype = cube.meta.exposure.type
             # Convert CubeModel into ModelContainer of 2-D DataModels
@@ -86,10 +86,10 @@ class Tso3Pipeline(Pipeline):
                 self.log.info("Performing scaled outlier detection on input images...")
                 cube = self.outlier_detection_scaled(cube)
 
-        if input_asn[0].meta.cal_step.outlier_detection == 'COMPLETE':
+        if input_models[0].meta.cal_step.outlier_detection == 'COMPLETE':
             self.log.info("Writing Level 2c cubes with updated DQ arrays...")
             suffix_2c = 'crfints'
-            for cube in input_asn:
+            for cube in input_models:
                 # preserve output filename
                 original_filename = cube.meta.filename
                 self.save_model(cube, suffix=suffix_2c)
@@ -102,7 +102,7 @@ class Tso3Pipeline(Pipeline):
             # Create name for extracted photometry (Level 3) product
             phot_tab_name = "{}_phot.ecsv".format(self.output_basename)
 
-            for cube in input_asn:
+            for cube in input_models:
                 # Extract Photometry from imaging data
                 phot_result_list.append(self.tso_photometry(cube))
         else:
@@ -113,10 +113,10 @@ class Tso3Pipeline(Pipeline):
             # define output for x1d (level 3) products
             x1d_result = datamodels.MultiSpecModel()
             # TODO: check to make sure the following line is working
-            x1d_result.update(input_asn)
+            x1d_result.update(input_models)
 
             # For each exposure in the TSO...
-            for cube in input_asn:
+            for cube in input_models:
                 # Process spectroscopic TSO data
                 # extract 1D
                 self.log.info("Extracting 1-D spectra...")
@@ -127,18 +127,15 @@ class Tso3Pipeline(Pipeline):
                 self.log.info("Performing white-light photometry...")
                 phot_result_list.append(self.white_light(result))
 
-
-            self.log.info("Writing Level 3 photometry catalog {}...".format(phot_tab_name))                 
-
-            self.log.info("Writing Level 3 X1DINTS product...")      
             # Update some metadata from the association
-            x1d_result.meta.asn.pool_name = asn['asn_pool']
+            x1d_result.meta.asn.pool_name = input_models.meta.asn_table.asn_pool
             x1d_result.meta.asn.table_name = input
 
             # Save the final x1d Multispec model
             self.save_model(x1d_result, suffix='x1dints')
 
         phot_results = vstack(phot_result_list)
+        self.log.info("Writing Level 3 photometry catalog {}...".format(phot_tab_name))
         phot_results.write(phot_tab_name, format='ascii.ecsv')
 
         return

commit 5ace671c84ed1c7909f22d946242e22570d7d9ab

Fix input_models var name bug introduced in #1231

jwst/pipeline/calwebb_tso3.py
@@ -64,23 +64,25 @@ class Tso3Pipeline(Pipeline):
             if input_exptype is None:
                 input_exptype = cube.meta.exposure.type
             # Convert CubeModel into ModelContainer of 2-D DataModels
-            input_models = datamodels.ModelContainer()
+            input_2dmodels = datamodels.ModelContainer()
             for i in range(cube.data.shape[0]):
                 # convert each plane of data cube into it own array
                 # for outlier detection...
                 image = datamodels.ImageModel(data=cube.data[i],
                         err=cube.err[i], dq=cube.dq[i])
-                image.meta = cube.meta
-                input_models.append(image)
+                image.update(cube)
+                input_2dmodels.append(image)
 
             if not self.scale_detection:
                 self.log.info("Performing outlier detection on input images...")
-                results = self.outlier_detection(input_models)
+                input_2dmodels = self.outlier_detection(input_2dmodels)
 
                 # Transfer updated DQ values to original input observation
                 for i in range(cube.data.shape[0]):
                     # Update DQ arrays with those from outlier_detection step
-                    cube.dq[i] = results[i].dq
+                    cube.dq[i] = input_2dmodels[i].dq
+                cube.meta.cal_step.outlier_detection = \
+                    input_2dmodels[0].meta.cal_step.outlier_detection
 
             else:
                 self.log.info("Performing scaled outlier detection on input images...")
@@ -88,11 +90,10 @@ class Tso3Pipeline(Pipeline):
 
         if input_models[0].meta.cal_step.outlier_detection == 'COMPLETE':
             self.log.info("Writing Level 2c cubes with updated DQ arrays...")
-            suffix_2c = 'crfints'
             for cube in input_models:
                 # preserve output filename
                 original_filename = cube.meta.filename
-                self.save_model(cube, suffix=suffix_2c)
+                self.save_model(cube, suffix='crfints')
                 cube.meta.filename = original_filename
 
         # Create final photometry results as a single output