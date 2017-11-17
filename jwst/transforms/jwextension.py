93d14e349599c6def0984aa4467e111750351a42
Mihai Cara <mihail.cara@gmail.com>
2017-11-17 12:37:06 -0500

    Resolve merge conflict

--- jwst/transforms/jwextension.py
+++ jwst/transforms/jwextension.py
@@ -21,6 +21,7 @@ class JWSTExtension(AsdfExtension):
                 SnellType,
                 NIRCAMGrismDispersionType,
                 NIRISSGrismDispersionType,
+                LogicalType,
                 TPCorrType,
                 ]
 
