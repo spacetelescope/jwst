d7fef684f279ad692128a9c08cbec13ad49c04d8
J.D.Eisenhamer <eisenhamer@stsci.edu>
2017-11-21 22:26:07 -0500

    Remove use of the six module
    WIP #1392

--- jwst/associations/association_io.py
+++ jwst/associations/association_io.py
@@ -4,7 +4,6 @@ Define the I/O methods for Level 3 associations
 import json as json_lib
 import logging
 import numpy as np
-import six
 import yaml as yaml_lib
 
 from .association import Association
@@ -44,7 +43,7 @@ class json(object):
         AssociationNotValidError
             Cannot create or validate the association.
         """
-        if isinstance(serialized, six.string_types):
+        if isinstance(serialized, str):
             loader = json_lib.loads
         else:
             # Presume a file object
