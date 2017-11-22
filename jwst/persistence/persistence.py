9a8b3124ce5435976cbd3303c7d8826760aa52fc
Philip Hodge <hodge@stsci.edu>
2017-11-22 09:39:43 -0500

    Change syntax for checking for existence of DQ
    The get_subarray method used 'if "dq" in ref:' to check whether a
    reference file model has a dq array.  This no longer works:
    TypeError: 'dict_items' object is not an iterator
    so it has been changed to 'if hasattr(ref, "dq"):'.  See issue #1511.

--- jwst/persistence/persistence.py
+++ jwst/persistence/persistence.py
@@ -1,3 +1,5 @@
+from __future__ import (absolute_import, unicode_literals, division,
+                        print_function)
 #
 #  Module for correcting for persistence
 
@@ -435,7 +437,7 @@ class DataSet():
         # two, use this syntax:
         # refsub.data = ref.data[..., slc[0], slc[1]].copy()
         refsub.data = ref.data[slc[0], slc[1]].copy()
-        if "dq" in ref:
+        if hasattr(ref, "dq"):
             refsub.dq = ref.dq[slc[0], slc[1]].copy()
 
         return refsub
