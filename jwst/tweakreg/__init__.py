6764fca2d19e499333440700961e142ab7676ffa
James Davies <jdavies@stsci.edu>
2017-12-08 14:30:56 -0500

    Remove tweakreg_catalog docs; update tweakreg docs (#1579)
    * Remove tweakreg_catalog docs; update tweakreg docs
    
    * Fix typo [skip ci]
    
    * Remove help function from tweakreg init

--- jwst/tweakreg/__init__.py
+++ jwst/tweakreg/__init__.py
@@ -1,13 +1,8 @@
-""" tweakreg
-
+"""
 This package provides support for image alignment.
 
 """
-from __future__ import (absolute_import, division, unicode_literals,
-                        print_function)
-
-import os
-import logging
+from __future__ import absolute_import
 
 __docformat__ = 'restructuredtext'
 
@@ -17,29 +12,3 @@ __vdate__ = '14-November-2017'
 __author__ = 'Mihai Cara'
 
 from .tweakreg_step import TweakRegStep
-from . import wcsutils
-from . import imalign
-from . import wcsimage
-from . import matchutils
-from . import tweakreg_step
-from . import linearfit
-
-
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-
-try:
-    from . import chelp
-except ImportError:  # RTD cannot build it
-    log.warning('cannot import chelp')
-
-def help():
-    msg = \
-"""
-The tweakreg package contains the following tasks that allow users
-perform WCS alignment.
-
-tweakreg:
-       align - primary task for performing image alignment
-"""
-    print(msg)
