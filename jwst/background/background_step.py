52c276ab2e67d55467be568c3ec91a831d5a4035
Megan Sosey <sosey@stsci.edu>
2018-03-26 13:16:43 -0400

    enabled master background subtraction for team to test wfss reference image

--- jwst/background/background_step.py
+++ jwst/background/background_step.py
@@ -42,20 +42,18 @@ class BackgroundStep(Step):
 
             if input_model.meta.exposure.type in ["NIS_WFSS", "NRC_GRISM"]:
                 # Delete these four lines when reference files are available.
-                self.log.info("No 'wfssbkg' reference file available; "
-                              "skipping background subtraction.")
-                result = input_model.copy()
-                result.meta.cal_step.back_sub = 'SKIPPED'
+                # self.log.info("No 'wfssbkg' reference file available; "
+                #               "skipping background subtraction.")
+                # result = input_model.copy()
+                # result.meta.cal_step.back_sub = 'SKIPPED'
 
-                # Uncomment this block when reference files are available.
-                # bkg_filename = self.get_reference_file(
-                #                 input_model, "wfssbkg")
-                # wl_range_name = self.get_reference_file(
-                #                 input_model, "wavelengthrange")
+                # Uncomment this block if reference files are available.
+                bkg_filename = self.get_reference_file(input_model, "wfssbkg")
+                wl_range_name = self.get_reference_file(input_model, "wavelengthrange")
+                
                 # Do the background subtraction for WFSS/GRISM data
-                # result = background_sub.subtract_wfss_bkg(
-                #                 input_model, bkg_filename, wl_range_name)
-                # result.meta.cal_step.back_sub = 'COMPLETE'
+                result = background_sub.subtract_wfss_bkg(input_model, bkg_filename, wl_range_name)
+                result.meta.cal_step.back_sub = 'COMPLETE'
             else:
                 # Do the background subtraction
                 result = background_sub.background_sub(input_model, bkg_list)
