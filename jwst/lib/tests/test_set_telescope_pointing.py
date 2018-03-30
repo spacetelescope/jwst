cae8f9627ff40aa569605f56a9017ebd19dd16b1
J.D.Eisenhamer <eisenhamer@stsci.edu>
2018-03-30 10:01:13 -0400

    Removed misleading comments
    WIP #1765

--- jwst/lib/tests/test_set_telescope_pointing.py
+++ jwst/lib/tests/test_set_telescope_pointing.py
@@ -1,12 +1,5 @@
 """
 Test suite for set_telescope_pointing
-
-Notes
------
-This file has been named specifically so it is not
-automatically found by py.test. This is because, to test,
-a connection to the internal engineering service is needed,
-which is generally not available.
 """
 import copy
 import numpy as np
