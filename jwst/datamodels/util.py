363ac14e035737f3d7929ebab9b92a577f2c5918
Bernie Simon <bsimon@stsci.edu>
2018-04-05 11:35:09 -0400

    Modify code that clears meta.model_type
    The generic open function in datamodels does not set the
    meta.model_type attribute because the class it uses to open an image
    is only a best guess. Previously the code cleared the model_type by
    setting it to None. This is not desirable, because if the metadata is
    re-validated, a value of None with throw a warning or error. This
    revision deletes the model_type attribute instead. To keep datamodels
    from setting the value again, the code in on_save which sets model_type
    when not found was removed. It was redundant in any case.

--- jwst/datamodels/util.py
+++ jwst/datamodels/util.py
@@ -153,7 +153,7 @@ def open(init=None, extensions=None, **kwargs):
     # Actually open the model
     model = new_class(init, extensions=extensions, **kwargs)
     if not has_model_type:
-        model.meta.model_type = None
+        delattr(model.meta, 'model_type')
 
     # Close the hdulist if we opened it
     if file_to_close is not None:
