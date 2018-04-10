815e3f07d05eb6271b5534c688038ce9d6f7e0bf
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-04-10 10:00:29 -0400

    fgs tests

--- jwst/tests_nightly/general/fgs/test_guider_pipeline_1.py
+++ jwst/tests_nightly/general/fgs/test_guider_pipeline_1.py
@@ -12,7 +12,6 @@ def test_guider_pipeline1():
     """
 
     GuiderPipeline.call(BIGDATA+'/fgs/test_guiderpipeline/jw88600073001_gs-id_7_image-uncal.fits',
-                        config_file='calwebb_guider.cfg',
                         output_file='jw88600073001_gs-id_7_image-cal.fits')
 
     # Compare calibrated ramp product
@@ -29,8 +28,8 @@ def test_guider_pipeline1():
     )
 
     print (' Fitsdiff comparison between the calibrated product file - a:', n_cr )
-    print (' ... and the reference file - b:', n_ref)    
-    
+    print (' ... and the reference file - b:', n_ref)
+
     result.report()
     try:
         assert result.identical == True
