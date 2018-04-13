ffd861459285f91e833c907999baa77842795d4f
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-13 16:36:41 -0400

    nircam: bigdata/jail

--- jwst/tests_nightly/general/nircam/test_wfs_combine_step_refine.py
+++ jwst/tests_nightly/general/nircam/test_wfs_combine_step_refine.py
@@ -3,7 +3,12 @@ import pytest
 from astropy.io import fits as pf
 from jwst.wfs_combine.wfs_combine_step import WfsCombineStep
 
-BIGDATA = os.environ['TEST_BIGDATA']
+pytestmark = [
+    pytest.mark.usefixtures('_jail'),
+    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+                       reason='requires --bigdata')
+]
+
 
 def test_wfs_combine():
     """
@@ -24,12 +29,12 @@ def test_wfs_combine():
     except:
         pass
 
-    WfsCombineStep.call(BIGDATA+'/nircam/test_wfs_combine/wfs_3sets_asn2.json',
+    WfsCombineStep.call(_bigdata+'/nircam/test_wfs_combine/wfs_3sets_asn2.json',
                         do_refine=True )
 
     # compare 1st pair of output files
     h = pf.open('test_wfscom2_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscom_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscom_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
@@ -42,7 +47,7 @@ def test_wfs_combine():
 
     # compare 2nd pair of output files
     h = pf.open('test_wfscom2a_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscoma_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscoma_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
@@ -55,7 +60,7 @@ def test_wfs_combine():
 
     # compare 3rd pair of output files
     h = pf.open('test_wfscom2b_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscomb_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscomb_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
--- jwst/tests_nightly/general/nircam/test_wfs_combine_step_refine2.py
+++ jwst/tests_nightly/general/nircam/test_wfs_combine_step_refine2.py
@@ -3,7 +3,12 @@ import pytest
 from astropy.io import fits as pf
 from jwst.wfs_combine.wfs_combine_step import WfsCombineStep 
 
-BIGDATA = os.environ['TEST_BIGDATA']
+pytestmark = [
+    pytest.mark.usefixtures('_jail'),
+    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+                       reason='requires --bigdata')
+]
+
 
 def test_wfs_combine():
     """
@@ -12,12 +17,12 @@ def test_wfs_combine():
 
     """
 
-    WfsCombineStep.call(BIGDATA+'/nircam/test_wfs_combine/wfs_3sets_asn3.json',
+    WfsCombineStep.call(_bigdata+'/nircam/test_wfs_combine/wfs_3sets_asn3.json',
                          config_file='wfs_combine2.cfg' )
 
     # compare 1st pair of output files
     h = pf.open('test_wfscom3_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscom_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscom_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
@@ -30,7 +35,7 @@ def test_wfs_combine():
 
     # compare 2nd pair of output files
     h = pf.open('test_wfscom3a_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscoma_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscoma_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
@@ -43,7 +48,7 @@ def test_wfs_combine():
 
     # compare 3rd pair of output files
     h = pf.open('test_wfscom3b_wfscmb.fits')
-    href = pf.open(BIGDATA+'/nircam/test_wfs_combine/test_wfscomb_do_ref.fits')
+    href = pf.open(_bigdata+'/nircam/test_wfs_combine/test_wfscomb_do_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq']])
 
