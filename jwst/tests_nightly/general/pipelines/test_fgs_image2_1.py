92bec847c9d2337b070b5680d2f28d21ee8bc6b3
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-13 17:16:43 -0400

    pipelines: all the things

--- jwst/tests_nightly/general/pipelines/test_fgs_image2_1.py
+++ jwst/tests_nightly/general/pipelines/test_fgs_image2_1.py
@@ -3,7 +3,11 @@ import pytest
 from astropy.io import fits as pf
 from jwst.pipeline.calwebb_image2 import Image2Pipeline
 
-BIGDATA = os.environ['TEST_BIGDATA']
+pytestmark = [
+    pytest.mark.usefixtures('_jail'),
+    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+                       reason='requires --bigdata')
+]
 
 def test_fgs_image2pipeline1():
     """
@@ -12,11 +16,11 @@ def test_fgs_image2pipeline1():
 
     """
 
-    Image2Pipeline.call(BIGDATA+'/fgs/test_image2pipeline/jw86500007001_02101_00001_GUIDER2_rate.fits',
+    Image2Pipeline.call(_bigdata+'/fgs/test_image2pipeline/jw86500007001_02101_00001_GUIDER2_rate.fits',
                         output_file='jw86500007001_02101_00001_GUIDER2_cal.fits')
 
     h = pf.open('jw86500007001_02101_00001_GUIDER2_cal.fits')
-    href = pf.open(BIGDATA+'/fgs/test_image2pipeline/jw86500007001_02101_00001_GUIDER2_cal_ref.fits')
+    href = pf.open(_bigdata+'/fgs/test_image2pipeline/jw86500007001_02101_00001_GUIDER2_cal_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq'],h['area']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq'],href['area']])
     result = pf.diff.FITSDiff(newh,
@@ -24,10 +28,5 @@ def test_fgs_image2pipeline1():
                               ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
                               rtol = 0.00001
     )
-    result.report()
-    try:
-        assert result.identical == True
-    except AssertionError as e:
-        print(result.report())
-        raise AssertionError(e)
+    assert result.identical, result.report()
 
--- jwst/tests_nightly/general/pipelines/test_image2pipeline_2.py
+++ jwst/tests_nightly/general/pipelines/test_image2pipeline_2.py
@@ -3,7 +3,11 @@ import pytest
 from astropy.io import fits as pf
 from jwst.pipeline.calwebb_image2 import Image2Pipeline
 
-BIGDATA = os.environ['TEST_BIGDATA']
+pytestmark = [
+    pytest.mark.usefixtures('_jail'),
+    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+                       reason='requires --bigdata')
+]
 
 def test_image2pipeline2():
     """
@@ -12,11 +16,11 @@ def test_image2pipeline2():
 
     """
 
-    Image2Pipeline.call(BIGDATA+'/pipelines/jw82500001003_02101_00001_NRCALONG_rate_ref.fits',
+    Image2Pipeline.call(_bigdata+'/pipelines/jw82500001003_02101_00001_NRCALONG_rate_ref.fits',
                         output_file='jw82500001003_02101_00001_NRCALONG_cal.fits')
 
     h = pf.open('jw82500001003_02101_00001_NRCALONG_cal.fits')
-    href = pf.open(BIGDATA+'/pipelines/jw82500001003_02101_00001_NRCALONG_cal_ref.fits')
+    href = pf.open(_bigdata+'/pipelines/jw82500001003_02101_00001_NRCALONG_cal_ref.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['dq'],h['area']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['dq'],href['area']])
     result = pf.diff.FITSDiff(newh,
@@ -24,10 +28,4 @@ def test_image2pipeline2():
                               ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
                               rtol = 0.00001
     )
-    result.report()
-    try:
-        assert result.identical == True
-    except AssertionError as e:
-        print(result.report())
-        raise AssertionError(e)
-
+    assert result.identical, result.report()
