39dece80ac1d9490c5b9c5f496f257919a66ac48
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-13 16:44:35 -0400

    niriss: drop complex assertions

--- jwst/tests_nightly/general/niriss/test_flat_field.py
+++ jwst/tests_nightly/general/niriss/test_flat_field.py
@@ -39,9 +39,4 @@ def test_flat_field_niriss():
                               ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
                               rtol = 0.00001
     )
-    result.report()
-    try:
-        assert result.identical == True
-    except AssertionError as e:
-        print(result.report())
-        raise AssertionError(e)
+    assert result.identical, result.report()
