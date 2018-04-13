99748e1de6cc1ed29da7029c371fb83d3c841196
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-13 15:24:45 -0400

    miri: drop complex assertions

--- jwst/tests_nightly/general/miri/test_setpointing.py
+++ jwst/tests_nightly/general/miri/test_setpointing.py
@@ -34,9 +34,4 @@ def test_miri_setpointing():
     result = pf.diff.FITSDiff(newh, newhref,
                               ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
                               rtol = 0.000001)
-
-    result.report()
-    try:
-        assert result.identical == True
-    except AssertionError as e:
-        raise AssertionError(e)
+    assert result.identical, result.report()
