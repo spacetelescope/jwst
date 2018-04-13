0d3ed2c9c2d518ddfb17fae330c9a73d602bc6a2
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-04-13 17:31:47 -0400

    fix tests

--- jwst/assign_wcs/tests/test_nirspec.py
+++ jwst/assign_wcs/tests/test_nirspec.py
@@ -291,7 +291,7 @@ def test_msa_configuration_normal():
     msa_meta_id = 12
     msaconfl = get_file_path('msa_configuration.fits')
     slitlet_info = nirspec.get_open_msa_slits(msaconfl, msa_meta_id)
-    ref_slit = Slit(55, 9375, 251, 26, -5.15, 0.55, 4, 1, '1111x', '95065_1', '2122',
+    ref_slit = Slit(55, 9376, 251, 26, -5.15, 0.55, 4, 1, '1111x', '95065_1', '2122',
                       0.13, -0.31716078999999997, 0.18092266)
     _compare_slits(slitlet_info[0], ref_slit)
 
@@ -316,7 +316,7 @@ def test_msa_configuration_all_background():
     msa_meta_id = 14
     msaconfl = get_file_path('msa_configuration.fits')
     slitlet_info = nirspec.get_open_msa_slits(msaconfl, msa_meta_id)
-    ref_slit = Slit(57, 8645, 251, 24, -2.85, .55, 4, 1, '11x', '95065_1', '2122',
+    ref_slit = Slit(57, 8646, 251, 24, -2.85, .55, 4, 1, '11x', '95065_1', '2122',
                     0.13, -0.5, 0.5)
     _compare_slits(slitlet_info[0], ref_slit)
 
@@ -331,7 +331,7 @@ def test_msa_configuration_row_skipped():
     msa_meta_id = 15
     msaconfl = get_file_path('msa_configuration.fits')
     slitlet_info = nirspec.get_open_msa_slits(msaconfl, msa_meta_id)
-    ref_slit = Slit(58, 8645, 251, 24, -2.85, 5.15, 4, 1, '11x1011', '95065_1', '2122',
+    ref_slit = Slit(58, 8646, 251, 24, -2.85, 5.15, 4, 1, '11x1011', '95065_1', '2122',
                       0.130, -0.31716078999999997, 0.18092266)
     _compare_slits(slitlet_info[0], ref_slit)
 
@@ -344,9 +344,9 @@ def test_msa_configuration_multiple_returns():
     msa_meta_id = 16
     msaconfl = get_file_path('msa_configuration.fits')
     slitlet_info = nirspec.get_open_msa_slits(msaconfl, msa_meta_id)
-    ref_slit1 = Slit(59, 8650, 256, 24, -2.85, 5.15, 4, 1, '11x1011', '95065_1', '2122',
+    ref_slit1 = Slit(59, 8651, 256, 24, -2.85, 5.15, 4, 1, '11x1011', '95065_1', '2122',
                      0.13000000000000003, -0.31716078999999997, 0.18092266)
-    ref_slit2 = Slit(60, 11572, 258, 32, -2.85, 4, 4, 2, '11x111', '95065_2', '172',
+    ref_slit2 = Slit(60, 11573, 258, 32, -2.85, 4, 4, 2, '11x111', '95065_2', '172',
                      0.70000000000000007, -0.31716078999999997, 0.18092266)
     _compare_slits(slitlet_info[0], ref_slit1)
     _compare_slits(slitlet_info[1], ref_slit2)
