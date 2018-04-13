f1383770e8c28c7969bb074dca1fa6f41931fbc6
Joseph Hunkeler <jhunkeler@gmail.com>
2018-04-13 16:36:41 -0400

    nircam: bigdata/jail

--- jwst/tests_nightly/general/nircam/test_saturation.py
+++ jwst/tests_nightly/general/nircam/test_saturation.py
@@ -5,7 +5,12 @@ from jwst.saturation.saturation_step import SaturationStep
 
 from ..helpers import add_suffix
 
-BIGDATA = os.environ['TEST_BIGDATA']
+pytestmark = [
+    pytest.mark.usefixtures('_jail'),
+    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
+                       reason='requires --bigdata')
+]
+
 
 def test_saturation_nircam():
     """
@@ -20,11 +25,11 @@ def test_saturation_nircam():
     except:
         pass
 
-    SaturationStep.call(BIGDATA+'/nircam/test_saturation/jw00017001001_01101_00001_NRCA1_bias_drift.fits',
+    SaturationStep.call(_bigdata+'/nircam/test_saturation/jw00017001001_01101_00001_NRCA1_bias_drift.fits',
                         output_file=output_file_base
                         )
     h = pf.open(output_file)
-    href = pf.open(BIGDATA+'/nircam/test_saturation/jw00017001001_01101_00001_NRCA1_saturation.fits')
+    href = pf.open(_bigdata+'/nircam/test_saturation/jw00017001001_01101_00001_NRCA1_saturation.fits')
     newh = pf.HDUList([h['primary'],h['sci'],h['err'],h['pixeldq'],h['groupdq']])
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['pixeldq'],href['groupdq']])
     result = pf.diff.FITSDiff(newh,
