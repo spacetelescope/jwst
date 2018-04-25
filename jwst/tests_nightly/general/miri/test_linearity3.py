84f5a533e384566ab42e70e7e623680185a37dce
James Davies <jdavies@stsci.edu>
2018-04-25 17:04:13 -0400

    Fix NIRCam regtests

--- jwst/tests_nightly/general/miri/test_linearity3.py
+++ jwst/tests_nightly/general/miri/test_linearity3.py
@@ -37,7 +37,7 @@ def test_linearity_miri3(_bigdata):
     newhref = pf.HDUList([href['primary'],href['sci'],href['err'],href['pixeldq'],href['groupdq']])
     result = pf.diff.FITSDiff(newh,
                               newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX', 'R_LINEAR'],
+                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
                               rtol = 0.00001
     )
     assert result.identical, result.report()
--- jwst/tests_nightly/general/nircam/test_photom_step.py
+++ jwst/tests_nightly/general/nircam/test_photom_step.py
@@ -18,7 +18,8 @@ def test_photom_nircam(_bigdata):
     Regression test of photom step performed on NIRCam imaging data.
 
     """
-    output_file_base, output_file = add_suffix('photom1_output.fits', 'photom')
+    suffix = 'photom'
+    output_file_base, output_file = add_suffix('photom1_output.fits', suffix)
 
     try:
         os.remove(output_file)
@@ -28,7 +29,7 @@ def test_photom_nircam(_bigdata):
 
 
     PhotomStep.call(_bigdata+'/nircam/test_photom/jw00017001001_01101_00001_NRCA1_emission.fits',
-                    output_file=output_file_base
+                    output_file=output_file_base, suffix=suffix
                     )
     h = pf.open(output_file)
     href = pf.open(_bigdata+'/nircam/test_photom/jw00017001001_01101_00001_NRCA1_photom.fits')
