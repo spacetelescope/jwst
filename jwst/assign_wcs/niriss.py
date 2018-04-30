56154a9ad0de0de97d2d3576949c1e1ac7d5b6b1
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-04-30 17:00:09 -0400

    add explanaiton

--- jwst/assign_wcs/niriss.py
+++ jwst/assign_wcs/niriss.py
@@ -160,8 +160,11 @@ def imaging_distortion(input_model, reference_files):
         shape = input_model.data.shape
         # Note: Since bounding_box is attached to the model here
         # it's in reverse order.
-        # If the input is a CubeModel, need to set bbox to the                                                                   
-        # size of 2nd and 3rd dimension.                                                                                          
+        """
+        A CubeModel is always treated as a stack (in dimension 1)
+        of 2D images, as opposed to actual 3D data. In this case
+        the bounding box is set to the 2nd and 3rd dimension.
+        """
         if isinstance(input_model, CubeModel):
             bb = ((-0.5, shape[1] - 0.5),
                   (-0.5, shape[2] - 0.5))
