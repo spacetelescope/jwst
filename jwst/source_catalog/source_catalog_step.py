spacetelescope/jwst

commit d64d296ad68ab99f8728a1375f59763468719f4b

update SourceCatalogStep for standard Step I/O API (#2097)
Changes:
* Update tests
* Update source_catalog_step with the io api
* Force calwebb_image3 to save the catalog
* Update `calwebb_image3.cfg` to add `save_results` for the source catalog step
* Update tests to use current configuration
* Update suffix removal for `cat`

jwst/source_catalog/source_catalog_step.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-from ..stpipe import Step, cmdline
+from ..stpipe import Step
 from ..datamodels import DrizProductModel
-from ..lib.catalog_utils import replace_suffix_ext
 from . import source_catalog
 
 
@@ -18,12 +17,14 @@ class SourceCatalogStep(Step):
     """
 
     spec = """
-        kernel_fwhm = float(default=2.0)    # Gaussian kernel FWHM in pixels
-        kernel_xsize = float(default=5)     # Kernel x size in pixels
-        kernel_ysize = float(default=5)     # Kernel y size in pixels
-        snr_threshold = float(default=3.0)  # SNR threshold above the bkg
-        npixels = float(default=5.0)        # min number of pixels in source
-        deblend = boolean(default=False)    # deblend sources?
+        kernel_fwhm = float(default=2.0)      # Gaussian kernel FWHM in pixels
+        kernel_xsize = float(default=5)       # Kernel x size in pixels
+        kernel_ysize = float(default=5)       # Kernel y size in pixels
+        snr_threshold = float(default=3.0)    # SNR threshold above the bkg
+        npixels = float(default=5.0)          # min number of pixels in source
+        deblend = boolean(default=False)      # deblend sources?
+        output_ext = string(default='.ecsv')  # Default type of output
+        suffix = string(default='cat')        # Default suffix for output files
     """
 
     def process(self, input):
@@ -46,17 +47,14 @@ class SourceCatalogStep(Step):
 
             self.log.info('Detected {0} sources'.format(len(catalog)))
 
-            old_suffixes = ['i2d']
-            output_dir = self.search_attr('output_dir')
-            cat_filepath = replace_suffix_ext(model.meta.filename,
-                                              old_suffixes, 'cat',
-                                              output_ext='ecsv',
-                                              output_dir=output_dir)
-            catalog.write(cat_filepath, format='ascii.ecsv', overwrite=True)
-            self.log.info('Wrote source catalog: {0}'
-                          .format(cat_filepath))
-            model.meta.source_catalog.filename = cat_filepath
+            if self.save_results:
+                cat_filepath = self.make_output_path()
+                catalog.write(
+                    cat_filepath, format='ascii.ecsv', overwrite=True
+                )
+                self.log.info('Wrote source catalog: {0}'
+                              .format(cat_filepath))
+                model.meta.source_catalog.filename = cat_filepath
 
         # nothing is returned because this is the last step
         return
-

jwst/pipeline/calwebb_image3.cfg
@@ -13,3 +13,4 @@ class = "jwst.pipeline.Image3Pipeline"
         config_file = resample.cfg
       [[source_catalog]]
         config_file = source_catalog.cfg
+        save_results = true

jwst/pipeline/tests/test_calwebb_spec2_nrs_msa.py
@@ -28,14 +28,25 @@ def test_run_msaflagging(mk_tmp_dirs, caplog):
     """Test msa flagging operation"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    # Copy msa config files from DATAPATH to
-    # current working directory
-    file_copy(path.join(DATAPATH, 'jw95065006001_0_msa_twoslit.fits'), '.')
-
-    asn_path = update_asn_basedir(
-        path.join(DATAPATH, 'mos_udf_g235m_twoslit_spec2_asn.json'),
-        root=path.join(DATAPATH, 'level2a_twoslit')
+    # Copy necessary data to the tmp_data_path
+    file_copy(
+        path.join(DATAPATH, 'jw95065006001_0_msa_twoslit.fits'),
+        tmp_data_path
     )
+    file_copy(
+        path.join(DATAPATH, 'mos_udf_g235m_twoslit_spec2_asn.json'),
+        tmp_data_path
+    )
+    asn_path = path.join(tmp_data_path, 'mos_udf_g235m_twoslit_spec2_asn.json')
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2a_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec2_basic.cfg'),
         asn_path,
@@ -46,9 +57,6 @@ def test_run_msaflagging(mk_tmp_dirs, caplog):
 
     assert 'Step msa_flagging running with args' in caplog.text
     assert 'Step msa_flagging done' in caplog.text
-
-    with open(asn_path) as fp:
-        asn = load_asn(fp)
 
     for product in asn['products']:
         prod_name = product['name'] + '_cal.fits'

jwst/pipeline/tests/test_calwebb_spec3_nrs_msa.py
@@ -3,6 +3,7 @@
 from glob import glob
 from os import path
 import pytest
+from shutil import copy as file_copy
 
 from .helpers import (
     SCRIPT_PATH,
@@ -32,10 +33,22 @@ def test_run_outlier_only(mk_tmp_dirs):
     """Test a basic run"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,
@@ -55,10 +68,22 @@ def test_run_outlier_only_mock(mk_tmp_dirs):
     """Test a basic run"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_mock.cfg'),
         asn_path,
@@ -96,10 +121,22 @@ def test_run_resample_only(mk_tmp_dirs):
     """Test resample step only."""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
-        path.join(DATAPATH, 'mos_udf_g235M_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b')
+    # Copy necessary data to the tmp_data_path
+    file_copy(
+        path.join(DATAPATH, 'two_member_spec3_asn.json'),
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,
@@ -132,10 +169,22 @@ def test_run_resample_mock_only(mk_tmp_dirs):
     """Test resample step only."""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_mock.cfg'),
         asn_path,
@@ -167,10 +216,22 @@ def test_run_cube_build(mk_tmp_dirs):
     """NRS MSA data is not cube data. Nothing should happen"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,
@@ -202,10 +263,22 @@ def test_run_extract_1d_only(mk_tmp_dirs):
     """
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,
@@ -241,10 +314,22 @@ def test_run_extract_1d_resample_mock(mk_tmp_dirs):
     """
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_mock.cfg'),
         asn_path,
@@ -281,10 +366,22 @@ def test_run_nosteps(mk_tmp_dirs):
     """Test where no steps execute"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,
@@ -322,10 +419,22 @@ def test_run_full(mk_tmp_dirs):
     """Test a full run"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
+    # Copy necessary data to the tmp_data_path
+    file_copy(
         path.join(DATAPATH, 'two_member_spec3_asn.json'),
-        root=path.join(DATAPATH, 'level2b_twoslit')
+        tmp_data_path
     )
+    asn_path = path.join(tmp_data_path, 'two_member_spec3_asn.json')
+
+    with open(asn_path) as fp:
+        asn = load_asn(fp)
+    for product in asn['products']:
+        for member in product['members']:
+            file_copy(
+                path.join(DATAPATH, 'level2b_twoslit', member['expname']),
+                tmp_data_path
+            )
+
     args = [
         path.join(SCRIPT_DATA_PATH, 'calwebb_spec3_default.cfg'),
         asn_path,

jwst/pipeline/tests/test_nrc_image3.py
@@ -15,6 +15,7 @@ from .helpers import (
 
 from ...associations import load_asn
 from ...stpipe.step import (Step, remove_suffix)
+from ..collect_pipeline_cfgs import collect_pipeline_cfgs
 
 DATAPATH = abspath(
     path.join('$TEST_BIGDATA', 'pipelines', 'nircam_calimage3')
@@ -27,12 +28,12 @@ def test_run_full(mk_tmp_dirs):
     """Test a full run"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
-    asn_path = update_asn_basedir(
-        path.join(DATAPATH, 'mosaic_long_asn.json'),
-        root=DATAPATH
-    )
+    cfg_dir = path.join(tmp_data_path, 'cfgs')
+    collect_pipeline_cfgs(cfg_dir)
+
+    asn_path = path.join(DATAPATH, 'mosaic_long_asn.json')
     args = [
-        path.join(SCRIPT_DATA_PATH, 'cfgs', 'calwebb_image3.cfg'),
+        path.join(cfg_dir, 'calwebb_image3.cfg'),
         asn_path,
     ]
 
@@ -64,9 +65,12 @@ def test_single_image(mk_tmp_dirs):
     """Test a full run"""
     tmp_current_path, tmp_data_path, tmp_config_path = mk_tmp_dirs
 
+    cfg_dir = path.join(tmp_data_path, 'cfgs')
+    collect_pipeline_cfgs(cfg_dir)
+
     input_file = 'nrca5_47Tuc_subpix_dither1_newpos_cal.fits'
     args = [
-        path.join(SCRIPT_DATA_PATH, 'cfgs', 'calwebb_image3.cfg'),
+        path.join(cfg_dir, 'calwebb_image3.cfg'),
         path.join(
             DATAPATH,
             input_file

jwst/stpipe/suffix.py
@@ -41,7 +41,7 @@ logger.addHandler(logging.NullHandler())
 SUFFIXES_TO_ADD = [
     'ami', 'amiavg', 'aminorm',
     'blot', 'bsub', 'bsubints',
-    'cal', 'calints', 'crf', 'crfints',
+    'cal', 'calints', 'cat', 'crf', 'crfints',
     'dark',
     'i2d',
     'median',

commit 3961bbe5696f9fe40154a8679d655951af1dbb88

fix for save_catalog bug introduced in #2097

jwst/source_catalog/source_catalog_step.py
@@ -25,6 +25,7 @@ class SourceCatalogStep(Step):
         deblend = boolean(default=False)      # deblend sources?
         output_ext = string(default='.ecsv')  # Default type of output
         suffix = string(default='cat')        # Default suffix for output files
+        save_results = boolean(default=True)  # Save the output catalog
     """
 
     def process(self, input):
@@ -48,7 +49,7 @@ class SourceCatalogStep(Step):
             self.log.info('Detected {0} sources'.format(len(catalog)))
 
             if self.save_results:
-                cat_filepath = self.make_output_path()
+                cat_filepath = self.make_output_path(basepath=model.meta.filename)
                 catalog.write(
                     cat_filepath, format='ascii.ecsv', overwrite=True
                 )