a489104d117e51bfae5c61a464b3c812977cdb3d
James Davies <jdavies@stsci.edu>
2018-06-22 12:44:56 -0400

    Update NIRSpec fixed slit and MSA regression tests

--- jwst/tests_nightly/general/pipelines/test_nrs_fs_brightobj_spec2.py
+++ jwst/tests_nightly/general/pipelines/test_nrs_fs_brightobj_spec2.py
@@ -11,12 +11,9 @@ pytestmark = [
 
 def test_nrs_fs_brightobj_spec2(_bigdata):
     """
-
     Regression test of calwebb_spec2 pipeline performed on NIRSpec fixed-slit data
     that uses the NRS_BRIGHTOBJ mode (S1600A1 slit).
-
     """
-
     step = Spec2Pipeline()
     step.save_bsub = True
     step.save_results = True
@@ -25,16 +22,16 @@ def test_nrs_fs_brightobj_spec2(_bigdata):
     step.extract_1d.save_results = True
     step.run(_bigdata+'/pipelines/jw84600042001_02101_00001_nrs2_rateints.fits')
 
+    ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX']
+
     na = 'jw84600042001_02101_00001_nrs2_calints.fits'
     nb = _bigdata+'/pipelines/jw84600042001_02101_00001_nrs2_calints_ref.fits'
     h = pf.open(na)
     href = pf.open(nb)
-    newh = pf.HDUList([h['primary'],h['sci',1],h['err',1],h['dq',1],h['relsens',1],
-                                    h['wavelength',1]])
-    newhref = pf.HDUList([href['primary'],href['sci',1],href['err',1],href['dq',1],href['relsens',1],
-                                          href['wavelength',1]])
-    result = pf.diff.FITSDiff(newh, newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
+    result = pf.diff.FITSDiff(h,
+                              href,
+                              ignore_hdus=['ASDF'],
+                              ignore_keywords=ignore_keywords,
                               rtol = 0.00001)
     assert result.identical, result.report()
 
@@ -42,9 +39,9 @@ def test_nrs_fs_brightobj_spec2(_bigdata):
     nb = _bigdata+'/pipelines/jw84600042001_02101_00001_nrs2_x1dints_ref.fits'
     h = pf.open(na)
     href = pf.open(nb)
-    newh = pf.HDUList([h['primary'],h['extract1d',1],h['extract1d',2],h['extract1d',3]])
-    newhref = pf.HDUList([href['primary'],href['extract1d',1],href['extract1d',2],href['extract1d',3]])
-    result = pf.diff.FITSDiff(newh, newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
+    result = pf.diff.FITSDiff(h,
+                              href,
+                              ignore_hdus=['ASDF'],
+                              ignore_keywords=ignore_keywords,
                               rtol = 0.00001)
     assert result.identical, result.report()
--- jwst/tests_nightly/general/pipelines/test_nrs_fs_multi_spec2_3.py
+++ jwst/tests_nightly/general/pipelines/test_nrs_fs_multi_spec2_3.py
@@ -11,10 +11,8 @@ pytestmark = [
 
 def test_nrs_fs_multi_spec2_3(_bigdata):
     """
-
     Regression test of calwebb_spec2 pipeline performed on NIRSpec fixed-slit data
     using the ALLSLITS subarray and detector NRS2.
-
     """
     step = Spec2Pipeline()
     step.save_bsub = True
@@ -24,18 +22,16 @@ def test_nrs_fs_multi_spec2_3(_bigdata):
     step.extract_1d.save_results = True
     step.run(_bigdata+'/pipelines/jw84600002001_02101_00001_nrs2_rate.fits')
 
+    ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX']
+
     na = 'jw84600002001_02101_00001_nrs2_cal.fits'
     nb = _bigdata+'/pipelines/jw84600002001_02101_00001_nrs2_cal_ref.fits'
     h = pf.open(na)
     href = pf.open(nb)
-    newh = pf.HDUList([h['primary'],h['sci',1],h['err',1],h['dq',1],h['relsens',1],h['wavelength',1],
-                                    h['pathloss_pointsource',1],h['wavelength_pointsource',1],
-                                    h['pathloss_uniformsource',1],h['wavelength_uniformsource',1]])
-    newhref = pf.HDUList([href['primary'],href['sci',1],href['err',1],href['dq',1],href['relsens',1],href['wavelength',1],
-                                          href['pathloss_pointsource',1],href['wavelength_pointsource',1],
-                                          href['pathloss_uniformsource',1],href['wavelength_uniformsource',1]])
-    result = pf.diff.FITSDiff(newh, newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
+    result = pf.diff.FITSDiff(h,
+                              href,
+                              ignore_hdus=['ASDF'],
+                              ignore_keywords=ignore_keywords,
                               rtol = 0.00001)
     assert result.identical, result.report()
 
@@ -43,10 +39,10 @@ def test_nrs_fs_multi_spec2_3(_bigdata):
     nb = _bigdata+'/pipelines/jw84600002001_02101_00001_nrs2_s2d_ref.fits'
     h = pf.open(na)
     href = pf.open(nb)
-    newh = pf.HDUList([h['primary'],h['sci',1],h['wht',1],h['con',1],h['relsens',1]])
-    newhref = pf.HDUList([href['primary'],href['sci',1],href['wht',1],href['con',1],href['relsens',1]])
-    result = pf.diff.FITSDiff(newh, newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
+    result = pf.diff.FITSDiff(h,
+                              href,
+                              ignore_hdus=['ASDF'],
+                              ignore_keywords=ignore_keywords,
                               rtol = 0.00001)
     assert result.identical, result.report()
 
@@ -54,9 +50,9 @@ def test_nrs_fs_multi_spec2_3(_bigdata):
     nb = _bigdata+'/pipelines/jw84600002001_02101_00001_nrs2_x1d_ref.fits'
     h = pf.open(na)
     href = pf.open(nb)
-    newh = pf.HDUList([h['primary'],h['extract1d',1]])
-    newhref = pf.HDUList([href['primary'],href['extract1d',1]])
-    result = pf.diff.FITSDiff(newh, newhref,
-                              ignore_keywords = ['DATE','CAL_VER','CAL_VCS','CRDS_VER','CRDS_CTX'],
+    result = pf.diff.FITSDiff(h,
+                              href,
+                              ignore_hdus=['ASDF'],
+                              ignore_keywords=ignore_keywords,
                               rtol = 0.00001)
     assert result.identical, result.report()
