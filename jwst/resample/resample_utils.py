9fad8ea739e284c4cf71d7f38f5b5bc526bdabcf
James Davies <jdavies@stsci.edu>
2018-07-17 16:13:04 -0400

    Fix build_driz_weight() weight_type strings

--- jwst/resample/resample_utils.py
+++ jwst/resample/resample_utils.py
@@ -177,13 +177,13 @@ def build_driz_weight(model, weight_type=None, good_bits=None):
     dqmask = build_mask(model.dq, good_bits)
     exptime = model.meta.exposure.exposure_time
 
-    if weight_type == 'err':
+    if weight_type == 'error':
         err_model = np.nan_to_num(model.err)
         inwht = (exptime / err_model)**2 * dqmask
         log.debug("DEBUG weight mask: {} {}".format(type(inwht), np.sum(inwht)))
     # elif weight_type == 'ivm':
     #     _inwht = img.buildIVMmask(chip._chip,dqarr,pix_ratio)
-    elif weight_type == 'exp':
+    elif weight_type == 'exptime':
         inwht = exptime * dqmask
     else:
         inwht = np.ones(model.data.shape, dtype=model.data.dtype)
