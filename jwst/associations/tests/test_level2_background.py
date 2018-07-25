432b87d2404478b2f03a8c7e38e5a4ec61b3cea4
J.D.Eisenhamer <eisenhamer@stsci.edu>
2018-07-24 23:22:53 -0400

    Add specific test using pool_024
    WIP #2127

--- jwst/associations/tests/test_level2_background.py
+++ jwst/associations/tests/test_level2_background.py
@@ -6,6 +6,7 @@ from .helpers import (
 )
 
 from .. import generate
+from ..main import constrain_on_candidates
 
 
 def test_nrs_msa_nod():
@@ -14,3 +15,16 @@ def test_nrs_msa_nod():
     assert len(asns) == 12
     for asn in asns:
         assert len(asn['products'][0]['members']) == 3
+
+
+def test_nrs_fixedslit_nod():
+    """Test NIRSpec Fixed-slit background nods"""
+    pool = combine_pools(t_path('data/pool_024_nirspec_fss_nods.csv'))
+    constraint_all_candidates = constrain_on_candidates(None)
+    asns = generate(pool, registry_level2_only(
+        global_constraints=constraint_all_candidates)
+    )
+    assert len(asns) == 6
+    for asn in asns:
+        nods = int(asn.constraints['nods'].value)
+        assert len(asn['products'][0]['members']) == nods
