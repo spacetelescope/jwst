e20a6192ba3e3c68c051dafcd92b6400381d2c17
Jane Morrison <morrison@as.arizona.edu>
2018-08-15 16:38:40 -0700

    Pep8 changes for lastframe step

--- jwst/lastframe/lastframe_sub.py
+++ jwst/lastframe/lastframe_sub.py
@@ -1,10 +1,8 @@
 #
 #  Module for the lastframe correction for MIRI science data sets
 #
-
 import numpy as np
 import logging
-from .. import datamodels
 from ..datamodels import dqflags
 
 log = logging.getLogger(__name__)
@@ -40,7 +38,7 @@ def do_correction(input_model):
     # the final group to 'DO_NOT_USE'
     if sci_ngroups > 2:
         output.groupdq[:, -1, :, :] = \
-            np.bitwise_or(output.groupdq[:,-1,:,:], dqflags.group['DO_NOT_USE'])
+            np.bitwise_or(output.groupdq[:, -1, :, :], dqflags.group['DO_NOT_USE'])
         log.debug("LastFrame Sub: resetting GROUPDQ in last frame to DO_NOT_USE")
         output.meta.cal_step.lastframe = 'COMPLETE'
     else:   # too few groups
