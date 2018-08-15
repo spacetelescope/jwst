be7dfd3c92a3995f4cd0eb7eea8e383116965b45
Bernie Simon <bsimon@stsci.edu>
2018-08-15 10:33:13 -0400

    Back out unwanted changes
    The changes from branch fix_file_leaks got mixed into this
    branch. This update backs these changes out.

--- jwst/datamodels/fits_support.py
+++ jwst/datamodels/fits_support.py
@@ -554,18 +554,12 @@ def from_fits_hdu(hdu, schema):
     Read the data from a fits hdu into a numpy ndarray
     """
     data = hdu.data
+    data2 = properties._cast(data, schema)
 
-    # Save the column listeners for possible restoration
+    # Casting a table loses the listeners, so restore them
     if hasattr(data, '_coldefs'):
-        listeners = data._coldefs._listeners
-    else:
-        listeners = None
+        coldefs = data._coldefs
+        coldefs2 = data2._coldefs
+        coldefs2._listeners = coldefs._listeners
 
-    # Cast array to type mentioned in schema
-    data = properties._cast(data, schema)
-
-    # Casting a table loses the column listeners, so restore them
-    if listeners is not None:
-        data._coldefs._listeners = listeners
-
-    return data
+    return data2
