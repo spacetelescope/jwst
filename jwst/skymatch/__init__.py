65900967cccaab559d2a26dbf81b05c1e98a024e
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-08-23 17:23:39 -0400

    more flake8

--- jwst/skymatch/__init__.py
+++ jwst/skymatch/__init__.py
@@ -4,13 +4,7 @@ This package provides support for sky background subtraction and equalization
 
 """
 import logging
-
 from .skymatch_step import SkyMatchStep
-from . import skystatistics
-from . import skymatch
-from . import skyimage
-from . import skymatch_step
-
 
 __author__ = 'Mihai Cara'
 
@@ -18,5 +12,4 @@ __author__ = 'Mihai Cara'
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-__all__ = ["skyimage", "skymatch", "skymatch_step", "skystatistics",
-           "SkyMatchStep"]
+__all__ = ["SkyMatchStep"]
