1d3355e87e558448884c1860c7f8252f967c1c69
Warren Hack <hack@stsci.edu>
2018-09-12 12:51:21 -0400

    First 2 tests with ci_watson

---
+++ jwst/tests_nightly/general/resources.py
@@ -0,0 +1,25 @@
+from ci_watson.base_classes import BaseTest
+
+class BaseJWSTTest(BaseTest):
+    ignore_hdus = ['ASDF']
+    ignore_keywords = ['DATE', 'CAL_VER', 'CAL_VCS', 'CRDS_VER', 'CRDS_CTX']
+    input_repo = 'scsb-jwst-pipeline'
+    copy_local = False  # Do not make additional copy by default
+
+    def set_environ(self):
+        self.tree = ''
+
+class NIRCamTest(BaseJWSTTest):
+    input_loc = 'nircam'
+
+class MIRITest(BaseJWSTTest):
+    input_loc = 'miri'
+
+class NIRISSTest(BaseJWSTTest):
+    input_loc = 'niriss'
+
+class NIRSpecTest(BaseJWSTTest):
+    input_loc = 'nirspec'
+
+class FGSTest(BaseJWSTTest):
+    input_loc = 'fgs'
