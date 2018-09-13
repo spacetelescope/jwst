4c1072210751b80c520ac73d7b4f3d87ca687a93
Philip Hodge <hodge@stsci.edu>
2018-09-13 15:52:27 -0400

    Remove comparison with previous version; add a test for vertical dispersion.  See issue #2487.

--- jwst/flatfield/tests/test_find_dispaxis.py
+++ jwst/flatfield/tests/test_find_dispaxis.py
@@ -7,28 +7,24 @@ from jwst.flatfield import flat_field
 
 def test_find_dispaxis():
 
-    # Create a dummy array of wavelengths.  The array is mostly zero,
-    # but the non-zero values are clearly increasing more rapidly
-    # in the horizontal direction than in the vertical direction.
-    wl = np.zeros((30, 200), dtype=np.float32)
-    wl[16, :] = np.arange(200, dtype=np.float32)
-    wl[17, :] = np.arange(200, dtype=np.float32) + 0.01
-    wl[18, :] = np.arange(200, dtype=np.float32) + 0.02
-    wl[19, :] = np.arange(200, dtype=np.float32) + 0.03
+    # Create two dummy arrays of wavelengths.  The arrays are mostly zero,
+    # but the non-zero values are clearly increasing more rapidly in one
+    # direction rather than the other.
 
-    dispaxis = flat_field.find_dispaxis(wl)
+    wl_h = np.zeros((30, 200), dtype=np.float32)
+    wl_h[16, :] = np.arange(200, dtype=np.float32)
+    wl_h[17, :] = np.arange(200, dtype=np.float32) + 0.01
+    wl_h[18, :] = np.arange(200, dtype=np.float32) + 0.02
+    wl_h[19, :] = np.arange(200, dtype=np.float32) + 0.03
+
+    wl_v = np.zeros((200, 30), dtype=np.float32)
+    wl_v[:, 17] = np.arange(200, dtype=np.float32)
+    wl_v[:, 18] = np.arange(200, dtype=np.float32) + 0.01
+    wl_v[:, 19] = np.arange(200, dtype=np.float32) + 0.02
+    wl_v[:, 20] = np.arange(200, dtype=np.float32) + 0.03
+
+    dispaxis = flat_field.find_dispaxis(wl_h)
     assert dispaxis == 1
 
-    # This is the code that was in combine_fast_slow and that find_dispaxis
-    # replaced.
-    (ny, nx) = wl.shape
-    mid_x = nx // 2
-    mid_y = ny // 2
-    dwlx = abs(wl[mid_y, mid_x+1] - wl[mid_y, mid_x-1])
-    dwly = abs(wl[mid_y+1, mid_x] - wl[mid_y-1, mid_x])
-    if dwlx >= dwly:
-        dispaxis = 1
-    else:
-        dispaxis = 2
-
+    dispaxis = flat_field.find_dispaxis(wl_v)
     assert dispaxis == 2
