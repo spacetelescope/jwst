8fed39a942eb86cb0806aca69b7148e26c04d906
James Davies <jdavies@stsci.edu>
2018-09-13 16:06:45 -0400

    Fix NRS FS WCS regression test

--- jwst/tests_nightly/general/nirspec/test_nrs1_fs_wcs.py
+++ jwst/tests_nightly/general/nirspec/test_nrs1_fs_wcs.py
@@ -1,18 +1,16 @@
 import os
 import pytest
-import numpy as np
-from numpy.testing import utils
+from numpy.testing import assert_allclose
+from gwcs.wcstools import grid_from_bounding_box
 
 from jwst.assign_wcs import AssignWcsStep, nirspec
+from jwst.datamodels import ImageModel
 
 pytestmark = [
     pytest.mark.usefixtures('_jail'),
     pytest.mark.skipif(not pytest.config.getoption('bigdata'),
                        reason='requires --bigdata')
 ]
-from jwst.datamodels import ImageModel
-
-from ..helpers import add_suffix
 
 
 def test_nirspec_nrs1_wcs(_bigdata):
@@ -21,35 +19,25 @@ def test_nirspec_nrs1_wcs(_bigdata):
     Regression test of creating a WCS object and doing pixel to sky transformation.
 
     """
-    output_file_base, output_file = add_suffix('nrs1_fs_wcs_output.fits', 'assignwcsstep')
-
-    try:
-        os.remove(output_file)
-    except:
-        pass
-
     input_file = os.path.join(_bigdata, 'nirspec', 'test_wcs', 'nrs1-fs', 'jw00023001001_01101_00001_NRS1_ramp_fit.fits')
-    ref_file = os.path.join(_bigdata, 'nirspec', 'test_wcs', 'nrs1-fs', 'jw00023001001_01101_00001_NRS1_assign_wcs.fits')
+    ref_file = os.path.join(_bigdata, 'nirspec', 'test_wcs', 'nrs1-fs', 'jw00023001001_01101_00001_NRS1_ramp_fit_assign_wcs.fits')
 
-    AssignWcsStep.call(input_file,
-                       output_file=output_file_base, name='assignwcsstep'
+    result = AssignWcsStep.call(input_file,
+                       save_results=True,
+                       suffix='assign_wcs'
                        )
-    im = ImageModel(output_file)
+    result.close()
+
+    im = ImageModel(result.meta.filename)
     imref = ImageModel(ref_file)
-    #ystart = im.meta.subarray.ystart
-    #yend = im.meta.subarray.ystart + im.meta.subarray.ysize-1
-    #xstart = im.meta.subarray.xstart
-    #xend = im.meta.subarray.xstart + im.meta.subarray.xsize -1
-    #x, y = np.mgrid[ystart:yend, xstart: xend]
+
     for slit in ['S200A1', 'S200A2', 'S400A1', 'S1600A1']:
         w = nirspec.nrs_wcs_set_input(im, slit)
-        #ra, dec, lam = getattr(im.meta, 'wcs_'+slit)(y, x)
-        #raref, decref, lamref = getattr(imref.meta, 'wcs_'+slit)(y, x)
-        y, x = np.mgrid[w.bounding_box[1][0]:w.bounding_box[1][1],
-                        w.bounding_box[0][0]: w.bounding_box[0][1]]
-        ra, dec, lam = w(x, y)
-        wref = nirspec.nrs_wcs_set_input(im, slit)
-        raref, decref, lamref = wref(x, y)
-        utils.assert_allclose(ra, raref, equal_nan=True)
-        utils.assert_allclose(dec, decref, equal_nan=True)
-        utils.assert_allclose(lam, lamref, equal_nan=True)
+        grid = grid_from_bounding_box(w.bounding_box)
+        ra, dec, lam = w(*grid)
+        wref = nirspec.nrs_wcs_set_input(imref, slit)
+        raref, decref, lamref = wref(*grid)
+
+        assert_allclose(ra, raref, equal_nan=True)
+        assert_allclose(dec, decref, equal_nan=True)
+        assert_allclose(lam, lamref, equal_nan=True)
