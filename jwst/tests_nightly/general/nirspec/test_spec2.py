cf12c28f9ac563e93c08ccb5c79c0322c71a9f09
Warren Hack <hack@stsci.edu>
2018-09-27 23:49:58 -0400

    Fix test bugs, clean up conftest

--- jwst/tests_nightly/general/nirspec/test_spec2.py
+++ jwst/tests_nightly/general/nirspec/test_spec2.py
@@ -7,14 +7,9 @@ from ci_watson.artifactory_helpers import get_bigdata
 from jwst.assign_wcs.util import NoDataOnDetectorError
 from jwst.pipeline import Spec2Pipeline
 
-pytestmark = [
-    pytest.mark.usefixtures('_jail'),
-    pytest.mark.skipif(not pytest.config.getoption('bigdata'),
-                       reason='requires --bigdata')
-]
 
-
-def test_nrs2_nodata_api(_bigdata):
+@pytest.mark.bigdata
+def test_nrs2_nodata_api(envopt, _jail):
     """
 
     Regression test of handling NRS2 detector that has no data.
@@ -27,16 +22,19 @@ def test_nrs2_nodata_api(_bigdata):
     step.assign_wcs.skip = False
 
     with pytest.raises(NoDataOnDetectorError):
-        step.run(get_bigdata(
-            _bigdata, 'nirspec', 'test_assignwcs', 'jw84700006001_02101_00001_nrs2_rate.fits'
+        step.run(get_bigdata('jwst-pipeline', envopt, 
+                             'nirspec', 'test_assignwcs', 
+                             'jw84700006001_02101_00001_nrs2_rate.fits'
         ))
 
 
-def test_nrs2_nodata_strun(_bigdata):
+@pytest.mark.bigdata
+def test_nrs2_nodata_strun(envopt, _jail):
     """Ensure that the appropriate exit status is returned from strun"""
 
-    data_file = get_bigdata(
-        _bigdata, 'nirspec', 'test_assignwcs', 'jw84700006001_02101_00001_nrs2_rate.fits'
+    data_file = get_bigdata('jwst-pipeline', envopt, 
+                            'nirspec', 'test_assignwcs', 
+                            'jw84700006001_02101_00001_nrs2_rate.fits'
     )
 
     cmd = [
