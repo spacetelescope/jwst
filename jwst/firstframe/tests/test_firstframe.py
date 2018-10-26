e836ea690385eb50e9bcacb0b00c07599521d33a
cracraft <cracraft@stsci.edu>
2018-10-26 09:43:21 -0400

    Updated firstframe to test NIR data

--- jwst/firstframe/tests/test_firstframe.py
+++ jwst/firstframe/tests/test_firstframe.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 from jwst.datamodels.miri_ramp import MIRIRampModel
+from jwst.datamodels import RampModel
 from jwst.datamodels import dqflags
 from jwst.firstframe.firstframe_sub import do_correction
+from jwst.firstframe import FirstFrameStep
+
 
 def test_firstframe_set_groupdq():
     """ 
     Test if the firstframe code set the groupdq flag on the first
-    group to 'do_not_use'
+    group to 'do_not_use' for 5 integrations
     """
 
     # size of integration
@@ -135,7 +138,8 @@ def test_firstframe_3groups():
     dm_ramp_firstframe = do_correction(dm_ramp)
 
     # check that the difference in the groupdq flags is equal to
-    #   the 0 since the step should not run if frames 3 or fewer
+
+    #  0 since the step should not run if frames 3 or fewer
     dq_diff = dm_ramp_firstframe.groupdq[0, 0, :, :] - dm_ramp.groupdq[0, 0, :, :]
     
     np.testing.assert_array_equal(np.full((ysize, xsize),
@@ -145,9 +149,71 @@ def test_firstframe_3groups():
                                   err_msg='Diff in groupdq flags is not '
                                   + 'equal to 0')
 
-    
-if __name__ == '__main__':
-    test_firstframe_set_groupdq()
-    test_firstframe_single_group()
-    test_firstframe_add1_groupdq()
-    test_firstframe_3groups()
+
+def test_nircam():
+    # test that the code skips processing for a NIR instrument
+
+    # size of integration
+    ngroups = 3
+    xsize = 2048
+    ysize = 2048
+
+    # create the data and groupdq arrays
+    csize = (1, ngroups, ysize, xsize)
+    data = np.full(csize, 1.0)
+    groupdq = np.zeros(csize, dtype=int)
+
+    # create a JWST datamodel for data
+    dm_ramp = RampModel(data=data, groupdq=groupdq)
+
+    dm_ramp.meta.instrument.name = 'NIRCAM'
+    dm_ramp.meta.instrument.detector = 'NRCA1'
+
+    # run the first frame correction step
+    dm_ramp_firstframe = FirstFrameStep.call(dm_ramp)
+
+    # check that the difference in the groupdq flags is equal to
+    #   the 0 since the step should not run for NIR data
+    dq_diff = dm_ramp_firstframe.groupdq[0, 0, :, :] - dm_ramp.groupdq[0, 0, :, :]
+
+    np.testing.assert_array_equal(np.full((ysize, xsize),
+                                          0,
+                                          dtype=int),
+                                  dq_diff,
+                                  err_msg='Diff in groupdq flags is not '
+                                          + 'equal to 0')
+
+
+def test_miri():
+    # test that the code runs given MIRI as an instrument
+
+    # size of integration
+    ngroups = 5
+    xsize = 1032
+    ysize = 1024
+
+    # create the data and groupdq arrays
+    csize = (1, ngroups, ysize, xsize)
+    data = np.full(csize, 1.0)
+    groupdq = np.zeros(csize, dtype=int)
+
+    # create a JWST datamodel for data
+    dm_ramp = RampModel(data=data, groupdq=groupdq)
+
+    dm_ramp.meta.instrument.name = 'MIRI'
+    dm_ramp.meta.instrument.detector = 'MIRIMAGE'
+
+    # run the first frame correction step
+    dm_ramp_firstframe = FirstFrameStep.call(dm_ramp)
+
+    # check that the difference in the groupdq flags is equal to
+    #  DO_NOT_USE flag
+    dq_diff = dm_ramp_firstframe.groupdq[0, 0, :, :] - dm_ramp.groupdq[0, 0, :, :]
+
+    np.testing.assert_array_equal(np.full((ysize, xsize),
+                                          dqflags.group['DO_NOT_USE'],
+                                          dtype=int),
+                                  dq_diff,
+                                  err_msg='Diff in groupdq flags is not '
+                                          + 'equal to DO_NOT_USE')
+
