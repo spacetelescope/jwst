65a69bc524dae2206a261f2126c61fd992584c40
Michael Regan <mregan@stsci.edu>
2018-10-29 13:32:31 -0400

    Removed commented out code
    This removes the code that was commented out to test the change for issue 2790.

--- jwst/ramp_fitting/ramp_fit.py
+++ jwst/ramp_fitting/ramp_fit.py
@@ -634,8 +634,6 @@ def ols_ramp_fit(model, buffsize, save_opt, readnoise_model, gain_model,
     var_p2 = 1/(s_inv_var_p3.sum(axis=0))
     var_r2 = 1/(s_inv_var_r3.sum(axis=0))
 
-    # convert the read noise variance back to (DN/S)**2
-#    var_r2 /= gain_2d**2
     
     del s_inv_var_p3
     del s_inv_var_r3
