548185c0678989969826210f09415acaf41576cf
J.D.Eisenhamer <eisenhamer@stsci.edu>
2018-11-14 15:42:04 -0500

    docstring bug fix [skip ci]

--- jwst/associations/main.py
+++ jwst/associations/main.py
@@ -36,9 +36,10 @@ class Main():
     ----------
     args : [str, ...], or None
         The command line arguments. Can be one of
-            - `None`: `sys.argv` is then used.
-            - `[str, ...]`: A list of strings which create the command line
-              with the similar structure as `sys.argv`
+
+        - `None`: `sys.argv` is then used.
+        - `[str, ...]`: A list of strings which create the command line
+          with the similar structure as `sys.argv`
 
     pool : None or AssociationPool
         If `None`, a pool file must be specified in the `args`.
