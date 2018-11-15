spacetelescope/jwst

commit 143bf5c928d56de77280a0a45a6b49eea7a31842

Add a function to generate documentation from schema files (#2786)
This update adds the function build_docstring to schema.py. It takes a
datamodels class and extracts field values from its schema for each
schema item associated with a fits hdu. It then inserts the field
values into a format string, which is passed as the second
argument. This update also includes a new script, schemadoc, which
calls it.

jwst/datamodels/tests/test_schema.py
@@ -15,20 +15,10 @@ from astropy.io import fits
 from astropy.modeling import models
 from astropy import time
 
-from jwst.datamodels import util, validate
-from jwst.datamodels import _defined_models as defined_models
-from jwst.datamodels import (
-    DataModel,
-    ImageModel,
-    RampModel,
-    MaskModel,
-    MultiSlitModel,
-    AsnModel,
-    CollimatorModel,
-    SourceModelContainer,
-    MultiExposureModel,
-)
-from jwst.datamodels.schema import merge_property_trees
+from .. import util, validate
+from .. import (DataModel, ImageModel, RampModel, MaskModel,
+                MultiSlitModel, AsnModel, CollimatorModel)
+from ..schema import merge_property_trees, build_docstring
 
 from asdf import schema as mschema
 
@@ -629,6 +619,13 @@ def test_merge_property_trees(combiner):
     assert f == s
 
 
+
+def test_schema_docstring():
+    template = "{fits_hdu} {title}"
+    docstring = build_docstring(ImageModel, template).split("\n")
+    for i, hdu in enumerate(('SCI', 'DQ', 'ERR', 'ZEROFRAME')):
+        assert docstring[i].startswith(hdu)
+
 @pytest.mark.parametrize("model", [m for m in defined_models.values()])
 def test_all_datamodels_init(model):
     """
@@ -640,3 +637,4 @@ def test_all_datamodels_init(model):
     else:
         m = model()
     del m
+

jwst/datamodels/schema.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import re
 from collections import OrderedDict
 
 from asdf import AsdfFile
@@ -7,6 +8,8 @@ from asdf import schema as asdf_schema
 
 from jwst.transforms.jwextension import JWSTExtension
 from gwcs.extension import GWCSExtension
+
+from . import model_base
 
 # return_result included for backward compatibility
 def find_fits_keyword(schema, keyword, return_result=False):
@@ -295,3 +298,103 @@ def read_schema(schema_file, extensions=None):
 
     schema = merge_property_trees(schema)
     return schema
+
+
+def build_docstring(klass, template):
+    """
+    Build a docstring for the specified DataModel class from its schema.
+
+
+    Parameters
+    ----------
+
+    klass : Python class
+        A class instance of a datamodel
+    template : str
+        A string format template to be applied to each schema item
+
+    Returns
+    -------
+    field_info : str
+        Information about each schema item associated with a FITS hdu
+    """
+
+    def get_field_info(subschema, path, combiner, info, recurse):
+        # Return all schema fields representing fits hdus
+        if 'fits_hdu' in subschema and not 'fits_keyword' in subschema:
+            attr = '.'.join(path)
+            info[attr] = subschema
+        return 'fits_hdu' in subschema or 'fits_keyword' in subschema
+
+    # Silly rabbit, only datamodels have schemas
+    if not (klass == model_base.DataModel or
+            issubclass(klass, model_base.DataModel)):
+        raise ValueError("Class must be a subclass of DataModel: %s",
+                          klass.__name__)
+
+    # Create a new model just to get its shape
+    null_object = klass(init=None)
+    shape = null_object.shape
+    if shape is None:
+        shaped_object = null_object
+    else:
+        # Instantiate an object with correctly dimensioned shape
+        null_object.close()
+        shape = tuple([1 for i in range(len(shape))])
+        shaped_object = klass(init=shape)
+
+    # Get schema fields which have an associated hdu
+    info = {}
+    walk_schema(shaped_object._schema, get_field_info, ctx=info)
+
+    # Extract field names from template to set defaults
+    # so format won't crash while using them when they aren't there
+    default_schema = {}
+    fields = re.findall(r'\{([^\\:}]*)[\:\}]', template)
+    for field in fields:
+        default_schema[field] = ''
+
+    buffer = []
+    for attr, subschema in info.items():
+        schema = {}
+        schema.update(default_schema)
+        schema.update(subschema)
+        schema['path'] = attr
+
+        # Determine if attribute has a default value
+        instance = shaped_object.instance
+        for field in attr.split('.'):
+            try:
+                instance = instance.get(field)
+            except:
+                instance = None
+            if instance is None:
+                break
+        schema['default'] = instance is not None
+
+        # Extract table field names from datatype
+        if type(schema['datatype']) == str:
+            schema['array'] = True
+        else:
+            schema['records'] = True
+            fields = []
+            for field_info in schema['datatype']:
+                fields.append(field_info['name'])
+            schema['fields'] = ', '.join(fields)
+            schema['datatype'] = 'table'
+
+        # Convert boolean fields to their field names
+        for field, value in schema.items():
+            if type(value) == bool:
+                schema[field] = field
+
+        # Apply format to svhema fields
+        # Delete blank lines
+        lines = template.format(**schema)
+        for line in lines.split("\n"):
+            if line and not line.isspace():
+                buffer.append(line)
+
+    field_info = "\n".join(buffer) + "\n"
+    return field_info
+

scripts/schemadoc
@@ -1,0 +1,76 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2018 Association of Universities for Research in Astronomy (AURA)
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+#     1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+
+#     2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+
+#     3. The name of AURA and its representatives may not be used to
+#       endorse or promote products derived from this software without
+#       specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY AURA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL AURA BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+
+import sys
+import argparse
+from jwst.datamodels.schema import build_docstring
+
+def get_docstrings(template, model_names, all=False):
+    # Get the docstring for every model class
+    from jwst.datamodels import _defined_models as defined_models
+
+    if all:
+        klasses = defined_models
+    else:
+        klasses = {}
+        for model_name in model_names:
+            klasses[model_name] = defined_models[model_name]
+
+    for klassname, klass in klasses.items():
+        try:
+            docstring = build_docstring(klass, template)
+        except Exception as err:
+            print(klassname, ':', str(err), file=sys.stderr)
+        else:
+            print(".. " + klassname + " ..")
+            print(docstring, end='')
+
+long_description = """
+Create documentation from the schema file of a datamodel class
+"""
+
+parser = argparse.ArgumentParser(description=long_description)
+parser.add_argument("-a", "--all", action="store_true",
+                    help="generate docstring for all models")
+parser.add_argument("-t", "--template", type=str,
+                    help="input file containing templates")
+parser.add_argument("models", nargs='*',
+                    help="Models to generate docstrings for")
+args = parser.parse_args()
+
+if args.template is None:
+    template = "{path} : {title} ({datatype})\n"
+else:
+    with open(args.template, "r") as fd:
+        template = fd.readlines()
+        template = ''.join(template)
+
+get_docstrings(template, args.models, all=args.all)

commit e66b9c2b0f30f7a487bfc3923af0d60fc164a20a

Fix bugs introduced in #2786

jwst/datamodels/tests/test_schema.py
@@ -15,10 +15,20 @@ from astropy.io import fits
 from astropy.modeling import models
 from astropy import time
 
-from .. import util, validate
-from .. import (DataModel, ImageModel, RampModel, MaskModel,
-                MultiSlitModel, AsnModel, CollimatorModel)
-from ..schema import merge_property_trees, build_docstring
+from jwst.datamodels import util, validate
+from jwst.datamodels import _defined_models as defined_models
+from jwst.datamodels import (
+    DataModel,
+    ImageModel,
+     RampModel,
+     MaskModel,
+     MultiSlitModel,
+     AsnModel,
+     CollimatorModel,
+     SourceModelContainer,
+     MultiExposureModel,
+ )
+from jwst.datamodels.schema import merge_property_trees, build_docstring
 
 from asdf import schema as mschema
 