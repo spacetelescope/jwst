041ceaf047b2ee166163cee0deacb14212a21226
Karl Gordon <kgordon@stsci.edu>
2018-12-13 09:36:49 -0500

    Simplied the code a bit

--- jwst/ramp_fitting/gls_fit.py
+++ jwst/ramp_fitting/gls_fit.py
@@ -520,17 +520,11 @@ def compute_slope(data_sect, input_var_sect,
             saturated_data[k] = saturated[k][ncr_mask]
 
         (result, variances) = \
-                gls_fit(ramp_data, input_var_data,
+                gls_fit(ramp_data,
                         prev_fit_data, prev_slope_data,
                         readnoise, gain,
                         frame_time, group_time, nframes_used,
-                        num_cr, cr_flagged_2d, saturated_data,
-                        use_extra_terms=use_extra_terms)
-                # gls_fit_simple(ramp_data, input_var_data,
-                #        prev_fit_data, prev_slope_data,
-                #        readnoise, gain,
-                #        frame_time, group_time, nframes_used,
-                #        num_cr, cr_flagged_2d, saturated_data)
+                        num_cr, cr_flagged_2d, saturated_data)
         # Copy the intercept, slope, and cosmic-ray amplitudes and their
         # variances to the arrays to be returned.
         # ncr_mask is a mask array that is True for each pixel that has the
@@ -550,228 +544,8 @@ def compute_slope(data_sect, input_var_sect,
     return (intercept_sect, int_var_sect, slope_sect, slope_var_sect,
             cr_sect, cr_var_sect)
 
-def gls_fit(ramp_data, input_var_data,
-             prev_fit_data, prev_slope_data,
-             readnoise, gain,
-             frame_time, group_time, nframes_used,
-             num_cr, cr_flagged_2d, saturated_data,
-             use_extra_terms=True):
-    """Generalized least squares linear fit.
 
-    It is assumed that every input pixel has num_cr cosmic-ray hits
-    somewhere within the ramp.  This function should be called separately
-    for different values of num_cr.
-
-    Parameters
-    ----------
-    ramp_data: 2-D ndarray; indices:  group, pixel number
-        The ramp data for one of the integrations in an exposure.  This
-        may be a subset in detector coordinates, but covering all groups.
-        The shape is (ngroups, nz), where ngroups is the length of the
-        ramp, and nz is the number of pixels in the current subset.
-
-    input_var_data: 2-D ndarray, shape (ngroups, nz)
-        The square of the input ERR array, matching ramp_data.
-
-    prev_fit_data: 2-D ndarray, shape (ngroups, nz)
-        The fit to ramp_data, based on applying the values of intercept,
-        slope, and cosmic-ray amplitudes that were determined in a previous
-        call to gls_fit.  This array is only used for setting up the
-        covariance matrix.
-
-    prev_slope_data: 1-D ndarray, length nz.
-        An estimate (e.g. from a previous iteration) of the slope at each
-        pixel, in electrons per second.
-
-    readnoise: 1-D ndarray, length nz.
-        The read noise in electrons at each detector pixel.
-
-    gain: 1-D ndarray, shape (nz,)
-        The analog-to-digital gain (electrons per dn) at each detector
-        pixel.
-
-    frame_time: float
-        The time to read one frame, in seconds (e.g. 10.6 s).
-
-    group_time: float
-        Time increment between groups, in seconds.
-
-    nframes_used: int
-        Number of frames that were averaged together to make a group.
-        Note that this value does not include the number (if any) of
-        skipped frames.
-
-    num_cr: int
-        The number of cosmic rays that will be handled.  All pixels in the
-        current set (ramp_data) are assumed to have this many cosmic ray
-        hits somewhere within the ramp.
-
-    cr_flagged_2d: 2-D ndarray, shape (ngroups, nz)
-        The values should be 0 or 1; 1 indicates that a cosmic ray was
-        detected (by another step) at that point.
-
-    saturated_data: 2-D ndarray, shape (ngroups, nz)
-        Normal values are zero; the value will be a huge number for
-        saturated pixels.  This will be added to the main diagonal of the
-        inverse of the weight matrix to greatly reduce the weight for
-        saturated pixels.
-
-    use_extra_terms: bool
-        True if we should include Massimo Robberto's terms in the inverse
-        weight matrix.
-        See JWST-STScI-003193.pdf
-
-    Returns
-    -------
-    tuple:  (result2d, variances)
-        result2d is a 2-D ndarray; shape (nz, 2 + num_cr)
-        The computed values of intercept, slope, and cosmic-ray amplitudes
-        (there will be num_cr cosmic-ray amplitudes) for each of the nz
-        pixels.
-
-        variances is a 2-D ndarray; shape (nz, 2 + num_cr)
-        The variance for the intercept, slope, and for the amplitude of
-        each cosmic ray that was detected.
-    """
-
-    M = float(nframes_used)
-
-    ngroups = ramp_data.shape[0]
-    nz = ramp_data.shape[1]
-    num_cr = int(num_cr)
-
-    # x is an array (length nz) of matrices, each of which is the
-    # independent variable of a linear equation.  Each such matrix
-    # has ngroups rows and 2 + num_cr columns.  The first column is set
-    # to 1, for finding the intercept.  The second column is the time at
-    # each group, for finding the slope.  The remaining columns (if any),
-    # are 0 for all rows prior to a certain point, then 1 for all
-    # subsequent rows (i.e. the Heaviside function).  The transition from
-    # 0 to 1 is the location of a cosmic ray hit; the first 1 in a column
-    # corresponds to the value in cr_flagged_2d being 1.
-    x = np.zeros((nz, ngroups, 2 + num_cr), dtype=np.float64)
-    # KDG - should this be 1 or some particular time?
-    x[:, :, 0] = 1.
-    x[:, :, 1] = np.arange(ngroups, dtype=np.float64) * group_time + \
-                 frame_time * (M + 1.) / 2.
-
-    if num_cr > 0:
-        sum_crs = cr_flagged_2d.cumsum(axis=0)
-        for k in range(ngroups):
-            s = slice(k, ngroups)
-            for n in range(1, num_cr + 1):
-                temp = np.where(np.logical_and(cr_flagged_2d[k] == 1,
-                                               sum_crs[k] == n))
-                if len(temp[0]) > 0:
-                    index = (temp[0], s, n + 1)
-                    x[index] = 1
-        del temp, index
-
-    y = np.transpose(ramp_data, (1, 0)).reshape((nz, ngroups, 1))
-
-    # cov is an array of nz matrices, each ngroups x ngroups.  The
-    # inverse of each of these matrices is a weight matrix.
-    # Note that there are two objects that are called the covariance
-    # matrix:  (1) the ngroups x ngroups matrices in cov, and (2) the
-    # smaller matrix (see near the end of this function) that contains
-    # the variances and covariances of the fitted parameters.
-
-    cov = np.ones((nz, ngroups, ngroups), dtype=np.float64)
-
-    # Use the previous fit to the data to populate the covariance matrix,
-    # for each of the nz pixels.  prev_fit_data has shape (ngroups, nz),
-    # similar to the ramp data, but we want the nz axis to be the first
-    # (we're constructing an array of nz matrix equations), so transpose
-    # prev_fit_data.
-    prev_fit_T = np.transpose(prev_fit_data, (1, 0))
-    for k in range(ngroups):
-        # Populate the upper right, row by row.
-        cov[:, k, k:ngroups] = prev_fit_T[:, k:k + 1]
-        # Populate the lower left, column by column.
-        cov[:, k:ngroups, k] = prev_fit_T[:, k:k + 1]
-        # Propagate errors from input.
-        cov[:, k, k] += input_var_data[k, :]
-        # Give saturated pixels very low weight (i.e. high variance).
-        cov[:, k, k] += saturated_data[k, :]
-    del prev_fit_T
-
-    # I is 2-D, but it can broadcast to 4-D.  This is used to add terms to
-    # the diagonal of the covariance matrix.
-    I = np.identity(ngroups)
-
-    # Divide by sqrt(2) to convert the readnoise from CDS to single readout.
-    rn3d = readnoise.reshape((nz, 1, 1)) * SINGLE_READOUT_RN_FACTOR
-    cov += (I * (rn3d**2 / M))
-
-    # prev_slope_data must be non-negative.
-    flags = prev_slope_data < 0.
-    prev_slope_data[flags] = 1.
-
-    if use_extra_terms:
-        # Include two dummy axes to allow broadcasting with cov.
-        slope3d = prev_slope_data.reshape((nz, 1, 1))
-        # diagonal:
-        if gain is not None:
-            g3d = gain.reshape((nz, 1, 1))
-        else:
-            g3d = 1.
-        delta_diag = I * (slope3d * frame_time *
-                          (M - 1.) * (M - 2.) / (3. * M) +
-                          (g3d * M)**2 / 12.)
-        cov += delta_diag
-        del delta_diag
-
-    # This is the solution:  (xT @ weight @ x)^-1 @ [xT @ weight @ y]
-    # where @ means matrix multiplication.
-
-    # shape of xT is (nz, 2 + num_cr, ngroups)
-    xT = np.transpose(x, (0, 2, 1))
-
-    # shape of `weight` is (nz, ngroups, ngroups)
-    I = I.reshape((1, ngroups, ngroups))
-    weight = la.solve(cov, I)                   # inverse of cov
-    del I
-
-    # temp1 = xT @ weight
-    # shape of temp1 is (nz, 2 + num_cr, ngroups)
-    temp1 = np.einsum('...ij,...jk->...ik', xT, weight)
-
-    # temp_var = xT @ weight @ x
-    # shape of temp_var is (nz, 2 + num_cr, 2 + num_cr)
-    temp_var = np.einsum('...ij,...jk->...ik', temp1, x)
-
-    # `covar` is an array of nz covariance matrices.
-    # covar = (xT @ weight @ x)^-1
-    # shape of covar is (nz, 2 + num_cr, 2 + num_cr)
-    I_2 = np.eye(2 + num_cr).reshape((1, 2 + num_cr, 2 + num_cr))
-    try:
-        covar = la.solve(temp_var, I_2)         # inverse of temp_var
-    except la.LinAlgError as msg:
-        for z in range(nz):
-            try:
-                dummy = la.solve(temp_var[z], I_2)
-            except la.LinAlgError as msg2:
-                log.warning("singular matrix, z = %d" % z)
-                raise la.LinAlgError(msg2)
-    del I_2
-
-    # [xT @ weight @ y]
-    # shape of temp2 is (nz, 2 + num_cr, 1)
-    temp2 = np.einsum('...ij,...jk->...ik', temp1, y)
-
-    # shape of result is (nz, 2 + num_cr, 1)
-    result = np.einsum('...ij,...jk->...ik', covar, temp2)
-    r_shape = result.shape
-    result2d = result.reshape((r_shape[0], r_shape[1]))
-    del result
-
-    # shape of both result2d and variances is (nz, 2 + num_cr)
-    variances = covar.diagonal(axis1=1, axis2=2).copy()
-
-    return (result2d, variances)
-
-
-def gls_fit_simple(ramp_data, input_var_data,
+def gls_fit(ramp_data,
              prev_fit_data, prev_slope_data,
              readnoise, gain,
              frame_time, group_time, nframes_used,
@@ -796,9 +570,6 @@ def gls_fit_simple(ramp_data, input_var_data,
         may be a subset in detector coordinates, but covering all groups.
         The shape is (ngroups, nz), where ngroups is the length of the
         ramp, and nz is the number of pixels in the current subset.
-
-    input_var_data: 2-D ndarray, shape (ngroups, nz)
-        The square of the input ERR array, matching ramp_data.
 
     prev_fit_data: 2-D ndarray, shape (ngroups, nz)
         The fit to ramp_data, based on applying the values of intercept,
--- jwst/ramp_fitting/tests/test_gls_ramp_fit.py
+++ jwst/ramp_fitting/tests/test_gls_ramp_fit.py
@@ -1,72 +1,16 @@
-import pytest
 import numpy as np
 
-from jwst.ramp_fitting.ramp_fit import ramp_fit
-from jwst.datamodels import dqflags
-from jwst.datamodels import MIRIRampModel
-from jwst.datamodels import GainModel, ReadnoiseModel
-#from nose.tools import assert_almost_equals
+from jwst.ramp_fitting.tests.check_gls_stats import simple_ramp
 
 
 #ramp_fit_step hardcodes the input to be OLS. So you can't get to the GLS code.
 #@pytest.mark.xfail(reason="Fails, not implemented")
-def test_simple_gls_ramp():
+def test_gls_ramp_40():
     #Here given a 10 group ramp with an exact slope of 20/group.
     # The output slope should be 20.
-    ngroups = 5
-    slope_per_group = 20.
-    model1, gdq, rnModel, pixdq, err, gain = setup_inputs(ngroups=ngroups)
-    for k in range(ngroups):
-        model1.data[0, k, :, :] = 10.0 + k*slope_per_group
+    slopes = simple_ramp(fit_method='GLS', ngroups=40, cr_group=None)
 
-    slopes = ramp_fit(model1, 1024*1., False, rnModel, gain, 'GLS', 'optimal')
     # print(slopes[0].data)
     # take the ratio of the slopes to get the relative error
     # assert_almost_equals(slopes[0].data[500, 500]/ 20.0, 1.0, places=6)
     np.testing.assert_allclose(slopes[0].data[500, 500]/ 20.0, 1.0)
-
-
-#Need test for multi-ints near zero with positive and negative slopes
-
-def setup_inputs(ngroups=10, readnoise=10, nints=1,
-                 nrows=1032, ncols=1024, nframes=1, grouptime=1.0,gain=1, deltatime=1):
-
-        print('readnoise', readnoise)
-        print('gain',gain)
-        times = np.array(list(range(ngroups)),dtype=np.float64) * deltatime
-        gain = np.ones(shape=(nrows, ncols), dtype=np.float64) * gain
-        err = np.ones(shape=(nints, ngroups, nrows, ncols), dtype=np.float64)
-        data = np.zeros(shape=(nints, ngroups, nrows, ncols), dtype=np.float64)
-        pixdq = np.zeros(shape=(nrows, ncols), dtype= np.float64)
-        read_noise = np.full((nrows, ncols), readnoise, dtype=np.float64)
-        gdq = np.zeros(shape=(nints, ngroups, nrows, ncols), dtype=np.int32)
-        model1 = MIRIRampModel(data=data, err=err, pixeldq=pixdq, groupdq=gdq, times=times)
-        model1.meta.instrument.name='MIRI'
-        model1.meta.instrument.detector='MIRIMAGE'
-        model1.meta.instrument.filter='F480M'
-        model1.meta.observation.date='2015-10-13'
-        model1.meta.exposure.type='MIR_IMAGE'
-        model1.meta.exposure.group_time = deltatime
-        model1.meta.subarray.name='FULL'
-        model1.meta.subarray.xstart=1
-        model1.meta.subarray.ystart = 1
-        model1.meta.subarray.xsize = 1024
-        model1.meta.subarray.ysize = 1032
-        model1.meta.exposure.frame_time =deltatime
-        model1.meta.exposure.ngroups = ngroups
-        model1.meta.exposure.group_time = deltatime
-        model1.meta.exposure.nframes = 1
-        model1.meta.exposure.groupgap = 0
-        gain = GainModel(data=gain)
-        gain.meta.instrument.name='MIRI'
-        gain.meta.subarray.xstart = 1
-        gain.meta.subarray.ystart = 1
-        gain.meta.subarray.xsize = 1024
-        gain.meta.subarray.ysize = 1032
-        rnModel = ReadnoiseModel(data=read_noise)
-        rnModel.meta.instrument.name='MIRI'
-        rnModel.meta.subarray.xstart = 1
-        rnModel.meta.subarray.ystart = 1
-        rnModel.meta.subarray.xsize = 1024
-        rnModel.meta.subarray.ysize = 1032
-        return model1, gdq, rnModel, pixdq, err, gain
