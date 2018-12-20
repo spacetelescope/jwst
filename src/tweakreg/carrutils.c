4f261a5c5638c95980e3dbcdc07ae925dadef01c
James Davies <jdavies@stsci.edu>
2018-12-20 14:49:58 -0500

    Initialize PyArrayObject in carrutils.c

--- src/tweakreg/carrutils.c
+++ src/tweakreg/carrutils.c
@@ -83,6 +83,7 @@ arrxyzero(PyObject *obj, PyObject *args)
   dimensions[0] = (integer_t)(searchrad*2) + 1;
   dimensions[1] = (integer_t)(searchrad*2) + 1;
   ozpmat = (PyArrayObject *)PyArray_SimpleNew(2, dimensions, NPY_DOUBLE);
+  PyArray_FILLWBYTE(ozpmat, 0);
   if (!ozpmat) {
     goto _exit;
   }
