986df1937c6a5df6018d1d14df894a855441ec64
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-01-18 10:09:44 -0500

    Split out slow tests and move them into regression

---
+++ jwst/tests_nightly/general/associations/test_generate.py
@@ -0,0 +1,31 @@
+"""Test general asn_generate operations"""
+from jwst.associations.tests.helpers import full_pool_rules
+
+from jwst.associations import (
+    generate,
+    load_asn
+)
+
+
+def test_generate(full_pool_rules):
+    """Run a full sized pool using all rules"""
+    pool, rules, pool_fname = full_pool_rules
+    asns = generate(pool, rules)
+    assert len(asns) == 34
+    for asn in asns:
+        asn_name, asn_store = asn.dump()
+        asn_table = load_asn(asn_store)
+        schemas = rules.validate(asn_table)
+        assert len(schemas) > 0
+
+
+def test_serialize(full_pool_rules):
+    """Test serializing roundtripping"""
+    pool, rules, pool_fname = full_pool_rules
+    asns = generate(pool, rules)
+    for asn in asns:
+        for format in asn.ioregistry:
+            fname, serialized = asn.dump(format=format)
+            assert serialized is not None
+            recovered = load_asn(serialized)
+            assert recovered is not None
