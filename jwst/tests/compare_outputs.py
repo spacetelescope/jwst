771f9f337cfb00826ad0db3fce36b183433dd132
James Davies <jdavies@stsci.edu>
2019-01-23 12:28:20 -0500

    WIP: artifactory okify script

--- jwst/tests/compare_outputs.py
+++ jwst/tests/compare_outputs.py
@@ -8,6 +8,7 @@ from ci_watson.artifactory_helpers import (
     get_bigdata,
     BigdataError,
     generate_upload_schema,
+    UPLOAD_SCHEMA,
 )
 
 from astropy.io import fits
@@ -347,3 +348,52 @@ def generate_upload_params(results_root, updated_outputs, verbose=True):
             print("    {}".format(truth))
 
     return schema_pattern, tree, testname
+
+
+def generate_okify_schema(pattern, target, testname, recursive=False):
+    """
+    Build an upload schema for updating truth files on Artifactory.
+
+    This function uses Artifactory JSON spec files for uploading data using
+    the Artifactory Jenkins plugin.  Docs can be found at
+    https://www.jfrog.com/confluence/display/RTF/Using+File+Specs
+
+    Parameters
+    ----------
+    pattern : str or list of strings
+        Specifies the local file system path to test results which should be
+        uploaded to Artifactory. You can specify multiple artifacts by using
+        wildcards or a regular expression as designated by the regexp property.
+    target : str
+        Specifies the target path in Artifactory in the following format::
+            [repository_name]/[repository_path]
+    testname : str
+        Name of test that generate the results. This will be used to create the
+        name of the JSON file to enable these results to be uploaded to
+        Artifactory.
+    recursive : bool, optional
+        Specify whether or not to identify files listed in sub-directories
+        for uploading.  Default: `False`
+    """
+    jsonfile = "{}_update_truth.json".format(testname)
+    recursive = repr(recursive).lower()
+
+    if not isinstance(pattern, str):
+        # Populate schema for this test's data
+        upload_schema = {"files": []}
+
+        for p in pattern:
+            temp_schema = copy.deepcopy(UPLOAD_SCHEMA["files"][0])
+            temp_schema.update({"pattern": p, "target": target,
+                                "recursive": recursive})
+            upload_schema["files"].append(temp_schema)
+
+    else:
+        # Populate schema for this test's data
+        upload_schema = copy.deepcopy(UPLOAD_SCHEMA)
+        upload_schema["files"][0].update({"pattern": pattern, "target": target,
+                                          "recursive": recursive})
+
+    # Write out JSON file with description of test results
+    with open(jsonfile, 'w') as outfile:
+        json.dump(upload_schema, outfile, indent=2)
