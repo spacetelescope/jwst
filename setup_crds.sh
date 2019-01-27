53fd1494e0e1127d21c3ca8fed5d72c831ca8052
Andrea Zonca <code@andreazonca.com>
2019-01-26 23:13:38 -0800

    setup crds script

---
+++ setup_crds.sh
@@ -0,0 +1,5 @@
+export CRDS_PATH="$HOME/crds_cache"
+#export CRDS_SERVER_URL="https://jwst-crds.stsci.edu"
+export CRDS_CONTEXT=tmt_0001.pmap
+export CRDS_SERVER_URL=https://crds-serverless-mode.stsci.edu
+mkdir -p $CRDS_PATH
