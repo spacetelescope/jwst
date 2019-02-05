6aecf48058e9fda9444e77b81aa972283c4b77f1
James Davies <jdavies@stsci.edu>
2019-02-05 15:57:51 -0500

    Add master background step init test

---
+++ jwst/master_background/tests/test_master_background.py
@@ -0,0 +1,20 @@
+"""
+Unit tests for master background subtraction
+"""
+
+import pytest
+
+from jwst.master_background import MasterBackgroundStep
+from jwst import datamodels
+
+
+@pytest.mark.parametrize('input_data', [
+    datamodels.ImageModel(),
+    datamodels.MultiSlitModel(),
+    ])
+def test_master_background_init(input_data):
+    """Verify an empty imagemodel can run through the step"""
+    step = MasterBackgroundStep()
+    result = step.run(input_data)
+
+    assert type(input_data) is type(result)
