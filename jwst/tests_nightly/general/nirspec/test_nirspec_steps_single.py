1a456bf5e6c14aaee339482ba7b37156080cccd3
Jane Morrison <morrison@as.arizona.edu>
2019-02-11 13:30:45 -0700

    First NIRSPEC FS Masterbackground test

--- jwst/tests_nightly/general/nirspec/test_nirspec_steps_single.py
+++ jwst/tests_nightly/general/nirspec/test_nirspec_steps_single.py
@@ -1,14 +1,19 @@
 import pytest
+from astropy.convolution import convolve, Box2DKernel
 from numpy.testing import assert_allclose
 from gwcs.wcstools import grid_from_bounding_box
 
 from jwst.tests.base_classes import BaseJWSTTest
 
 from jwst.assign_wcs import AssignWcsStep, nirspec
+
 from jwst.datamodels import ImageModel
 from jwst.pipeline import Detector1Pipeline, Spec2Pipeline
 from jwst.imprint import ImprintStep
 from jwst.ramp_fitting import RampFitStep
+from jwst.extract_1d import Extract1dStep
+#from jwst.master_background_step import MasterBackgroundStep
+from jwst import datamodels
 
 
 @pytest.mark.bigdata
@@ -168,3 +173,90 @@ class TestNIRISSSpec2(BaseJWSTTest):
                     'jw84600002001_02101_00001_nrs2_x1d_ref.fits')
                   ]
         self.compare_outputs(outputs)
+
+@pytest.mark.bigdata
+class TestNIRSpecMasterBackGround_FS(BaseJWSTTest):
+    input_loc = 'nirspec'
+    ref_loc = ['test_masterbackground', 'nrs-fs', 'truth']
+    test_dir = ['test_masterbackground','nrs-fs']
+    rtol = 0.0001
+
+    def test_nirspec_masterbackground_fs_user1d(self):
+        """
+
+        Regression test of master background subtraction got NRS FS  when a user 1-D spectrum is provided.
+
+        """
+        # input file has 2-D background image added to it
+        input_file = self.get_data(*self.test_dir,
+                                  'nrs1_sci_bkg_cal.fits')
+        # user provide 1-D background was created from the 2-D background image
+        input_1dbgd_file = self.get_data(*self.test_dir,
+                                         'nrs1_bkg_x1d_user.fits')
+
+        #result = MasterBackgroundStep.call(input_file,user_background=input_1dbgd_file, 
+        #save_results=True)
+       
+        #____________________________________________________________________________
+        # Test 1 compare 4 FS 1D extracted spectra from sciene data with no background added
+        # to 4 FS 1D extracted spectra from the output from MasterBackground subtraction
+
+        # run 1-D extract on results from MasterBackground step
+        #result_1d = Extract1dstep.call(result,save_results=True)
+        # just to get some data to test set make up this data
+        input_test= self.get_data(*self.test_dir, 
+                                            'nrs1_sci_cal.fits')  
+        result_1d = Extract1dStep.call(input_test,save_results=True)
+
+        # run 1-D extract on original science data without background
+        input_sci_cal_file = self.get_data(*self.test_dir, 
+                                 'nrs1_sci_cal.fits')  
+        # Should I instead run this off line to produce this file and make this
+        # one of the input files or truth files ?
+        sci_cal_1d = Extract1dStep.call(input_sci_cal_file,save_results=True)
+
+        # Compare the FS  1D extracted data. These types of data are
+        #  MultiSpec Models
+        num_spec = len(result_1d.spec)
+        atol = 0.01
+        rtol = 0.0005
+        for i in range(num_spec):
+            sci_spec_1d = sci_cal_1d.spec[i].spec_table.FLUX
+            result_spec_1d = result_1d.spec[i].spec_table.FLUX
+            assert_allclose(sci_spec_1d,result_spec_1d,rtol=rtol,atol=atol)
+        #____________________________________________________________________________
+        # Test 2  compare the science MultiSlit data with no background to the
+        # MultiSlit output from the masterBackground Subtraction step 
+        # background subtracted science image. For FS case 
+        result = datamodels.open(input_test) # override result just for testing
+
+        input_sci_model = datamodels.open(input_sci_cal_file)
+        num_slits = len(input_sci_model.slits)
+        
+        atol = 0.01
+        rtol = 0.0005
+        for i in range(num_slits):
+            slit_sci = input_sci_model.slits[i].data
+            slit_result = result.slits[i].data
+            
+            sub = slit_sci - slit_result
+            sub_smo = convolve(sub,Box2DKernel(3))
+            sub_smo_zero = sub_smo*0.0
+            assert_allclose(sub_smo,sub_smo_zero,rtol=rtol,atol=atol)
+
+
+        #____________________________________________________________________________
+        # Test 3 Compare background sutracted science data (results) to truth file 
+        # This data is MultiSlit data
+
+        ref_file = self.get_data(*self.ref_loc,
+                                 'nrs1_sci_cal.fits') # temp file replace when we have 
+                                                      # a real truth file
+
+        result_file = result.meta.filename
+        result.save(result_file)
+
+        
+        outputs = [(result_file,ref_file)]
+        self.compare_outputs(outputs)
+        result.close()
