6c8bd6b38e9636e07a1c5889387346225a6320b1
James Davies <jdavies@stsci.edu>
2019-02-22 11:00:33 -0500

    Add atol to coron3 regtest; ignore NAXIS1, TFORM7 in fitsdiff

--- jwst/tests_nightly/general/nircam/test_coron3_1.py
+++ jwst/tests_nightly/general/nircam/test_coron3_1.py
@@ -7,6 +7,7 @@ from jwst.pipeline import Coron3Pipeline
 @pytest.mark.bigdata
 class TestCoron3Pipeline(BaseJWSTTest):
     rtol = 0.00001
+    atol = 0.00001
     input_loc = 'nircam'
     ref_loc = ['test_coron3', 'truth']
 
@@ -15,7 +16,7 @@ class TestCoron3Pipeline(BaseJWSTTest):
     def test_coron3_1(self):
         """Regression test of calwebb_coron3 pipeline.
 
-        Test will be performed on NIRCam simulated data.
+        Test is performed on NIRCam simulated data.
         """
         asn_name = 'jw99999-a3001_20170327t121212_coron3_001_asn.json'
         override_psfmask = 'jwst_nircam_psfmask_somb.fits'
@@ -30,6 +31,8 @@ class TestCoron3Pipeline(BaseJWSTTest):
         pipe.align_refs.override_psfmask = psfmask_file
         pipe.outlier_detection.resample_data = False
         pipe.run(asn_file)
+
+        self.ignore_keywords += ['NAXIS1', 'TFORM7']
 
         outputs = [( # Compare psfstack product
                     'jw99999-a3001_t1_nircam_f140m-maskbar_psfstack.fits',
