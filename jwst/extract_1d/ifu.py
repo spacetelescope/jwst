51acac2090894e12106d021fa2e646d80e416c08
Nadia Dencheva <nadia.dencheva@gmail.com>
2019-02-26 09:56:06 -0500

    fix logging

--- jwst/extract_1d/ifu.py
+++ jwst/extract_1d/ifu.py
@@ -59,10 +59,10 @@ def ifu_extract1d(input_model, refname, source_type, subtract_background):
     sub_bkg = extract_params['subtract_background']
     if subtract_background is not None:
         extract_params['subtract_background'] = subtract_background
-    if subtract_background and not sub_bkg:
-        log.info("Background subtraction was enabled - performing it.")
-    elif sub_bkg and not subtract_background:
-        log.info("Background subtraction was disabled - skipping it.")
+        if subtract_background and not sub_bkg:
+            log.info("Background subtraction was enabled - performing it.")
+        elif sub_bkg and not subtract_background:
+            log.info("Background subtraction was disabled - skipping it.")
 
     if extract_params:
         (ra, dec, wavelength, net, background, npixels, dq) = extract_ifu(
