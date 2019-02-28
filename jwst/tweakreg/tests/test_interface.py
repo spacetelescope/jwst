13fc0172e1d7178a8a47d6f6ff2ddb62952a7fb5
James Davies <jdavies@stsci.edu>
2019-02-28 01:27:46 -0500

    Unfail tweakreg chelp test

--- jwst/tweakreg/tests/test_interface.py
+++ jwst/tweakreg/tests/test_interface.py
@@ -1,6 +1,3 @@
-import pytest
-
-@pytest.mark.xfail(run=False, reason="The chelp module is not getting installed correctly under pip")
 def test_tweakreg_chelp_importable():
     """Test that C extension for arrxyzero can be imported"""
     from jwst.tweakreg import chelp
