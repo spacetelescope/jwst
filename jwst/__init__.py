5401f6a4e036ef2567a6c4a098fab0d1f3cb4687
James Davies <jdavies@stsci.edu>
2019-03-06 13:25:31 -0500

    Add back datamodels BaseExtension, revert #2430
     Please enter the commit message for your changes. Lines starting

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -9,4 +9,5 @@ if sys.version_info < (3, 5):
     raise ImportError("JWST does not support Python 2.x, 3.0, 3.1, 3.2, 3.3 or 3.4."
                       "Beginning with JWST 0.9, Python 3.5 and above is required.")
 
-
+from .transforms.jwextension import JWSTExtension
+from .datamodels.extension import BaseExtension
