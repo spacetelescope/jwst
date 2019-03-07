a7dd66476af56f8b8f64a12525172e3ad12467a1
James Davies <jdavies@stsci.edu>
2019-03-06 22:57:58 -0500

    Remove read_schema() function
    This function is no longer used, as we use asdf.schema.load_schema()

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -6,8 +6,4 @@ except ImportError:  # Not available for RTD
 import sys
 
 if sys.version_info < (3, 5):
-    raise ImportError("JWST does not support Python 2.x, 3.0, 3.1, 3.2, 3.3 or 3.4."
-                      "Beginning with JWST 0.9, Python 3.5 and above is required.")
-
-from .transforms.jwextension import JWSTExtension
-from .datamodels.extension import BaseExtension
+    raise ImportError("JWST requires Python 3.5 and above.")
