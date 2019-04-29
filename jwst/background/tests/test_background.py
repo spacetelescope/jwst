3c08bcd79ed4408100dbec74860dfdd93e1fe776
Jane Morrison <morrison@as.arizona.edu>
2019-04-29 10:40:24 -0700

    make GWA test - 3 different tests

--- jwst/background/tests/test_background.py
+++ jwst/background/tests/test_background.py
@@ -15,7 +15,7 @@ def background(tmpdir_factory):
 
     filename = tmpdir_factory.mktemp('background_input')
     filename = str(filename.join('background.fits'))
-    image = datamodels.IFUImageModel((10,10))
+    image = datamodels.IFUImageModel((10, 10))
     image.data[:,:] = 10
     image.meta.instrument.name = 'NIRSPEC'
     image.meta.instrument.detector = 'NRS1'
@@ -37,11 +37,10 @@ def background(tmpdir_factory):
     return filename
 
 
-def test_background_nirspec_gwa(_jail, background):
+def test_nirspec_gwa(_jail, background):
     """Verify NIRSPEC GWA logic for in the science and background"""
-
     # set up the IFU science image
-    image = datamodels.IFUImageModel((10,10))
+    image = datamodels.IFUImageModel((10, 10))
     image.data[:,:] = 100
     image.meta.instrument.name = 'NIRSPEC'
     image.meta.instrument.detector = 'NRS1'
@@ -51,7 +50,6 @@ def test_background_nirspec_gwa(_jail, background):
     image.meta.observation.date = '2019-02-27'
     image.meta.observation.time = '13:37:18.548'
     image.meta.date = '2019-02-27T13:37:18.548'
-
     image.meta.subarray.xstart = 1
     image.meta.subarray.ystart = 1
 
@@ -62,8 +60,7 @@ def test_background_nirspec_gwa(_jail, background):
     image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
 
     bkg = [background]
-    # Test 1
-    # Run with GWA values the same - confirm it runs
+    # Test Run with GWA values the same - confirm it runs
     # And gives the predicted result
 
     collect_pipeline_cfgs('./config')
@@ -77,7 +74,33 @@ def test_background_nirspec_gwa(_jail, background):
     assert type(image) is type(result)
     assert result.meta.cal_step.back_sub == 'COMPLETE'
 
-    # Test 2 - change xtilt
+
+def test_nirspec_gwa_xtilt(_jail, background):
+    """Verify NIRSPEC GWA Xtilt must be the same in the science and background image"""
+
+    # set up the IFU science image
+    image = datamodels.IFUImageModel((10, 10))
+    image.data[:,:] = 100
+    image.meta.instrument.name = 'NIRSPEC'
+    image.meta.instrument.detector = 'NRS1'
+    image.meta.instrument.filter = 'CLEAR'
+    image.meta.instrument.grating = 'PRISM'
+    image.meta.exposure.type = 'NRS_IFU'
+    image.meta.observation.date = '2019-02-27'
+    image.meta.observation.time = '13:37:18.548'
+    image.meta.date = '2019-02-27T13:37:18.548'
+    image.meta.subarray.xstart = 1
+    image.meta.subarray.ystart = 1
+
+    # open the background to read in the GWA values
+    back_image = datamodels.open(background)
+    image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
+    image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
+    image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+
+    bkg = [background]
+
+    # Test change xtilt
     image.meta.instrument.gwa_xtilt = image.meta.instrument.gwa_xtilt + 0.00001
     collect_pipeline_cfgs('./config')
     result = BackgroundStep.call(
@@ -86,8 +109,33 @@ def test_background_nirspec_gwa(_jail, background):
         )
     assert result.meta.cal_step.back_sub == 'SKIPPED'
 
-    # Test 3 - change ytilt - set xtilt back to back_image value
+
+def test_nirspec_gwa_ytitl(_jail, background):
+    """Verify NIRSPEC GWA Ytilt must be the same in the science and background image"""
+
+    # set up the IFU science image
+    image = datamodels.IFUImageModel((10, 10))
+    image.data[:,:] = 100
+    image.meta.instrument.name = 'NIRSPEC'
+    image.meta.instrument.detector = 'NRS1'
+    image.meta.instrument.filter = 'CLEAR'
+    image.meta.instrument.grating = 'PRISM'
+    image.meta.exposure.type = 'NRS_IFU'
+    image.meta.observation.date = '2019-02-27'
+    image.meta.observation.time = '13:37:18.548'
+    image.meta.date = '2019-02-27T13:37:18.548'
+    image.meta.subarray.xstart = 1
+    image.meta.subarray.ystart = 1
+
+    # open the background to read in the GWA values
+    back_image = datamodels.open(background)
     image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
+    image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
+    image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+
+    bkg = [background]
+
+    # Test different ytilt
     image.meta.instrument.gwa_ytilt = image.meta.instrument.gwa_ytilt + 0.00001
     collect_pipeline_cfgs('./config')
     result = BackgroundStep.call(
