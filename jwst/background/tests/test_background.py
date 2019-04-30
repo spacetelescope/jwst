eff19dc8d4941f8bbe91c52bfe95bda922b46a75
Jane Morrison <morrison@as.arizona.edu>
2019-04-30 05:17:49 -0700

    updated test_background unit test

--- jwst/background/tests/test_background.py
+++ jwst/background/tests/test_background.py
@@ -37,9 +37,12 @@ def background(tmpdir_factory):
     return filename
 
 
-def test_nirspec_gwa(_jail, background):
-    """Verify NIRSPEC GWA logic for in the science and background"""
-    # set up the IFU science image
+@pytest.fixture(scope='module')
+def science_image(tmpdir_factory):
+    """Generate science image"""
+
+    filename = tmpdir_factory.mktemp('background_input')
+    filename = str(filename.join('science_image.fits'))
     image = datamodels.IFUImageModel((10, 10))
     image.data[:,:] = 100
     image.meta.instrument.name = 'NIRSPEC'
@@ -53,6 +56,18 @@ def test_nirspec_gwa(_jail, background):
     image.meta.subarray.xstart = 1
     image.meta.subarray.ystart = 1
 
+    image.meta.instrument.gwa_xtilt = 0.0001
+    image.meta.instrument.gwa_ytilt = 0.0001
+    image.meta.instrument.gwa_tilt = 37.0610
+
+    image.save(filename)
+    return filename
+
+
+def test_nirspec_gwa(_jail, background, science_image):
+    """Verify NIRSPEC GWA logic for in the science and background"""
+
+    image = datamodels.open(science_image)
     # open the background to read in the GWA values
     back_image = datamodels.open(background)
     image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
@@ -73,24 +88,15 @@ def test_nirspec_gwa(_jail, background):
     assert_allclose(result.data, test)
     assert type(image) is type(result)
     assert result.meta.cal_step.back_sub == 'COMPLETE'
+    back_image.close()
+    image.close()
 
 
-def test_nirspec_gwa_xtilt(_jail, background):
+def test_nirspec_gwa_xtilt(_jail, background, science_image):
     """Verify NIRSPEC GWA Xtilt must be the same in the science and background image"""
 
     # set up the IFU science image
-    image = datamodels.IFUImageModel((10, 10))
-    image.data[:,:] = 100
-    image.meta.instrument.name = 'NIRSPEC'
-    image.meta.instrument.detector = 'NRS1'
-    image.meta.instrument.filter = 'CLEAR'
-    image.meta.instrument.grating = 'PRISM'
-    image.meta.exposure.type = 'NRS_IFU'
-    image.meta.observation.date = '2019-02-27'
-    image.meta.observation.time = '13:37:18.548'
-    image.meta.date = '2019-02-27T13:37:18.548'
-    image.meta.subarray.xstart = 1
-    image.meta.subarray.ystart = 1
+    image = datamodels.open(science_image)
 
     # open the background to read in the GWA values
     back_image = datamodels.open(background)
@@ -108,24 +114,15 @@ def test_nirspec_gwa_xtilt(_jail, background):
         config_file='config/background.cfg',
         )
     assert result.meta.cal_step.back_sub == 'SKIPPED'
+    image.close()
+    back_image.close()
 
 
-def test_nirspec_gwa_ytitl(_jail, background):
+def test_nirspec_gwa_ytitl(_jail, background, science_image):
     """Verify NIRSPEC GWA Ytilt must be the same in the science and background image"""
 
     # set up the IFU science image
-    image = datamodels.IFUImageModel((10, 10))
-    image.data[:,:] = 100
-    image.meta.instrument.name = 'NIRSPEC'
-    image.meta.instrument.detector = 'NRS1'
-    image.meta.instrument.filter = 'CLEAR'
-    image.meta.instrument.grating = 'PRISM'
-    image.meta.exposure.type = 'NRS_IFU'
-    image.meta.observation.date = '2019-02-27'
-    image.meta.observation.time = '13:37:18.548'
-    image.meta.date = '2019-02-27T13:37:18.548'
-    image.meta.subarray.xstart = 1
-    image.meta.subarray.ystart = 1
+    image = datamodels.open(science_image)
 
     # open the background to read in the GWA values
     back_image = datamodels.open(background)
@@ -143,3 +140,6 @@ def test_nirspec_gwa_ytitl(_jail, background):
         config_file='config/background.cfg',
         )
     assert result.meta.cal_step.back_sub == 'SKIPPED'
+
+    back_image.close()
+    image.close()
