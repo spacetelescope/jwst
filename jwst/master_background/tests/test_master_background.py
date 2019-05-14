553314ab0cbb4261d66b47195959913f8d7d7095
James Davies <jdavies@stsci.edu>
2019-05-14 16:13:52 -0400

    Fix bug in copy_background_to_flux for master background

--- jwst/master_background/tests/test_master_background.py
+++ jwst/master_background/tests/test_master_background.py
@@ -1,19 +1,15 @@
 """
 Unit tests for master background subtraction
 """
-import pytest
 import numpy as np
+import pytest
 
-from jwst.master_background import MasterBackgroundStep
-from jwst.assign_wcs import AssignWcsStep
-# from jwst.assign_wcs.tests.test_nirspec import (
-#     create_nirspec_fs_file,
-#     create_nirspec_ifu_file,
-#     )
-from jwst.master_background.create_master_bkg import create_background
 from jwst import datamodels
+from jwst.assign_wcs import AssignWcsStep
+from jwst.master_background import MasterBackgroundStep
+from jwst.master_background.create_master_bkg import create_background
+from jwst.master_background.master_background_step import copy_background_to_flux
 from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
-# from jwst.extract_2d import Extract2dStep
 
 
 @pytest.fixture(scope='module')
@@ -94,3 +90,24 @@ def test_master_background_logic(_jail, user_background, science_image):
 
     assert result.meta.cal_step.master_background == 'COMPLETE'
     assert type(science_image) is type(result)
+
+
+def test_copy_background_to_flux():
+    """Test the copy_background_to_flux function"""
+
+    wavelength = np.linspace(0.5, 25, num=100)
+    flux = np.linspace(2.0, 2.2, num=100) + (np.random.random(100) - 0.5) * 0.001
+    error = np.random.random(100) * 0.01
+    background = np.random.random(100) * 0.01 + 1
+    berror = np.random.random(100) * 0.01
+    data = create_background(wavelength, flux)
+    data.spec[0].spec_table['error'] = error
+    data.spec[0].spec_table['background'] = background
+    data.spec[0].spec_table['berror'] = berror
+
+    newdata = data.copy()
+    copy_background_to_flux(newdata)
+
+    assert (newdata.spec[0].spec_table['flux'] == background).all()
+    assert (newdata.spec[0].spec_table['error'] == berror).all()
+    assert (newdata.spec[0].spec_table['background'] == 0).all()
