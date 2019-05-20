75dbffb36c3efb2c6743f10fe0d15403d6c5355f
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-05-19 22:39:47 -0400

    Update standards tests to allow specificity in error and test

--- jwst/tests_nightly/general/associations/conftest.py
+++ jwst/tests_nightly/general/associations/conftest.py
@@ -8,13 +8,24 @@ from jwst.tests_nightly.general.associations.sdp_pools_source import SDPPoolsSou
 def pytest_addoption(parser):
     parser.addoption(
         '--sdp-pool', metavar='sdp_pool', default=None,
-        help='SDP pool to run. Specify the name only, not extension or path'
+        help='SDP test pool to run. Specify the name only, not extension or path'
+    )
+    parser.addoption(
+        '--standard-pool', metavar='standard_pool', default=None,
+        help='Standard test pool to run. Specify the name only, not extension or path'
     )
 
 
 @pytest.fixture
 def sdp_pool(request):
+    """Retrieve a specific SDP pool to test"""
     return request.config.getoption('--sdp-pool')
+
+
+@pytest.fixture
+def standard_pool(request):
+    """Retrieve a specific standard pool to test"""
+    return request.config.getoption('--standard-pool')
 
 
 def pytest_generate_tests(metafunc):
