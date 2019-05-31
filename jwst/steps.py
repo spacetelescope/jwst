dfba96f9f4264942d61428f4520e49586b3057fb
James Davies <jdavies@stsci.edu>
2019-05-31 12:14:10 -0400

    Add test to make sure submodules are importable

--- jwst/steps.py
+++ jwst/steps.py
@@ -1,47 +1,10 @@
-from . import datamodels
-from . import ami
-from . import assign_wcs
-from . import associations
-from . import background
-from . import combine_1d
-from . import coron
-from . import csv_tools
-from . import cube_build
-from . import cube_skymatch
-from . import dark_current
-from . import dq_init
-from . import emission
-from . import extract_1d
-from . import extract_2d
-from . import fits_generator
-from . import flatfield
-from . import fringe
-from . import guider_cds
-from . import imprint
-from . import ipc
-from . import jump
-from . import jwpsf
-from . import lastframe
-from . import linearity
-from . import mrs_imatch
-from . import outlier_detection
-from . import persistence
-from . import photom
-from . import pipeline
-from . import ramp_fitting
-from . import refpix
-from . import resample
-from . import reset
-from . import rscd
-from . import saturation
-from . import skymatch
-from . import source_catalog
-from . import srctype
-from . import stpipe
-from . import straylight
-from . import superbias
-# Requires non-standard modules and enviroment settings
-#from . import timeconversion
-from . import transforms
-from . import tweakreg
-from . import wfs_combine
+import importlib
+from pkgutil import iter_modules
+
+import jwst
+
+submodules = [mod for _, mod, ispkg in iter_modules(jwst.__path__) if ispkg]
+# timeconversion requires non-standard dependencies and enviroment settings
+submodules.remove('timeconversion')
+for module in submodules:
+    importlib.import_module("jwst." + module)
