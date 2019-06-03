0fd899b9115bebf119c7c324f45a9f610faeeb1b
Howard Bushouse <bushouse@stsci.edu>
2019-06-03 15:47:22 -0400

    Update MultiExposure model to include all slit arrays

--- jwst/datamodels/multiexposure.py
+++ jwst/datamodels/multiexposure.py
@@ -62,7 +62,13 @@ class MultiExposureModel(DataModel):
             self.exposures[0].data = init.data
             self.exposures[0].dq = init.dq
             self.exposures[0].err = init.err
+            self.exposures[0].wavelength = init.wavelength
+            self.exposures[0].barshadow = init.barshadow
             self.exposures[0].area = init.area
+            self.exposures[0].var_poisson = init.var_poisson
+            self.exposures[0].var_rnoise = init.var_rnoise
+            self.exposures[0].var_flat = init.var_flat
+            self.exposures[0].pathloss = init.pathloss
             return
 
         super(MultiExposureModel, self).__init__(
