9c3a6d29e9693a83d72c0e07aaa5f58f8c38fb56
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2019-06-07 08:40:58 -0400

    Add the fxd_slit keyword as the third optical component (#3607)
    Changes:
    * Update sdp test to allow pool-specific generator parameters
    * Unify the TSO checking
    * Ignore non-science pointings for TSO determination
    * Handle coronographic as not TSO but with integrations naming
    * Update SDP tests to handle new known errors
    * Update commentary concerning TSO and coronagraphic
    * Add fxd_slit to third optical element in Level2
    * Added fxd_slit as third optical element to Level3
    * Improve level3 naming build using more elements
    * Update changelog
    * Fix a test and update changelog

--- jwst/associations/lib/dms_base.py
+++ jwst/associations/lib/dms_base.py
@@ -556,27 +556,21 @@ class DMSBaseMixin(ACIDMixin):
             The Level3 Product name representation
             of the optical elements.
         """
-        opt_elem = ''
-        join_char = ''
-        try:
-            value = format_list(self.constraints['opt_elem'].found_values)
-        except KeyError:
-            pass
-        else:
-            if value not in _EMPTY and value != 'clear':
-                opt_elem = value
-                join_char = '-'
-        try:
-            value = format_list(self.constraints['opt_elem2'].found_values)
-        except KeyError:
-            pass
-        else:
-            if value not in _EMPTY and value != 'clear':
-                opt_elem = join_char.join(
-                    [opt_elem, value]
-                )
+        # Retrieve all the optical elements
+        opt_elems = []
+        for opt_elem in ['opt_elem', 'opt_elem2', 'opt_elem3']:
+            try:
+                value = format_list(self.constraints[opt_elem].found_values)
+            except KeyError:
+                pass
+            else:
+                if value not in _EMPTY and value != 'clear':
+                    opt_elems.append(value)
+
+        opt_elem = '-'.join(opt_elems)
         if opt_elem == '':
             opt_elem = 'clear'
+
         return opt_elem
 
     def _get_subarray(self):
