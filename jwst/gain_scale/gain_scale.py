c6beb4f77167f3e9dc7e1c13548c1959f8a6fed7
Howard Bushouse <bushouse@stsci.edu>
2019-07-09 09:52:53 -0400

    Update gain_scale step to work on variance arrays

--- jwst/gain_scale/gain_scale.py
+++ jwst/gain_scale/gain_scale.py
@@ -1,7 +1,4 @@
-#
-#  Module for rescaling data for non-standard gain value
-#
-
+import numpy as np
 import logging
 
 log = logging.getLogger(__name__)
@@ -13,29 +10,40 @@ def do_correction(input_model, gain_factor):
     Short Summary
     -------------
     Rescales all integrations in an exposure by gain_factor, to
-    account for non-standard detector gain settings. The SCI
-    and ERR arrays are rescaled.
+    account for non-standard detector gain settings. The SCI,
+    ERR, and variance arrays are rescaled.
 
     Parameters
     ----------
-    input_model: data model object
-        science data to be corrected
+    input_model : `~jwst.datamodels.DataModel`
+        Input datamodel to be corrected
 
     Returns
     -------
-    output_model: data model object
-        rescaled science data
+    output_model : `~jwst.datamodels.DataModel`
+        Output datamodel with rescaled data
 
     """
 
     # Create output as a copy of the input science data model
     output_model = input_model.copy()
 
-    # Apply the rescaling to the entire data array
+    # Apply the gain factor to the SCI and ERR arrays
     log.info('Rescaling by {0}'.format(gain_factor))
     output_model.data *= gain_factor
     output_model.err *= gain_factor
 
+    # Apply the square of the gain factor to the variance arrays,
+    # if they exist
+    if (output_model.var_poisson is not None and
+            np.size(output_model.var_poisson)) > 0:
+                output_model.var_poisson *= gain_factor**2
+
+    if (output_model.var_rnoise is not None and
+            np.size(output_model.var_rnoise)) > 0:
+                output_model.var_rnoise *= gain_factor**2
+
+    # Set step status info
     output_model.meta.exposure.gain_factor = gain_factor
     output_model.meta.cal_step.gain_scale = 'COMPLETE'
 
