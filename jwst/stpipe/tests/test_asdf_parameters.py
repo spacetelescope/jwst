d951edeaec25f7ad6f92d38f6847912ef9fce7dc
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-07-17 11:12:05 -0400

    Implement initial basic test using ASDF for step configuation
    WIP JP-863/#3813
    WIP JP-864/#3812

--- jwst/stpipe/tests/test_asdf_parameters.py
+++ jwst/stpipe/tests/test_asdf_parameters.py
@@ -1,12 +1,22 @@
 """Test initializing steps using ASDF and CRDS"""
+from pathlib import Path
 import pytest
 
 from jwst.stpipe.config_parser import ValidationError
 
 from .steps import MakeListStep
+from .util import t_path
 
 DEFAULT_PAR1 = 42.0
 DEFAULT_PAR2 = 'Yes, a string'
+
+
+def test_asdf_from_call():
+    """Test using an ASDF file from call"""
+    config_file = t_path(Path('data') / 'step_parameters' / 'jwst_all_pars-makeliststep_0001.asdf')
+    results = MakeListStep.call(config_file=config_file)
+
+    assert results == [42.0, 'Yes, a string', False]
 
 
 def test_makeliststep_missingpars():
