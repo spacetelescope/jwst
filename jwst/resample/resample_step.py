622fed67dde707784877d2723a77df6a5a175d69
Nadia Dencheva <nadia.dencheva@gmail.com>
2019-07-17 16:50:20 -0400

    fix issues revealed by testing with new modeling

--- jwst/resample/resample_step.py
+++ jwst/resample/resample_step.py
@@ -100,7 +100,8 @@ class ResampleStep(Step):
         used a resample.cfg file or run ResampleStep using command line args,
         then these will overwerite the defaults pulled from the reference file.
         """
-        drizpars_table = datamodels.DrizParsModel(ref_filename).data
+        with datamodels.DrizParsModel(ref_filename) as drpt:
+            drizpars_table =drpt.data
 
         num_groups = len(input_models.group_names)
         filtname = input_models[0].meta.instrument.filter
