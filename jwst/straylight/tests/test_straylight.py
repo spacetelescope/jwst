80e877f1c1a5860a186cd0faea64ad1372f40424
Jane Morrison <morrison@as.arizona.edu>
2019-08-08 12:25:43 -0700

    clean up

--- jwst/straylight/tests/test_straylight.py
+++ jwst/straylight/tests/test_straylight.py
@@ -1,9 +1,9 @@
-""" 
+"""
 Unit tests for straylight correction
 """
 
 from jwst.datamodels import IFUImageModel
-from jwst.straylight import StraylightStep 
+from jwst.straylight import StraylightStep
 from jwst.straylight.straylight import correct_mrs_modshepard, shepard_2d_kernel
 from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
 import numpy as np
