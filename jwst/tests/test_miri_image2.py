2d1a0bfb4844bb96d89f9eaff001eae6f4b1be22
James Davies <jdavies@stsci.edu>
2019-08-16 10:09:06 -0400

    WIP

--- jwst/tests/test_miri_image2.py
+++ jwst/tests/test_miri_image2.py
@@ -3,26 +3,37 @@ import os
 
 import pytest
 from astropy.io import fits
+from astropy.io.fits.diff import FITSDiff
 from ci_watson.artifactory_helpers import get_bigdata
 
 from jwst.pipeline import Image2Pipeline
 from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
-from .helpers import fitsdiff_default_args
+
+
+@pytest.mark.bigdata
+def test_foo(request, rtdata, fitsdiff_defaults, _jail):
+    request.node.user_properties = [('output', rtdata.output)]
+    rtdata.input_remote = "miri/test_image2pipeline/jw00001001001_01101_00001_mirimage_rate.fits"
+    input_data = rtdata.get_data(rtdata.input_remote)
+    Image2Pipeline.call(input_data, save_results=True)
+
+    rtdata.output = "jw00001001001_01101_00001_mirimage_cal.fits"
+    rtdata.truth_local = rtdata.get_truth(rtdata.output)
+
+    diff = FITSDiff(output, truth, **fitsdiff_defaults)
+    assert diff.identical, diff.report
 
 
 @pytest.fixture(scope="module")
-def run_pipeline(request, artifactory_repos, jail):
+def run_pipeline(rtdata_module, jail):
     """Run calwebb_image2 pipeline on MIRI imaging data."""
-    inputs_root, results_root = artifactory_repos
-    envopt = request.config.getoption("env")
-    input_data = get_bigdata(inputs_root, envopt, 'miri',
-        'test_image2pipeline', 'jw00001001001_01101_00001_mirimage_rate.fits')
+    rtdata = rtdata_module
+    rtdata.input_remote = "miri/test_image2pipeline/jw00001001001_01101_00001_mirimage_rate.fits"
+    input_data = rtdata.get_data(rtdata.input_remote)
 
     collect_pipeline_cfgs('config')
     config_file = os.path.join('config', 'calwebb_image2.cfg')
     Image2Pipeline.call(input_data, config_file=config_file)
-
-    # truth_data = get_bigdata(inputs_root, envopt, )
 
     return jail
 
@@ -41,11 +52,10 @@ def test_miri_image2_completion(run_pipeline):
     'jw00001001001_01101_00001_mirimage_i2d.fits',],
     ids=['cal', 'i2d'],
 )
-def test_miri_image2(run_pipeline, request, output):
+def test_miri_image2(run_pipeline, request, fitsdiff_defaults, output):
     """
     Regression test of calwebb_image2 pipeline performed on MIRI data.
     """
     request.node.user_properties = [('output', output)]
-    diff = fits.diff.FITSDiff(output, output, **fitsdiff_default_args)
+    diff = FITSDiff(output, output, **fitsdiff_defaults)
     assert diff.identical, diff.report()
-    assert output == 'foo'
