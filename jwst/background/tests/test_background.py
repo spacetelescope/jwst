b2b666dba61cc8821b4ef4904085a905acaabc85
Mees Fix <mfix@stsci.edu>
2019-10-02 08:51:46 -0400

    Adding bones for NIS WFSS unit tests

--- jwst/background/tests/test_background.py
+++ jwst/background/tests/test_background.py
@@ -2,10 +2,14 @@
 Unit tests for background subtraction
 """
 import pytest
+import numpy as np
 from numpy.testing.utils import assert_allclose
-from jwst.background import BackgroundStep
 
 from jwst import datamodels
+from jwst.assign_wcs import AssignWcsStep
+from jwst.assign_wcs.util import create_grism_bbox
+from jwst.background import BackgroundStep
+from jwst.background.background_sub import robust_mean, subtract_wfss_bkg, mask_from_source_cat, no_NaN
 from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
 
 
@@ -134,3 +138,70 @@ def test_nirspec_gwa_ytitl(_jail, background, science_image):
     assert result.meta.cal_step.back_sub == 'SKIPPED'
 
     back_image.close()
+
+
+def make_wfss_datamodel():
+    """Generate NIRISS WFSS Observation"""
+
+    wcsinfo = {
+        'dec_ref': -27.79156387419731,
+        'ra_ref': 53.16247756038121,
+        'roll_ref': 0.04254766236781744,
+        'v2_ref': -290.1,
+        'v3_ref': -697.5,
+        'v3yangle': 0.56987,
+        'vparity': -1}
+
+    observation = {
+        'date': '2016-09-05',
+        'time': '8:59:37'}
+
+    exposure = {
+        'duration': 11.805952,
+        'end_time': 58119.85416,
+        'exposure_time': 11.776,
+        'frame_time': 0.11776,
+        'group_time': 0.11776,
+        'groupgap': 0,
+        'integration_time': 11.776,
+        'nframes': 1,
+        'ngroups': 8,
+        'nints': 1,
+        'nresets_between_ints': 0,
+        'nsamples': 1,
+        'sample_time': 10.0,
+        'start_time': 58668.72509857639,
+        'type': 'NIS_WFSS',
+        'zero_frame': False}
+
+    subarray = {'xsize':2048,
+                'ysize':2048}
+
+    instrument = {
+        'detector': 'NIS',
+        'filter': 'GR150C',
+        'pupil': 'F090W',
+        'name': 'NIRISS',
+        'filter_position':1,
+        'pupil_position':1}
+
+    image = datamodels.ImageModel((2048, 2048))
+
+    image.meta.wcsinfo._instance.update(wcsinfo)
+    image.meta.instrument._instance.update(instrument)
+    image.meta.observation._instance.update(observation)
+    image.meta.subarray._instance.update(subarray)
+    image.meta.exposure._instance.update(exposure)
+    image.data = np.random.rand(2048, 2048)
+    image.meta.source_catalog.filename = '/Users/mfix/Desktop/swara_nis_cat.ecsv'
+
+    return image
+
+def test_wfss_background():
+    """Test background subtraction for NIRISS WFSS modes."""
+    data = make_wfss_datamodel()
+    bkg = make_wfss_datamodel()
+    
+    wcs_corrected = AssignWcsStep.call(data)
+
+    step_result = BackgroundStep.call(wcs_corrected, [bkg])
