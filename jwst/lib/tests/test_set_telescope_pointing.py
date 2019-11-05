351e4d7f8e228fc11ef84acac29673a325156bc2
James Davies <jdavies@stsci.edu>
2019-11-05 12:27:26 -0500

    Use np.isclose in time comparison in test_pointing_averaging

--- jwst/lib/tests/test_set_telescope_pointing.py
+++ jwst/lib/tests/test_set_telescope_pointing.py
@@ -144,7 +144,7 @@ def test_pointing_averaging(eng_db_jw703):
         3.41404261e-06,  9.99994300e-01, -3.38429548e-03
     ])
     fsmcorr_exp = np.array([-0.02558673, -0.00200601])
-    obstime_exp = Time(1559582740.488000, format='unix')
+    obstime_exp = Time(1559582740.4880004, format='unix')
 
     (q,
      j2fgs_matrix,
@@ -154,10 +154,10 @@ def test_pointing_averaging(eng_db_jw703):
          Time('2019-06-03T17:25:56', format='isot').mjd,
      )
 
-    assert np.isclose(q, q_exp).all()
-    assert np.isclose(j2fgs_matrix, j2fgs_exp).all()
-    assert np.isclose(fsmcorr, fsmcorr_exp).all()
-    assert obstime == obstime_exp
+    assert np.allclose(q, q_exp)
+    assert np.allclose(j2fgs_matrix, j2fgs_exp)
+    assert np.allclose(fsmcorr, fsmcorr_exp)
+    assert np.isclose(obstime.value, obstime_exp.value)
 
 
 def test_get_pointing_fail():
