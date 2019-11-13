c6f96bff7c2eda88021c71b86488f4cf15ed0a08
Mees Fix <mfix@stsci.edu>
2019-11-13 11:08:10 -0500

    Added skip to NRC tests

--- jwst/background/tests/test_background.py
+++ jwst/background/tests/test_background.py
@@ -22,131 +22,131 @@ def get_file_path(filename):
     """Construct an absolute path."""
     return os.path.join(data_path, filename)
 
-# @pytest.fixture(scope='module')
-# def background(tmpdir_factory):
-#     """Generate a  background image to feed to background step"""
+@pytest.fixture(scope='module')
+def background(tmpdir_factory):
+    """Generate a  background image to feed to background step"""
 
-#     filename = tmpdir_factory.mktemp('background_input')
-#     filename = str(filename.join('background.fits'))
-#     image = datamodels.IFUImageModel((10, 10))
-#     image.data[:,:] = 10
-#     image.meta.instrument.name = 'NIRSPEC'
-#     image.meta.instrument.detector = 'NRS1'
-#     image.meta.instrument.filter = 'CLEAR'
-#     image.meta.instrument.grating = 'PRISM'
-#     image.meta.exposure.type = 'NRS_IFU'
-#     image.meta.observation.date = '2019-02-27'
-#     image.meta.observation.time = '13:37:18.548'
-#     image.meta.date = '2019-02-27T13:37:18.548'
+    filename = tmpdir_factory.mktemp('background_input')
+    filename = str(filename.join('background.fits'))
+    image = datamodels.IFUImageModel((10, 10))
+    image.data[:,:] = 10
+    image.meta.instrument.name = 'NIRSPEC'
+    image.meta.instrument.detector = 'NRS1'
+    image.meta.instrument.filter = 'CLEAR'
+    image.meta.instrument.grating = 'PRISM'
+    image.meta.exposure.type = 'NRS_IFU'
+    image.meta.observation.date = '2019-02-27'
+    image.meta.observation.time = '13:37:18.548'
+    image.meta.date = '2019-02-27T13:37:18.548'
 
-#     image.meta.subarray.xstart = 1
-#     image.meta.subarray.ystart = 1
+    image.meta.subarray.xstart = 1
+    image.meta.subarray.ystart = 1
 
-#     image.meta.instrument.gwa_xtilt = 0.0001
-#     image.meta.instrument.gwa_ytilt = 0.0001
-#     image.meta.instrument.gwa_tilt = 37.0610
+    image.meta.instrument.gwa_xtilt = 0.0001
+    image.meta.instrument.gwa_ytilt = 0.0001
+    image.meta.instrument.gwa_tilt = 37.0610
 
-#     image.save(filename)
-#     return filename
-
-
-# @pytest.fixture(scope='function')
-# def science_image():
-#     """Generate science image"""
-
-#     image = datamodels.IFUImageModel((10, 10))
-#     image.data[:,:] = 100
-#     image.meta.instrument.name = 'NIRSPEC'
-#     image.meta.instrument.detector = 'NRS1'
-#     image.meta.instrument.filter = 'CLEAR'
-#     image.meta.instrument.grating = 'PRISM'
-#     image.meta.exposure.type = 'NRS_IFU'
-#     image.meta.observation.date = '2019-02-27'
-#     image.meta.observation.time = '13:37:18.548'
-#     image.meta.date = '2019-02-27T13:37:18.548'
-#     image.meta.subarray.xstart = 1
-#     image.meta.subarray.ystart = 1
-
-#     image.meta.instrument.gwa_xtilt = 0.0001
-#     image.meta.instrument.gwa_ytilt = 0.0001
-#     image.meta.instrument.gwa_tilt = 37.0610
-
-#     return image
+    image.save(filename)
+    return filename
 
 
-# def test_nirspec_gwa(_jail, background, science_image):
-#     """Verify NIRSPEC GWA logic for in the science and background"""
+@pytest.fixture(scope='function')
+def science_image():
+    """Generate science image"""
 
-#     # open the background to read in the GWA values
-#     back_image = datamodels.open(background)
-#     science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
-#     science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
-#     science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+    image = datamodels.IFUImageModel((10, 10))
+    image.data[:,:] = 100
+    image.meta.instrument.name = 'NIRSPEC'
+    image.meta.instrument.detector = 'NRS1'
+    image.meta.instrument.filter = 'CLEAR'
+    image.meta.instrument.grating = 'PRISM'
+    image.meta.exposure.type = 'NRS_IFU'
+    image.meta.observation.date = '2019-02-27'
+    image.meta.observation.time = '13:37:18.548'
+    image.meta.date = '2019-02-27T13:37:18.548'
+    image.meta.subarray.xstart = 1
+    image.meta.subarray.ystart = 1
 
-#     bkg = [background]
-#     # Test Run with GWA values the same - confirm it runs
-#     # And gives the predicted result
+    image.meta.instrument.gwa_xtilt = 0.0001
+    image.meta.instrument.gwa_ytilt = 0.0001
+    image.meta.instrument.gwa_tilt = 37.0610
 
-#     collect_pipeline_cfgs('./config')
-#     result = BackgroundStep.call(
-#         science_image, bkg,
-#         config_file='config/background.cfg',
-#         )
-
-#     test = science_image.data - back_image.data
-#     assert_allclose(result.data, test)
-#     assert type(result) is type(science_image)
-#     assert result.meta.cal_step.back_sub == 'COMPLETE'
-#     back_image.close()
+    return image
 
 
-# def test_nirspec_gwa_xtilt(_jail, background, science_image):
-#     """Verify NIRSPEC GWA Xtilt must be the same in the science and background image"""
+def test_nirspec_gwa(_jail, background, science_image):
+    """Verify NIRSPEC GWA logic for in the science and background"""
 
-#     # open the background to read in the GWA values
-#     back_image = datamodels.open(background)
-#     science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
-#     science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
-#     science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+    # open the background to read in the GWA values
+    back_image = datamodels.open(background)
+    science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
+    science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
+    science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
 
-#     bkg = [background]
+    bkg = [background]
+    # Test Run with GWA values the same - confirm it runs
+    # And gives the predicted result
 
-#     # Test change xtilt
-#     science_image.meta.instrument.gwa_xtilt = \
-#         science_image.meta.instrument.gwa_xtilt + 0.00001
-#     collect_pipeline_cfgs('./config')
-#     result = BackgroundStep.call(
-#         science_image, bkg,
-#         config_file='config/background.cfg',
-#         )
-#     assert type(result) is type(science_image)
-#     assert result.meta.cal_step.back_sub == 'SKIPPED'
-#     back_image.close()
+    collect_pipeline_cfgs('./config')
+    result = BackgroundStep.call(
+        science_image, bkg,
+        config_file='config/background.cfg',
+        )
+
+    test = science_image.data - back_image.data
+    assert_allclose(result.data, test)
+    assert type(result) is type(science_image)
+    assert result.meta.cal_step.back_sub == 'COMPLETE'
+    back_image.close()
 
 
-# def test_nirspec_gwa_ytitl(_jail, background, science_image):
-#     """Verify NIRSPEC GWA Ytilt must be the same in the science and background image"""
+def test_nirspec_gwa_xtilt(_jail, background, science_image):
+    """Verify NIRSPEC GWA Xtilt must be the same in the science and background image"""
 
-#     # open the background to read in the GWA values
-#     back_image = datamodels.open(background)
-#     science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
-#     science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
-#     science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+    # open the background to read in the GWA values
+    back_image = datamodels.open(background)
+    science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
+    science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
+    science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
 
-#     bkg = [background]
+    bkg = [background]
 
-#     # Test different ytilt
-#     science_image.meta.instrument.gwa_ytilt = \
-#         science_image.meta.instrument.gwa_ytilt + 0.00001
-#     collect_pipeline_cfgs('./config')
-#     result = BackgroundStep.call(
-#         science_image, bkg,
-#         config_file='config/background.cfg',
-#         )
-#     assert type(result) is type(science_image)
-#     assert result.meta.cal_step.back_sub == 'SKIPPED'
+    # Test change xtilt
+    science_image.meta.instrument.gwa_xtilt = \
+        science_image.meta.instrument.gwa_xtilt + 0.00001
+    collect_pipeline_cfgs('./config')
+    result = BackgroundStep.call(
+        science_image, bkg,
+        config_file='config/background.cfg',
+        )
+    assert type(result) is type(science_image)
+    assert result.meta.cal_step.back_sub == 'SKIPPED'
+    back_image.close()
 
-#     back_image.close()
+
+def test_nirspec_gwa_ytitl(_jail, background, science_image):
+    """Verify NIRSPEC GWA Ytilt must be the same in the science and background image"""
+
+    # open the background to read in the GWA values
+    back_image = datamodels.open(background)
+    science_image.meta.instrument.gwa_xtilt = back_image.meta.instrument.gwa_xtilt
+    science_image.meta.instrument.gwa_ytilt = back_image.meta.instrument.gwa_ytilt
+    science_image.meta.instrument.gwa_tilt = back_image.meta.instrument.gwa_tilt
+
+    bkg = [background]
+
+    # Test different ytilt
+    science_image.meta.instrument.gwa_ytilt = \
+        science_image.meta.instrument.gwa_ytilt + 0.00001
+    collect_pipeline_cfgs('./config')
+    result = BackgroundStep.call(
+        science_image, bkg,
+        config_file='config/background.cfg',
+        )
+    assert type(result) is type(science_image)
+    assert result.meta.cal_step.back_sub == 'SKIPPED'
+
+    back_image.close()
 
 @pytest.fixture(scope='module')
 def make_wfss_datamodel():
@@ -200,7 +200,7 @@ def make_wfss_datamodel():
 
     return image
 
-
+@pytest.mark.skip(reason="Algorithm to calculate WFSS background needs refactoring, see jira ticket JP-1136")
 @pytest.mark.parametrize("pupils", ['GRISMC', 'GRISMR'])
 @pytest.mark.parametrize("filters", ['F250M', 'F277W', 'F300M', 'F322W2', 'F335M', 'F356W', 'F356W', 'F410M', 'F430M', 'F444W', 'F460M', 'F480M'])
 @pytest.mark.parametrize("detectors", ['NRCALONG', 'NRCBLONG'])
@@ -237,70 +237,71 @@ def test_nrc_wfss_background(filters, pupils, detectors, make_wfss_datamodel):
     pipeline_reference_mean = robust_mean(bkg_ref.data[mask])
     test_reference_mean,_,_ = sigma_clipped_stats(bkg_ref.data, sigma=2)
 
-    assert (np.isclose([pipeline_data_mean], [test_data_mean], rtol=1e-3) and
-            np.isclose([pipeline_reference_mean], [test_reference_mean], rtol=1e-1))
+    assert np.isclose([pipeline_data_mean], [test_data_mean], rtol=1e-3)
+    assert np.isclose([pipeline_reference_mean], [test_reference_mean], rtol=1e-1)
 
 
-# @pytest.mark.parametrize("filters", ['GR150C', 'GR150R'])
-# @pytest.mark.parametrize("pupils", ['F090W', 'F115W', 'F140M', 'F150W', 'F158M', 'F200W'])
-# def test_nrc_wfss_background(filters, pupils, make_wfss_datamodel):
-#     """Test background subtraction for NIRISS WFSS modes."""
-#     data = make_wfss_datamodel
+@pytest.mark.parametrize("filters", ['GR150C', 'GR150R'])
+@pytest.mark.parametrize("pupils", ['F090W', 'F115W', 'F140M', 'F150W', 'F158M', 'F200W'])
+def test_nis_wfss_background(filters, pupils, make_wfss_datamodel):
+    """Test background subtraction for NIRISS WFSS modes."""
+    data = make_wfss_datamodel
 
-#     data.meta.instrument.filter = filters
-#     data.meta.instrument.pupil = pupils
-#     data.meta.instrument.name = 'NIRISS'
-#     data.meta.exposure.type = 'NIS_WFSS'
+    data.meta.instrument.filter = filters
+    data.meta.instrument.pupil = pupils
+    data.meta.instrument.detector = 'NIS'
+    data.meta.instrument.name = 'NIRISS'
+    data.meta.exposure.type = 'NIS_WFSS'
 
-#     wcs_corrected = AssignWcsStep.call(data)
+    wcs_corrected = AssignWcsStep.call(data)
 
-#     # Get References
-#     wavelenrange = Step().get_reference_file(wcs_corrected, "wavelengthrange")
-#     mask = mask_from_source_cat(wcs_corrected, wavelenrange)
+    # Get References
+    wavelenrange = Step().get_reference_file(wcs_corrected, "wavelengthrange")
+    mask = mask_from_source_cat(wcs_corrected, wavelenrange)
 
-#     bkg_file = Step().get_reference_file(wcs_corrected, 'wfssbkg')
-#     bkg_ref = datamodels.open(bkg_file)
-#     bkg_ref = no_NaN(bkg_ref)
+    bkg_file = Step().get_reference_file(wcs_corrected, 'wfssbkg')
+    bkg_ref = datamodels.open(bkg_file)
+    bkg_ref = no_NaN(bkg_ref)
 
-#     # calculate backgrounds
-#     pipeline_data_mean = robust_mean(wcs_corrected.data[mask])
-#     test_data_mean,_,_ = sigma_clipped_stats(wcs_corrected.data, sigma=2)
+    # calculate backgrounds
+    pipeline_data_mean = robust_mean(wcs_corrected.data[mask])
+    test_data_mean,_,_ = sigma_clipped_stats(wcs_corrected.data, sigma=2)
 
-#     pipeline_reference_mean = robust_mean(bkg_ref.data[mask])
-#     test_reference_mean,_,_ = sigma_clipped_stats(bkg_ref.data, sigma=2)
+    pipeline_reference_mean = robust_mean(bkg_ref.data[mask])
+    test_reference_mean,_,_ = sigma_clipped_stats(bkg_ref.data, sigma=2)
 
-#     assert (np.isclose([pipeline_data_mean], [test_data_mean], rtol=1e-3) and
-#             np.isclose([pipeline_reference_mean], [test_reference_mean], rtol=1e-1))
+    assert np.isclose([pipeline_data_mean], [test_data_mean], rtol=1e-3)
+    assert np.isclose([pipeline_reference_mean], [test_reference_mean], rtol=1e-1)
 
-# def test_robust_mean():
-#     """Test robust mean calculation"""
-#     data = np.random.rand(2048, 2048)
-#     result = robust_mean(data)
-#     test = np.mean(data)
+def test_robust_mean():
+    """Test robust mean calculation"""
+    data = np.random.rand(2048, 2048)
+    result = robust_mean(data)
+    test = np.mean(data)
 
-#     assert np.isclose([test], [result], rtol=1e-3)
+    assert np.isclose([test], [result], rtol=1e-3)
 
 
-# def test_no_Nan():
-#     """Make sure that nan values are filled with fill value"""
-#     # Make data model
-#     model = datamodels.ImageModel()
-#     data = np.random.rand(10,10)
+def test_no_Nan():
+    """Make sure that nan values are filled with fill value"""
+    # Make data model
+    model = datamodels.ImageModel()
+    data = np.random.rand(10,10)
 
-#     # Randomly insert NaNs
-#     data.ravel()[np.random.choice(data.size, 10, replace=False)] = np.nan
-#     model.data = data
+    # Randomly insert NaNs
+    data.ravel()[np.random.choice(data.size, 10, replace=False)] = np.nan
+    model.data = data
 
-#     # Randomly select fill value
-#     fill_val = np.random.randint(0, 20)
+    # Randomly select fill value
+    fill_val = np.random.randint(0, 20)
 
-#     # Call no_NaN
-#     result = no_NaN(model, fill_value=fill_val)
+    # Call no_NaN
+    result = no_NaN(model, fill_value=fill_val)
 
-#     # Use np.NaN to find NaNs.
-#     test_result = np.isnan(model.data)
-#     # Assign fill values to NaN indices
-#     model.data[test_result] = fill_val
+    # Use np.NaN to find NaNs.
+    test_result = np.isnan(model.data)
+    # Assign fill values to NaN indices
+    model.data[test_result] = fill_val
 
-#     # Make sure arrays are equal.
-#     assert np.array_equal(model.data, result.data)
+    # Make sure arrays are equal.
+    assert np.array_equal(model.data, result.data)
