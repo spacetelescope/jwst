c986d3ddeec5c8be61b75f15d1ce7da93aaedf04
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-11-26 14:17:56 -0500

    Update the xfail with reference to actual issue jp-1144

--- jwst/tests_nightly/general/nirspec/test_nirspec_fs_spec3.py
+++ jwst/tests_nightly/general/nirspec/test_nirspec_fs_spec3.py
@@ -46,6 +46,10 @@ class TestSpec3Pipeline(BaseJWSTTest):
             assert False
 
 
+    @pytest.mark.xfail(
+        reason='See Issue JP-1144',
+        run=False
+    )
     def test_nrs_fs_spec3(self):
         """
         Regression test of calwebb_spec3 pipeline performed on
