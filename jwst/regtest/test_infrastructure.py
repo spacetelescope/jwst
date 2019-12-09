3c6415cddc52b1dd11f2cf5d61a4e1284603561a
James Davies <jdavies@stsci.edu>
2019-12-09 15:28:32 -0500

    Fix monkeypatch of TEST_BIGDATA in regtest infrastructure tests

--- jwst/regtest/test_infrastructure.py
+++ jwst/regtest/test_infrastructure.py
@@ -1,11 +1,14 @@
-import os
 from pathlib import Path
 
 import pytest
 
 
-def test_regtestdata_get_data(rtdata, tmpdir, _jail, monkeypatch):
+@pytest.fixture(autouse=True)
+def patch_bigdata(tmpdir, monkeypatch):
     monkeypatch.setenv("TEST_BIGDATA", str(tmpdir))
+
+
+def test_regtestdata_get_data(rtdata, tmpdir, _jail):
     rtdata._env = ''
     rtdata._inputs_root = ''
     rtdata.output = "foo.fits"
@@ -15,8 +18,7 @@ def test_regtestdata_get_data(rtdata, tmpdir, _jail, monkeypatch):
     assert rtdata.input == str(tmpdir.join("foo_uncal.fits"))
 
 
-def test_regtestdata_get_truth(rtdata, tmpdir, _jail, monkeypatch):
-    monkeypatch.setenv("TEST_BIGDATA", str(tmpdir))
+def test_regtestdata_get_truth(rtdata, tmpdir, _jail):
     rtdata._env = ''
     rtdata._inputs_root = ''
     Path("truth").mkdir()
@@ -27,36 +29,6 @@ def test_regtestdata_get_truth(rtdata, tmpdir, _jail, monkeypatch):
 
     assert rtdata.truth == str(tmpdir.join("truth/foo_cal.fits"))
     assert rtdata.bigdata_root is not None
-
-
-@pytest.fixture(scope="module")
-def run_pipeline(jail, rtdata_module):
-    rtdata = rtdata_module
-    rtdata.input = "foo_uncal.fits"
-
-    # Pretend to run pipeline and return rtdata
-    return rtdata
-
-
-JAIL = None
-
-@pytest.mark.parametrize("foo", ["foo1.fits", "foo2.fits"], ids=["foo1", "foo2"])
-def test_jail_module(foo, request, run_pipeline):
-    rtdata = run_pipeline
-    rtdata.output = foo
-    rtdata.truth = "truth/" + foo
-    rtdata.truth_remote = "jwst-pipeline/truth/" + foo
-
-    # Assert that both tests run in the same tmpdir provided by run_pipeline
-    global JAIL
-    if JAIL is None:
-        JAIL = os.path.dirname(foo)
-    else:
-        assert JAIL == os.path.dirname(foo)
-
-    # from pprint import pprint
-    # pprint(request.node.__dict__)
-    # assert 0
 
 
 def test_fitsdiff_defaults(fitsdiff_default_kwargs):
