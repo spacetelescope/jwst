69b005401095dc668ba1d3990e312bbc1ec3ba90
Howard Bushouse <bushouse@stsci.edu>
2019-12-17 17:16:21 -0500

    Updates based on review

--- jwst/regtest/test_nirspec_image2.py
+++ jwst/regtest/test_nirspec_image2.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from astropy.io.fits.diff import FITSDiff
 
@@ -7,7 +5,7 @@ from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
 from jwst.stpipe import Step
 
 @pytest.mark.bigdata
-def test_nirspec_image2_cal(_jail, rtdata, fitsdiff_default_kwargs):
+def test_nirspec_image2(_jail, rtdata, fitsdiff_default_kwargs):
     rtdata.get_data("nirspec/imaging/jw84600010001_02102_00001_nrs2_rate.fits")
 
     collect_pipeline_cfgs("config")
@@ -15,7 +13,7 @@ def test_nirspec_image2_cal(_jail, rtdata, fitsdiff_default_kwargs):
     Step.from_cmdline(args)
     rtdata.output = "jw84600010001_02102_00001_nrs2_cal.fits"
 
-    rtdata.get_truth("truth/test_nirspec_image2_cal/jw84600010001_02102_00001_nrs2_cal.fits")
+    rtdata.get_truth("truth/test_nirspec_image2/jw84600010001_02102_00001_nrs2_cal.fits")
 
     diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
     assert diff.identical, diff.report()
