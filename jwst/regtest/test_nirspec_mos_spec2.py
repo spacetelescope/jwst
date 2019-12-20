d87ce43aa781812166c52b9fdd33402fdb66a492
Howard Bushouse <bushouse@stsci.edu>
2019-12-20 15:22:39 -0500

    JP-1195: calspec2 regtest for NIRSpec MOS

---
+++ jwst/regtest/test_nirspec_mos_spec2.py
@@ -0,0 +1,54 @@
+import os
+import pytest
+from astropy.io.fits.diff import FITSDiff
+
+from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
+from jwst.stpipe import Step
+
+@pytest.fixture(scope="module")
+def run_pipeline(jail, rtdata_module):
+    """Run the calwebb_spec2 pipeline on a single NIRSpec MOS exposure."""
+
+    rtdata = rtdata_module
+
+    # Get the cfg files
+    collect_pipeline_cfgs("config")
+
+    # Get the MSA metadata file referenced in the input exposure
+    rtdata.get_data("nirspec/spectroscopic/jw95065006001_0_short_msa.fits")
+
+    # Get the input exposure
+    rtdata.get_data("nirspec/spectroscopic/f170lp-g235m_mos_observation-6-c0e0_001_dn_nrs1_mod.fits")
+
+    # Run the calwebb_spec2 pipeline; save results from intermediate steps
+    args = ["config/calwebb_spec2.cfg", rtdata.input,
+            "--steps.assign_wcs.save_results=true",
+            "--steps.msa_flagging.save_results=true",
+            "--steps.extract_2d.save_results=true",
+            "--steps.flat_field.save_results=true",
+            "--steps.srctype.save_results=true",
+            "--steps.pathloss.save_results=true",
+            "--steps.barshadow.save_results=true"]
+    Step.from_cmdline(args)
+
+    return rtdata
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize("output",[
+    "assign_wcs", "msa_flagging", "extract_2d", "flat_field", "srctype",
+    "pathloss", "barshadow", "cal", "s2d", "x1d"])
+def test_nirspec_mos_spec2(run_pipeline, fitsdiff_default_kwargs, output):
+    """Regression test of the calwebb_spec2 pipeline on a
+       NIRSpec MOS exposure."""
+
+    # Run the pipeline and retrieve outputs
+    rtdata = run_pipeline
+    rtdata.output = "f170lp-g235m_mos_observation-6-c0e0_001_dn_nrs1_mod_" + output + ".fits"
+
+    # Get the truth files
+    rtdata.get_truth(os.path.join("truth", "test_nirspec_mos_spec2", rtdata.output))
+
+    # Compare the results
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
