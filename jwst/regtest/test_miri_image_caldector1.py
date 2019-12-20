e1ddd5f4f8c76ecf365375adfc2ab82af7f5b2bf
Jane Morrison <morrison@as.arizona.edu>
2019-12-20 12:18:41 -0700

    fix trailing space

--- jwst/regtest/test_miri_image_caldector1.py
+++ jwst/regtest/test_miri_image_caldector1.py
@@ -5,6 +5,7 @@ from astropy.io.fits.diff import FITSDiff
 from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
 from jwst.stpipe import Step
 
+
 @pytest.fixture(scope="module")
 def run_pipeline(jail, rtdata_module):
     """Run calwebb_detector1 pipeline on MIRI imaging data."""
@@ -12,7 +13,7 @@ def run_pipeline(jail, rtdata_module):
     rtdata.get_data("miri/image/jw00001001001_01101_00001_MIRIMAGE_uncal.fits")
 
     collect_pipeline_cfgs("config")
-    args = ["config/calwebb_detector1.cfg", rtdata.input, 
+    args = ["config/calwebb_detector1.cfg", rtdata.input,
             "--steps.dq_init.save_results=True",
             "--steps.lastframe.save_results=True",
             "--steps.firstframe.save_results=True",
@@ -30,7 +31,8 @@ def run_pipeline(jail, rtdata_module):
 @pytest.mark.bigdata
 @pytest.mark.parametrize("output", ['rate', 'rateints', 'linearity', 'rscd',
                                     'dq_init', 'firstframe', 'lastframe',
-                                    'saturation', 'dark_current', 'refpix', 'jump'])
+                                    'saturation', 'dark_current', 'refpix',
+                                    'jump'])
 def test_miri_image_caldetector1(run_pipeline, request, fitsdiff_default_kwargs, output):
     """
     Regression test of calwebb_detector1 pipeline performed on MIRI data.
