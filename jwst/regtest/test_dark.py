73fe9f4df036ccb26700f941851f96620ede7bc9
James Davies <jdavies@stsci.edu>
2019-12-23 16:24:28 -0500

    Turn off rscd output for dark pipeline regtest

--- jwst/regtest/test_dark.py
+++ jwst/regtest/test_dark.py
@@ -5,17 +5,15 @@ from jwst.pipeline.collect_pipeline_cfgs import collect_pipeline_cfgs
 from jwst.stpipe import Step
 
 @pytest.mark.bigdata
-def test_miri_dark_cal( _jail, rtdata, fitsdiff_default_kwargs ):
+def test_miri_dark_pipeline( _jail, rtdata, fitsdiff_default_kwargs ):
     rtdata.get_data("miri/image/jw00001001001_01101_00001_MIRIMAGE_uncal.fits")
 
     collect_pipeline_cfgs("config")
-    args = ["config/calwebb_dark.cfg", rtdata.input,
-            "--steps.rscd.save_results=True"]
+    args = ["config/calwebb_dark.cfg", rtdata.input]
     Step.from_cmdline( args )
     rtdata.output = "jw00001001001_01101_00001_MIRIMAGE_dark.fits"
 
-    rtdata.get_truth("truth/test_miri_dark_pipeline/\
-        jw00001001001_01101_00001_MIRIMAGE_dark.fits")
+    rtdata.get_truth("truth/test_miri_dark_pipeline/jw00001001001_01101_00001_MIRIMAGE_dark.fits")
 
-    diff = FITSDiff( rtdata.output, rtdata.truth, **fitsdiff_default_kwargs )
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
     assert diff.identical, diff.report()
