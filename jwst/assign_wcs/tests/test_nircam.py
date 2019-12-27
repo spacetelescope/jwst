b71a84e0342c1c081b8ff4f81ffc97a901b8d066
Howard Bushouse <bushouse@stsci.edu>
2019-12-27 10:17:46 -0500

    Update failing unit test for keyword change

--- jwst/assign_wcs/tests/test_nircam.py
+++ jwst/assign_wcs/tests/test_nircam.py
@@ -45,7 +45,7 @@ wcs_wfss_kw = {'wcsaxes': 2, 'ra_ref': 53.1423683802, 'dec_ref': -27.8171119969,
 
 wcs_tso_kw = {'wcsaxes': 2, 'ra_ref': 86.9875, 'dec_ref': 23.423,
               'v2_ref': 95.043034, 'v3_ref': -556.150466, 'roll_ref': 359.9521,
-              'crpix1': 887.0, 'crpix2': 35.0,
+              'xref_sci': 887.0, 'yref_sci': 35.0,
               'cdelt1': 1.76686111111111e-05, 'cdelt2': 1.78527777777777e-05,
               'ctype1': 'RA---TAN', 'ctype2': 'DEC--TAN',
               'pc1_1': -1, 'pc1_2': 0,
@@ -172,11 +172,11 @@ def test_traverse_tso_grism():
     x0, y0, lam, orderdet = grism_to_detector(xin, yin, order)
     x, y, orderdet = detector_to_grism(x0, y0, lam, order)
 
-    assert x0 == wcs_tso_kw['crpix1']
-    assert y0 == wcs_tso_kw['crpix2']
+    assert x0 == wcs_tso_kw['xref_sci']
+    assert y0 == wcs_tso_kw['yref_sci']
     assert order == orderdet
     assert_allclose(x, xin)
-    assert y == wcs_tso_kw['crpix2']
+    assert y == wcs_tso_kw['yref_sci']
 
 
 def test_imaging_frames():
