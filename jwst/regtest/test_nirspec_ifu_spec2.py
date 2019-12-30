813cf09dc2666331bad68168ae89ab969a6c7974
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-12-30 13:55:02 -0500

    Implement NIRSpec IFU Level2b

---
+++ jwst/regtest/test_nirspec_ifu_spec2.py
@@ -0,0 +1,45 @@
+"""Regression tests for NIRSpec"""
+import os
+
+import pytest
+
+from . import regtestdata as rt
+
+
+@pytest.fixture(scope='module')
+def run_nrs_ifu(jail, rtdata_module):
+    """Run the pipelines"""
+    step_params = {
+        'input_path': 'nirspec/spectroscopic/ifu/nrs_ifu_rate.fits',
+        'step': 'calwebb_spec2.cfg',
+        'args': [
+            '--steps.bkg_subtract.save_results=true',
+            '--steps.assign_wcs.save_results=true',
+            '--steps.imprint_subtract.save_results=true',
+            '--steps.msa_flagging.save_results=true',
+            '--steps.extract_2d.save_results=true',
+            '--steps.flat_field.save_results=true',
+            '--steps.srctype.save_results=true',
+            '--steps.straylight.save_results=true',
+            '--steps.fringe.save_results=true',
+            '--steps.pathloss.save_results=true',
+            '--steps.barshadow.save_results=true',
+            '--steps.photom.save_results=true',
+            '--steps.resample_spec.save_results=true',
+            '--steps.cube_build.save_results=true',
+            '--steps.extract_1d.save_results=true',
+        ]
+    }
+
+    return rt.run_step_from_dict(rtdata_module, **step_params)
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize(
+    'suffix',
+    ['assign_wcs', 'cal', 'flat_field', 'msa_flagging', 'pathloss', 'photom', 's3d', 'srctype', 'x1d']
+)
+def test_nrs_ifu(run_nrs_ifu, fitsdiff_default_kwargs, suffix):
+    """Regression test matching output files"""
+    rt.is_like_truth(run_nrs_ifu, fitsdiff_default_kwargs, suffix,
+                     truth_path='truth/nirspec/test_nirspec_ifu_spec2')
