3c390bd476ef103cf7b9b0b953d0649409ce17d8
Phil Hodge <hodge@stsci.edu>
2019-12-30 14:18:40 -0500

    JP-1152 Check whether the output pixel number is NaN (#4409)
    * JP-1152 Check whether the output pixel number is NaN
    
    For each input spectrum, the array of corresponding pixel numbers in the
    output, combined spectrum is computed by using the inverse WCS function.
    It is possible for some of these pixel numbers (probably only at an endpoint
    of the input spectrum) to be NaN due to the finite bounding box for the WCS.
    A check for NaN was added.
    
    While testing this change, another bug was uncovered.  In the section that
    tests for the possibility that there was no input data for some output pixel,
    the error estimate for flux was being assigned to the error estimate for
    surface brightness.  This has been fixed.
    
    * Move the call to np.isnan outside the loop over pixels.

--- jwst/combine_1d/combine1d.py
+++ jwst/combine_1d/combine1d.py
@@ -187,6 +187,7 @@ class OutputSpectrumModel:
         self.weight = np.zeros(nelem, dtype=np.float64)
         self.count = np.zeros(nelem, dtype=np.float64)
 
+        n_nan = 0                                       # initial value
         for in_spec in input_spectra:
             # Get the pixel numbers in the output corresponding to the
             # wavelengths of the current input spectrum.
@@ -195,11 +196,17 @@ class OutputSpectrumModel:
                                         in_spec.wavelength)
             # i is a pixel number in the current input spectrum, and
             # k is the corresponding pixel number in the output spectrum.
+            nan_flag = np.isnan(out_pixel)
+            n_nan += nan_flag.sum()
             for i in range(len(out_pixel)):
                 if in_spec.dq[i] & datamodels.dqflags.pixel['DO_NOT_USE'] > 0:
                     continue
                 # Round to the nearest pixel.
+                if nan_flag[i]:         # skip if the pixel number is NaN
+                    continue
                 k = round(float(out_pixel[i]))
+                if k < 0 or k >= nelem:
+                    continue
                 weight = in_spec.weight[i]
                 self.dq[k] |= in_spec.dq[i]
                 self.flux[k] += in_spec.flux[i] * weight
@@ -208,6 +215,8 @@ class OutputSpectrumModel:
                 self.sb_error[k] += (in_spec.sb_error[i] * weight)**2
                 self.weight[k] += weight
                 self.count[k] += 1.
+        if n_nan > 0:
+            log.warning("%d output pixel numbers were NaN", n_nan)
 
         # Since the output wavelengths will not usually be exactly the same
         # as the input wavelengths, it's possible that there will be output
@@ -223,7 +232,7 @@ class OutputSpectrumModel:
             self.flux = self.flux[index]
             self.error = self.error[index]
             self.surf_bright = self.surf_bright[index]
-            self.sb_error = self.error[index]
+            self.sb_error = self.sb_error[index]
             self.dq = self.dq[index]
             self.weight = self.weight[index]
             self.count = self.count[index]
