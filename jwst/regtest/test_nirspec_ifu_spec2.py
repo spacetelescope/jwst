a5b9f39bb2636f6075356038ee2833b703cd4744
J.D.Eisenhamer <eisenhamer@stsci.edu>
2019-12-30 22:24:21 -0500

    Fix pep8 and data naming issues

--- jwst/regtest/test_nirspec_ifu_spec2.py
+++ jwst/regtest/test_nirspec_ifu_spec2.py
@@ -1,6 +1,4 @@
 """Regression tests for NIRSpec"""
-import os
-
 import pytest
 
 from . import regtestdata as rt
@@ -10,7 +8,7 @@ from . import regtestdata as rt
 def run_nrs_ifu(jail, rtdata_module):
     """Run the pipelines"""
     step_params = {
-        'input_path': 'nirspec/spectroscopic/ifu/nrs_ifu_rate.fits',
+        'input_path': 'nirspec/spectroscopic/ifu/nrs_ifu_nrs1_rate.fits',
         'step': 'calwebb_spec2.cfg',
         'args': [
             '--steps.bkg_subtract.save_results=true',
