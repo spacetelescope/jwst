6de814290c3e94c3bf36dac2ca9911222cfac9d3
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-01-08 11:14:35 -0500

    JP-1210 Implement NIRSpec IFU Spec3Pipeline regression (#4417)
    Changes
    * Generalize the pipeline running
    * Fix bad test naming for NIRISS from nrs to nis
    * Implement the detector1 test for IFU
    * Fix pep8 and data naming issues
    * Update data paths from reviewer comments
    * Rename NIRSpec IFU test module
    * Initial test implmentation but not complete
      Test exists but assertions are not complete.
      Test will fail.
    * Check for the actual output files
       - Implemented new API to `is_like_truth` to allow for
          complete file name specification
       - Introduced `run_step_from_dict_mock` to allow copying expected
          output to the working directory to avoid re-running long running
          pipelines
    * Fix where the truth lies

---
+++ jwst/regtest/test_nirspec_ifu.py
@@ -0,0 +1,82 @@
+"""Regression tests for NIRSpec"""
+import pytest
+
+from . import regtestdata as rt
+
+
+@pytest.fixture(scope='module')
+def run_spec2(jail, rtdata_module):
+    """Run the pipelines"""
+    step_params = {
+        'input_path': 'nirspec/ifu/nrs_ifu_nrs1_rate.fits',
+        'step': 'calwebb_spec2.cfg',
+        'args': [
+            '--steps.bkg_subtract.save_results=true',
+            '--steps.assign_wcs.save_results=true',
+            '--steps.imprint_subtract.save_results=true',
+            '--steps.msa_flagging.save_results=true',
+            '--steps.extract_2d.save_results=true',
+            '--steps.flat_field.save_results=true',
+            '--steps.srctype.save_results=true',
+            '--steps.straylight.save_results=true',
+            '--steps.fringe.save_results=true',
+            '--steps.pathloss.save_results=true',
+            '--steps.barshadow.save_results=true',
+            '--steps.photom.save_results=true',
+            '--steps.resample_spec.save_results=true',
+            '--steps.cube_build.save_results=true',
+            '--steps.extract_1d.save_results=true',
+        ]
+    }
+
+    return rt.run_step_from_dict(rtdata_module, **step_params)
+
+
+@pytest.fixture(scope='module')
+def run_spec3(jail, rtdata_module):
+    """Run Spec3Pipeline"""
+    step_params = {
+        'input_path': 'nirspec/ifu/nrs_ifu_spec3_asn.json',
+        'step': 'calwebb_spec3.cfg',
+        'args': {
+            '--steps.master_background.save_results=true',
+            '--steps.mrs_imatch.save_results=true',
+            '--steps.outlier_detection.save_results=true',
+            '--steps.resample_spec.save_results=true',
+            '--steps.cube_build.save_results=true',
+            '--steps.extract_1d.save_results=true',
+            '--steps.combine_1d.save_results=true',
+        }
+    }
+
+    return rt.run_step_from_dict(rtdata_module, **step_params)
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize(
+    'suffix',
+    ['assign_wcs', 'cal', 'flat_field', 'msa_flagging', 'pathloss', 'photom', 's3d', 'srctype', 'x1d']
+)
+def test_spec2(run_spec2, fitsdiff_default_kwargs, suffix):
+    """Regression test matching output files"""
+    rt.is_like_truth(run_spec2, fitsdiff_default_kwargs, suffix,
+                     truth_path='truth/test_nirspec_ifu')
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize(
+    'output',
+    [
+        'nrs_ifu_g395h-f290lp_s3d.fits',
+        'nrs_ifu_g395h-f290lp_x1d.fits',
+        'nrs_ifu_nrs1_o028_crf.fits',
+        'nrs_ifu_nrs2_o028_crf.fits',
+    ]
+)
+def test_spec3(run_spec3, fitsdiff_default_kwargs, output):
+    """Regression test matching output files"""
+    rt.is_like_truth(
+        run_spec3, fitsdiff_default_kwargs, output,
+        truth_path='truth/test_nirspec_ifu',
+        is_suffix=False
+    )
