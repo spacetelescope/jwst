452707d1068185a70a35a1286a84fb2386edc55f
Jane Morrison <morrison@as.arizona.edu>
2020-02-12 13:57:16 -0700

    added slitmeta.schema

--- jwst/datamodels/multiresamp.py
+++ jwst/datamodels/multiresamp.py
@@ -5,21 +5,11 @@ __all__ = ['MultiResampModel']
 
 class MultiResampModel(model_base.DataModel):
     """
-    A data model for multi-resampled images or resampled multi-slit images.
+    A data model for spectroscopic resampled slits.
 
-    This model has a special member `products` that can be used to
-    deal with each resampled image/slit data  at a time.  It behaves like a list::
+    This model has a special member `slits` that can be used to
+    deal with each DrizProduct at a time.  It behaves like a list::
 
-       >>> from . import MultiResampModel
-       >>> multiresamp_model = MultiResampModel()
-       >>> multiresamp_model.products.append(multiResampModel())
-       >>> multiresap_model.products[0] # doctest: +SKIP
-
-    If `init` is a file name or an `MultiResamptModel` instance, an empty
-    `MultiResampModel` will be created and assigned to attribute `products[0]`,
-    and the `data`, `wht`, and `con` attributes from the
-    input file or `DrizProductModel` will be copied to the first element of
-    `products`.
 
     Parameters
     __________
@@ -34,13 +24,6 @@ class MultiResampModel(model_base.DataModel):
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/multiresamp.schema"
 
-    def __init__(self, init=None, **kwargs):
-        super(MultiResampModel, self).__init__(init=None, **kwargs)
-        self.update(init)
-        #self.products.append(self.products.item())
-        #self.products[0].data = init.data
-        #self.products[0].wht = init.wht
-        #self.products[0].con = init.con
-        #return
 
+    def __init__(self, init=None, **kwargs):
         super(MultiResampModel, self).__init__(init=init, **kwargs)
