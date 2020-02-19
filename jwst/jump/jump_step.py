64d1d7bd0a131ff339454dfda99e4172a34e8742
mwregan2 <mregan@stsci.edu>
2020-02-19 15:59:01 -0500

    Improve config parameter comments
    The units of the various thresholds were update to show that they are in units of sigma.

--- jwst/jump/jump_step.py
+++ jwst/jump/jump_step.py
@@ -15,11 +15,11 @@ class JumpStep(Step):
     """
 
     spec = """
-        rejection_threshold = float(default=4.0,min=0) # CR rejection threshold
+        rejection_threshold = float(default=4.0,min=0) # CR sigma rejection threshold
         maximum_cores = option('quarter', 'half', 'all', default=None) # max number of processes to create
         flag_4_neighbors = boolean(default=True) # flag the four perpendicular neighbors of each CR
-        max_jump_to_flag_neighbors = float(default=200) # maximum jump that will trigger neighbor flagging
-        min_jump_to_flag_neighbors = float(default=10) # minimum jump that will trigger neighbor flagging
+        max_jump_to_flag_neighbors = float(default=200) # maximum jump sigma that will trigger neighbor flagging
+        min_jump_to_flag_neighbors = float(default=10) # minimum jump sigma that will trigger neighbor flagging
     """
 
     reference_file_types = ['gain', 'readnoise']
