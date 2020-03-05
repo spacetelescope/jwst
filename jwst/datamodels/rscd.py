b850f9298fd8b06c6e7955b227c27024f74dbb75
Jane Morrison <morrison@as.arizona.edu>
2020-03-05 15:13:06 -0500

    updated data models

--- jwst/datamodels/rscd.py
+++ jwst/datamodels/rscd.py
@@ -10,9 +10,29 @@ class RSCDModel(ReferenceFileModel):
 
     Parameters
     __________
-    rscd_table : numpy table
-        Reference file for RSCD correction
-        A table with seven columns, three string-valued that identify which
-        row to select, and four float columns containing coefficients.
+    rscd_group_skip_table : numpy table
+        Reference table for RSCD correction baseline correction
+        A table with 3 columms that set the number of group to skip for each
+        subarray and readpatt
+
+    rscd_gen_table : numpy table
+        Reference table for RSCD correction enhanced correction
+        A table with 5 columms that sets up general parameters for the enhanced 
+
+    rscd_int1_table : numpy table
+        Reference table for RSCD correction enhanced correction
+        A table with 7 columms that sets up correction parameters  for the enhanced 
+        correction for integration 1 based on subarray, readpatt, even or odd row
+
+    rscd_int2_table : numpy table
+        Reference table for RSCD correction enhanced correction
+        A table with 7 columms that sets up correction parameters  for the enhanced 
+        correction for integration 2 based on subarray, readpatt, even or odd row
+
+    rscd_int3_table : numpy table
+        Reference table for RSCD correction enhanced correction
+        A table with 7 columms that sets up correction parameters  for the enhanced 
+        correction for integration 3 based on subarray, readpatt, even or odd row
+
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/rscd.schema"
