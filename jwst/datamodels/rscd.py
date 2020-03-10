a982101aeaba71f10e805e7c1a401718641122d0
Karl Gordon <kgordon@stsci.edu>
2020-03-10 12:08:48 -0400

    removing tailing whitespace (flake8)

--- jwst/datamodels/rscd.py
+++ jwst/datamodels/rscd.py
@@ -17,21 +17,21 @@ class RSCDModel(ReferenceFileModel):
 
     rscd_gen_table : numpy table
         Reference table for RSCD correction enhanced correction
-        A table with 5 columms that sets up general parameters for the enhanced 
+        A table with 5 columms that sets up general parameters for the enhanced
 
     rscd_int1_table : numpy table
         Reference table for RSCD correction enhanced correction
-        A table with 7 columms that sets up correction parameters  for the enhanced 
+        A table with 7 columms that sets up correction parameters  for the enhanced
         correction for integration 1 based on subarray, readpatt, even or odd row
 
     rscd_int2_table : numpy table
         Reference table for RSCD correction enhanced correction
-        A table with 7 columms that sets up correction parameters  for the enhanced 
+        A table with 7 columms that sets up correction parameters  for the enhanced
         correction for integration 2 based on subarray, readpatt, even or odd row
 
     rscd_int3_table : numpy table
         Reference table for RSCD correction enhanced correction
-        A table with 7 columms that sets up correction parameters  for the enhanced 
+        A table with 7 columms that sets up correction parameters  for the enhanced
         correction for integration 3 based on subarray, readpatt, even or odd row
 
     """
