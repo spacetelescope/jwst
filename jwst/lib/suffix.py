4f0dccb3c93a07b39a2a1fec70fd7eebdc3c98cd
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-04-02 08:03:51 -0400

    Restrict some exceptions

--- jwst/lib/suffix.py
+++ jwst/lib/suffix.py
@@ -327,7 +327,7 @@ def load_local_pkg(fpath):
     sys.path.insert(0, package_fpath)
     try:
         for module_fpath in folder_traverse(
-            fpath, basename_regex='[^_].+\.py$', path_exclude_regex='tests'
+            fpath, basename_regex=r'[^_].+\.py$', path_exclude_regex='tests'
         ):
             folder_path, fname = path.split(module_fpath[package_fpath_len:])
             module_path = folder_path.split('/')
@@ -335,7 +335,7 @@ def load_local_pkg(fpath):
             module_path = '.'.join(module_path)
             try:
                 module = import_module(module_path)
-            except Exception as err:
+            except ImportError as err:
                 logger.debug(f'Cannot load module "{module_path}": {str(err)}')
             else:
                 yield module
