e3ab708a6a4aaf68bbbb3ec24ad643a83701b05a
Howard Bushouse <bushouse@stsci.edu>
2020-05-04 10:35:55 -0400

    Update RT context and remove abvega overrides

--- jwst/regtest/test_nircam_mtimage.py
+++ jwst/regtest/test_nircam_mtimage.py
@@ -12,11 +12,9 @@ from jwst.stpipe import Step
 def test_nircam_image_moving_target(rtdata, fitsdiff_default_kwargs):
     """Test resampled i2d of moving target exposures for NIRCam imaging"""
     collect_pipeline_cfgs("config")
-    rtdata.get_data("nircam/image/jwst_nircam_abvega_offset_0001.asdf")
     rtdata.get_asn("nircam/image/mt_asn.json")
     rtdata.output = "mt_assoc_i2d.fits"
-    args = ["config/calwebb_image3.cfg", rtdata.input,
-            "--steps.source_catalog.override_abvegaoffset='jwst_nircam_abvega_offset_0001.asdf'"]
+    args = ["config/calwebb_image3.cfg", rtdata.input]
     Step.from_cmdline(args)
     rtdata.get_truth("truth/test_nircam_mtimage/mt_assoc_i2d.fits")
 
