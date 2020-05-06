c1141ea6998583e40e08e888b6cf4887ebbffdf7
James White <jwhite@stsci.edu>
2020-05-06 15:57:58 -0400

    Refactor ApCorr and split on functionality

--- jwst/extract_1d/apply_apcorr.py
+++ jwst/extract_1d/apply_apcorr.py
@@ -1,29 +1,48 @@
+import abc
+import numpy as np
+
 from typing import Tuple
-from scipy.interpolate import interp2d, interp1d
+from scipy.interpolate import interp2d
 from astropy.io import fits
 
 from ..assign_wcs.util import compute_scale
 from ..datamodels import DataModel
 
 
-class ApCorr:
+class ApCorrBase(abc.ABC):
     """Perform aperture correction on input extraction data.
 
     """
-    def __init__(self, input_model: DataModel, appcorr_table: fits.FITS_rec, slitname: str = None,
-                 location: Tuple[float, float] = None):
+    match_pars = {
+        'MIRI': {
+            'LRS': {'subarray': ['name']},
+            'MRS': {'instrument': []},  # Only one row is available for this mode; no selection criteria
+        },
+        'NIRSPEC': {
+            'IFU': {'instrument': ['filter', 'grating']},
+            'MSASPEC': {'instrument': ['filter', 'grating']},
+            'FIXEDSLIT': {'instrument': ['filter', 'grating']},  # Slit is also required; passed in as init arg
+        },
+        'NIRCAM': {
+            'WFSS': {'insturment': ['filter', 'pupil']}
+        },
+        'NIRISS': {
+            'WFSS': {'instrument': ['filter', 'pupil']}
+        }
+    }
+
+    def __init__(self, input_model: DataModel, appcorr_table: fits.FITS_rec, location: Tuple[float, float] = None,
+                 **match_kwargs):
         self.correction = None
 
         self.model = input_model
         self._reference_table = appcorr_table
-        self.slitname = slitname
         self.location = location
 
         self.match_keys = self._get_match_keys()
-        self.match_pars = self._get_match_pars()
 
-        if self.slitname and self.slitname != self.model.meta.exposure.type:
-            self.match_pars['slit'] = self.slitname
+        self.match_pars = self._get_match_pars()
+        self.match_pars.update(match_kwargs)
 
         self.reference = self._reduce_reftable()
 
@@ -36,38 +55,21 @@ class ApCorr:
                     f'(RA, DEC) must be provided in order to convert to pixels.'
                 )
 
-        self.apcorr_func = self._approx_apcorr_fn()
+        self.apcorr_func = self.approximate()
 
     def _get_match_keys(self):
-        """Get column keys needed for reducing the reference table."""
-        match_pars = {
-            'MIRI': {
-                'LRS': {'subarray': ['name']},
-                'MRS': {'instrument': []},
-            },
-            'NIRSPEC': {
-                'IFU': {'instrument': ['filter', 'grating']},
-                'MSASPEC': {'instrument': ['filter', 'grating']},
-                'FIXEDSLIT': {'instrument': ['filter', 'grating', 'slit']},
-            },
-            'NIRCAM': {
-                'WFSS': {'insturment': ['filter', 'pupil']}
-            },
-            'NIRISS': {
-                'WFSS': {'instrument': ['filter', 'pupil']}
-            }
-        }
-
+        """Get column keys needed for reducing the reference table based on input."""
         instrument = self.model.meta.instrument.name.upper()
         exptype = self.model.meta.exposure.type.upper()
 
-        relevant_pars = match_pars[instrument]
+        relevant_pars = self.match_pars[instrument]
 
         for key in relevant_pars.keys():
             if key in exptype:
                 return relevant_pars[key]
 
     def _get_match_pars(self):
+        """Get meta parameters required for reference table row-selection."""
         match_pars = {}
 
         for node, keys in self.match_keys.items():
@@ -83,27 +85,22 @@ class ApCorr:
         table = self._reference_table.copy()
 
         for key, value in self.match_pars.items():
-            table = table[table[key] == value]
+            if isinstance(value, str):  # Not all files will have the same format as input model metadata values.
+                table = table[table[key].upper() == value.upper()]
+            else:
+                table = table[table[key] == value]
 
-        return table
+        if len(table) != 1:
+            raise ValueError('Could not resolve APCORR reference for input.')
 
-    def _approx_apcorr_fn(self):
-        """Generate an approximate function for interpolating apcorr values to input wavelength and size."""
-        wavelength = self.reference['wavelength'][0][:self.reference['nelem_wl'][0]]
+        return table[0]
 
-        if self.model.meta.exposure.type == 'MIR_MRS':
-            size = self.reference['radius'][0][:self.reference['nelem_wl'][0]]
-            apcorr = self.reference['apcorr'][0][:self.reference['nelem_wl'][0]]
+    @abc.abstractmethod
+    def approximate(self):
+        pass
 
-            return interp1d(size, wavelength, apcorr)
-
-        size = self.reference['size'][0][:self.reference['nelem_size'][0]]
-        apcorr = self.reference['apcorr'][0][:self.reference['nelem_size'][0], :self.reference['nelem_wl'][0]]
-
-        return interp2d(size, wavelength, apcorr)
-
-    def apply_apcorr(self, spec_table: fits.FITS_rec):
-        """Apply interpolated aperture correction value to source-related extraction results.
+    def apply(self, spec_table: fits.FITS_rec):
+        """Apply interpolated aperture correction value to source-related extraction results in-place.
 
         Parameters
         ----------
@@ -118,3 +115,69 @@ class ApCorr:
 
             for col in cols_to_correct:
                 row[col] *= correction
+
+
+class ApCorrPhase(ApCorrBase):
+    def __init__(self, pixphase: float = 0.5, *args, **kwargs):
+        self.phase = pixphase  # In the future we'll attempt to measure the pixel phase from inputs.
+
+        super().__init__(*args, **kwargs)
+
+    def approximate(self):
+        """Generate an approximate function for interpolating apcorr values to input wavelength and size."""
+        wavelength = self.reference['wavelength'][:self.reference['nelem_wl']]
+        phase_idx = np.where(self.reference['pixphase'] == self.phase)
+
+        size = self.reference['size'][phase_idx][:self.reference['nelem_size'][phase_idx]]
+
+        apcorr = (
+            self.reference['apcorr'][phase_idx][:self.reference['nelem_size'], :self.reference['nelem_wl']]
+        )
+
+        return interp2d(wavelength, size, apcorr)
+
+    def measure_phase(self):  # Future method in determining pixel phase
+        pass
+
+
+class ApCorrRadial(ApCorrBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def approximate(self):
+        """Generate an approximate function for interpolating apcorr values to input wavelength and radius."""
+        wavelength = self.reference['wavelength'][:self.reference['nelem_wl']]
+        size = self.reference['radius'][:self.reference['nelem_wl']]
+        apcorr = self.reference['apcorr'][:self.reference['nelem_wl']]
+
+        return interp2d(wavelength, size, apcorr)
+
+
+class ApCorr(ApCorrBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def approximate(self):
+        """Generate an approximate function for interpolating apcorr values to input wavelength and size."""
+        wavelength = self.reference['wavelength'][:self.reference['nelem_wl']]
+        size = self.reference['size'][:self.reference['nelem_size']]
+        apcorr = self.reference['apcorr'][:self.reference['nelem_size'], :self.reference['nelem_wl']]
+
+        return interp2d(wavelength, size, apcorr)
+
+
+def select_apcorr(input_model):
+    if input_model.meta.instrument.name == 'MIRI':
+        if 'MRS' in input_model.meta.exposure.type:
+            return ApCorrRadial
+        else:
+            return ApCorr
+
+    if input_model.meta.instrument.name == 'NIRCAM':
+        return ApCorr
+
+    if input_model.meta.instrument.name == 'NIRISS':
+        return ApCorr
+
+    if input_model.meta.instrument.name == 'NIRSPEC':
+        return ApCorrPhase
