558b2cafd4523e287b879ebec4d8638bacb1eeae
James Davies <jdavies@stsci.edu>
2020-05-07 21:41:42 -0400

    Fix doctest jail dir behavoir for pytest-doctestplus>=0.5

--- docs/conftest.py
+++ docs/conftest.py
@@ -5,7 +5,7 @@ def _docdir(request):
 
     # Trigger ONLY for doctestplus.
     doctest_plugin = request.config.pluginmanager.getplugin("doctestplus")
-    if isinstance(request.node, doctest_plugin._doctest_textfile_item_cls):
+    if isinstance(request.node.parent, doctest_plugin._doctest_textfile_item_cls):
         tmpdir = request.getfixturevalue('tmpdir')
         with tmpdir.as_cwd():
             yield
