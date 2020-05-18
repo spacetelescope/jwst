bc79a4a5e689aa77f692cb38f5716ac826120365
Mike Regan <mregan@stsci.edu>
2020-05-18 11:36:43 -0400

    add spaces to comments in jump.py

--- jwst/jump/jump.py
+++ jwst/jump/jump.py
@@ -122,7 +122,7 @@ def detect_jumps (input_model, gain_model, readnoise_model,
     else:
         log.info("Creating %d processes for jump detection " % numslices)
         pool = multiprocessing.Pool(processes=numslices)
-        #Starts each slice in it's own process. Starmap allows more than one parameter to be passed.
+        # Starts each slice in it's own process. Starmap allows more than one parameter to be passed.
         real_result = pool.starmap(twopt.find_crs, slices)
         pool.close()
         pool.join()
@@ -136,13 +136,13 @@ def detect_jumps (input_model, gain_model, readnoise_model,
                 gdq[:, :, k * yincrement:(k + 1) * yincrement, :] = resultslice[0]
             row_below_gdq[:, :, :] = resultslice[1]
             row_above_gdq[:, :, :] = resultslice[2]
-            if k != 0: #for all but the first slice, flag any CR neighbors in the top row of the previous slice and
-                #flag any neighbors in the bottom row of this slice saved from the top of the previous slice
+            if k != 0: # for all but the first slice, flag any CR neighbors in the top row of the previous slice and
+                # flag any neighbors in the bottom row of this slice saved from the top of the previous slice
                 gdq[:, :, k * yincrement - 1, :] = np.bitwise_or(gdq[:, :, k * yincrement - 1, :],
                                                                      row_below_gdq[:, :, :])
                 gdq[:, :, k * yincrement, :] = np.bitwise_or(gdq[:, :, k * yincrement, :],
                                                                      previous_row_above_gdq[:, :, :])
-            #save the neighbors to be flagged that will be in the next slice
+            # save the neighbors to be flagged that will be in the next slice
             previous_row_above_gdq = row_above_gdq.copy()
             k += 1
         pool.terminate()
