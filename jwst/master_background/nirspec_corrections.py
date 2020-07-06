a01af7c114cc555cfbb3f3bbcd3029c966aa077f
Howard Bushouse <bushouse@stsci.edu>
2020-07-06 14:53:36 -0400

    Include unit tests and check for pathloss array existence

--- jwst/master_background/nirspec_corrections.py
+++ jwst/master_background/nirspec_corrections.py
@@ -21,6 +21,23 @@ def correct_nrs_ifu_bkg(input):
     """
 
     log.info('Applying point source pathloss updates to IFU background')
-    input.data *= (input.pathloss_point / input.pathloss_uniform)
+
+    # Try to load the appropriate pathloss correction arrays
+    try:
+        pl_point = input.getarray_noinit('pathloss_point')
+    except AttributeError:
+        log.warning('Pathloss_point array not found in input')
+        log.warning('Skipping pathloss background updates')
+        return input
+
+    try:
+        pl_uniform = input.getarray_noinit('pathloss_uniform')
+    except AttributeError:
+        log.warning('Pathloss_uniform array not found in input')
+        log.warning('Skipping pathloss background updates')
+        return input
+
+    # Apply the corrections
+    input.data *= (pl_point / pl_uniform)
 
     return input
