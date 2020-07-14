cf34ec20118e3e8499fda4d985bf96f45f9dd0c8
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-07-14 11:13:19 -0400

    Rename dq flag functions to a simpler name per review

--- jwst/datamodels/dqflags.py
+++ jwst/datamodels/dqflags.py
@@ -107,33 +107,43 @@ def interpret_bit_flags(bit_flags, flip_bits=None):
     return ap_interpret_bit_flags(bit_flags_dm, flip_bits=flip_bits)
 
 
-def bit_flags_to_mnemonics(bit_flags):
+def dqflags_to_mnemonics(dqflags, mnemonic_map=pixel):
     """Interpret value as bit flags and return the mnemonics
 
     Parameters
     ----------
-    bit_flags : int-like
-        The value to interpret as bit flags
+    dqflags : int-like
+        The value to interpret as DQ flags
+
+    mnemonic_map: {str: int[,...]}
+        Dictionary associating the mnemonic string to an integer value
+        representing the set bit for that mnemonic.
 
     Returns
     -------
     mnemonics : {str[,...]}
-        Set of mnemonics represented by the set bits
+        Set of mnemonics represented by the set bit flags
 
     Examples
     --------
-    >>> bit_flags_to_mnemonics(1)
+    >>> dqflags_to_mnemonics(1)
     {'DO_NOT_USE'}
 
-    >>> bit_flags_to_mnemonics(7)             #doctest: +SKIP
+    >>> dqflags_to_mnemonics(7)             #doctest: +SKIP
     {'JUMP_DET', 'DO_NOT_USE', 'SATURATED'}
 
-    >>> bit_flags_to_mnemonics(7) == {'JUMP_DET', 'DO_NOT_USE', 'SATURATED'}
+    >>> dqflags_to_mnemonics(7) == {'JUMP_DET', 'DO_NOT_USE', 'SATURATED'}
     True
+
+    >>> dqflags_to_mnemonics(1, mnemonic_map=pixel)
+    {'DO_NOT_USE'}
+
+    >>> dqflags_to_mnemonics(1, mnemonic_map=group)
+    {'DO_NOT_USE'}
     """
     mnemonics = {
         mnemonic
-        for mnemonic, value in pixel.items()
-        if (bit_flags & value)
+        for mnemonic, value in mnemonic_map.items()
+        if (dqflags & value)
     }
     return mnemonics
