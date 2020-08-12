2333cef2334c9dd693a66c34ce1885857e74e01c
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-08-12 10:23:21 -0400

    Setup the correction parameters attributes for Step

--- jwst/stpipe/step.py
+++ jwst/stpipe/step.py
@@ -56,6 +56,12 @@ class Step():
     input_dir          = string(default=None)        # Input directory
     """
 
+    # Correction parameters. These store and use whatever information a Step
+    # may need to perform its operations without re-calculating, or to use
+    # from a previous run of the Step.  The structure is up to each Step.
+    self.correction_pars = None
+    self.use_correction_pars = False
+
     # Reference types for both command line override
     # definition and reference prefetch
     reference_file_types = []
