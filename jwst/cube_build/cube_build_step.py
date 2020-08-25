740c85af123af2537ec889e2d2c3dc6eb412b776
Jane Morrison <morrison@as.arizona.edu>
2020-08-25 08:10:17 -0700

    JP-1653 changed default weighting to emsm (#5277)
    * changed default weighting to emsm
    
    * Updated change log

--- jwst/cube_build/cube_build_step.py
+++ jwst/cube_build/cube_build_step.py
@@ -41,7 +41,7 @@ class CubeBuildStep (Step):
          scale1 = float(default=0.0) # cube sample size to use for axis 1, arc seconds
          scale2 = float(default=0.0) # cube sample size to use for axis 2, arc seconds
          scalew = float(default=0.0) # cube sample size to use for axis 3, microns
-         weighting = option('emsm','msm','miripsf',default = 'msm') # Type of weighting function
+         weighting = option('emsm','msm','miripsf',default = 'emsm') # Type of weighting function
          coord_system = option('skyalign','world','internal_cal','ifualign',default='skyalign') # Output Coordinate system.
          rois = float(default=0.0) # region of interest spatial size, arc seconds
          roiw = float(default=0.0) # region of interest wavelength size, microns
