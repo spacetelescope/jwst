b61a398c0ad7ebf6e4356f96d6f0a666a8fbe6a5
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-09-15 09:07:18 -0400

    Start creating data on-the-fly

--- jwst/lib/tests/test_pointing_summary.py
+++ jwst/lib/tests/test_pointing_summary.py
@@ -28,7 +28,23 @@ def engdb():
         yield engdb
 
 
-def test_calc_pointing_deltas(engdb):
+@pytest.fixture(scope='module')
+def data_path(jail):
+    """Create data file with needed header parameters"""
+    model = dm.ImageModel()
+
+    model.meta.target.ra = 90.75541666666666
+    model.meta.target.dec = -66.56055555555554
+    model.meta.pointing.ra_v1 = 91.08142004561715
+    model.meta.pointing.dec_v1 = -66.60547868904696
+    model.meta.wcsinfo.ra_ref = 90.70377653291781
+    model.meta.wcsinfo.dec_ref = -66.59540223936895
+
+    model.save('data.fits')
+    return model.meta.filename
+
+
+def test_calc_pointing_deltas(engdb, data_path):
     """Test `calc_pointing_deltas` basic running"""
     truth = ('Delta(target=<SkyCoord (ICRS): (ra, dec) in deg'
              '\n    (90.75541667, -66.56055556)>, v1=<SkyCoord (ICRS): (ra, dec) in deg'
@@ -36,15 +52,15 @@ def test_calc_pointing_deltas(engdb):
              '\n    (90.70377653, -66.59540224)>, delta_v1=<Angle 0.13712727'
              ' deg>, delta_refpoint=<Angle 0.04044315 deg>)'
     )
-    model = dm.ImageModel(str(DATA_PATH / 'empty.fits'))
+    model = dm.ImageModel(str(data_path))
     deltas = ps.calc_pointing_deltas(model)
 
     assert truth == str(deltas)
 
 
-def test_calc_deltas(engdb):
+def test_calc_deltas(engdb, data_path):
     """Test `calc_deltas` basic running"""
-    model = dm.ImageModel(str(DATA_PATH / 'empty.fits'))
+    model = dm.ImageModel(data_path)
     deltas = ps.calc_deltas([model])
 
     truth = Table.read(DATA_PATH / 'calc_deltas_truth.ecsv')
