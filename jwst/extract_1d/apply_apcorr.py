fix radius scaling

jwst/extract_1d/apply_apcorr.py
@@ -7,7 +7,7 @@ from stdatamodels import DataModel
 
 from ..assign_wcs.util import compute_scale
 from ..datamodels import MultiSlitModel
-
+import numpy as np
 
 class ApCorrBase(abc.ABC):
     """Base class for aperture correction classes.
@@ -132,7 +132,7 @@ class ApCorrBase(abc.ABC):
                 if table[key] == 'ANY':
                     table = table
                 else:
-                    table = table[table[key].upper() == value.upper()]                    
+                    table = table[table[key].upper() == value.upper()]
             else:
                 table = table[table[key] == value]
 
@@ -246,17 +246,170 @@ class ApCorrRadial(ApCorrBase):
     """Aperture correction class used with spectral data produced from an extraction aperture radius."""
     size_key = 'radius'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+ match_pars = {
+        'MIRI': {
+            'MRS': {'instrument': ['channel','band']},  # Only one row is available for this mode; no selection criteria
+        },
+        'NIRSPEC': {
+            'IFU': {'instrument': ['filter', 'grating']},
+        }
+    }
 
-    def approximate(self):
-        """Generate an approximate function for interpolating apcorr values to input wavelength and radius."""
-        wavelength = self.reference['wavelength']
-        size = self.reference['radius']
-        apcorr = self.reference['apcorr']
-        #size = size[0:3,0]
-        print('in apcorr radial',wavelength.shape,size.shape,apcorr.shape)
-        return interp2d(wavelength,size, apcorr)
+    size_key = None
+
+    def __init__(self, input_model: DataModel, apcorr_table,
+                 location: Tuple[float, float, float] = None):
+
+        self.correction = None
+        self._reference_table = apcorr_table
+        self.model = input_model
+        self.location = location
+        instrument = self.model.meta.instrument.name.upper()
+
+        self.match_keys = self.get_match_keys()
+        self.match_pars = self.get_match_pars()
+        self.reference = self.reduce_reftable(instrument)
+        self.apcorr_sizeunits = self.reference.radius_units
+        self._convert_size_units()
+        self.apcorr_func = self.approximate()
+
+def _convert_size_units(self):
+        """If the SIZE or Radius column is in units of arcseconds, convert to pixels."""
+        if self.apcorr_sizeunits.startswith('arcsec'):
+            # compute_scale returns scale in degrees
+            if self.location is not None:
+                scale_degrees =  compute_scale(
+                    self.model.meta.wcs,
+                    self.location,
+                    disp_axis=self.model.meta.wcsinfo.dispersion_direction)
+                scale_arcsec = scale_degrees*3600.00
+                self.reference.radius /= scale_arcsec
+            else:
+                raise ValueError(
+                    'If the size column for the input APCORR reference file is in units with arcseconds, a location '
+                    '(RA, DEC, wavelength) must be provided in order to compute a pixel scale to convert arcseconds to '
+                    'pixels.'
+                )
+
+    def get_match_keys(self) -> dict:
+        """Get column keys needed for reducing the reference table based on input."""
+        instrument = self.model.meta.instrument.name.upper()
+        exptype = self.model.meta.exposure.type.upper()
+
+        relevant_pars = self.match_pars[instrument]
+        for key in relevant_pars.keys():
+            if key in exptype:
+                return relevant_pars[key]
+
+    def get_match_pars(self) -> dict:
+        """Get meta parameters required for reference table row-selection."""
+        match_pars = {}
+
+        for node, keys in self.match_keys.items():
+            meta_node = self.model.meta[node]
+            for key in keys:
+                match_pars[key if key != 'name' else node] = getattr(meta_node, key)
+        return match_pars
+def reduce_reftable(self, instrument):
+        """Find the correct table for channel/band or filter/grating."""
+
+        tabletype =''
+        set_multiple = False
+        for key, value in self.match_pars.items():
+            tabletype = tabletype + '_' + value.lower()
+
+            if value.lower() == 'multiple':
+                set_multiple  = True
+
+        if set_multiple or instrument == 'MIRI':
+            tabletype = '_multiple_multiple'
+
+        # need a better way to do this- but for now to get things working
+        # I was trying to do something like
+        # table = self_reference_table.'apcorr_table'+ tabletype - but that did not work
+        if tabletype == '_multiple_multiple':
+            table = self._reference_table.apcorr_table
+        elif tabletype == '_clear_prism':
+            table = self._reference_table.apcorr_table_clear_prism
+        elif tabletype == '_f070lp_g140h':
+            table = self._reference_table.apcorr_table_f070lp_g140h
+        elif tabletype == '_f070lp_g140m':
+            table = self._reference_table.apcorr_table_f070lp_g140m
+        elif tabletype == '_f100lp_g140h':
+            table = self._reference_table.apcorr_table_f100lp_g140h
+        elif tabletype == '_f100lp_g140m':
+            table = self._reference_table.apcorr_table_f100lp_g140m
+        elif tabletype == '_f170lp_g235h':
+            table = self._reference_table.apcorr_table_f290lp_g395h
+        elif tabletype == '_f170lp_g235m':
+            table = self._reference_table.apcorr_table_f290lp_g395m
+        elif tabletype == '_f290lp_g395h':
+            table = self._reference_table.apcorr_table_f290lp_g395h
+        elif tabletype == '_f290lp_g395h':
+            table = self._reference_table.apcorr_table_f290lp_g395m
+
+        return table
+def approximate(self):
+        # the wavelength and extraction radius is need to interpolate apcor.
+        # This is done in find apcorr_func
+        pass
+
+
+    def apply(self, spec_table: fits.FITS_rec):
+        """Apply interpolated aperture correction value to source-related extraction results in-place.
+
+        Parameters
+        ----------
+        spec_table : `~fits.FITS_rec`
+            Table of aperture corrections values from apcorr reference file.
+
+        """
+        cols_to_correct = ('flux', 'surf_bright', 'error', 'sb_error')
+
+        for i, row in enumerate(spec_table):
+            correction = self.apcorr_correction[i]
+            for col in cols_to_correct:
+                row[col] *= correction
+
+def match_wavelengths(self, wavelength_ifu):
+        # given the ifu wavelength value - redefine the apcor func and radius to this wavelength
+        # apcor reference data
+        self.wavelength = self.reference.wavelength.flatten()
+        self.size = self.reference.radius
+        self.apcorr = self.reference.apcorr
+
+        dim = self.apcorr.shape[0]
+        size_match = np.zeros((dim, wavelength_ifu.shape[0]))
+        apcorr_match  = np.zeros((dim, wavelength_ifu.shape[0]))
+        self.apcorr_correction= [] # set up here defined in find_apcor_func
+        # loop over each radius dependent plane and interpolate to ifu wavelength
+        for i in range (dim):
+            radi = self.size[i,:]
+            frad = interp1d(self.wavelength, radi, bounds_error=False, fill_value="extrapolate")
+            radius_match = frad(wavelength_ifu)
+            size_match[i,:] = radius_match
+
+            appi = self.apcorr[i,:]
+            fap = interp1d(self.wavelength, appi, bounds_error=False, fill_value="extrapolate")
+            ap_match = fap(wavelength_ifu)
+            apcorr_match[i,:] = ap_match
+
+        self.apcorr = apcorr_match
+        self.size = size_match
+
+
+    def find_apcorr_func(self, iwave, radius_ifu):
+        # at ifu wavelength plane (iwave), the extraction radius is radius_ifu
+        # pull out the radius values (self.size)  to use in the apcor ref file for this iwave
+        # self.size and self.apcorr have already been interpolated in wavelength to match the
+        # the ifu wavelength range.
+
+        radius_apcor = self.size[:,iwave]
+        temparray=self.apcorr[:,iwave]
+        fap=interp1d(radius_apcor,temparray,fill_value="extrapolate")
+        correction=fap(radius_ifu)
+        self.apcorr_correction.append(correction)
+        return
 
 
 class ApCorr(ApCorrBase):

jwst/extract_1d/ifu.py
@@ -8,6 +8,8 @@ from photutils import CircularAperture, CircularAnnulus, \
                       RectangularAperture, aperture_photometry
 
 from .apply_apcorr import select_apcorr
+from ..assign_wcs.util import compute_scale
+
 from .. import datamodels
 from ..datamodels import dqflags
 from . import spec_wcs
@@ -95,10 +97,9 @@ def ifu_extract1d(input_model, ref_dict, source_type, subtract_background, apcor
                      "the source is extended.")
         extract_params['subtract_background'] = subtract_background
 
-    #print('extract_params',extract_params, extract_params['ref_file_type'])
     if extract_params:
         if extract_params['ref_file_type'] == FILE_TYPE_TABLE:
-            (ra, dec, wavelength, temp_flux, background, npixels, dq, npixels_bkg) = \
+            (ra, dec, wavelength, temp_flux, background, npixels, dq, npixels_bkg, radius_match) = \
                     extract_ifu(input_model, source_type, extract_params)
         elif extract_params['ref_file_type'] == FILE_TYPE_JSON:
             (ra, dec, wavelength, temp_flux, background, npixels, dq, npixels_bkg) = \
@@ -171,6 +172,14 @@ def ifu_extract1d(input_model, ref_dict, source_type, subtract_background, apcor
             input_model, apcorr_ref_model.apcorr_table, apcorr_ref_model.sizeunit, location=(ra, dec, wl)
         )
 
+        # determine apcor function to use at each wavelength - Move this in to apply_apcorr but
+        # does not work for method approximate because as input we need to know the wavelength and radius
+        # of the extract location we are at. 
+        for i, wave in enumerate(wavelength):
+            radius = radius_match[i]
+            func = apcorr.find_apcorr_func(wave,radius)
+
+
         apcorr.apply(spec.spec_table)
 
     output_model.spec.append(spec)
@@ -179,6 +188,7 @@ def ifu_extract1d(input_model, ref_dict, source_type, subtract_background, apcor
     output_model.meta.wcs = None
 
     return output_model
+
 
 
 def get_extract_parameters(ref_dict, slitname):
@@ -353,6 +363,7 @@ def extract_ifu(input_model, source_type, extract_params):
         ra_targ = input_model.meta.target.ra
         dec_targ = input_model.meta.target.dec
         locn = locn_from_wcs(input_model, ra_targ, dec_targ)
+        
         if locn is None or np.isnan(locn[0]):
             log.warning("Couldn't determine pixel location from WCS, so "
                         "source offset correction will not be applied.")
@@ -368,10 +379,7 @@ def extract_ifu(input_model, source_type, extract_params):
     method = extract_params['method']
     # subpixels is only needed if method = 'subpixel'.
     subpixels = extract_params['subpixels']
-
     subtract_background = extract_params['subtract_background']
-
-    #print('extract params',subpixels,subtract_background)
 
     smaller_axis = float(min(shape[-2], shape[-1]))     # for defaults
     radius = None
@@ -389,7 +397,7 @@ def extract_ifu(input_model, source_type, extract_params):
     # find the wavelength array for the IFU cube 
     (ra, dec, wavelength) = get_coordinates(input_model, x0, y0)
 
-    print('wavelength endpoints ifu cube',wavelength[0], wavelength[-1])
+    #print('wavelength endpoints ifu cube',wavelength[0], wavelength[-1])
     # interpolate the extraction parameters to the wavelength of the IFU cube
     if source_type == 'POINT':
         wave_extract = extract_params['wavelength'].flatten()
@@ -397,27 +405,31 @@ def extract_ifu(input_model, source_type, extract_params):
         outer_bkg = extract_params['outer_bkg'].flatten()
         radius = extract_params['radius'].flatten()
         axis_pa = extract_params['axis_pa'].flatten()
-        print('wavelength table',wave_extract[0],wave_extract[-1])
+
         frad = interp1d(wave_extract, radius, bounds_error=False, fill_value="extrapolate")
         radius_match = frad(wavelength)
+        # radius_match is in arc seconds - need to convert to pixels
+        
+        # QUESTION should I compute the scale at each wavelength since radius varies with wavelength
+        # FIX LATER - probably for point locn is not NONE but for my test data it was (I forced EXTENDED = POINT)
+        if locn is None:
+            locn_use = (input_model.meta.wcsinfo.crval1, input_model.meta.wcsinfo.crval2,wavelength[0])
+        else:
+            locn_use = locn
+        scale_degrees =  compute_scale(
+            input_model.meta.wcs,
+            locn_use,
+            disp_axis=input_model.meta.wcsinfo.dispersion_direction)
+
+        scale_arcsec = scale_degrees*3600.00
+
+        radius_match /= scale_arcsec
 
         finner = interp1d(wave_extract, inner_bkg, bounds_error=False, fill_value="extrapolate")
-        inner_bkg_match = finner(wavelength)
+        inner_bkg_match = finner(wavelength)/scale_arcsec
 
         fouter = interp1d(wave_extract, outer_bkg, bounds_error=False, fill_value="extrapolate")
-        outer_bkg_match = fouter(wavelength)
-
-    #    radius = extract_params['radius']
-    #    if radius is None:
-    #        radius = smaller_axis / 4.
-    #    if subtract_background:
-    #        inner_bkg = extract_params['inner_bkg']
-    #        if inner_bkg is None:
-    #            inner_bkg = radius
-    #        outer_bkg = extract_params['outer_bkg']
-    #        if outer_bkg is None:
-    #            outer_bkg = min(inner_bkg * math.sqrt(2.),
-    #                            smaller_axis / 2. - 1.)
+        outer_bkg_match = fouter(wavelength)/scale_arcsec
 
     elif  source_type == 'EXTENDED':
         # Ignore any input parameters, and extract the whole image.
@@ -445,7 +457,6 @@ def extract_ifu(input_model, source_type, extract_params):
         if method == "subpixel":
             log.debug("  subpixels = %s", str(subpixels))
 
-
     position = (x_center, y_center)
 
     # get aperture for extended it will not change with wavelength
@@ -458,7 +469,7 @@ def extract_ifu(input_model, source_type, extract_params):
         outer_bkg = None
         
         if source_type == 'POINT':
-            radius = radius_match[k]
+            radius = radius_match[k] # this radius has been converted to pixels
             aperture = CircularAperture(position, r=radius)
 
             inner_bkg = inner_bkg_match[k]
@@ -544,7 +555,7 @@ def extract_ifu(input_model, source_type, extract_params):
     (wavelength, temp_flux, background, npixels, dq, npixels_annulus) = \
         nans_in_wavelength(wavelength, temp_flux, background, npixels, dq, npixels_annulus)
 
-    return (ra, dec, wavelength, temp_flux, background, npixels, dq, npixels_annulus)
+    return (ra, dec, wavelength, temp_flux, background, npixels, dq, npixels_annulus, radius_match)
 
 
 def locn_from_wcs(input_model, ra_targ, dec_targ):