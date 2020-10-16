1ecff620199566adf6b7c3bc78984d4255cc4fe8
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2020-10-16 11:43:31 -0400

    Remove duplicate test

--- jwst/regtest/test_miri_mrs.py
+++ jwst/regtest/test_miri_mrs.py
@@ -191,38 +191,3 @@ def test_miri_mrs_wcs(run_spec2, fitsdiff_default_kwargs):
         xtruth, ytruth = im_truth.meta.wcs.backward_transform(ratruth, dectruth, lamtruth)
         assert_allclose(xtest, xtruth)
         assert_allclose(ytest, ytruth)
-
-
-@pytest.fixture(scope='module')
-def run_cube_build_single_output(rtdata_module):
-    """Run cube_build on multiple inputs but single output"""
-    rtdata = rtdata_module
-    rtdata.get_asn('miri/mrs/two_spec3_asn.json')
-
-    args = [
-        'jwst.cube_build.CubeBuildStep',
-        rtdata.input,
-        '--save_results=true'
-    ]
-    Step.from_cmdline(args)
-
-    return rtdata
-
-
-@pytest.mark.bigdata
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    'output',
-    ['outtest_ch1-short_s3d.fits', 'outtest_ch2-short_s3d.fits']
-)
-def test_cube_build_single_output(run_cube_build_single_output, output, fitsdiff_default_kwargs):
-    """Test just running cube build and ensure that output happens"""
-    rtdata = run_cube_build_single_output
-    rtdata.output = output
-
-    # Get the truth files
-    rtdata.get_truth('truth/test_miri_mrs/' +  output)
-
-    # Compare the results
-    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
-    assert diff.identical, diff.report()
