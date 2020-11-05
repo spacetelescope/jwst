ddb5cf96f504c42f1131e553de940960f8782335
James Davies <jdavies@stsci.edu>
2020-11-05 02:55:20 -0500

    Clean up msaflagopen tests

--- jwst/msaflagopen/tests/test_msa_open.py
+++ jwst/msaflagopen/tests/test_msa_open.py
@@ -1,25 +1,31 @@
 import numpy as np
+from numpy.testing import assert_array_equal
 import os
 
 from jwst.assign_wcs import AssignWcsStep
-from jwst.datamodels import ImageModel
-from jwst.msaflagopen.msaflag_open import (boundingbox_to_indices,
-                                           create_slitlets,
-                                           flag,
-                                           get_failed_open_shutters,
-                                           id_from_xy,
-                                           or_subarray_with_array,
-                                           wcs_to_dq)
-from jwst.msaflagopen.msaflagopen_step import create_reference_filename_dictionary
+from jwst.datamodels import ImageModel, dqflags
+from jwst.msaflagopen.msaflag_open import (
+    boundingbox_to_indices,
+    create_slitlets,
+    get_failed_open_shutters,
+    id_from_xy,
+    or_subarray_with_array,
+    wcs_to_dq
+)
+from jwst.msaflagopen import MSAFlagOpenStep
 from jwst.assign_wcs.tests import data
 from jwst.transforms.models import Slit
 from jwst.stpipe.step import Step
 
-data_path = os.path.split(os.path.abspath(data.__file__))[0]
+
+MSA_FAILED_OPEN = dqflags.pixel["MSA_FAILED_OPEN"]
+
 
 def get_file_path(filename):
     """Construct an absolute path."""
+    data_path = os.path.abspath(os.path.dirname(data.__file__))
     return os.path.join(data_path, filename)
+
 
 def test_or_subarray_with_array():
     """test bitwise or with array and subarray."""
@@ -37,7 +43,8 @@ def test_or_subarray_with_array():
     # array since we wont be using it anymore in this test.
     dq_array[ymin:ymax, xmin:xmax] = dq_array[ymin:ymax, xmin:xmax] | dq_subarray
 
-    assert np.array_equal(result, dq_array)
+    assert_array_equal(result, dq_array)
+
 
 def test_id_from_xy():
     """Test id from x y location of shutter"""
@@ -54,7 +61,8 @@ def test_id_from_xy():
 
     result = id_from_xy(x, y)
 
-    assert (x + (y-1)*shutters_per_row == result)
+    assert x + (y - 1) * shutters_per_row == result
+
 
 def test_get_failed_open_shutters():
     """test that failed open shutters are returned from reference file"""
@@ -72,9 +80,10 @@ def test_get_failed_open_shutters():
     # get_failed_open_shutters returns 3 flaggable states
     # state, Internal state, and TA state.
     for shutter in result:
-        assert (shutter['state'] == 'open' or
-                shutter['Internal state'] == 'open' or
-                shutter['TA state'] == 'open')
+        assert shutter['state'] == 'open' or \
+                shutter['Internal state'] == 'open' or \
+                shutter['TA state'] == 'open'
+
 
 def test_create_slitlets():
     """Test that slitlets are Slit type and have all the necessary fields"""
@@ -97,38 +106,39 @@ def test_create_slitlets():
         assert type(slit) == Slit
         assert slit._fields == slit_fields
 
+
 def test_wcs_to_dq():
     """Test that non nan values are assigned the values of flags"""
 
     # Make data array
-    wcs_array = np.random.randn(100,100)
+    grid = np.zeros((10, 10))
+    wcs_array = np.array((grid, grid))
 
     # Put in some nans randomly in the data.
-    wcs_array.ravel()[np.random.choice(wcs_array.size, 100, replace=False)] = np.nan
+    wcs_array.ravel()[np.random.choice(wcs_array.size, 10, replace=False)] = np.nan
+    nans = np.isnan(wcs_array[0])
 
-    FLAG = 1024
-    result = wcs_to_dq(wcs_array, FLAG)
+    result = wcs_to_dq(wcs_array, MSA_FAILED_OPEN)
 
-    nans = np.where(np.isnan(wcs_array[0]))
-    non_nan = np.where(~np.isnan(wcs_array[0]))
     # wcs_to_dq create an array of zeros and if nans are present they
     # will have value zero where are non nan elements will have the value
     # of FLAG.
-    assert (all(x == 0 for x in result[nans]))
-    assert (all(x == FLAG for x in result[non_nan]))
+    assert_array_equal(result[nans], 0)
+    assert_array_equal(result[~nans], MSA_FAILED_OPEN)
+
 
 def test_boundingbox_from_indices():
-    dm = ImageModel()
-    dm.data = np.ones((10,10))
-
+    dm = ImageModel((10,10))
     bbox = ((1,2), (3,4))
 
     result = boundingbox_to_indices(dm, bbox)
 
-    assert (result == (1, 3, 3, 5))
+    assert result == (1, 3, 3, 5)
 
-def test_flag():
-    wcsinfo = {
+
+def test_msaflagopen_step():
+    im = ImageModel((2048, 2048))
+    im.meta.wcsinfo = {
         'dec_ref': -0.00601415671349804,
         'ra_ref': -0.02073605215697509,
         'roll_ref': -0.0,
@@ -137,7 +147,7 @@ def test_flag():
         'v3yangle': 0.0,
         'vparity': -1}
 
-    instrument = {
+    im.meta.instrument = {
         'detector': 'NRS1',
         'filter': 'F100LP',
         'grating': 'G140M',
@@ -145,13 +155,13 @@ def test_flag():
         'gwa_tilt': 37.0610,
         'gwa_xtilt': 0.0001,
         'gwa_ytilt': 0.0001,
-        'msa_metadata_id':12}
+        'msa_metadata_id': 12}
 
-    observation = {
+    im.meta.observation = {
         'date': '2016-09-05',
         'time': '8:59:37'}
 
-    exposure = {
+    im.meta.exposure = {
         'duration': 11.805952,
         'end_time': 58119.85416,
         'exposure_time': 11.776,
@@ -169,36 +179,11 @@ def test_flag():
         'start_time': 58119.8333,
         'type': 'NRS_MSASPEC',
         'zero_frame': False}
-
-    im = ImageModel()
-    im.data = np.random.rand(2048, 2048)
-    im.error = np.random.rand(2048, 2048)
-    im.dq = np.zeros((2048, 2048))
-
-    im.meta.wcsinfo._instance.update(wcsinfo)
-    im.meta.instrument._instance.update(instrument)
-    im.meta.observation._instance.update(observation)
-    im.meta.exposure._instance.update(exposure)
-
-    metafl = get_file_path('msa_configuration.fits')
-    im.meta.instrument.msa_metadata_file = metafl
+    im.meta.instrument.msa_metadata_file = get_file_path('msa_configuration.fits')
     im.meta.dither.position_number = 1
 
-    step = AssignWcsStep()
-    msa_oper = step.get_reference_file(im, 'msaoper')
+    im = AssignWcsStep.call(im)
+    result = MSAFlagOpenStep.call(im)
 
-    im = step.call(im)
-
-    wcs_ref_files = create_reference_filename_dictionary(im)
-
-    failed_slitlets = create_slitlets(im, msa_oper)
-
-    result = flag(im, failed_slitlets, wcs_ref_files)
-
-    msa_open_dq = 536870912
-
-    # Get all dqflags that are nonzero
     nonzero = np.nonzero(result.dq)
-
-    # Make sure that all nonzero dqs are equal to the msa_open_dq
-    assert (all(x == msa_open_dq for x in result.dq[nonzero]))
+    assert_array_equal(result.dq[nonzero], MSA_FAILED_OPEN)
