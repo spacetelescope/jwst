9c0e1bd25f9f95ed0c6ce63211eb2b1f37179514
Tyler Pauly <tapastro@gmail.com>
2021-01-04 15:34:58 -0500

    Attempt to hide blatant plagiarism from multispec.py

--- jwst/datamodels/multicombinedspec.py
+++ jwst/datamodels/multicombinedspec.py
@@ -12,16 +12,16 @@ class MultiCombinedSpecModel(JwstDataModel):
     This model has a special member `spec` that can be used to
     deal with an entire spectrum at a time.  It behaves like a list::
 
-       >>> from . import SpecModel
-       >>> multispec_model = MultiSpecModel()
-       >>> multispec_model.spec.append(SpecModel())
+       >>> from . import CombinedSpecModel
+       >>> multispec_model = MultiCombinedSpecModel()
+       >>> multispec_model.spec.append(CombinedSpecModel())
        >>> multispec_model.spec[0] # doctest: +SKIP
-       <SpecModel>
+       <CombinedSpecModel>
 
-    If `init` is a `SpecModel` instance, an empty `SpecModel` will be
-    created and assigned to attribute `spec[0]`, and the `spec_table`
-    attribute from the input `SpecModel` instance will be copied to
-    the first element of `spec`.  `SpecModel` objects can be appended
+    If `init` is a `CombinedSpecModel` instance, an empty `CombinedSpecModel`
+    will be created and assigned to attribute `spec[0]`, and the `spec_table`
+    attribute from the input `CombinedSpecModel` instance will be copied to
+    the first element of `spec`.  `CombinedSpecModel` objects can be appended
     to the `spec` attribute by using its `append` method.
 
     Parameters
@@ -32,19 +32,6 @@ class MultiCombinedSpecModel(JwstDataModel):
     spec.items.spec_table : numpy table
          Extracted spectral data table
 
-    Examples
-    --------
-    >>> output_model = MultiSpecModel()
-    >>> spec = SpecModel()       # for the default data type
-    >>> for slit in input_model.slits:  # doctest: +SKIP
-    ...     slitname = slit.name
-    ...     slitmodel = ExtractModel()
-    ...     slitmodel.fromJSONFile(extref, slitname)
-    ...     column, wavelength, countrate = slitmodel.extract(slit.data)
-    ...     otab = np.array(zip(column, wavelength, countrate),
-    ...                     dtype=spec.spec_table.dtype)
-    ...     spec = datamodels.SpecModel(spec_table=otab)
-    ...     output_model.spec.append(spec)
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/multicombinedspec.schema"
 
