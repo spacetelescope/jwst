759ab2f5e899621f66c6c20ad9b4fb1b24efdbc9
Ed Slavich <eslavich@stsci.edu>
2021-01-14 11:31:43 -0500

    JwstPipeline must inherit from JwstStep

--- jwst/stpipe/jwst.py
+++ jwst/stpipe/jwst.py
@@ -12,7 +12,10 @@ class JwstStep(Step):
         return datamodels.open(init, **kwargs)
 
 
-class JwstPipeline(Pipeline):
-    @classmethod
-    def datamodels_open(cls, init, **kwargs):
-        return datamodels.open(init, **kwargs)
+# JwstPipeline needs to inherit from Pipeline, but also
+# be a subclass of JwstStep so that it will pass checks
+# when constructing a pipeline using JwstStep class methods.
+# It's important that Pipeline occur first so that it
+# takes precedence in the resolution order.
+class JwstPipeline(Pipeline, JwstStep):
+    pass
