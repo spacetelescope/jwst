4e82712528ea45d9874b77012888d4c6d0893871
Mihai Cara <mihail.cara@gmail.com>
2021-01-26 21:19:51 -0500

    Change offsets to apparent position. Combine functions

--- jwst/lib/set_velocity_aberration.py
+++ jwst/lib/set_velocity_aberration.py
@@ -21,21 +21,19 @@ in the header other than what is required by the standard.
 import logging
 import numpy as np
 import astropy.io.fits as fits
-from gwcs.geometry import SphericalToCartesian
+from gwcs.geometry import SphericalToCartesian, CartesianToSpherical
 from scipy.constants import speed_of_light
-import math
 
 # Configure logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-SPEED_OF_LIGHT = speed_of_light / 1000     # km / s
-d_to_r = math.pi / 180.
+SPEED_OF_LIGHT = speed_of_light / 1000  # km / s
 
 
-def aberration_scale(velocity_x, velocity_y, velocity_z,
-                     targ_ra, targ_dec):
-    """Compute the scale factor due to velocity aberration.
+def compute_va_effects(velocity_x, velocity_y, velocity_z, targ_ra, targ_dec):
+    """ Computes constant scale factor due to velocity aberration as well as
+    corrected ``RA`` and ``DEC`` values.
 
     Parameters
     ----------
@@ -47,8 +45,9 @@ def aberration_scale(velocity_x, velocity_y, velocity_z,
 
     targ_ra, targ_dec: float
         The right ascension and declination of the target (or some other
-        point, such as the center of a detector).  The equator and equinox
-        should be the same as the coordinate system for the velocity.
+        point, such as the center of a detector) in the barycentric coordinate
+        system.  The equator and equinox should be the same as the coordinate
+        system for the velocity.
 
     Returns
     -------
@@ -56,59 +55,40 @@ def aberration_scale(velocity_x, velocity_y, velocity_z,
         Multiply the nominal image scale (e.g. in degrees per pixel) by
         this value to obtain the image scale corrected for the "aberration
         of starlight" due to the velocity of JWST with respect to the Sun.
-    """
-    beta = np.array([velocity_x, velocity_y, velocity_z]) / SPEED_OF_LIGHT
-    beta2 = np.dot(beta, beta)
-    u = SphericalToCartesian()(targ_ra, targ_dec)
-    beta_u =  np.dot(beta, u)
-    igamma = np.sqrt(1.0 - beta2)  # inverse of usual gamma
-    scale_factor = (1.0 + beta_u) /  igamma
-    return scale_factor
 
+    apparent_ra: float
+        Aparent star position in the moving telescope frame.
 
-def aberration_offset(velocity_x, velocity_y, velocity_z, targ_ra, targ_dec):
-    """Compute the RA/Dec offsets due to velocity aberration.
-
-    Parameters
-    ----------
-    velocity_x, velocity_y, velocity_z: float
-        The components of the velocity of JWST, in km / s with respect to
-        the Sun.  These are celestial coordinates, with x toward the
-        vernal equinox, y toward right ascension 90 degrees and declination
-        0, z toward the north celestial pole.
-
-    targ_ra, targ_dec: float
-        The right ascension and declination of the target (or some other
-        point, such as the center of a detector).  The equator and equinox
-        should be the same as the coordinate system for the velocity.
-
-    Returns
-    -------
-    delta_ra, delta_dec: float
-        The offset to be added to the input RA/Dec, in units of radians.
+    apparent_dec: float
+        Aparent star position in the moving telescope frame.
 
     """
-    # Algorithm from Colin Cox notebook
     beta = np.array([velocity_x, velocity_y, velocity_z]) / SPEED_OF_LIGHT
-    u = np.array(SphericalToCartesian()(targ_ra, targ_dec))
     beta2 = np.dot(beta, beta)
     if beta2 == 0.0:
-        return 0.0, 0.0
+        return 1.0, targ_ra, targ_dec
+
+    u = np.asanyarray(SphericalToCartesian()(targ_ra, targ_dec))
+    beta_u =  np.dot(beta, u)
     igamma = np.sqrt(1.0 - beta2)  # inverse of usual gamma
-    beta_u = np.dot(beta, u)
+    scale_factor = (1.0 + beta_u) / igamma
+
+    # Algorithm below is from Colin Cox notebook.
+    # Also see: Instrument Science Report OSG-CAL-97-06 by Colin Cox (1997).
     u_corr = (igamma * u + beta * (1.0 + (1.0 - igamma) * beta_u / beta2)) / (1.0 + beta_u)
-    ra_corr = np.arctan2(u_corr[1], u_corr[0])
-    dec_corr = np.arcsin(u_corr[2])
-    return ra_corr - np.deg2rad(targ_ra), dec_corr -  np.deg2rad(targ_dec)
+
+    apparent_ra, apparent_dec = CartesianToSpherical()(*u_corr)
+    return scale_factor, apparent_ra, apparent_dec
 
 
 def add_dva(filename):
-    '''
+    """
     Given the name of a valid partially populated level 1b JWST file,
-    determine the velocity aberration scale factor.
+    determine the velocity aberration scale factor and apparent target position
+    in the moving (telescope) frame.
 
     It presumes all the accessed keywords are present (see first block).
-    '''
+    """
     hdulist = fits.open(filename, 'update')
     pheader = hdulist[0].header
     sheader = hdulist['SCI'].header
@@ -119,14 +99,13 @@ def add_dva(filename):
     dec_ref = float(sheader['DEC_REF'])
 
     # compute the velocity aberration information
-    scale_factor = aberration_scale(jwst_dx, jwst_dy, jwst_dz,
-                                    ra_ref, dec_ref)
-    ra_off, dec_off = aberration_offset(jwst_dx, jwst_dy, jwst_dz,
-                                        ra_ref, dec_ref)
+    scale_factor, apparent_ra, apparent_dec = compute_va_effects(
+        jwst_dx, jwst_dy, jwst_dz, ra_ref, dec_ref
+    )
 
     # update header
-    pheader['DVA_RA'] = ra_off
-    pheader['DVA_DEC'] = dec_off
+    pheader['VA_RA'] = apparent_ra
+    pheader['VA_DEC'] = apparent_dec
     sheader['VA_SCALE'] = scale_factor
     hdulist.flush()
     hdulist.close()
