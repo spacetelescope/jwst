4b8fc034ebd72fd09294b8ece24707b909a1769a
aliciacanipe <acanipe@stsci.edu>
2021-01-27 13:07:47 -0500

    Updating conftest.py to write docstring with test
    I was hoping to test a plug-in for pytest that would print the docstring in a human-readable format for reporting purposes. INS is moving toward automated testing reports and it would be helpful to have this functionality for part of the process and generating a summary testing report document.

--- jwst/conftest.py
+++ jwst/conftest.py
@@ -2,6 +2,7 @@
 import os
 import tempfile
 import pytest
+import inspect
 
 from jwst.associations import (AssociationRegistry, AssociationPool)
 from jwst.associations.tests.helpers import t_path
@@ -73,3 +74,27 @@ def jail(request, tmpdir_factory):
     os.chdir(str(newpath))
     yield newpath
     os.chdir(old_dir)
+
+@pytest.mark.trylast
+def pytest_configure(config):
+    terminal_reporter = config.pluginmanager.getplugin('terminalreporter')
+    config.pluginmanager.register(TestDescriptionPlugin(terminal_reporter), 'testdescription')
+
+class TestDescriptionPlugin:
+
+    def __init__(self, terminal_reporter):
+        self.terminal_reporter = terminal_reporter
+        self.desc = None
+
+    def pytest_runtest_protocol(self, item):
+        self.desc = inspect.getdoc(item.obj)
+
+    @pytest.hookimpl(hookwrapper=True, tryfirst=True)
+    def pytest_runtest_logstart(self, nodeid, location):
+        if self.terminal_reporter.verbosity == 0:
+            yield
+        else:
+            self.terminal_reporter.write('\n')
+            yield
+            if self.desc:
+                    self.terminal_reporter.write(f'\n{self.desc} ')
