d460fa68798d362fb27b438a905934a0dd4ca0d2
Ed Slavich <eslavich@stsci.edu>
2021-02-04 16:20:58 -0500

    Fix wayward import in test_ami_interface.py

--- jwst/ami/tests/test_ami_interface.py
+++ jwst/ami/tests/test_ami_interface.py
@@ -1,5 +1,7 @@
 import pytest
 
+import stpipe
+
 from jwst import datamodels
 from jwst.ami import AmiAnalyzeStep
 import jwst
@@ -37,7 +39,7 @@ def test_ami_analyze_no_reffile_fail(monkeypatch):
 
     def mockreturn(input_model, reftype, observatory=None, asn_exptypes=None):
         return("N/A")
-    monkeypatch.setattr(jwst.stpipe.crds_client, 'get_reference_file', mockreturn)
+    monkeypatch.setattr(stpipe.crds_client, 'get_reference_file', mockreturn)
 
     with pytest.raises(RuntimeError):
         AmiAnalyzeStep.call(model)
