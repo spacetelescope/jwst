99c4ae92aff34bc854582d0f1b927798d1d1d3fe
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-02-05 10:05:38 -0500

    Add unit test for duplicate names

--- jwst/associations/tests/test_level3_duplicate.py
+++ jwst/associations/tests/test_level3_duplicate.py
@@ -1,4 +1,5 @@
 """Test for duplication/missing associations"""
+import pytest
 
 from .helpers import (
     level3_rule_path,
@@ -8,6 +9,21 @@ from .helpers import (
 
 from .. import (AssociationPool, generate)
 from ..main import (Main, constrain_on_candidates)
+
+
+def test_duplicate_names():
+    """Ensure errors are generated for duplicate names
+
+    For Level 3 association, there should be no association
+    with the same product name. Generation should generate
+    an `AssociationDuplicateProductName` error in this situation.
+    """
+    pool = AssociationPool.read(t_path('data/jw00632_dups.csv'))
+    constrain_all_candidates = constrain_on_candidates(None)
+    rules = registry_level3_only(global_constraints=constrain_all_candidates)
+
+    with pytest.raises(AssociationDuplicateProductName):
+        asns = generate(pool, rules)
 
 
 def test_duplicate_generate():
