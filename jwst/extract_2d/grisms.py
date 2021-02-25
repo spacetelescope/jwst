32521f0dc9db39ccc099df732944b5626518759b
jemorrison <morrison@as.arizona.edu>
2021-02-25 15:45:39 -0700

    set value of SRCTYPE for NIRISS WFSS

--- jwst/extract_2d/grisms.py
+++ jwst/extract_2d/grisms.py
@@ -476,7 +476,11 @@ def extract_grism_objects(input_model,
                 # The overall subarray offset is recorded in model.meta.subarray.
                 # nslit = obj.sid - 1  # catalog id starts at zero
                 new_slit.name = "{0}".format(obj.sid)
-                new_slit.source_type = 'UNKNOWN'
+                #new_slit.source_type = 'UNKNOWN'
+                if obj.is_star:
+                    new_slit.source_type = 'POINT'
+                else:
+                    new_slit.source_type = 'EXTENDED'
                 new_slit.xstart = xmin + 1  # fits pixels
                 new_slit.xsize = ext_data.shape[1]
                 new_slit.ystart = ymin + 1  # fits pixels
