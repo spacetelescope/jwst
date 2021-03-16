9cee97d137974ce4ec770a70fa570ad04906bb43
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-03-16 13:18:16 -0400

    Update test to not use config file

--- jwst/regtest/test_miri_image.py
+++ jwst/regtest/test_miri_image.py
@@ -61,7 +61,7 @@ def run_image3(run_image2, rtdata_module):
     image3 pipeline on all _cal files listed in association"""
     rtdata = rtdata_module
     rtdata.get_data("miri/image/det_dithered_5stars_image3_asn.json")
-    args = ["config/calwebb_image3.cfg", rtdata.input,
+    args = ["jwst.pipeline.Image3Pipeline", rtdata.input,
             # Set some unique param values needed for these data
             "--steps.tweakreg.snr_threshold=200",
             "--steps.tweakreg.use2dhist=False",
