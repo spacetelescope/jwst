7baf5a1d4e3b94028514349d4f71c18eed119b22
James Davies <jdavies@stsci.edu>
2021-05-15 23:56:03 -0400

    Split single resample from resample+coadd; fix total exptime calc

--- jwst/resample/resample.py
+++ jwst/resample/resample.py
@@ -1,8 +1,6 @@
 import logging
 import re
 
-import numpy as np
-
 from . import gwcs_drizzle
 from . import resample_utils
 from .. import datamodels
@@ -75,8 +73,9 @@ class ResampleData:
         )
         if not can_allocate:
             raise OutputTooLargeError(
-                f'Combined ImageModel size {self.output_wcs.data_size} requires {bytes2human(required_memory)}'
-                f'\nModel cannot be instantiated.'
+                f'Combined ImageModel size {self.output_wcs.data_size} '
+                f'requires {bytes2human(required_memory)}. '
+                f'Model cannot be instantiated.'
             )
         self.blank_output = datamodels.ImageModel(self.output_wcs.data_size)
 
@@ -87,84 +86,25 @@ class ResampleData:
 
         self.output_models = datamodels.ModelContainer()
 
-    def update_driz_outputs(self):
-        """ Define output arrays for use with drizzle operations.
-        """
-        numchips = len(self.input_models)
-        numplanes = (numchips // 32) + 1
+    # TODO: Following method is not used, and never has been.  Find out if needed
+    # def update_driz_outputs(self):
+    #     """ Define output arrays for use with drizzle operations.
+    #     """
+    #     numchips = len(self.input_models)
+    #     numplanes = (numchips // 32) + 1
 
-        # Replace CONTEXT array with full set of planes needed for all inputs
-        outcon = np.zeros((numplanes, self.output_wcs.data_size[0],
-                           self.output_wcs.data_size[1]), dtype=np.int32)
-        self.blank_output.con = outcon
+    #     # Replace CONTEXT array with full set of planes needed for all inputs
+    #     outcon = np.zeros((numplanes, self.output_wcs.data_size[0],
+    #                        self.output_wcs.data_size[1]), dtype=np.int32)
+    #     self.blank_output.con = outcon
 
     def do_drizzle(self):
-        """ Perform drizzling operation on input images's to create a new output
+        """Perform drizzling operation on input images's to create a new output
         """
-        # Look for input configuration parameter telling the code to run
-        # in single-drizzle mode (mosaic all detectors in a single observation)
         if self.single:
-            driz_outputs = self.input_models.group_names
-            exposures = self.input_models.models_grouped
-            group_exptime = []
-            for exposure in exposures:
-                group_exptime.append(exposure[0].meta.exposure.exposure_time)
+            return self.resample_many_to_many()
         else:
-            driz_outputs = [self.output_filename]
-            exposures = [self.input_models]
-
-            total_exposure_time = 0.0
-            for exposure in exposures:
-                total_exposure_time += exposure[0].meta.exposure.exposure_time
-            group_exptime = [total_exposure_time]
-        pointings = len(self.input_models.group_names)
-
-        for obs_product, exposure, texptime in zip(driz_outputs, exposures,
-                                                   group_exptime):
-            output_model = self.blank_output.copy()
-            output_model.meta.filename = obs_product
-
-            if self.blendheaders:
-                self.blend_output_metadata(output_model)
-
-            exposure_times = {'start': [], 'end': []}
-
-            # Initialize the output with the wcs
-            driz = gwcs_drizzle.GWCSDrizzle(output_model,
-                                            pixfrac=self.pixfrac,
-                                            kernel=self.kernel,
-                                            fillval=self.fillval)
-
-            for n, img_exp in enumerate(exposure):
-                # Make a copy as this is operated on in-place below
-                img = img_exp.copy()
-                exposure_times['start'].append(img.meta.exposure.start_time)
-                exposure_times['end'].append(img.meta.exposure.end_time)
-
-                # apply sky subtraction
-                blevel = img.meta.background.level
-                if not img.meta.background.subtracted and blevel is not None:
-                    img.data -= blevel
-
-                inwht = resample_utils.build_driz_weight(img,
-                                                         weight_type=self.weight_type,
-                                                         good_bits=self.good_bits)
-                driz.add_image(img.data, img.meta.wcs, inwht=inwht,
-                               expin=img.meta.exposure.exposure_time)
-
-            # Update some basic exposure time values based on all the inputs
-            output_model.meta.exposure.exposure_time = texptime
-            output_model.meta.exposure.start_time = min(exposure_times['start'])
-            output_model.meta.exposure.end_time = max(exposure_times['end'])
-            output_model.meta.resample.product_exposure_time = texptime
-            output_model.meta.resample.weight_type = self.weight_type
-            output_model.meta.resample.pointings = pointings
-
-            self.update_fits_wcs(output_model)
-
-            self.output_models.append(output_model)
-
-        return self.output_models
+            return self.resample_many_to_one()
 
     def blend_output_metadata(self, output_model):
         """Create new output metadata based on blending all input metadata."""
@@ -174,6 +114,90 @@ class ResampleData:
         log.info('Blending metadata for {}'.format(output_file))
         blendmeta.blendmodels(output_model, inputs=self.input_models,
                               output=output_file)
+
+    def resample_many_to_many(self):
+        """Resample many inputs to many outputs where outputs have a common frame.
+
+        Coadd only different detectors of the same exposure, i.e. map NRCA5 and
+        NRCB5 onto the same output image, as they image different areas of the
+        sky.
+
+        Used for outlier detection
+        """
+        for exposure in self.input_models.models_grouped:
+            output_model = self.blank_output.copy()
+            output_model.update(exposure, only="PRIMARY")
+
+            # Initialize the output with the wcs
+            driz = gwcs_drizzle.GWCSDrizzle(output_model, pixfrac=self.pixfrac,
+                                            kernel=self.kernel, fillval=self.fillval)
+            for img in exposure:
+                # TODO: should weight_type=None here?
+                inwht = resample_utils.build_driz_weight(img, weight_type=self.weight_type,
+                                                         good_bits=self.good_bits)
+                # apply sky subtraction
+                blevel = img.meta.background.level
+                if not img.meta.background.subtracted and blevel is not None:
+                    data = img.data - blevel
+                else:
+                    data = img.data
+
+                driz.add_image(data, img.meta.wcs, inwht=inwht)
+
+            self.output_models.append(output_model)
+
+        return self.output_models
+
+    def resample_many_to_one(self):
+        """Resample and coadd many inputs to a single output.
+
+        Used for stage 3 resampling
+        """
+        output_model = self.blank_output.copy()
+        output_model.meta.filename = self.output_filename
+        output_model.meta.resample.weight_type = self.weight_type
+        output_model.meta.resample.pointings = len(self.input_models.group_names)
+
+        if self.blendheaders:
+            self.blend_output_metadata(output_model)
+
+        # Initialize the output with the wcs
+        driz = gwcs_drizzle.GWCSDrizzle(output_model, pixfrac=self.pixfrac,
+                                        kernel=self.kernel, fillval=self.fillval)
+
+        for img in self.input_models:
+            inwht = resample_utils.build_driz_weight(img,
+                                                     weight_type=self.weight_type,
+                                                     good_bits=self.good_bits)
+            # apply sky subtraction
+            blevel = img.meta.background.level
+            if not img.meta.background.subtracted and blevel is not None:
+                data = img.data - blevel
+            else:
+                data = img.data
+
+            driz.add_image(data, img.meta.wcs, inwht=inwht)
+
+        self.update_fits_wcs(output_model)
+        self.update_exposure_times(output_model)
+        self.output_models.append(output_model)
+
+        return self.output_models
+
+    def update_exposure_times(self, output_model):
+        """Modify exposure time metadata in-place"""
+        total_exposure_time = 0.
+        exposure_times = {'start': [], 'end': []}
+        for exposure in self.input_models.models_grouped:
+            total_exposure_time += exposure[0].meta.exposure.exposure_time
+            exposure_times['start'].append(exposure[0].meta.exposure.start_time)
+            exposure_times['end'].append(exposure[0].meta.exposure.end_time)
+
+        # Update some basic exposure time values based on output_model
+        output_model.meta.exposure.exposure_time = total_exposure_time
+        output_model.meta.exposure.start_time = min(exposure_times['start'])
+        output_model.meta.exposure.end_time = max(exposure_times['end'])
+        output_model.meta.resample.product_exposure_time = total_exposure_time
 
     def update_fits_wcs(self, model):
         """
