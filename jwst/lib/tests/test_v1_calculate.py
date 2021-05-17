be0c1e3a5883b190dfd1dfb48c1b326174a3cadb
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-05-17 10:30:51 -0400

    Update more truths

--- jwst/lib/tests/test_v1_calculate.py
+++ jwst/lib/tests/test_v1_calculate.py
@@ -28,7 +28,7 @@ def engdb():
         yield engdb_tools.ENGDB_Service()
 
 
-def test_from_models(engdb):
+def test_from_models(engdb, tmp_path):
     """Test v1_calculate_from_models for basic running"""
     model = ImageModel()
     model.meta.exposure.start_time = Time(GOOD_STARTTIME).mjd
@@ -37,16 +37,22 @@ def test_from_models(engdb):
     v1_table = v1c.v1_calculate_from_models([model])
     v1_formatted = v1c.simplify_table(v1_table)
 
-    truth = Table.read(DATA_PATH / 'v1_calc_truth.ecsv')
+    # Save for post-test examination
+    v1_formatted.write(tmp_path / 'test_from_models.ecsv', format='ascii.ecsv')
+
+    truth = Table.read(DATA_PATH / 'test_from_models.ecsv')
     assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
 
 
-def test_over_time(engdb):
+def test_over_time(engdb, tmp_path):
     """Test v1_calculate_over_time for basic running"""
     v1_table = v1c.v1_calculate_over_time(
         Time(GOOD_STARTTIME).mjd, Time(GOOD_ENDTIME).mjd
     )
     v1_formatted = v1c.simplify_table(v1_table)
 
-    truth = Table.read(DATA_PATH / 'v1_time_truth.ecsv')
+    # Save for post-test examination
+    v1_formatted.write(tmp_path / 'test_over_time.ecsv', format='ascii.ecsv')
+
+    truth = Table.read(DATA_PATH / 'test_over_time.ecsv')
     assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
