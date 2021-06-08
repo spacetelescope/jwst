JP-2132: Fix AMI pupil phases sign error (#6128)
* Fix AMI pupil phases sign error

* Fix logging of rotation search params

jwst/ami/utils.py
@@ -851,7 +851,7 @@ def fringes2pistons(fringephases, nholes):
     Anrm = makeA(nholes)
     Apinv = np.linalg.pinv(Anrm)
 
-    return -np.dot(Apinv, fringephases)
+    return np.dot(Apinv, fringephases)
 
 
 def rebin(a=None, rc=(2, 2)):