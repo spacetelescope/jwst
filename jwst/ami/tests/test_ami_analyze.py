JP-2132: Fix AMI pupil phases sign error (#6128)
* Fix AMI pupil phases sign error

* Fix logging of rotation search params

jwst/ami/tests/test_ami_analyze.py
@@ -103,7 +103,7 @@ def test_utils_fringes2pistons():
 
     result = utils.fringes2pistons(fringephases, nholes)
 
-    true_result = np.array([0.02, 0.034, 0.02, -0.014, -0.06])
+    true_result = np.array([-0.02, -0.034, -0.02, 0.014, 0.06])
     assert_allclose(result, true_result)
 
 