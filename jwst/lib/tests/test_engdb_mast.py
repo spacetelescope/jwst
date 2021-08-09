1aba7a0395bb4cedbdeedc701a9399707a23ad34
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-08-09 11:52:12 -0400

    update docstrings for tests

--- jwst/lib/tests/test_engdb_mast.py
+++ jwst/lib/tests/test_engdb_mast.py
@@ -38,6 +38,7 @@ def test_aliveness():
 
 
 def test_negative_aliveness():
+    """Ensure failure occurs with a bad url"""
     with pytest.raises(RuntimeError):
         engdb_mast.EngdbMast(base_url='https://127.0.0.1/_engdb_mast_test', token='dummytoken')
 
