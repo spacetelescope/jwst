8e95446a880775ba35ca6aebc2e333e51eb27e71
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-08-12 13:53:01 -0400

    Add v1_calculate tests to use the Mast interface

--- jwst/lib/tests/test_v1_calculate.py
+++ jwst/lib/tests/test_v1_calculate.py
@@ -9,6 +9,7 @@ from astropy.time import Time
 from astropy.utils.diff import report_diff_values
 
 from jwst.datamodels import ImageModel
+from jwst.lib import engdb_mast
 from jwst.lib import engdb_tools
 import jwst.lib.set_telescope_pointing as stp
 import jwst.lib.v1_calculate as v1c
@@ -28,35 +29,69 @@ pytest.importorskip('pysiaf')
 
 
 @pytest.fixture
-def engdb():
+def engdb_service():
     """Setup the service to operate through the mock service"""
     with EngDB_Mocker():
         yield engdb_tools.ENGDB_Service()
 
 
-def test_from_models(engdb, tmp_path):
+def test_from_models(engdb_service, tmp_path):
     """Test v1_calculate_from_models for basic running"""
     model = ImageModel()
     model.meta.exposure.start_time = GOOD_STARTTIME.mjd
     model.meta.exposure.end_time = GOOD_ENDTIME.mjd
 
-    v1_table = v1c.v1_calculate_from_models([model], method=stp.Methods.TR_202105)
+    v1_table = v1c.v1_calculate_from_models([model], method=stp.Methods.COARSE_TR_202107)
     v1_formatted = v1c.simplify_table(v1_table)
 
     # Save for post-test examination
-    v1_formatted.write(tmp_path / 'test_from_models.ecsv', format='ascii.ecsv')
+    v1_formatted.write(tmp_path / 'test_from_models_service.ecsv', format='ascii.ecsv')
 
-    truth = Table.read(DATA_PATH / 'test_from_models.ecsv')
+    truth = Table.read(DATA_PATH / 'test_from_models_service.ecsv')
     assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
 
 
-def test_over_time(engdb, tmp_path):
+def test_over_time(engdb_service, tmp_path):
     """Test v1_calculate_over_time for basic running"""
-    v1_table = v1c.v1_calculate_over_time(GOOD_STARTTIME.mjd, GOOD_ENDTIME.mjd, method=stp.Methods.TR_202105)
+    v1_table = v1c.v1_calculate_over_time(GOOD_STARTTIME.mjd, GOOD_ENDTIME.mjd, method=stp.Methods.COARSE_TR_202107)
     v1_formatted = v1c.simplify_table(v1_table)
 
     # Save for post-test examination
-    v1_formatted.write(tmp_path / 'test_over_time.ecsv', format='ascii.ecsv')
+    v1_formatted.write(tmp_path / 'test_over_time_service.ecsv', format='ascii.ecsv')
 
-    truth = Table.read(DATA_PATH / 'test_over_time.ecsv')
+    truth = Table.read(DATA_PATH / 'test_over_time_service.ecsv')
+    assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
+
+
+def test_from_models_mast(tmp_path):
+    """Test v1_calculate_from_models for basic running"""
+    model = ImageModel()
+    model.meta.exposure.start_time = GOOD_STARTTIME.mjd
+    model.meta.exposure.end_time = GOOD_ENDTIME.mjd
+
+    try:
+        v1_table = v1c.v1_calculate_from_models([model], method=stp.Methods.COARSE_TR_202107, engdb_url=engdb_mast.MAST_BASE_URL)
+    except ValueError as exception:
+        pytest.xfail(f'MAST engineering database not available, possibly no token specified: {exception}')
+    v1_formatted = v1c.simplify_table(v1_table)
+
+    # Save for post-test examination
+    v1_formatted.write(tmp_path / 'test_from_models_mast.ecsv', format='ascii.ecsv')
+
+    truth = Table.read(DATA_PATH / 'test_from_models_mast.ecsv')
+    assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
+
+
+def test_over_time_mast(tmp_path):
+    """Test v1_calculate_over_time for basic running"""
+    try:
+        v1_table = v1c.v1_calculate_over_time(GOOD_STARTTIME.mjd, GOOD_ENDTIME.mjd, method=stp.Methods.COARSE_TR_202107, engdb_url=engdb_mast.MAST_BASE_URL)
+    except ValueError as exception:
+        pytest.xfail(f'MAST engineering database not available, possibly no token specified: {exception}')
+    v1_formatted = v1c.simplify_table(v1_table)
+
+    # Save for post-test examination
+    v1_formatted.write(tmp_path / 'test_over_time_mast.ecsv', format='ascii.ecsv')
+
+    truth = Table.read(DATA_PATH / 'test_over_time_mast.ecsv')
     assert report_diff_values(truth, v1_formatted, fileobj=sys.stderr)
