70c9af1085eb9529ec357814f86d13722db72d0c
Ken MacDonald <kmacdonald@stsci.edu>
2021-09-10 10:14:46 -0400

    Fixing style issues.

--- jwst/dark_current/dark_class.py
+++ jwst/dark_current/dark_class.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class DarkData:
     """
     Class removing data model dependencies.
--- jwst/dark_current/dark_sub.py
+++ jwst/dark_current/dark_sub.py
@@ -6,7 +6,6 @@ import copy
 
 import numpy as np
 import logging
-from .. import datamodels
 
 from . import dark_class
 
