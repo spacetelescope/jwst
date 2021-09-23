74ed6b27b0537135c2dab1a989f412121f19c3e2
David Law <dlaw@stsci.edu>
2021-09-23 10:26:49 -0400

    JP-2289: Fix specwcs in wcs_ref_models (#6362)
    Changes
    * Fix specwcs in wcs_ref_models
    * Update changes.rst
    * Revert to SimpleModel and add better check in SimpleModel validation
    * Remove now unnecessary on_save call

--- jwst/datamodels/wcs_ref_models.py
+++ jwst/datamodels/wcs_ref_models.py
@@ -52,7 +52,7 @@ class _SimpleModel(ReferenceFileModel):
     def validate(self):
         super().validate()
         try:
-            assert isinstance(self.model, Model)
+            assert isinstance(self.model, Model) or all([isinstance(m, Model) for m in self.model])
             assert self.meta.instrument.name in ["NIRCAM", "NIRSPEC", "MIRI", "TFI", "FGS", "NIRISS"]
         except AssertionError:
             if self._strict_validation:
@@ -138,7 +138,8 @@ class DistortionMRSModel(ReferenceFileModel):
             assert self.meta.instrument.name == "MIRI"
             assert self.meta.exposure.type == "MIR_MRS"
             assert self.meta.instrument.channel in ("12", "34", "1", "2", "3", "4")
-            assert self.meta.instrument.band in ("SHORT", "LONG", "MEDIUM")
+            assert self.meta.instrument.band in ("SHORT", "LONG", "MEDIUM", "SHORT-LONG", "SHORT-MEDIUM",
+                                                 "MEDIUM-SHORT", "MEDIUM-LONG", "LONG-SHORT", "LONG-MEDIUM")
             assert self.meta.instrument.detector in ("MIRIFUSHORT", "MIRIFULONG")
             assert all([isinstance(m, Model) for m in self.x_model])
             assert all([isinstance(m, Model) for m in self.y_model])
@@ -372,7 +373,8 @@ class RegionsModel(ReferenceFileModel):
             assert self.meta.instrument.name == "MIRI"
             assert self.meta.exposure.type == "MIR_MRS"
             assert self.meta.instrument.channel in ("12", "34", "1", "2", "3", "4")
-            assert self.meta.instrument.band in ("SHORT", "LONG")
+            assert self.meta.instrument.band in ("SHORT", "MEDIUM", "LONG", "SHORT-LONG", "SHORT-MEDIUM",
+                                                 "MEDIUM-SHORT", "MEDIUM-LONG", "LONG-SHORT", "LONG-MEDIUM")
             assert self.meta.instrument.detector in ("MIRIFUSHORT", "MIRIFULONG")
         except AssertionError:
             if self._strict_validation:
@@ -689,7 +691,7 @@ class DisperserModel(ReferenceFileModel):
 
 class FilteroffsetModel(ReferenceFileModel):
     """
-    A model for a NIRSPEC reference file of type "disperser".
+    A model for filter-dependent boresight offsets.
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/filteroffset.schema"
     reftype = "filteroffset"
