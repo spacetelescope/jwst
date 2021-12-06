Evidence calculation is now correct

jwst/residual_fringe/fitter.py
@@ -76,18 +76,17 @@ class ChiSqOutlierRejectionFitter:
 
     @staticmethod
     def _jacobian(model, x, weights=None):
-        # A bunch of hacks for finite Fourier series
-        fitter = LevMarLSQFitter() # access to _wrap_deriv which filters out fixed parameters
+        jacobian = np.array(model.jacobian(x, *model.parameters))
+        if weights is not None:
+            jacobian = np.array([np.ravel(this)
+                                 for this in np.array(weights) * jacobian])
 
-        jacobian = fitter._wrap_deriv(model.parameters, model, weights, x, x)
-
-        return np.array(jacobian)
+        return jacobian
 
     def _hessian(self, model, x, weights=None):
 
         jacobian = self._jacobian(model, x, weights)
         print(f"        {jacobian.shape=}")
-        np.save('jacobian.npy', jacobian)
 
         if weights is None:
             return 2 * np.inner(jacobian, jacobian)
@@ -95,7 +94,7 @@ class ChiSqOutlierRejectionFitter:
             return 2 * np.inner(jacobian, jacobian * weights)
 
     def _log(self, value):
-        err_state = np.seterr(invalid='raise')
+        err_state = np.seterr(all='raise')
         try:
             log = np.log(value)
         except FloatingPointError:
@@ -197,9 +196,8 @@ class ChiSqOutlierRejectionFitter:
             raise RuntimeError("Bad fit, method should have converged")
 
         if get_evidence:
-            evidence = None
-            # evidence = self._get_evidence(new_model, x, y, new_weights,
-            #                               limits, noise_limits, fixed_scale)
+            evidence = self._get_evidence(new_model, x, y, new_weights,
+                                          limits, noise_limits, fixed_scale)
 
             return new_model, evidence
         else:

jwst/residual_fringe/fourier.py
@@ -41,21 +41,18 @@ class FourierSeries1D(Fittable1DModel):
         return tuple(names)
 
     @staticmethod
-    def _fit_deriv_term(x, freq):
+    def _terms(x, freq):
         argument = TWOPI * (freq * x)
         if isinstance(argument, Quantity):
             argument = argument.value
 
-        d_coeff_a = np.sin(argument)
-        d_coeff_b = np.cos(argument)
-
-        return [d_coeff_a, d_coeff_b]
+        return np.sin(argument), np.cos(argument)
 
     def _evaluate_term(self, x, freq, coeff_a, coeff_b):
 
-        d_coeff_a, d_coeff_b = self._fit_deriv_term(x, freq)
+        sin, cos = self._terms(x, freq)
 
-        return coeff_a * d_coeff_a + coeff_b * d_coeff_b
+        return coeff_a * sin + coeff_b * cos
 
     def evaluate(self, x, *coeffs):
         coefficients = list(coeffs)
@@ -73,6 +70,29 @@ class FourierSeries1D(Fittable1DModel):
 
         deriv = []
         for index in range(self.n_terms):
-            deriv.extend(self._fit_deriv_term(x, self._frequencies[index]))
+            deriv.extend(list(self._terms(x, self._frequencies[index])))
+
+        return deriv
+
+    def _jacobian_term(self, x, freq, coeff_a, coeff_b):
+        sin, cos = self._terms(x, freq)
+
+        coeff = TWOPI * freq
+        if isinstance(coeff, Quantity):
+            coeff = coeff.value
+
+        d_freq = coeff * (coeff_a * cos - coeff_b * sin)
+
+        return [sin, cos, d_freq]
+
+    def jacobian(self, x, *coeffs):
+        coefficients = list(coeffs)
+
+        deriv = []
+        for index in range(self.n_terms):
+            coeff_a = coefficients.pop(0)
+            coeff_b = coefficients.pop(0)
+
+            deriv.extend(self._jacobian_term(x, self._frequencies[index], coeff_a, coeff_b))
 
         return deriv

jwst/residual_fringe/utils.py
@@ -11,7 +11,7 @@ from BayesicFitting import RobustShell
 from astropy.modeling.models import Spline1D
 from astropy.modeling.fitting import SplineExactKnotsFitter, LevMarLSQFitter
 
-from .fitter import ChiSqOutlierRejectionFitter
+from .fitter import ChiSqOutlierRejectionFitter, DegenerateEvidenceError
 from .fourier import FourierSeries1D
 
 import logging
@@ -671,6 +671,7 @@ def fit_1d_fringes_bayes_evidence(res_fringes, weights, wavenum, ffreq, dffreq, 
         log.debug("fit_1d_fringes_bayes: fit {} freqs incrementally, check bayes evidence".format(freqs.shape[0]))
 
         evidence = 1e-5  # arbitrarily small
+        jwst_evidence = 1e-5
         opt_nfringes = min_nfringes  # initialize
         if min_nfringes > freqs.shape[0]:
             warning = 'Number of fringes found is less then minimum number of required fringes for column'
@@ -697,13 +698,6 @@ def fit_1d_fringes_bayes_evidence(res_fringes, weights, wavenum, ffreq, dffreq, 
             fitter = LevenbergMarquardtFitter(wavenum, mdl_fit, verbose=0, keep=keep_dict)
             ftr = RobustShell(fitter, domain=10)
 
-            print("START: JWST fit")
-            mdl, _ = fit_nfringes(nfringes, freqs, wavenum, res_fringes,
-                                  limits=[-1, 1], noise_limits=[0.001, 1])
-            print("END: JWST fit")
-            mdl_val = mdl(wavenum)
-            print(mdl)
-
             print("START: BayesicFitting fit")
             try:
                 ftr.fit(res_fringes, weights=weights)
@@ -717,18 +711,36 @@ def fit_1d_fringes_bayes_evidence(res_fringes, weights, wavenum, ffreq, dffreq, 
             except RuntimeError as error:
                 print(f"FIT: {error=}")
                 new_evidence = -1e9
-            print("END: BayesicFitting fit")
 
             mdl_fit_val = mdl_fit.result(wavenum)
-            err = np.sum(np.abs(mdl_val - mdl_fit_val))
+            print("END: BayesicFitting fit")
 
-            print(f"{err=}, {evidence=}, {new_evidence=}, {len(wavenum)=}")
+            print("START: JWST fit")
+            try:
+                mdl, new_jwst_evidence = fit_nfringes(nfringes, freqs, wavenum, res_fringes,
+                                                      limits=[-1, 1], noise_limits=[0.001, 1])
+                mdl_val = mdl(wavenum)
+                err = np.sum(np.abs(mdl_val - mdl_fit_val))
+            except DegenerateEvidenceError as error:
+                print(f"    RAISED: {error=}")
+                new_jwst_evidence = -1e9
+                err = 'Invalid'
+            print("END: JWST fit")
+
+            print(f"{err=}, {new_jwst_evidence=}, {new_evidence=}, {len(wavenum)=}")
 
             log.debug("fit_1d_fringes_bayes_evidence: nfringe={} ev={} chi={}".format(nfringes, new_evidence, fitter.chisq))
 
             bayes_factor = new_evidence - evidence
-            # bayes_factor = new_evidence - evidence
-            # print(f"{bayes_factor=}, {old_bayes_factor=}\n")
+            print(f"{new_evidence=}, {evidence=}, {bayes_factor=}")
+            jwst_bayes_factor = new_jwst_evidence - jwst_evidence
+            print(f"{new_jwst_evidence=}, {jwst_evidence=}, {jwst_bayes_factor=}")
+
+            if jwst_bayes_factor > 1:
+                print("JWST UPDATE!!!!!!!!!!!!!")
+                jwst_evidence = new_jwst_evidence
+            else:
+                print("JWST FINISH !!!!!!!!!!!!!!!!!!!!")
 
             log.debug(
                 "fit_1d_fringes_bayes_evidence: bayes factor={}".format(bayes_factor))
@@ -737,10 +749,14 @@ def fit_1d_fringes_bayes_evidence(res_fringes, weights, wavenum, ffreq, dffreq, 
                 opt_nfringes = nfringes
                 log.debug(
                     "fit_1d_fringes_bayes_evidence: strong evidence for nfringes={} ".format(nfringes))
+                print("BayesicFitting UPDATE !!!")
             else:
                 log.debug(
                     "fit_1d_fringes_bayes_evidence: no evidence for nfringes={}".format(nfringes))
+                print("BayesicFitting Finish !!!")
                 break
+
+            print("\n")
 
         # mdl_fit = multi_sine(opt_nfringes)
         # log.debug("fit_1d_fringes_bayes_evidence: optimal={} fringes".format(opt_nfringes))