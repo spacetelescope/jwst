fcec55968c92c25edfad7a20a47e486e15cf4708
Howard Bushouse <bushouse@stsci.edu>
2021-12-28 15:08:25 -0500

    Update test_dark_sub.py

--- jwst/dark_current/tests/test_dark_sub.py
+++ jwst/dark_current/tests/test_dark_sub.py
@@ -6,7 +6,6 @@ import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 
-from stcal.dark_current.dark_sub import average_dark_frames
 from stcal.dark_current.dark_sub import average_dark_frames_3d as average_dark_frames
 from stcal.dark_current.dark_sub import do_correction as darkcorr
 
