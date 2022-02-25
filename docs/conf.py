295d669bd9db477df5106c0e5d8b1037346c9d43
Tyler Pauly <tapastro@gmail.com>
2022-02-25 12:37:24 -0500

    is it stsci-rtd-theme?

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,7 @@ asdf_schema_reference_mappings = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'stsci_rtd_theme'
+html_theme = 'sphinx-rtd-theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
