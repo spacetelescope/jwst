1615ee9ab4ccc0c114b0531958e9e7cc61d46fd2
William Jamieson <wjamieson@stsci.edu>
2022-03-07 14:37:53 -0500

    Bugfix for residual fringe background testing

--- jwst/residual_fringe/tests/test_background_fit.py
+++ jwst/residual_fringe/tests/test_background_fit.py
@@ -2,6 +2,8 @@
 Unit test for Residual Fringe Correction fitting of the background
 """
 
+from pathlib import Path
+
 from jwst.residual_fringe import utils
 from numpy.testing import assert_allclose
 from astropy.io import fits
@@ -13,14 +15,17 @@ def read_fit_column(file):
     # Data was pulled out of an exposure by modifying residual_fringe.py to write out a column of data
     # The function we are testing is fit_1d_background_complex.
 
-    hdu = fits.open(file)
+    file_dir = Path(__file__).parent.resolve()
+    file_path = str(file_dir / file)
+
+    hdu = fits.open(file_path)
     col_data = hdu[1].data
     col_weight = hdu[2].data
     col_wnum = hdu[3].data
     bg_fit = hdu[4].data
     store_freq = hdu[0].header['FFREQ']
-    bg_fit2, bgindx2 = utils.fit_1d_background_complex(col_data, col_weight,
-                                                       col_wnum, ffreq=store_freq)
+    bg_fit2, _ = utils.fit_1d_background_complex(col_data, col_weight,
+                                                 col_wnum, ffreq=store_freq)
 
     assert_allclose(bg_fit, bg_fit2, atol=0.001)
 
