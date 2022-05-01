95758a2c668849e133bc421024a0f3f40246e3b7
Mike Regan <mregan@stsci.edu>
2022-05-01 17:36:11 -0400

    Update jump.py
    Add max_cores as a parameter to detect_jumps

--- jwst/jump/jump.py
+++ jwst/jump/jump.py
@@ -36,14 +36,11 @@ def run_detect_jumps(input_model, gain_model, readnoise_model,
         log.info('Extracting readnoise subarray to match science data')
         readnoise_2d = reffile_utils.get_subarray_data(input_model,
                                                        readnoise_model)
-
-    # turn off MP for now
-    # max_cores = None
-
+    print("new jwst jump")
     new_gdq, new_pdq = detect_jumps(frames_per_group, data, gdq, pdq, err,
                                     gain_2d, readnoise_2d,
                                     rejection_thresh, three_grp_thresh,
-                                    four_grp_thresh,
+                                    four_grp_thresh, max_cores,
                                     max_jump_to_flag_neighbors,
                                     min_jump_to_flag_neighbors,
                                     flag_4_neighbors, dqflags.pixel)
