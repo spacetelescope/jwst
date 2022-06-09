63169b6c8751102fe13091df592f86eb8c2ea4ff
Tyler Pauly <tapastro@gmail.com>
2022-06-09 15:43:55 -0400

    exclude targacq members by only allowing background members in

--- jwst/associations/lib/rules_level2b.py
+++ jwst/associations/lib/rules_level2b.py
@@ -75,10 +75,17 @@ class Asn_Lv2Image(
             ),
             Constraint(
                 [
-                    SimpleConstraint(
-                        value='science',
-                        test=lambda value, item: self.get_exposure_type(item) != value,
-                        force_unique=False,
+                    Constraint(
+                        [SimpleConstraint(
+                            value='science',
+                            test=lambda value, item: self.get_exposure_type(item) != value,
+                            force_unique=False,
+                        ),
+                        SimpleConstraint(
+                            value='target_acquisition',
+                            test=lambda value, item: self.get_exposure_type(item) != value,
+                            force_unique=False,
+                        )], reduce=Constraint.all
                     ),
                     Constraint_Single_Science(self.has_science),
                 ], reduce=Constraint.any
