6ef3d54cc7153963c590d951fb4e554505eb6c01
jemorrison <morrison@as.arizona.edu>
2022-07-05 07:23:12 -0500

    removed duplicate code

--- jwst/extract_1d/ifu.py
+++ jwst/extract_1d/ifu.py
@@ -623,10 +623,6 @@ def extract_ifu(input_model, source_type, extract_params):
                                              method=method, subpixels=subpixels)
         f_var_flat[k] = float(var_flat_table['aperture_sum'][0])
 
-        var_flat_table = aperture_photometry(var_flat[k, :, :], aperture,
-                                             method=method, subpixels=subpixels)
-        f_var_flat[k] = float(var_flat_table['aperture_sum'][0])
-
         # Point source type of data with defined annulus size
         if subtract_background_plane:
             bkg_table = aperture_photometry(data[k, :, :], annulus,
