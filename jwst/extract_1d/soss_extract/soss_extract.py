d4f3303e2a36ba40e4e6db3770218a07cc3b55f5
Tyler Pauly <tapastro@gmail.com>
2022-10-24 13:24:09 -0400

    fix rebase missing code

--- jwst/extract_1d/soss_extract/soss_extract.py
+++ jwst/extract_1d/soss_extract/soss_extract.py
@@ -1276,4 +1276,9 @@ def run_extract1d(input_model, spectrace_ref_name, wavemap_ref_name,
         order_int = order_str_2_int[order]
         setattr(output_references, f'aperture{order_int}', box_w_ord)
 
+    if pipe_utils.is_tso(input_model):
+        log.info("Populating INT_TIMES keywords from input table.")
+        populate_time_keywords(input_model, output_model)
+        output_model.int_times = input_model.int_times.copy()
+
     return output_model, output_references, output_atoca
