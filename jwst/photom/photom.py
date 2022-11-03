76c93be5dce3863bf3b30fc2cb74fa915b431589
jemorrison <morrison@as.arizona.edu>
2022-11-03 05:46:39 -0700

    flake fix

--- jwst/photom/photom.py
+++ jwst/photom/photom.py
@@ -1038,8 +1038,6 @@ class DataSet():
                     xend = xstart + self.input.meta.subarray.xsize
                     self.input.area = pix_area.data[ystart: yend,
                                                     xstart: xend]
-                    print('xstart, xend, ystart, yend', xstart, xend, ystart, yend)
-                    
                 log.info('Pixel area map copied to output.')
             else:
                 self.save_area_nirspec(pix_area)
