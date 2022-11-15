9081394c90a94855dbc006a6a84496d3d0d6e848
Howard Bushouse <bushouse@stsci.edu>
2022-11-15 08:21:30 -0500

    add photom and extract_1d tests

--- jwst/regtest/test_nirspec_ifu_spec2.py
+++ jwst/regtest/test_nirspec_ifu_spec2.py
@@ -45,3 +45,57 @@ def test_spec2(run_spec2, fitsdiff_default_kwargs, suffix):
     """Regression test matching output files"""
     rt.is_like_truth(run_spec2, fitsdiff_default_kwargs, suffix,
                      truth_path=TRUTH_PATH)
+
+
+@pytest.fixture(scope='module')
+def run_photom(jail, rtdata_module):
+    """Run the photom step on an NRS IFU exposure with SRCTYPE=POINT"""
+    rtdata = rtdata_module
+
+    # Setup the inputs
+    rate_name = 'jw01251004001_03107_00002_nrs1_pathloss.fits'
+    rate_path = INPUT_PATH + '/' + rate_name
+
+    # Run the step
+    step_params = {
+        'input_path': rate_path,
+        'step': 'jwst.photom.PhotomStep',
+        'args': ['--save_results=True', ]
+    }
+
+    rtdata = rt.run_step_from_dict(rtdata, **step_params)
+    return rtdata
+
+
+@pytest.mark.bigdata
+def test_photom(run_photom, fitsdiff_default_kwargs):
+    """Test the photom step on an NRS IFU exposure with a point source"""
+    rt.is_like_truth(run_photom, fitsdiff_default_kwargs, 'photomstep',
+                     truth_path=TRUTH_PATH)
+
+
+@pytest.fixture(scope='module')
+def run_extract1d(jail, rtdata_module):
+    """Run the extract_1d step on an NRS IFU cube with SRCTYPE=POINT"""
+    rtdata = rtdata_module
+
+    # Setup the inputs
+    cube_name = 'jw01251004001_03107_00002_nrs1_s3d.fits'
+    cube_path = INPUT_PATH + '/' + cube_name
+
+    # Run the step
+    step_params = {
+        'input_path': cube_path,
+        'step': 'jwst.extract_1d.Extract1dStep',
+        'args': ['--save_results=True', ]
+    }
+
+    rtdata = rt.run_step_from_dict(rtdata, **step_params)
+    return rtdata
+
+
+@pytest.mark.bigdata
+def test_extract1d(run_extract1d, fitsdiff_default_kwargs):
+    """Test the extract_1d step on an NRS IFU cube with a point source"""
+    rt.is_like_truth(run_extract1d, fitsdiff_default_kwargs, 'extract1dstep',
+                     truth_path=TRUTH_PATH)
