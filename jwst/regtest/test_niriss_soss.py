f4be89d8a5379676711171132d08b4ba3b010110
Tyler Pauly <tapastro@gmail.com>
2022-11-29 09:52:22 -0500

    remove module scoping

--- jwst/regtest/test_niriss_soss.py
+++ jwst/regtest/test_niriss_soss.py
@@ -23,10 +23,10 @@ def run_tso_spec2(jail, rtdata):
     Step.from_cmdline(args)
 
 
-@pytest.fixture(scope="module")
-def run_tso_spec3(jail, rtdata_module, run_tso_spec2):
+@pytest.fixture()
+def run_tso_spec3(jail, rtdata, run_tso_spec2):
     """Run stage 3 pipeline on NIRISS SOSS data."""
-    rtdata = rtdata_module
+
     # Get the level3 association json file (though not its members) and run
     # the tso3 pipeline on all _calints files listed in association
     rtdata.get_data("niriss/soss/jw01091-o002_20220714t155100_tso3_001_asn.json")
@@ -64,9 +64,9 @@ def test_niriss_soss_stage2(rtdata, run_tso_spec2, fitsdiff_default_kwargs, suff
 
 
 @pytest.mark.bigdata
-def test_niriss_soss_stage3_crfints(rtdata_module, run_tso_spec3, fitsdiff_default_kwargs):
+def test_niriss_soss_stage3_crfints(rtdata, run_tso_spec3, fitsdiff_default_kwargs):
     """Regression test of tso-spec3 pipeline outlier_detection results performed on NIRISS SOSS data."""
-    rtdata = rtdata_module
+
     output = "jw01091002001_03101_00001-seg001_nis_short_o002_crfints.fits"
     rtdata.output = output
 
@@ -77,9 +77,8 @@ def test_niriss_soss_stage3_crfints(rtdata_module, run_tso_spec3, fitsdiff_defau
 
 
 @pytest.mark.bigdata
-def test_niriss_soss_stage3_x1dints(run_tso_spec3, rtdata_module, fitsdiff_default_kwargs):
+def test_niriss_soss_stage3_x1dints(run_tso_spec3, rtdata, fitsdiff_default_kwargs):
     """Regression test of tso-spec3 pipeline extract_1d results performed on NIRISS SOSS data."""
-    rtdata = rtdata_module
 
     output = "jw01091-o002_t001_niriss_clear-gr700xd-substrip256_x1dints.fits"
     rtdata.output = output
@@ -90,9 +89,8 @@ def test_niriss_soss_stage3_x1dints(run_tso_spec3, rtdata_module, fitsdiff_defau
 
 
 @pytest.mark.bigdata
-def test_niriss_soss_stage3_whtlt(run_tso_spec3, rtdata_module, diff_astropy_tables):
+def test_niriss_soss_stage3_whtlt(run_tso_spec3, rtdata, diff_astropy_tables):
     """Regression test of tso-spec3 pipeline white_light results performed on NIRISS SOSS data."""
-    rtdata = rtdata_module
 
     output = "jw01091-o002_t001_niriss_clear-gr700xd-substrip256_whtlt.ecsv"
     rtdata.output = output
