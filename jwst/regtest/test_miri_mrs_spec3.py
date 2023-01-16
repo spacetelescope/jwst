35ef068b476ad9c3ab4b95aafdabf9c1ad99c5df
jemorrison <morrison@as.arizona.edu>
2023-01-16 11:47:49 -0700

    update to miri_mrs_spec3

--- jwst/regtest/test_miri_mrs_spec3.py
+++ jwst/regtest/test_miri_mrs_spec3.py
@@ -15,32 +15,28 @@ TRUTH_PATH = 'truth/test_miri_mrs'
 
 @pytest.fixture(scope='module')
 def run_spec3(jail, rtdata_module):
-    """Run the Spec3Pipeline on the single cal result from the Spec2Pipeline run"""
+    """Run the Spec3Pipeline dithered flight data """
     rtdata = rtdata_module
 
     # Note that we use the truth file from spec2 processing as the input to spec3
-    rtdata.get_data(TRUTH_PATH + '/' + 'ifushort_ch12_cal.fits')
+    rtdata.get_asn('miri/mrs/jw01024-c10000_04101_spec3_00001_asn.json')
 
-    asn = asn_from_list([rtdata.input], product_name='ifushort_ch12_spec3')
-    asn.data["program"] = "00024"
-    asn.data["asn_type"] = "spec3"
-    asn.sequence = 1
-    asn_name, serialized = asn.dump(format="json")
-    with open(asn_name, "w") as f:
-        f.write(serialized)
+    # Run the calwebb_spec3 pipeline on the association
+    #args = ["calwebb_spec3", rtdata.input]
+    #Step.from_cmdline(args)
 
-    rtdata.input = asn_name
+    #return rtdata
+
+
+    #rtdata.input = asn_name
 
     args = [
         "calwebb_spec3",
         rtdata.input,
         '--steps.master_background.save_results=true',
-        '--steps.mrs_imatch.save_results=true',
         '--steps.outlier_detection.save_results=true',
-        '--steps.resample_spec.save_results=true',
         '--steps.cube_build.save_results=true',
         '--steps.extract_1d.save_results=true',
-        '--steps.combine_1d.save_results=true',
     ]
 
     Step.from_cmdline(args)
@@ -49,19 +45,16 @@ def run_spec3(jail, rtdata_module):
 
 @pytest.fixture(scope='module')
 def run_spec3_multi(jail, rtdata_module):
-    """Run the Spec3Pipeline on multi channel/multi filter data"""
+    """Run the Spec3Pipeline on multi channel data"""
 
     step_params = {
-        'input_path': INPUT_PATH + '/' + 'ifushort_set2_asn3.json',
+        'input_path': INPUT_PATH + '/' + 'jw01024-c10000_04101_spec3_00001_asn.json',
         'step': 'calwebb_spec3',
         'args': [
             '--steps.master_background.save_results=true',
-            '--steps.mrs_imatch.save_results=true',
             '--steps.outlier_detection.save_results=true',
-            '--steps.resample_spec.save_results=true',
             '--steps.cube_build.save_results=true',
             '--steps.extract_1d.save_results=true',
-            '--steps.combine_1d.save_results=true',
         ]
     }
 
@@ -73,13 +66,12 @@ def run_spec3_multi(jail, rtdata_module):
 @pytest.mark.parametrize(
     'output',
     [
-        'ifushort_ch12_spec3_mrs_imatch.fits',
-        'ifushort_ch12_spec3_ch1-medium_s3d.fits',
-        'ifushort_ch12_spec3_ch2-medium_s3d.fits',
-        'ifushort_ch12_spec3_ch1-medium_x1d.fits',
-        'ifushort_ch12_spec3_ch2-medium_x1d.fits',
+        'jw01024-c1000_t002_miri_ch1-short_s3d.fits',
+        'jw01024-c1000_t002_miri_ch1-short_x1d.fits',
+        'jw01024-c1000_t002_miri_ch3-short_s3d.fits',
+        'jw01024-c1000_t002_miri_ch3-short_x1d.fits'
     ],
-    ids=["mrs_imatch", "ch1-s3d", "ch2-s3d", "ch1-x1d", "ch2-x1d"]
+    ids=["ch1-short-s3d", "ch1-short-x1d", "ch3-", "ch2-x1d"]
 )
 def test_spec3(run_spec3, fitsdiff_default_kwargs, output):
     """Regression test matching output files"""
