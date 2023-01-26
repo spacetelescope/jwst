26e2600258cd5173cfcbffd0bfdbdfbdd0d4816e
jemorrison <morrison@as.arizona.edu>
2023-01-26 14:23:24 -0700

    miri_mrs_spec3

--- jwst/regtest/test_miri_mrs_spec3.py
+++ jwst/regtest/test_miri_mrs_spec3.py
@@ -14,21 +14,54 @@ TRUTH_PATH = 'truth/test_miri_mrs'
 
 
 @pytest.fixture(scope='module')
-def run_spec3(jail, rtdata_module):
-    """Run the Spec3Pipeline dithered flight data """
+def run_spec3_single(jail, rtdata_module):
+    """Run the Spec3Pipeline on single cal from Spec2Pipeline"""
+    
+    # Running Spec2 on MIRI data creates a single cube with 2 channels
+    # Running Spec3 on MIRI data creates 2 channel cubes
+    # This test will perform a regression tests on the 2 channel cubes
+
+
     rtdata = rtdata_module
 
     # Note that we use the truth file from spec2 processing as the input to spec3
-    rtdata.get_asn('miri/mrs/jw01024-c10000_04101_spec3_00001_asn.json')
+    rtdata.get_data(TRUTH_PATH + '/' + 'jw01024001001_04101_00001_mirifulong_cal.fits')
 
-    # Run the calwebb_spec3 pipeline on the association
-    #args = ["calwebb_spec3", rtdata.input]
-    #Step.from_cmdline(args)
+    asn = asn_from_list([rtdata.input], product_name='jw01024001001_04101_00001_mirifulong')
+    asn.data["program"] = "01024"
+    asn.data["asn_type"] = "spec3"
+    asn.sequence = 1
+    asn_name, serialized = asn.dump(format="json")
+    with open(asn_name, "w") as f:
+        f.write(serialized)
 
-    #return rtdata
+    rtdata.input = asn_name
+
+    args = [
+        "calwebb_spec3",
+        rtdata.input,
+        '--steps.master_background.save_results=true',
+        '--steps.outlier_detection.save_results=true',
+        '--steps.resample_spec.save_results=true',
+        '--steps.cube_build.save_results=true',
+        '--steps.extract_1d.save_results=true',
+        '--steps.combine_1d.save_results=true',
+    ]
+
+    Step.from_cmdline(args)
+    return rtdata
 
 
-    #rtdata.input = asn_name
+@pytest.fixture(scope='module')
+def run_spec3_multi(jail, rtdata_module):
+    """Run the Spec3Pipeline dithered flight data """
+    rtdata = rtdata_module
+
+    # Use an association that has the full 12 band so we can create channel cubes
+    # from the two detectors. We will run tests on a cube from IFUSHORT and one from
+    # IFULONG
+    
+    rtdata.get_asn('miri/mrs/jw01024-c10000_spec3_00001_asn.json')
 
     args = [
         "calwebb_spec3",
@@ -43,23 +76,6 @@ def run_spec3(jail, rtdata_module):
     return rtdata
 
 
-@pytest.fixture(scope='module')
-def run_spec3_multi(jail, rtdata_module):
-    """Run the Spec3Pipeline on multi channel data"""
-
-    step_params = {
-        'input_path': INPUT_PATH + '/' + 'jw01024-c10000_04101_spec3_00001_asn.json',
-        'step': 'calwebb_spec3',
-        'args': [
-            '--steps.master_background.save_results=true',
-            '--steps.outlier_detection.save_results=true',
-            '--steps.cube_build.save_results=true',
-            '--steps.extract_1d.save_results=true',
-        ]
-    }
-
-    return rt.run_step_from_dict(rtdata_module, **step_params)
-
 
 @pytest.mark.slow
 @pytest.mark.bigdata
@@ -71,12 +87,14 @@ def run_spec3_multi(jail, rtdata_module):
         'jw01024-c1000_t002_miri_ch3-short_s3d.fits',
         'jw01024-c1000_t002_miri_ch3-short_x1d.fits'
     ],
-    ids=["ch1-short-s3d", "ch1-short-x1d", "ch3-", "ch2-x1d"]
+    #ids=["ch1-short-s3d", "ch1-short-x1d", "ch3-", "ch2-x1d"]
 )
-def test_spec3(run_spec3, fitsdiff_default_kwargs, output):
+
+
+def test_spec3(run_spec3_single, fitsdiff_default_kwargs, output):
     """Regression test matching output files"""
 
-    rtdata = run_spec3
+    rtdata = run_spec3_single
     rtdata.output = output
 
     rtdata.get_truth(os.path.join(TRUTH_PATH, rtdata.output))
@@ -90,18 +108,24 @@ def test_spec3(run_spec3, fitsdiff_default_kwargs, output):
 @pytest.mark.parametrize(
     'output',
     [
-        'ifushort_set2_0_mrs_imatch.fits',
-        'ifushort_set2_1_mrs_imatch.fits',
-        'ifushort_set2_0_a3001_crf.fits',
-        'ifushort_set2_1_a3001_crf.fits',
-        'ifushort_set2_ch1-short_s3d.fits',
-        'ifushort_set2_ch2-short_s3d.fits',
-        'ifushort_set2_ch1-short_x1d.fits',
-        'ifushort_set2_ch2-short_x1d.fits',
+        'jw01024001001_04101_00001_mirifushort_c1000_crf.fits',
+        'jw01024001001_04101_00002_mirifushort_c1000_crf.fits',
+        'jw01024001001_04101_00001_mirifulong_c1000_crf.fits',
+        'jw01024001001_04101_00002_mirifulong_c1000_crf.fits',
+        'jw01024-c1000_miri_ch1-shortmedium_s3d.fits',
+        'jw01024-c1000_miri_ch2-shortmedium_s3d.fits',
+        'jw01024-c1000_miri_ch3-shortmedium_s3d.fits',
+        'jw01024-c1000_miri_ch4-shortmedium_s3d.fits',		
+        'jw01024-c1000_miri_ch1-shortmedium_x1d.fits',
+        'jw01024-c1000_miri_ch2-shortmedium_x1d.fits',	
+        'jw01024-c1000_miri_ch3-shortmedium_x1d.fits',
+        'jw01024-c1000_miri_ch4-shortmedium_x1d.fits'
     ],
-    ids=["ch1-mrs_imatch", "ch2-mrs_imatch", "ch1-crf", "ch2-crf",
-         "ch1-s3d", "ch2-s3d", "ch1-x1d", "ch2-x1d"]
+    #ids=["ch1-mrs_imatch", "ch2-mrs_imatch", "ch1-crf", "ch2-crf",
+    #     "ch1-s3d", "ch2-s3d", "ch1-x1d", "ch2-x1d"]
 )
+
+
 def test_spec3_multi(run_spec3_multi, fitsdiff_default_kwargs, output):
     """Regression test matching output files"""
     rt.is_like_truth(
