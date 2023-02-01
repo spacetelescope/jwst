5a0304c1029c6892b1510401586d9babe6b5856b
Jonathan Eisenhamer <eisenham@mentant.local.stsci.edu>
2023-02-01 10:26:03 -0500

    update get_wcs for new parameter to_detector
    test updated for the default value

--- jwst/lib/tests/test_siafdb.py
+++ jwst/lib/tests/test_siafdb.py
@@ -60,9 +60,9 @@ def test_create(source, prd, xml_path, exception, jail_environ):
                      crpix1=1024.5, crpix2=1024.5, cdelt1=0.06787747, cdelt2=0.06976441,
                      vertices_idl=(-70.7843, 69.9807, 68.6042, -69.4153, -74.3558, -71.5516, 71.3065, 69.3639))),
         ('MIRIM_TAMRS', False,
-         siafdb.SIAF(v2_ref=206.407, v3_ref=-697.765, v3yangle=-1.24120427, vparity=1,
-                     crpix1=1024.5, crpix2=1024.5, cdelt1=0.06839158, cdelt2=0.06993081,
-                     vertices_idl=(-68.8543, 70.1233, 71.5697, -70.2482, 72.1764, 68.8086, -75.5918, -70.7457))),
+         siafdb.SIAF(v2_ref=-481.987342, v3_ref=-318.206242, v3yangle=4.83544897, vparity=-1,
+                     crpix1=24.5, crpix2=24.5, cdelt1=0.10986808, cdelt2=0.10996394,
+                     vertices_idl=(-2.6187, 2.6558, 2.6186, -2.6535, -2.6097, -2.6715, 2.6068, 2.6683))),
     ]
 )
 def test_get_wcs(aperture, to_detector, expected):
