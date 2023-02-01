5e17d61d8dd53bc791648c69e2fc188c1a11ee58
Jonathan Eisenhamer <eisenham@mentant.local.stsci.edu>
2023-02-01 10:16:32 -0500

    initial test setup for new parameter `to_detector`

--- jwst/lib/tests/test_siafdb.py
+++ jwst/lib/tests/test_siafdb.py
@@ -49,22 +49,26 @@ def test_create(source, prd, xml_path, exception, jail_environ):
 
 
 @pytest.mark.parametrize(
-    'aperture, expected',
+    'aperture, to_detector, expected',
     [
-        ('FGS1_FULL_OSS',
+        ('FGS1_FULL_OSS', False,
          siafdb.SIAF(v2_ref=206.407, v3_ref=-697.765, v3yangle=-1.24120427, vparity=1,
                      crpix1=1024.5, crpix2=1024.5, cdelt1=0.06839158, cdelt2=0.06993081,
                      vertices_idl=(-68.8543, 70.1233, 71.5697, -70.2482, 72.1764, 68.8086, -75.5918, -70.7457))),
-        ('FGS2_FULL_OSS',
+        ('FGS2_FULL_OSS', False,
          siafdb.SIAF(v2_ref=22.835, v3_ref=-699.423, v3yangle=0.2914828, vparity=1,
                      crpix1=1024.5, crpix2=1024.5, cdelt1=0.06787747, cdelt2=0.06976441,
                      vertices_idl=(-70.7843, 69.9807, 68.6042, -69.4153, -74.3558, -71.5516, 71.3065, 69.3639))),
+        ('MIRIM_TAMRS', False,
+         siafdb.SIAF(v2_ref=206.407, v3_ref=-697.765, v3yangle=-1.24120427, vparity=1,
+                     crpix1=1024.5, crpix2=1024.5, cdelt1=0.06839158, cdelt2=0.06993081,
+                     vertices_idl=(-68.8543, 70.1233, 71.5697, -70.2482, 72.1764, 68.8086, -75.5918, -70.7457))),
     ]
 )
-def test_get_wcs(aperture, expected):
+def test_get_wcs(aperture, to_detector, expected):
     """Test retrieval of wcs information."""
     siaf_db = siafdb.SiafDb(SIAFXML_PATH)
-    siaf = siaf_db.get_wcs(aperture)
+    siaf = siaf_db.get_wcs(aperture, to_detector=to_detector)
     assert siaf == expected
 
 
