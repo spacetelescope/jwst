b0b0352a2d831d21332698292c1b2de91d3cbd1e
Tyler Pauly <tapastro@gmail.com>
2023-02-17 11:26:05 -0500

    point to stdatamodels location, for eventual move to separate PR

--- jwst/transforms/converters/jwst_models.py
+++ jwst/transforms/converters/jwst_models.py
@@ -13,12 +13,12 @@ class NIRCAMGrismDispersionConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/nircam_grism_dispersion-*"]
 
-    types = ["jwst.transforms.models.NIRCAMForwardRowGrismDispersion",
-             "jwst.transforms.models.NIRCAMForwardColumnGrismDispersion",
-             "jwst.transforms.models.NIRCAMBackwardGrismDispersion"]
+    types = ["stdatamodels.jwst.transforms.models.NIRCAMForwardRowGrismDispersion",
+             "stdatamodels.jwst.transforms.models.NIRCAMForwardColumnGrismDispersion",
+             "stdatamodels.jwst.transforms.models.NIRCAMBackwardGrismDispersion"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms import models
+        from stdatamodels.jwst.transforms import models
 
         _fname = getattr(models, node["model_type"])
         return _fname(list(node['orders']),
@@ -44,12 +44,12 @@ class NIRISSGrismDispersionConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/niriss_grism_dispersion-*"]
 
-    types = ["jwst.transforms.models.NIRISSForwardRowGrismDispersion",
-             "jwst.transforms.models.NIRISSForwardColumnGrismDispersion",
-             "jwst.transforms.models.NIRISSBackwardGrismDispersion"]
+    types = ["stdatamodels.jwst.transforms.models.NIRISSForwardRowGrismDispersion",
+             "stdatamodels.jwst.transforms.models.NIRISSForwardColumnGrismDispersion",
+             "stdatamodels.jwst.transforms.models.NIRISSBackwardGrismDispersion"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms import models
+        from stdatamodels.jwst.transforms import models
 
         _fname = getattr(models, node["model_type"])
         return _fname(list(node['orders']),
@@ -76,11 +76,11 @@ class Gwa2SlitConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/gwa_to_slit-*"]
 
-    types = ["jwst.transforms.models.Gwa2Slit"]
+    types = ["stdatamodels.jwst.transforms.models.Gwa2Slit"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
 
-        from jwst.transforms.models import Gwa2Slit
+        from stdatamodels.jwst.transforms.models import Gwa2Slit
 
         return Gwa2Slit(node['slits'], node['models'])
 
@@ -94,11 +94,11 @@ class Slit2MsaConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/slit_to_msa-*"]
 
-    types = ["jwst.transforms.models.Slit2Msa"]
+    types = ["stdatamodels.jwst.transforms.models.Slit2Msa"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
 
-        from jwst.transforms.models import Slit2Msa
+        from stdatamodels.jwst.transforms.models import Slit2Msa
 
         return Slit2Msa(node['slits'], node['models'])
 
@@ -111,10 +111,10 @@ class Slit2MsaConverter(TransformConverterBase):
 class LogicalConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/logical-*"]
-    types = ["jwst.transforms.models.Logical"]
+    types = ["stdatamodels.jwst.transforms.models.Logical"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import Logical
+        from stdatamodels.jwst.transforms.models import Logical
 
         return Logical(node['condition'], node['compareto'], node['value'])
 
@@ -129,11 +129,11 @@ class LogicalConverter(TransformConverterBase):
 class NirissSOSSConverter(TransformConverterBase):
     tags = ["tag:stsci.edu:jwst_pipeline/niriss-soss-*",
             "tag:stsci.edu:jwst_pipeline/niriss_soss-*"]
-    types = ["jwst.transforms.models.NirissSOSSModel"]
+    types = ["stdatamodels.jwst.transforms.models.NirissSOSSModel"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
 
-        from jwst.transforms.models import NirissSOSSModel
+        from stdatamodels.jwst.transforms.models import NirissSOSSModel
 
         return NirissSOSSModel(node['spectral_orders'], node['models'])
 
@@ -146,11 +146,11 @@ class NirissSOSSConverter(TransformConverterBase):
 
 class RefractionIndexConverter(TransformConverterBase):
     tags = ["tag:stsci.edu:jwst_pipeline/refraction_index_from_prism-*"]
-    types = ["jwst.transforms.models.RefractionIndexFromPrism"]
+    types = ["stdatamodels.jwst.transforms.models.RefractionIndexFromPrism"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
 
-        from jwst.transforms.models import RefractionIndexFromPrism
+        from stdatamodels.jwst.transforms.models import RefractionIndexFromPrism
 
         return RefractionIndexFromPrism(node['prism_angle'])
 
@@ -162,10 +162,10 @@ class RefractionIndexConverter(TransformConverterBase):
 
 class MIRI_AB2SliceConverter(TransformConverterBase):
     tags = ["tag:stsci.edu:jwst_pipeline/miri_ab2slice-*"]
-    types = ["jwst.transforms.models.MIRI_AB2Slice"]
+    types = ["stdatamodels.jwst.transforms.models.MIRI_AB2Slice"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import MIRI_AB2Slice
+        from stdatamodels.jwst.transforms.models import MIRI_AB2Slice
         return MIRI_AB2Slice(node['beta_zero'], node['beta_del'], node['channel'])
 
     def to_yaml_tree_transform(self, model, tag, ctx):
@@ -180,10 +180,10 @@ class Rotation3DToGWAConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/rotation_sequence-*"]
 
-    types = ["jwst.transforms.models.Rotation3DToGWA"]
+    types = ["stdatamodels.jwst.transforms.models.Rotation3DToGWA"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import Rotation3DToGWA
+        from stdatamodels.jwst.transforms.models import Rotation3DToGWA
 
         angles = node['angles']
         axes_order = node['axes_order']
@@ -199,12 +199,12 @@ class GratingEquationConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/grating_equation-*"]
 
-    types = ["jwst.transforms.models.WavelengthFromGratingEquation",
-             "jwst.transforms.models.AngleFromGratingEquation"
+    types = ["stdatamodels.jwst.transforms.models.WavelengthFromGratingEquation",
+             "stdatamodels.jwst.transforms.models.AngleFromGratingEquation"
              ]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import WavelengthFromGratingEquation, AngleFromGratingEquation
+        from stdatamodels.jwst.transforms.models import WavelengthFromGratingEquation, AngleFromGratingEquation
 
         groove_density = node['groove_density']
         order = node['order']
@@ -219,7 +219,7 @@ class GratingEquationConverter(TransformConverterBase):
         return model
 
     def to_yaml_tree_transform(self, model, tag, ctx):
-        from jwst.transforms.models import WavelengthFromGratingEquation, AngleFromGratingEquation
+        from stdatamodels.jwst.transforms.models import WavelengthFromGratingEquation, AngleFromGratingEquation
 
         node = {'order': model.order.value,
                 'groove_density': model.groove_density.value
@@ -238,10 +238,10 @@ class V23ToSkyConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/v23tosky-*"]
 
-    types = ["jwst.transforms.models.V23ToSky"]
+    types = ["stdatamodels.jwst.transforms.models.V23ToSky"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import V23ToSky
+        from stdatamodels.jwst.transforms.models import V23ToSky
         angles = node['angles']
         axes_order = node['axes_order']
         return V23ToSky(angles, axes_order=axes_order)
@@ -256,10 +256,10 @@ class SnellConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/snell-*"]
 
-    types = ["jwst.transforms.models.Snell"]
+    types = ["stdatamodels.jwst.transforms.models.Snell"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import Snell
+        from stdatamodels.jwst.transforms.models import Snell
         return Snell(node['prism_angle'], node['kcoef'], node['lcoef'], node['tcoef'],
                      node['ref_temp'], node['ref_pressure'], node['temp'], node['pressure'])
 
@@ -280,11 +280,11 @@ class CoordsConverter(TransformConverterBase):
 
     tags = ["tag:stsci.edu:jwst_pipeline/coords-*"]
 
-    types = ["jwst.transforms.models.Unitless2DirCos",
-             "jwst.transforms.models.DirCos2Unitless"]
+    types = ["stdatamodels.jwst.transforms.models.Unitless2DirCos",
+             "stdatamodels.jwst.transforms.models.DirCos2Unitless"]
 
     def from_yaml_tree_transform(self, node, tag, ctx):
-        from jwst.transforms.models import Unitless2DirCos, DirCos2Unitless
+        from stdatamodels.jwst.transforms.models import Unitless2DirCos, DirCos2Unitless
 
         model_type = node['model_type']
         if model_type in ('to_dircos', 'unitless2directional'):
@@ -295,7 +295,7 @@ class CoordsConverter(TransformConverterBase):
             raise TypeError("Unknown model_type")
 
     def to_yaml_tree_transform(self, model, tag, ctx):
-        from jwst.transforms.models import Unitless2DirCos, DirCos2Unitless
+        from stdatamodels.jwst.transforms.models import Unitless2DirCos, DirCos2Unitless
 
         if isinstance(model, DirCos2Unitless):
             model_type = 'directional2unitless'
