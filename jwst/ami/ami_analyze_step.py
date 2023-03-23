72d90aa1f483ca0caca523c9275561a83e5634d3
Rachel Cooper <rcooper@stsci.edu>
2023-03-23 17:59:41 -0400

    commented out now-unused code including throughput ref file use, to be deleted later.

--- jwst/ami/ami_analyze_step.py
+++ jwst/ami/ami_analyze_step.py
@@ -24,13 +24,7 @@ class AmiAnalyzeStep(Step):
         run_bpfix = boolean(default=True) # Run Fourier bad pixel fix on cropped data
     """
 
-    reference_file_types = ['throughput', 'nrm']
-
-    def save_model(self, model, *args, **kwargs):
-        # Override save_model to change suffix based on list of results
-        if "idx" in kwargs and kwargs.get("suffix", None) is None:
-            kwargs["suffix"] = ["ami-oi", "amimulti-oi", "amilg"][kwargs.pop("idx")]
-        return Step.save_model(self, model, *args, **kwargs)
+    #reference_file_types = ['throughput']
 
     def process(self, input):
         """
@@ -68,48 +62,44 @@ class AmiAnalyzeStep(Step):
         self.log.info(f"Initial rotation guess = {rotate} deg")
         self.log.info(f"Initial values to use for psf offset = {psf_offset}")
 
-        # Make sure oversample is odd
-        if oversample % 2 == 0:
-            raise ValueError("Oversample value must be an odd integer.")
+        # Open the input data model. Can be 2D or 3D image, so use general DataModel
+        # try:
+        #     input_model = datamodels.ImageModel(input)
+        # except ValueError as err:
+        #     raise RuntimeError(f"{err}. Input must be a 2D ImageModel.")
 
-        # Open the input data model. Can be 2D or 3D image
-        with datamodels.open(input) as input_model:
-            # Get the name of the filter throughput reference file to use
-            throughput_reffile = self.get_reference_file(input_model, 'throughput')
-            self.log.info(f'Using filter throughput reference file {throughput_reffile}')
+        # # check for 2D data array
+        # if len(input_model.data.shape) != 2:
+        #     raise RuntimeError("Only 2D ImageModel data can be processed")
+        try:
+            input_model = datamodels.DataModel(input)
+        except ValueError as err:
+            raise RuntimeError(f"{err}. Input unable to be read into a DataModel.")
 
-            # Check for a valid reference file or user-provided bandpass
-            if (throughput_reffile == 'N/A') & (bandpass is None):
-                raise RuntimeError("No THROUGHPUT reference file found. "
-                                   "ami_analyze cannot continue.")
+        # We don't want to use this file
+        # # Get the name of the filter throughput reference file to use
+        # throughput_reffile = self.get_reference_file(input_model, 'throughput')
+        # self.log.info(f'Using filter throughput reference file {throughput_reffile}')
 
-            # Get the name of the NRM reference file to use
-            nrm_reffile = self.get_reference_file(input_model, 'nrm')
-            self.log.info(f'Using NRM reference file {nrm_reffile}')
+        # # Check for a valid reference file
+        # if throughput_reffile == 'N/A':
+        #     self.log.warning('No THROUGHPUT reference file found')
+        #     self.log.warning('AMI analyze step will be skipped')
+        #     raise RuntimeError("No throughput reference file found. "
+        #                        "ami_analyze cannot continue.")
 
-            with (
-                datamodels.ThroughputModel(throughput_reffile) as throughput_model,
-                datamodels.NRMModel(nrm_reffile) as nrm_model,
-            ):
-                # Apply the LG+ methods to the data
-                oifitsmodel, oifitsmodel_multi, amilgmodel = ami_analyze.apply_LG_plus(
-                    input_model,
-                    throughput_model,
-                    nrm_model,
-                    oversample,
-                    rotate,
-                    psf_offset,
-                    rotsearch_parameters,
-                    bandpass,
-                    usebp,
-                    firstfew,
-                    chooseholes,
-                    affine2d,
-                    run_bpfix,
-                )
+        # Open the filter throughput reference file
+        # throughput_model = datamodels.ThroughputModel(throughput_reffile)
+        # this is then not actually used by apply_LG_plus??? Monochromatic 4.3 um filter hardcoded.
 
-        amilgmodel.meta.cal_step.ami_analyze = 'COMPLETE'
-        oifitsmodel.meta.cal_step.ami_analyze = 'COMPLETE'
-        oifitsmodel_multi.meta.cal_step.ami_analyze = 'COMPLETE'
+        # Apply the LG+ methods to the data
+        result = ami_analyze.apply_LG_plus(input_model,#, throughput_model,
+                                           oversample, rotate,
+                                           psf_offset,
+                                           rotsearch_parameters)
 
-        return oifitsmodel, oifitsmodel_multi, amilgmodel
+        # Close the reference file and update the step status
+        # throughput_model.close()
+        result.meta.cal_step.ami_analyze = 'COMPLETE'
+
+        return result
