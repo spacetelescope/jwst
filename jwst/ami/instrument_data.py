2beb9cda20136d3dc0aab96a38db9ab4e1743f33
Rachel Cooper <rcooper@stsci.edu>
2023-03-23 17:58:34 -0400

    Added keywords read in from datamodel and stored in NIRISS object for oifits writing, additional input args for flexibility

--- jwst/ami/instrument_data.py
+++ jwst/ami/instrument_data.py
@@ -30,8 +30,7 @@ class NIRISS:
                  rotsearch_parameters=None,
                  oversample=None,
                  psf_offset=None,
-                 run_bpfix=True
-                 ):
+                 **kwargs):
         """
         Short Summary
         ------------
@@ -74,21 +73,17 @@ class NIRISS:
         self.filt = filt
         self.throughput = bandpass
         self.firstfew = firstfew
-        self.nrm_model = nrm_model
+        if firstfew is not None:
+            log.info(f'Analyzing only the first {firstfew:d} integrations')
+
 
         self.lam_c, self.lam_w = utils.get_cw_beta(self.throughput)
-        self.wls = [
-            self.throughput,
-        ]
+        ####### 
+
+
+        self.wls = [self.throughput, ]
         # Wavelength info for NIRISS bands F277W, F380M, F430M, or F480M
-        self.wavextension = (
-            [
-                self.lam_c,
-            ],
-            [
-                self.lam_w,
-            ],
-        )
+        self.wavextension = ([self.lam_c,], [self.lam_w,])
         self.nwav = 1
 
         # only one NRM on JWST:
@@ -96,11 +91,10 @@ class NIRISS:
         self.instrument = "NIRISS"
         self.arrname = "jwst_g7s6c"
         self.holeshape = "hex"
-        self.mask = NRM_mask_definitions(
-            maskname=self.arrname,
-            chooseholes=self.chooseholes,
-            holeshape=self.holeshape,
-        )
+        self.mask = NRM_mask_definitions(maskname=self.arrname, chooseholes=chooseholes, holeshape=self.holeshape)
+        pscale_deg = utils.degrees_per_pixel(input_model)
+        self.pscale_rad = np.deg2rad(pscale_deg)
+        dim = input_model.data.shape[-1] # 80 pixels
 
         # save affine deformation of pupil object or create a no-deformation object.
         # We apply this when sampling the PSF, not to the pupil geometry.
@@ -109,9 +103,9 @@ class NIRISS:
         # Separating detector tilt pixel scale effects from pupil distortion effects is
         # yet to be determined... see comments in Affine class definition.
         if affine2d is None:
-            self.affine2d = utils.Affine2d(
-                mx=1.0, my=1.0, sx=0.0, sy=0.0, xo=0.0, yo=0.0, name="Ideal"
-            )
+            self.affine2d = utils.Affine2d(mx=1.0, my=1.0,
+                               sx=0.0, sy=0.0,
+                               xo=0.0, yo=0.0, name="Ideal")
         else:
             self.affine2d = affine2d
 
@@ -128,6 +122,7 @@ class NIRISS:
             "F480M": 0.02,
         }
         self.threshold = self.cvsupport_threshold[filt]
+
 
     def set_pscale(self, pscalex_deg=None, pscaley_deg=None):
         """
@@ -159,9 +154,7 @@ class NIRISS:
         """
         Short Summary
         -------------
-        Retrieve info from input data model and store in NIRISS class.
-        Trim refpix and roughly center science data and dq array.
-        Run Fourier bad pixel correction before returning science data.
+        Retrieve info from input data model and store in NIRISS class
 
         Parameters
         ----------
@@ -175,152 +168,88 @@ class NIRISS:
         dqmask_ctrd:
             Cropped, centered mask of bad pixels
         """
+        # The info4oif_dict will get pickled to disk when we write txt files of results.
+        # That way we don't drag in objects like instrument_data into code that reads text results
+        # and writes oifits files - a simple built-in dictionary is the only object used in this transfer.
+ 
 
-        # all instrumentdata attributes will be available when oifits files written out
-        scidata = copy.deepcopy(np.array(input_model.data))
-        bpdata = copy.deepcopy(np.array(input_model.dq))
+        # # Whatever we did set is averaged for isotropic pixel scale here
+        # self.pscale_mas = 0.5 * (pscalex_deg + pscaley_deg) * (60 * 60 * 1000)
+        # self.pscale_rad = utils.mas2rad(self.pscale_mas)
 
-        # pixel scale recalculated and averaged
-        pscaledegx, pscaledegy = utils.degrees_per_pixel(input_model)
-        # At some point we want to use different X and Y pixel scales
-        pscale_deg = np.mean([pscaledegx, pscaledegy])
+        # the following is done by updatewithheaderinfo in implaneia, 
+        # don't need to pickle a dict here though.
+        # all instrumentdata attributes will be available when oifits files written out?
+        scidata = input_model.data.copy()
+        bpdata = input_model.dq.copy()
+        # make dq mask again? for now
+        DO_NOT_USE = dqflags.pixel["DO_NOT_USE"]
+        JUMP_DET = dqflags.pixel["JUMP_DET"]
+        dq_dnu = bpdata & DO_NOT_USE == DO_NOT_USE
+        dq_jump = bpdata & JUMP_DET == JUMP_DET
+        dqmask = dq_dnu | dq_jump
+
+        pscale_deg = utils.degrees_per_pixel(input_model)
         self.pscale_rad = np.deg2rad(pscale_deg)
-        self.pscale_mas = pscale_deg * (60 * 60 * 1000)
+
         self.pav3 = input_model.meta.pointing.pa_v3
         self.vparity = input_model.meta.wcsinfo.vparity
         self.v3iyang = input_model.meta.wcsinfo.v3yangle
         self.parangh = input_model.meta.wcsinfo.roll_ref
+        self.ra = input_model.meta.target.ra
+        self.dec = input_model.meta.target.dec
         self.crpix1 = input_model.meta.wcsinfo.crpix1
         self.crpix2 = input_model.meta.wcsinfo.crpix2
         self.pupil = input_model.meta.instrument.pupil
-        self.proposer_name = input_model.meta.target.proposer_name
-        if input_model.meta.target.catalog_name == "UNKNOWN":
-            objname = input_model.meta.target.proposer_name
-        else:
-            objname = input_model.meta.target.catalog_name
-        self.objname = objname.replace("-", " ")
-        self.pi_name = input_model.meta.program.pi_name
-        self.ra = input_model.meta.target.ra
-        self.dec = input_model.meta.target.dec
-        self.pmra = input_model.meta.target.proper_motion_ra
-        self.pmdec = input_model.meta.target.proper_motion_dec
-        self.ra_uncertainty = input_model.meta.target.ra_uncertainty
-        self.dec_uncertainty = input_model.meta.target.dec_uncertainty
 
-
-        datestr = input_model.meta.visit.start_time.replace(" ", "T")
-        self.date = datestr  # is this the right start time?
+        datestr = input_model.meta.visit.start_time.replace(' ','T')
+        self.date = datestr # is this the right start time?
         self.year = datestr[:4]
         self.month = datestr[5:7]
         self.day = datestr[8:10]
         effinttm = input_model.meta.exposure.effective_exposure_time
         nints = input_model.meta.exposure.nints
         # if 2d input, model has already been expanded to 3d, so check 0th dimension
-        if input_model.data.shape[0] == 1:
-            self.itime = effinttm * nints
+        if input_model.data.shape[0] == 1: 
+            self.itime = effinttm*nints # CHECK THIS
         else:
             self.itime = effinttm
             if self.firstfew is not None:
                 if scidata.shape[0] > self.firstfew:
-                    log.info(f"Analyzing only the first {self.firstfew:d} integrations")
-                    scidata = scidata[: self.firstfew, :, :]
-                    bpdata = bpdata[: self.firstfew, :, :]
-                else:
-                    log.warning(f"Input firstfew={self.firstfew:d} is greater than the number of integrations")
-                    log.warning("All integrations will be analyzed")
-            self.nwav = scidata.shape[0]
-            [self.wls.append(self.wls[0]) for f in range(self.nwav - 1)]
-        # Rotate mask hole centers by pav3 + v3i_yang to be in equatorial coordinates
+                    scidata = scidata[:self.firstfew, :, :]
+                    dqmask = dqmask[:self.firstfew, :, :]
+            self.nwav = nints # update from 1 (number of slices)
+            [self.wls.append(self.wls[0]) for f in range(self.nwav-1)]
+
+        # Get integer-pixel position of target from siaf & header info
+        siaf = pysiaf.Siaf('NIRISS')
+        # select AMI aperture by name
+        xoffset, yoffset = input_model.meta.dither.x_offset, input_model.meta.dither.y_offset
+        apername = input_model.meta.aperture.name
+        nis_ami = siaf[apername]
+        xtarg_detpx, ytarg_detpx = nis_ami.idl_to_sci(xoffset, yoffset) # decimal pixel position in subarray, 1 indexed?
+        self.peak0, self.peak1 = int(np.floor(xtarg_detpx)), int(np.floor(ytarg_detpx))
+
+        # do all the stuff with rotating centers, save as attributes instead of info4oif_dict
         ctrs_sky = self.mast2sky()
         oifctrs = np.zeros(self.mask.ctrs.shape)
-        oifctrs[:, 0] = ctrs_sky[:, 1].copy() * -1
-        oifctrs[:, 1] = ctrs_sky[:, 0].copy() * -1
+        oifctrs[:,0] = ctrs_sky[:,1].copy() * -1
+        oifctrs[:,1] = ctrs_sky[:,0].copy() * -1
         self.ctrs_eqt = oifctrs
         self.ctrs_inst = self.mask.ctrs
         self.hdia = self.mask.hdia
         self.nslices = self.nwav
 
         # Trim refpix from all slices
-        scidata = scidata[:, 4:, :]
-        bpdata = bpdata[:, 4:, :]
+        scidata = scidata[:,4:, :]
+        dqmask = dqmask[:,4:, :]
 
-        # find peak in median of refpix-trimmed scidata
-        med_im = np.median(scidata, axis=0)
+        self.rootfn = input_model.meta.filename.replace('.fits','')
 
-        # Use median image to find big CR hits not already caught by pipeline
-        std_im = np.std(scidata, axis=0)
-        mediandiff = np.empty_like(scidata)
-        mediandiff[:, :, :] = scidata - med_im
-        nsigma = 10
-        outliers = np.where(mediandiff > nsigma * std_im)
-        outliers2 = np.argwhere(mediandiff > nsigma * std_im)
-
-        dqvalues = bpdata[outliers]
-        log.info(
-            f"{len(dqvalues)} additional pixels >10-sig from median of stack found"
-        )
-        # decompose DQ values to check if they are already flagged DNU
-        count = 0
-        for loc, dq_value in zip(outliers2, dqvalues):
-            bitarr = np.binary_repr(dq_value)
-            bad_types = []
-            for i, elem in enumerate(bitarr[::-1]):
-                if elem == str(1):
-                    badval = 2**i
-                    key = next(
-                        key for key, value in dqflags.pixel.items() if value == badval
-                    )
-                    bad_types.append(key)
-            if "DO_NOT_USE" not in bad_types:
-                bpdata[loc[0], loc[1], loc[2]] += 1
-                count += 1
-        log.info(f"{count} DO_NOT_USE flags added to DQ array for found outlier pixels")
-
-        # Roughly center scidata, bpdata around peak pixel position
-        peakx, peaky, r = utils.min_distance_to_edge(med_im)
-        scidata_ctrd = scidata[
-            :, int(peakx - r):int(peakx + r + 1), int(peaky - r):int(peaky + r + 1)
-        ]
-        bpdata_ctrd = bpdata[
-            :, int(peakx - r):int(peakx + r + 1), int(peaky - r):int(peaky + r + 1)
-        ]
-
-        log.info(
-            "Cropping all integrations to %ix%i pixels around peak (%i,%i)"
-            % (2 * r + 1, 2 * r + 1, peakx + 4, peaky)
-        )  # +4 because of trimmed refpx
-        # apply bp fix here
-        if self.run_bpfix:
-            log.info('Applying Fourier bad pixel correction to cropped data, updating DQ array')
-            scidata_ctrd, bpdata_ctrd = bp_fix.fix_bad_pixels(
-                scidata_ctrd,
-                bpdata_ctrd,
-                input_model.meta.instrument.filter,
-                self.pscale_mas,
-                self.nrm_model,
-            )
-        else:
-            log.info("Not running Fourier bad pixel fix")
-
-        self.rootfn = input_model.meta.filename.replace(".fits", "")
+        
 
         # all info needed to write out oifits should be stored in NIRISS object attributes
-
-        # Make a bad pixel mask, either from real DQ data or zeros if usebp=False
-        if self.usebp:
-            log.info(
-                "usebp flag set to TRUE: bad pixels will be excluded from model fit"
-            )
-            DO_NOT_USE = dqflags.pixel["DO_NOT_USE"]
-            JUMP_DET = dqflags.pixel["JUMP_DET"]
-            dq_dnu = bpdata_ctrd & DO_NOT_USE == DO_NOT_USE
-            dq_jump = bpdata_ctrd & JUMP_DET == JUMP_DET
-            dqmask_ctrd = dq_dnu | dq_jump
-        else:
-            log.info("usebp flag set to FALSE: all pixels will be used in model fit")
-            dqmask_ctrd = np.zeros_like(scidata_ctrd)
-
-        return scidata_ctrd, dqmask_ctrd
+        return scidata, dqmask
 
     def reset_nwav(self, nwav):
         """
@@ -338,43 +267,40 @@ class NIRISS:
         """
         self.nwav = nwav
 
+#####
     def mast2sky(self):
         """
-        Short Summary
-        -------------
         Rotate hole center coordinates:
             Clockwise by the V3 position angle - V3I_YANG from north in degrees if VPARITY = -1
             Counterclockwise by the V3 position angle - V3I_YANG from north in degrees if VPARITY = 1
         Hole center coords are in the V2, V3 plane in meters.
-
-        Returns
-        -------
-        ctrs_rot: array
-            Rotated coordinates to be put in OIFITS files.
+        Return rotated coordinates to be put in info4oif_dict.
+        implane2oifits.ObservablesFromText uses these to calculate baselines.
         """
+        pa = self.pav3
         mask_ctrs = copy.deepcopy(self.mask.ctrs)
         # rotate by an extra 90 degrees (RAC 9/21)
         # these coords are just used to orient output in OIFITS files
         # NOT used for the fringe fitting itself
-        mask_ctrs = utils.rotate2dccw(mask_ctrs, np.pi / 2.0)
-        vpar = self.vparity  # Relative sense of rotation between Ideal xy and V2V3
-        rot_ang = self.pav3 - self.v3iyang  # subject to change!
+        mask_ctrs = utils.rotate2dccw(mask_ctrs,np.pi/2.)
+        vpar = self.vparity # Relative sense of rotation between Ideal xy and V2V3
+        v3iyang = self.v3i_yang
+        rot_ang = pa - v3iyang # subject to change!
 
-        if self.pav3 == 0.0:
-            return mask_ctrs
-        else:
+        if pa != 0.0:
             # Using rotate2sccw, which rotates **vectors** CCW in a fixed coordinate system,
             # so to rotate coord system CW instead of the vector, reverse sign of rotation angle.  Double-check comment
             if vpar == -1:
-                # rotate clockwise  <rotate coords clockwise>
+                # rotate clockwise  <rotate coords clockwise?>
                 ctrs_rot = utils.rotate2dccw(mask_ctrs, np.deg2rad(-rot_ang))
-                log.info(
-                    f"Rotating mask hole centers clockwise by {rot_ang:.3f} degrees"
-                )
+                log.debug(f'Rotating mask hole centers clockwise by {rot_ang:.3f} degrees')
             else:
-                # counterclockwise  <rotate coords counterclockwise>
+                # counterclockwise  <rotate coords counterclockwise?>
                 ctrs_rot = utils.rotate2dccw(mask_ctrs, np.deg2rad(rot_ang))
-                log.info(
-                    f"Rotating mask hole centers counterclockwise by {rot_ang:.3f} degrees"
-                )
-            return ctrs_rot
+                log.debug(f'Rotating mask hole centers counterclockwise by {rot_ang:.3f} degrees')
+        else:
+            ctrs_rot = mask_ctrs
+        return ctrs_rot
+
+
+#####
