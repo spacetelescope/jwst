29b93a1186b3aea3864fe6649895fc940d921609
Mihai Cara <mihail.cara@gmail.com>
2023-04-12 03:15:01 -0400

    Add tests for memory minimization and repetitive runs

--- jwst/skymatch/tests/test_skymatch.py
+++ jwst/skymatch/tests/test_skymatch.py
@@ -12,6 +12,8 @@ from jwst.datamodels import ModelContainer
 from jwst.assign_wcs import AssignWcsStep
 from jwst.skymatch import SkyMatchStep
 from jwst.tweakreg.utils import adjust_wcs
+from jwst.associations.asn_from_list import asn_from_list
+from jwst.associations.lib.rules_level3_base import DMS_Level3_Base
 
 
 DO_NOT_USE = dqflags.pixel['DO_NOT_USE']
@@ -31,6 +33,7 @@ def nircam_rate():
     shape = (ysize, xsize)
     im = ImageModel(shape)
     im.var_rnoise += 0
+
     im.meta.wcsinfo = {
         'ctype1': 'RA---TAN',
         'ctype2': 'DEC--TAN',
@@ -41,7 +44,8 @@ def nircam_rate():
         'v3_ref': -493.385704,
         'v3yangle': -0.07385127,
         'vparity': -1,
-        'wcsaxes': 2}
+        'wcsaxes': 2
+    }
     im.meta.instrument = {
         'channel': 'LONG',
         'detector': 'NRCALONG',
@@ -49,7 +53,8 @@ def nircam_rate():
         'lamp_mode': 'NONE',
         'module': 'A',
         'name': 'NIRCAM',
-        'pupil': 'CLEAR'}
+        'pupil': 'CLEAR'
+    }
     im.meta.subarray = {
         'fastaxis': -1,
         'name': 'FULL',
@@ -57,7 +62,8 @@ def nircam_rate():
         'xsize': xsize,
         'xstart': 1,
         'ysize': ysize,
-        'ystart': 1}
+        'ystart': 1
+    }
     im.meta.observation = {
         'activity_id': '01',
         'date': '2021-10-25',
@@ -70,7 +76,8 @@ def nircam_rate():
         'time': '16:58:27.258',
         'visit_group': '01',
         'visit_id': '42424001001',
-        'visit_number': '001'}
+        'visit_number': '001'
+    }
     im.meta.exposure = {
         'duration': 161.05155,
         'end_time': 59512.70899968495,
@@ -88,7 +95,8 @@ def nircam_rate():
         'readpatt': 'BRIGHT1',
         'sample_time': 10,
         'start_time': 59512.70725993055,
-        'type': 'NRC_IMAGE'}
+        'type': 'NRC_IMAGE'
+    }
     im.meta.photometry = {
         'pixelarea_steradians': 1e-13,
         'pixelarea_arcsecsq': 4e-3,
@@ -336,3 +344,188 @@ def test_skymatch_overlap(nircam_rate, skymethod, subtract, skystat):
                 assert abs(np.mean(im.data[dq_mask]) - slev) < 0.01
             else:
                 assert abs(np.mean(im.data[dq_mask]) - lev) < 0.01
+
+
+def test_asn_input(nircam_rate, tmpdir):
+    # This is the same test as 'test_skymatch_overlap' with
+    # skymethod='match', subtract=True, skystat='mean' and with memory saving
+    # feature enabled (data loaded from files as needed).
+    np.random.seed(1)
+    im1 = nircam_rate.copy()
+    im2 = im1.copy()
+    im3 = im1.copy()
+
+    # add "bad" data
+    im1, dq_mask = _add_bad_pixels(im1, 1e6, 1e9)
+    im2, _ = _add_bad_pixels(im2, 5e6, 3e9)
+    im3, _ = _add_bad_pixels(im3, 7e6, 1e8)
+
+    # rotate images so that corner SATURATED pixels are not in the overlap
+    # region:
+    im2.meta.wcs = adjust_wcs(im2.meta.wcs, delta_roll=30)
+    im3.meta.wcs = adjust_wcs(im3.meta.wcs, delta_roll=60)
+
+    im2.meta.observation.sequence_id = "2"
+    im3.meta.observation.sequence_id = "3"
+
+    container = ModelContainer([im1, im2, im3])
+
+    # define some background:
+    levels = [9.12, 8.28, 2.56]
+
+    for im, lev in zip(container, levels):
+        im.data += np.random.normal(loc=lev, scale=0.1, size=im.data.shape)
+
+    im1_path = str(tmpdir / "skymatch_im1.fits")
+    im2_path = str(tmpdir / "skymatch_im2.fits")
+    im3_path = str(tmpdir / "skymatch_im3.fits")
+
+    im1.write(im1_path)
+    im2.write(im2_path)
+    im3.write(im3_path)
+
+    assoc_out = asn_from_list(
+        [im1_path, im2_path, im3_path],
+        rule=DMS_Level3_Base,
+        product_name='skymatch'
+    )
+    asn_out_fname, out_serialized = assoc_out.dump(format='json')
+    asn_out_fname = str(tmpdir / asn_out_fname)
+    with open(asn_out_fname, "w") as asn_out:
+        asn_out.write(out_serialized)
+
+    # We do not exclude SATURATED pixels. They should be ignored because
+    # images are rotated and SATURATED pixels in the corners are not in the
+    # common intersection of all input images. This is the purpose of this test
+    step = SkyMatchStep(
+        minimize_memory=True,
+        skymethod='match',
+        match_down=True,
+        subtract=True,
+        skystat='mean',
+        nclip=0,
+        dqbits='~DO_NOT_USE'  # specifically DO NOT add 'SATURATED' flag
+    )
+
+    result = step.run(asn_out_fname)
+
+    assert isinstance(result, str)
+
+    ref_levels = np.subtract(levels, min(levels))
+    sub_levels = np.subtract(levels, ref_levels)
+
+    result = ModelContainer(result)
+
+    for im, lev, rlev, slev in zip(result, levels, ref_levels, sub_levels):
+        # check that meta was set correctly:
+        assert im.meta.background.method == 'match'
+        assert im.meta.background.subtracted is True
+
+        # test computed/measured sky values:
+        assert abs(im.meta.background.level - rlev) < 0.01
+
+        # test
+        assert abs(np.mean(im.data[dq_mask]) - slev) < 0.01
+
+
+@pytest.mark.parametrize(
+    'skymethod, subtract',
+    tuple(
+        product(
+            ['local', 'match', 'global', 'global+match'],
+            [False, True]
+        )
+    )
+)
+def test_skymatch_2x(nircam_rate, tmpdir, skymethod, subtract):
+    # Test that repetitive applications of skymatch produce the same results
+    np.random.seed(1)
+    im1 = nircam_rate.copy()
+    im2 = im1.copy()
+    im3 = im1.copy()
+
+    # add "bad" data
+    im1, dq_mask = _add_bad_pixels(im1, 1e6, 1e9)
+    im2, _ = _add_bad_pixels(im2, 5e6, 3e9)
+    im3, _ = _add_bad_pixels(im3, 7e6, 1e8)
+
+    im2.meta.observation.sequence_id = "2"
+    im3.meta.observation.sequence_id = "3"
+
+    container = ModelContainer([im1, im2, im3])
+
+    # define some background:
+    levels = [9.12, 8.28, 2.56]
+
+    for im, lev in zip(container, levels):
+        im.data += np.random.normal(loc=lev, scale=0.1, size=im.data.shape)
+
+    im1_path = str(tmpdir / "skymatch_im1.fits")
+    im2_path = str(tmpdir / "skymatch_im2.fits")
+    im3_path = str(tmpdir / "skymatch_im3.fits")
+
+    im1.write(im1_path)
+    im2.write(im2_path)
+    im3.write(im3_path)
+
+    assoc_out = asn_from_list(
+        [im1_path, im2_path, im3_path],
+        rule=DMS_Level3_Base,
+        product_name='skymatch'
+    )
+    asn_out_fname, out_serialized = assoc_out.dump(format='json')
+    asn_out_fname = str(tmpdir / asn_out_fname)
+    with open(asn_out_fname, "w") as asn_out:
+        asn_out.write(out_serialized)
+
+    # We do not exclude SATURATED pixels. They should be ignored because
+    # images are rotated and SATURATED pixels in the corners are not in the
+    # common intersection of all input images. This is the purpose of this test
+    step = SkyMatchStep(
+        minimize_memory=True,
+        skymethod=skymethod,
+        match_down=True,
+        subtract=subtract,
+        skystat='mean',
+        nclip=0,
+        dqbits='~DO_NOT_USE+SATURATED'
+    )
+
+    result = step.run(asn_out_fname)
+
+    with pytest.raises(ValueError):
+        step.subtract = not subtract
+        step.run(result)
+
+    # 2nd run.
+    step.subtract = subtract
+    result2 = step.run(result)
+
+    # compute expected levels
+    if skymethod in ['local', 'global+match']:
+        ref_levels = levels
+
+    elif skymethod == 'match':
+        ref_levels = np.subtract(levels, min(levels))
+
+    elif skymethod == 'global':
+        ref_levels = 3 * [min(levels)]
+
+    sub_levels = np.subtract(levels, ref_levels)
+
+    result2 = ModelContainer(result2)
+
+    # compare results
+    for im2, lev, rlev, slev in zip(result2, levels, ref_levels, sub_levels):
+        # check that meta was set correctly:
+        assert im2.meta.background.method == skymethod
+        assert im2.meta.background.subtracted == subtract
+
+        # test computed/measured sky values:
+        assert abs(im2.meta.background.level - rlev) < 0.01
+
+        # test
+        if subtract:
+            assert abs(np.mean(im2.data[dq_mask]) - slev) < 0.01
+        else:
+            assert abs(np.mean(im2.data[dq_mask]) - lev) < 0.01
