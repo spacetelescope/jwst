6d3578b0313539c96de70eb04edb8a82bbb87007
Zach Burnett <zburnett@stsci.edu>
2023-05-12 09:30:57 -0400

    remove deprecated imports

--- jwst/datamodels/ramp.py
+++ jwst/datamodels/ramp.py
@@ -1,3 +1,3 @@
-from stdatamodels.jwst.datamodels.ramp import RampModel, MIRIRampModel
+from stdatamodels.jwst.datamodels.ramp import RampModel
 
-__all__ = ['RampModel', "MIRIRampModel"]
+__all__ = ['RampModel']
