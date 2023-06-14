4837ce664c305699e822913608340d9c7a51c419
jemorrison <morrison@as.arizona.edu>
2023-06-14 13:13:27 -0700

    test

--- jwst/outlier_detection/outlier_detection_ifu.py
+++ jwst/outlier_detection/outlier_detection_ifu.py
@@ -98,7 +98,7 @@ class OutlierDetectionIFU(OutlierDetection):
 
         # outlier_detection.py has the basic class that is used by all favors of outlier
         # detection. This class sets up self.inputs. We need to fill in self.input_models
-        # with flagged outliers (this is what outlier_detection_step.py returns)
+        # with flagged outliers (this is what outlier_detection_step.py returns).
         self.input_models = self.inputs
         self.build_suffix(**self.outlierpars)
         save_intermediate_results = \
