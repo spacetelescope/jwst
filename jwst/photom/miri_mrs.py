45e1174fa8c9089719c67a1d7cf08e0d6a6d3f29
jemorrison <morrison@as.arizona.edu>
2023-06-15 07:57:53 -0700

    more updates from review

--- jwst/photom/miri_mrs.py
+++ jwst/photom/miri_mrs.py
@@ -1,13 +1,45 @@
 import logging
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
+from gwcs.wcstools import grid_from_bounding_box
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
+# Set of routines to find a  correction for a time-wavelength
+# photometric response that is particularly significant at long wavelengths.
+
 
 def expfunc_bounded(x, a, b, c, x0):
-    """ Function using time dependent coefficients """
+    """
+    Short Summary
+    --------------
+    Time dependent flux loss function.
+
+    The model parameters are a,b,c, x0. x0 is the reference day from which the
+    time dependent parameters were derived. Each parameter is an array for
+    different wavelength regions. This function will return an array of
+    corrections at the given x day (MJD).
+
+    Parameters
+    ----------
+    x : int
+        Modified Julian Day (EXPMID) of the observation
+    a : numpy float array
+        Model parameter for time dependence
+    b : numpy float array
+        Model parameter for time dependence
+    c : numpy float array
+        Model parameter for time dependence
+    x0 : int
+        Reference day from which the time dependence
+        coefficients were derived.
+        (x-x0)/100 puts time in relevant units of 100 days
+
+    Returns
+    -------
+    The time-dependent correction flux loss array.
+    """
 
     temp = a * np.exp(-b * (x - x0) / 100) + c
     temp[temp > 1] = 1
@@ -15,7 +47,34 @@ def expfunc_bounded(x, a, b, c, x0):
 
 
 def get_correction_function(side, timecoeff, mid_time):
-    """ Constructing the time and wavelength dependent function """
+    """
+    Short Summary
+    -------------
+    Constructing the time and wavelength dependent function
+
+    The time dependence of the flux loss is described by the
+    exponential function contained in expfunc_bounded.
+    The MIRI MRS flux loss is time and wavelength dependent. The flux
+    loss is much larger at the long wavelengths of the MRS.
+
+    Parameters
+    ----------
+    side : str
+        Either 'left' or 'right'. The MRS contains 2 channels in every
+        exposure. The time-wavelength dependent correction is different
+        for every MRS band.
+    timecoeff : dictionary
+        A dictionay holding the correction factors for the time/wavelength
+        correction.  binwave, acoeff, bcoeff, ccoeff, x0 are the parameters
+        for an MRS time-wavelength photom correction.
+    mid-time : int
+        Modified Julian day
+
+    Returns
+    -------
+    Time-wavelength dependent photom loss correction
+
+    """
 
     binwave = timecoeff[side]['binwave']
     a = timecoeff[side]['acoeff']
@@ -23,8 +82,9 @@ def get_correction_function(side, timecoeff, mid_time):
     c = timecoeff[side]['ccoeff']
     x0 = timecoeff[side]['x0']
 
-    # time depenpent function
+    # time dependent function. Time is set by mid_time (modified julian day)
     corgrid = expfunc_bounded(mid_time, a, b, c, x0)
+
     # now fold in the wavelength dependence
     func = RegularGridInterpolator([binwave], corgrid,
                                    bounds_error=False, fill_value=None)
@@ -46,7 +106,7 @@ def time_correction(input, detector, ftab, mid_time):
     ftab : stdatamodels.jwst.datamodels.MirMrsPhotomModel
         MRS Photom reference file
 
-    mid_time: float
+    mid_time : float
         Exposure mid time in MJD
 
     Returns
@@ -55,8 +115,8 @@ def time_correction(input, detector, ftab, mid_time):
         An array of corrections to apply to data.
     """
 
-    # Tables to be read in from MRS Photom reference file
-    # Read in the time coefficients for each channel
+    # Read in the time-wavelength dependent coefficients
+    # for each channel from the MRS photom reference file
     table_ch = {}
     table_ch['ch1'] = ftab.timecoeff_ch1
     table_ch['ch2'] = ftab.timecoeff_ch2
@@ -65,9 +125,9 @@ def time_correction(input, detector, ftab, mid_time):
 
     # Each MIRI MRS exposure has 2 channels
     # We want to correct the channels seperately
-    # If we have MIRIFUSHORT data then channel 1 is on the left and channel 2
-    # is on the right. If we have MIRIFULONG  data then channel 4 is on the left
-    # and channel 3 is on right.
+    # If we have MIRIFUSHORT data then channel 1 is on the left
+    # and channel 2 is on the right. If we have MIRIFULONG  data
+    # then channel 4 is on the left and channel 3 is on right.
 
     timecoeff = {}
     timecoeff['left'] = {}
@@ -76,7 +136,7 @@ def time_correction(input, detector, ftab, mid_time):
     right = 'ch2'
     timecoeff['left']['xstart'] = 0
     timecoeff['left']['xend'] = 512
-    timecoeff['right']['xstart'] = 513
+    timecoeff['right']['xstart'] = 512
     timecoeff['right']['xend'] = 1031
     if detector == 'MIRIFULONG':
         left = 'ch4'
@@ -98,9 +158,9 @@ def time_correction(input, detector, ftab, mid_time):
     timecoeff['right']['x0'] = table_ch[right]['x0']
 
     ysize, xsize = input.data.shape
-    y, x = np.mgrid[:ysize, :xsize]
     # the correction is time and wavelength dependent. Pull out the
     # wavelength of the data
+    x, y = grid_from_bounding_box(input.meta.wcs.bounding_box)
     _, _, wave = input.meta.wcs(x, y)
 
     # correction for left side: set up the pixels to extract this region
@@ -115,8 +175,7 @@ def time_correction(input, detector, ftab, mid_time):
     # Determine the correction based input wavelength
     result1_1d = func(waveim.ravel())
     result1 = np.reshape(result1_1d, waveim.shape)
-    indx = np.where(waveim == 0)
-    result1[indx] = 0.0
+    result1[waveim == 0] = 0.0
 
     # correction of the right side: set up the pixels to extract this region
     side = 'right'
@@ -130,8 +189,7 @@ def time_correction(input, detector, ftab, mid_time):
     # Determine the correction based on input wavelength
     result2_1d = func(waveim.ravel())
     result2 = np.reshape(result2_1d, waveim.shape)
-    indx = np.where(waveim == 0)
-    result2[indx] = 0.0
+    result2[waveim == 0] = 0.0
 
     # Combine the bands to have 1 correction for the entire detector
     result = np.zeros((ysize, xsize))
@@ -139,7 +197,6 @@ def time_correction(input, detector, ftab, mid_time):
     result[:, r_xstart: r_xend] = result2
     indx = np.where(result == 0)
     result[indx] = 1.0
-    indx = np.where(np.isnan(result))
-    result[indx] = 1.0
+    result[np.isnan(result)] = 1.0
 
     return result
