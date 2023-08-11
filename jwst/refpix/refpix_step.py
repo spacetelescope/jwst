e0b35a629a9e919175853f5306714c29589164de
James Davies <jdavies@mpia.de>
2023-08-11 09:41:17 +0200

    Remove parentheses from refpix spec comments

--- jwst/refpix/refpix_step.py
+++ jwst/refpix/refpix_step.py
@@ -17,11 +17,11 @@ class RefPixStep(Step):
     class_alias = "refpix"
 
     spec = """
-        odd_even_columns = boolean(default=True) # Compute reference signal separately for even/odd columns (NIR only)
-        use_side_ref_pixels = boolean(default=True) # Use side reference pixels for reference signal for each row (NIR only)
-        side_smoothing_length = integer(default=11) # Median window smoothing height for side reference signal (NIR only)
-        side_gain = float(default=1.0) # Multiplicative factor for side reference signal before subtracting from rows (NIR only)
-        odd_even_rows = boolean(default=True) # Compute reference signal separately for even- and odd-numbered rows (MIRI only)
+        odd_even_columns = boolean(default=True) # Compute reference signal separately for even/odd columns
+        use_side_ref_pixels = boolean(default=True) # Use side reference pixels for reference signal for each row
+        side_smoothing_length = integer(default=11) # Median window smoothing height for side reference signal
+        side_gain = float(default=1.0) # Multiplicative factor for side reference signal before subtracting from rows
+        odd_even_rows = boolean(default=True) # Compute reference signal separately for even- and odd-numbered rows
         ovr_corr_mitigation_ftr = float(default=1.8) # Factor to avoid overcorrection of bad reference pixels for IRS2
     """
 
