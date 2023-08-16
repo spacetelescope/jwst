bdafeb53ed4f11af7f341e85f592bf5ac608c931
dmggh <grumm@stsci.edu>
2023-08-15 20:54:19 -0400

    Update tests per code updates

--- jwst/undersampling_correction/tests/test_undersampling_correction.py
+++ jwst/undersampling_correction/tests/test_undersampling_correction.py
@@ -12,9 +12,7 @@ test_dq_flags = dqflags.pixel
 GOOD = test_dq_flags["GOOD"]
 DNU = test_dq_flags["DO_NOT_USE"]
 UNSA = test_dq_flags["UNDERSAMP"]
-ADFL = test_dq_flags["AD_FLOOR"]
 DROU = test_dq_flags["DROPOUT"]
-
 UNSA_DNU = UNSA + DNU
 
 
@@ -44,40 +42,12 @@ def test_pix_0():
 
 def test_pix_1():
     """
-    All input GROUPDQ = 'GOOD'. Some ramp data exceed the signal threshold, so the
-    only non-GOOD groups in the output GROUPDQ should be UNSA + DNU for the first
-    group exceeding the signal threshold and all subsequent groups.
-    """
-    ngroups, nints, nrows, ncols = 10, 1, 1, 1
-    ramp_model, pixdq, groupdq, err = create_mod_arrays(
-        ngroups, nints, nrows, ncols)
-
-    signal_threshold = 20000.
-
-    # Populate pixel-specific SCI and GROUPDQ arrays
-    # Set seom groups' SCI to be above the signal threshold, and all input
-    # GROUPDQ to be GOOD
-    ramp_model.data[0, 3, 0, 0] = np.array((signal_threshold + 100.), dtype=np.float32)
-    ramp_model.data[0, 5, 0, 0] = np.array((signal_threshold - 200.), dtype=np.float32)
-    ramp_model.data[0, 8, 0, 0] = np.array((signal_threshold + 100.), dtype=np.float32)
-    ramp_model.groupdq[0, :, 0, 0] = [GOOD] * ngroups
-
-    true_out_gdq = ramp_model.groupdq.copy()  # all GOOD
-    true_out_gdq[0, :, 0, 0] = [GOOD] * ngroups
-    true_out_gdq[0, 3:, 0, 0] = np.bitwise_or(UNSA, DNU)
-
-    out_model = undersampling_correction(ramp_model, signal_threshold)
-    out_gdq = out_model.groupdq
-
-    npt.assert_array_equal(out_gdq, true_out_gdq)
-
-
-def test_pix_2():
-    """
-    Tests groups having data exceeding the signal threshold, Some groups are
-    already flagged as DO_NOT_USE; they will not be checked for UC,  Other
-    groups will have 'DNU'+'UNSA' added to their GROUPDQ, as will all later
-    groups.
+    Tests groups whose data exceeds the signal threshold; 1 group is already
+    flagged as DNU from a previous calibration step, and 1 group is GOOD.
+    Also tests groups whose data does not exceed the signal threshold;
+    similarly 1 group is already flagged as DNU from a previous calibration
+    step, and 1 is GOOD. All data beyond the first exceedance are also
+    flagged as UNDERSAMP and DNU.
     """
     ngroups, nints, nrows, ncols = 10, 1, 1, 1
     ramp_model, pixdq, groupdq, err = create_mod_arrays(
@@ -86,17 +56,21 @@ def test_pix_2():
     signal_threshold = 20000.
 
     # Populate SCI and GROUPDQ arrays.
-    ramp_model.data[0, 1, 0, 0] = np.array((signal_threshold + 100.), dtype=np.float32)
-    ramp_model.data[0, 2, 0, 0] = np.array((signal_threshold + 100.), dtype=np.float32)
-    ramp_model.data[0, 3, 0, 0] = np.array((signal_threshold + 100.), dtype=np.float32)
+    ramp_model.data[0, 1, 0, 0] = np.array((0.5 * signal_threshold), dtype=np.float32)
 
-    ramp_model.groupdq[0, 1, 0, 0] = DNU  # should not get UNSA
-    ramp_model.groupdq[0, 2, 0, 0] = np.bitwise_or(ADFL, DNU)  # should not get UNSA
-    ramp_model.groupdq[0, 3, 0, 0] = ADFL  # should get UNSA + DNU
+    ramp_model.data[0, 2, 0, 0] = np.array((0.8 * signal_threshold), dtype=np.float32)
+    ramp_model.groupdq[0, 2, 0, 0] = DNU  # should not get UNSA, not an exceedance
+
+    ramp_model.data[0, 3, 0, 0] = np.array((signal_threshold + 5000.), dtype=np.float32)
+    ramp_model.groupdq[0, 3, 0, 0] = DNU  # should not get UNSA, although exceedance
+
+    ramp_model.data[0, 4:, 0, 0] = np.array((signal_threshold + 6000.), dtype=np.float32)
+    ramp_model.groupdq[0, 4:, 0, 0] = GOOD
 
     true_out_gdq = ramp_model.groupdq.copy()
-    true_out_gdq[0, 3, 0, 0] = np.bitwise_or(np.bitwise_or(DNU, UNSA), ADFL)
-    true_out_gdq[0, 4:, 0, 0] = np.bitwise_or(DNU, UNSA)
+    true_out_gdq[0, 2, 0, 0] = DNU
+    true_out_gdq[0, 3, 0, 0] = DNU
+    true_out_gdq[0, 4:, 0, 0] = UNSA_DNU
 
     out_model = undersampling_correction(ramp_model, signal_threshold)
     out_gdq = out_model.groupdq
@@ -125,13 +99,14 @@ def test_too_few_groups():
 
 def test_flag_neighbors():
     """
-    Test flagging of 4 nearest neighbors of exceedances
+    Test flagging of 4 nearest neighbors of exceedances. Tests pixels on
+    array edges, Tests exclusion of groups previously flagged as DO_NOT_USE.
     """
     ngroups, nints, nrows, ncols = 6, 1, 4, 3
     ramp_model, pixdq, groupdq, err = create_mod_arrays(
         ngroups, nints, nrows, ncols)
 
-    signal_threshold = 4000.
+    signal_threshold = 4400.
 
     # Populate pixel-specific SCI and GROUPDQ arrays
     ramp_model.data[0, :, :, :] = \
@@ -166,6 +141,11 @@ def test_flag_neighbors():
             [4832., 4044., 4588.],
             [4799., 4233., 4000.]]], dtype=np.float32)
 
+    # These group DQ values should propagate unchanged to the output
+    ramp_model.groupdq[:, 4, 2, 0] = [DNU]
+    ramp_model.groupdq[:, 1, 2, 2] = [DNU]
+    ramp_model.groupdq[:, 2, 1, 1] = [DROU + DNU]
+
     out_model = undersampling_correction(ramp_model, signal_threshold)
     out_gdq = out_model.groupdq
 
@@ -177,27 +157,27 @@ def test_flag_neighbors():
             [0,   0,   0],
             [0,   0,   0]],
 
-           [[UNSA_DNU,   0,   0],
-            [UNSA_DNU, UNSA_DNU,   0],
-            [UNSA_DNU, UNSA_DNU,   0],
-            [UNSA_DNU,   0,   0]],
+           [[0,   0,   0],
+            [0,   0,   0],
+            [0,   0,   DNU],
+            [0,   0,   0]],
 
-           [[UNSA_DNU,   0,   0],
-            [UNSA_DNU, UNSA_DNU,   0],
-            [UNSA_DNU, UNSA_DNU,   0],
-            [UNSA_DNU, UNSA_DNU,   0]],
+           [[0,   0,   0],
+            [0,   9,   0],
+            [0,   0,   0],
+            [0,   0,   0]],
 
-           [[UNSA_DNU,   0,   0],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU]],
+           [[UNSA_DNU,   0,      0],
+            [UNSA_DNU, UNSA_DNU, 0],
+            [UNSA_DNU, UNSA_DNU, 0],
+            [UNSA_DNU,   0,      0]],
 
-           [[UNSA_DNU,   0,   0],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU],
-            [UNSA_DNU, UNSA_DNU, UNSA_DNU]],
+           [[UNSA_DNU,   0,      0],
+            [UNSA_DNU, UNSA_DNU, 0],
+            [DNU,        0,      0],
+            [UNSA_DNU, UNSA_DNU, 0]],
 
-           [[UNSA_DNU,   0, UNSA_DNU],
+           [[UNSA_DNU,   0,       0],
             [UNSA_DNU, UNSA_DNU, UNSA_DNU],
             [UNSA_DNU, UNSA_DNU, UNSA_DNU],
             [UNSA_DNU, UNSA_DNU, UNSA_DNU]]], dtype=np.uint8)
