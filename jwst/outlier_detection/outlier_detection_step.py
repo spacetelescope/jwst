87cf40db4af3c6f497d0e2b6d0de100b72386886
Tyler Pauly <tapastro@gmail.com>
2023-09-20 12:53:02 -0400

    add median file to cleanup block

--- jwst/outlier_detection/outlier_detection_step.py
+++ jwst/outlier_detection/outlier_detection_step.py
@@ -177,18 +177,20 @@ class OutlierDetectionStep(Step):
                 for model in self.input_models:
                     model.meta.cal_step.outlier_detection = state
                     if not self.save_intermediate_results:
-                        # remove unwanted files
-                        crf_file = self.make_output_path(basepath=model.meta.filename)
-                        outlr_basename = os.path.basename(crf_file)
-                        outlr_file = model.meta.filename.replace('cal', 'outlier_i2d')
-                        outlr_path = crf_file.replace(outlr_basename, outlr_file)
-                        if os.path.isfile(outlr_path):
-                            os.remove(outlr_path)
-                            self.log.debug(f"    {outlr_path}")
-                        blot_file = crf_file.replace('crf', 'blot')
-                        if os.path.isfile(blot_file):
-                            os.remove(blot_file)
-                            self.log.debug(f"    {blot_file}")
+                        # Remove unwanted files
+                        crf_path = self.make_output_path(basepath=model.meta.filename)
+                        crf_file = os.path.basename(crf_path)
+                        outlr_file = crf_file.replace('crf', 'outlier_i2d')
+                        outlr_path = crf_path.replace(crf_file, outlr_file)
+                        blot_path = outlr_path.replace('outlier_i2d', 'blot')
+                        # Need to insert outlier into filename, as median filename is generated from drizzled model
+                        undersplit = outlr_file.split('_')
+                        median_file = '_'.join(undersplit[:-3]) + '_'.join(['_outlier', undersplit[-3], 'median.fits'])
+                        median_path = crf_path.replace(crf_file, median_file)
+                        for fle in [outlr_path, blot_path, median_path]:
+                            if os.path.isfile(fle):
+                                os.remove(fle)
+                                self.log.debug(f"    {fle}")
             else:
                 self.input_models.meta.cal_step.outlier_detection = state
             return self.input_models
