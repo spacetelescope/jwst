f8fb8deaea4e6a11afc19e215fb2794a098eb780
Melanie Clarke <mclarke@stsci.edu>
2023-09-28 16:35:55 -0400

    Fix comments and consolidate code

--- jwst/flatfield/flat_field.py
+++ jwst/flatfield/flat_field.py
@@ -440,9 +440,9 @@ def nirspec_fs_msa(output_model, f_flat_model, s_flat_model, d_flat_model, dispa
         # Combine the science and flat DQ arrays
         slit.dq |= slit_flat.dq
 
-        # Make sure any new DO_NOT_USE pixels are NaN
-        dnu = np.bitwise_and(slit.dq, dqflags.pixel['DO_NOT_USE'])
-        slit.data[np.where(dnu)] = np.nan
+        # Make sure all DO_NOT_USE pixels are set to NaN,
+        # including those flagged by this step
+        slit.data[np.where(slit.dq & dqflags.pixel['DO_NOT_USE'])] = np.nan
 
         any_updated = True
 
@@ -519,9 +519,9 @@ def nirspec_brightobj(output_model, f_flat_model, s_flat_model, d_flat_model, di
         output_model.var_poisson + output_model.var_rnoise + output_model.var_flat
     )
 
-    # Make sure any new DO_NOT_USE pixels are NaN
-    dnu = np.bitwise_and(output_model.dq, dqflags.pixel['DO_NOT_USE'])
-    output_model.data[np.where(dnu)] = np.nan
+    # Make sure all DO_NOT_USE pixels are set to NaN,
+    # including those flagged by this step
+    output_model.data[np.where(output_model.dq & dqflags.pixel['DO_NOT_USE'])] = np.nan
 
     output_model.meta.cal_step.flat_field = 'COMPLETE'
 
@@ -589,9 +589,9 @@ def nirspec_ifu(output_model, f_flat_model, s_flat_model, d_flat_model, dispaxis
             output_model.var_poisson + output_model.var_rnoise + output_model.var_flat
         )
 
-        # Make sure any new DO_NOT_USE pixels are NaN
-        dnu = np.bitwise_and(output_model.dq, dqflags.pixel['DO_NOT_USE'])
-        output_model.data[np.where(dnu)] = np.nan
+        # Make sure all DO_NOT_USE pixels are set to NaN,
+        # including those flagged by this step
+        output_model.data[np.where(output_model.dq & dqflags.pixel['DO_NOT_USE'])] = np.nan
 
         output_model.meta.cal_step.flat_field = 'COMPLETE'
 
