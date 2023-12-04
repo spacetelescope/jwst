2f83b568f033b11f7ce7346138259d36ae5159f9
Maria Pena-Guerrero <pena@stsci.edu>
2023-12-04 13:54:08 -0500

    modified test according to code

--- jwst/emicorr/tests/test_emicorr.py
+++ jwst/emicorr/tests/test_emicorr.py
@@ -9,21 +9,33 @@ import numpy as np
 from jwst.emicorr import emicorr
 from stdatamodels.jwst.datamodels import Level1bModel
 
+default_subarray_cases = emicorr.default_subarray_cases
+
+def mk_emi_mdl(data, subarray, readpatt, detector):
+    # create input_model
+    input_model = Level1bModel(data=data)
+    input_model.meta.instrument.detector = detector
+    input_model.meta.exposure.type = 'MIR_4QPM'
+    input_model.meta.subarray.name = subarray
+    input_model.meta.exposure.readpatt = readpatt
+    input_model.meta.subarray.xsize = 288
+    input_model.meta.subarray.xstart = 1
+    input_model.meta.exposure.nsamples = 1
+    return input_model
+
 
 def test_get_subarcase():
-    # set up a real subarray
-    subarray, readpatt = 'MASK1550', 'FASTR1'
-    subarray_info_r = emicorr.get_subarcase(subarray, readpatt)
+    data = np.ones((1, 5, 20, 20))
+    subarray, readpatt, detector = 'MASK1550', 'FASTR1', 'MIRIMAGE'
+    subarray_info_r = emicorr.get_subarcase(default_subarray_cases, subarray, readpatt, detector)
     subname_r, rowclocks_r, frameclocks_r, freqs2correct_r = subarray_info_r
 
     # set up a fake configuration
-    subarray, readpatt = 'FULL', 'kkkkk'
-    subarray_info_f = emicorr.get_subarcase(subarray, readpatt)
+    subarray_info_f = emicorr.get_subarcase(default_subarray_cases, 'FULL', 'kkkkk', detector)
     subname_f, rowclocks_f, frameclocks_f, freqs2correct_f = subarray_info_f
 
     # test if we get the right configuration
-    compare_real = ['MASK1550', 82, 23968, [390.625, 218.52055, 218.520470,
-                    218.520665, 164.9305, 10.039216]]
+    compare_real = ['MASK1550', 82, 23968, ["Hz390", "Hz10"]]
     subname_real, rowclocks_real, frameclocks_real, freqs2correct_real = compare_real
 
     compare_fake = ['FULL_kkkkk', None, None, None]
@@ -41,13 +53,7 @@ def test_get_subarcase():
 
 def test_apply_emicorr():
     data = np.ones((1, 5, 20, 20))
-    input_model = Level1bModel(data=data)
-    input_model.meta.exposure.type = 'MIR_4QPM'
-    input_model.meta.subarray.name = 'MASK1550'
-    input_model.meta.exposure.readpatt = 'FASTR1'
-    input_model.meta.subarray.xsize = 288
-    input_model.meta.subarray.xstart = 1
-    input_model.meta.exposure.nsamples = 1
+    input_model = mk_emi_mdl(data, 'MASK1550', 'FASTR1', 'MIRIMAGE')
     emicorr_model = None
     save_onthefly_reffile = None
     output_model = emicorr.apply_emicorr(input_model, emicorr_model, save_onthefly_reffile)
