b38eeb9ab499426fd15f0f58f7076b78c217983c
Nadia Dencheva <nadia.dencheva@gmail.com>
2023-12-15 20:15:04 -0500

    Fix a typo in version_commit string (#8145)

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -5,7 +5,7 @@ from importlib.metadata import version
 __version__ = version(__name__)
 
 _regex_git_hash = re.compile(r".*\+g(\w+)")
-__version__commit__ = ""
+__version_commit__ = ""
 if "+" in __version__:
     commit = _regex_git_hash.match(__version__).groups()
     if commit:
