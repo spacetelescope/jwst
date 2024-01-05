0190f0b4d44fae1dcf55372eb8feaf4b0f9306e5
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2024-01-05 17:20:19 +0800

    Add test for reproject

--- jwst/resample/tests/test_utils.py
+++ jwst/resample/tests/test_utils.py
@@ -1,15 +1,19 @@
 """Test various utility functions"""
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
+from astropy import wcs as fitswcs
+from astropy.io import fits
 import numpy as np
 import pytest
 
 from stdatamodels.jwst.datamodels import SlitModel, ImageModel, dqflags
 
+from jwst.assign_wcs import AssignWcsStep
 from jwst.resample.resample_spec import find_dispersion_axis
 from jwst.resample.resample_utils import (
     build_mask,
     build_driz_weight,
-    decode_context
+    decode_context,
+    reproject
 )
 
 
@@ -23,6 +27,55 @@ BITVALUES_STR = f'{2**0}, {2**2}'
 BITVALUES_INV_STR = f'~{2**0}, {2**2}'
 JWST_NAMES = 'DO_NOT_USE, JUMP_DET'
 JWST_NAMES_INV = '~' + JWST_NAMES
+
+
+@pytest.fixture
+def create_hdul(wcskeys={
+        'wcsaxes': 2,
+        'ra_ref': 22.02351763251896,
+        'dec_ref': 11.99875540218638,
+        'v2_ref': 86.039011,
+        'v3_ref': -493.385704,
+        'roll_ref': 0.005076934167039675},
+        data_shape=(2048, 2048)):
+    """Create nircam hdulist specifically for the SIP test."""
+    hdul = fits.HDUList()
+    phdu = fits.PrimaryHDU()
+    phdu.header['DATAMODL'] = 'ImageModel'
+    phdu.header['TELESCOP'] = "JWST"
+    phdu.header['FILENAME'] = "test+F444W"
+    phdu.header['INSTRUME'] = 'NIRCAM'
+    phdu.header['CHANNEL'] = 'LONG'
+    phdu.header['DETECTOR'] = 'NRCALONG'
+    phdu.header['FILTER'] = 'F444W'
+    phdu.header['PUPIL'] = 'CLEAR'
+    phdu.header['MODULE'] = 'A'
+    phdu.header['TIME-OBS'] = '16:58:27.258'
+    phdu.header['DATE-OBS'] = '2021-10-25'
+    phdu.header['EXP_TYPE'] = 'NRC_IMAGE'
+    scihdu = fits.ImageHDU()
+    scihdu.header['EXTNAME'] = "SCI"
+    scihdu.header['SUBARRAY'] = 'FULL'
+
+    scihdu.header.update(wcskeys)
+
+    scihdu.data = np.zeros(data_shape)
+
+    hdul.append(phdu)
+    hdul.append(scihdu)
+
+    return hdul
+
+
+@pytest.fixture
+def create_gwcs():
+    hdu1 = create_hdul()
+    im = ImageModel(hdu1)
+    
+    pipe = AssignWcsStep()
+    result = pipe.call(im)
+    w = result.meta.wcs
+    return w
 
 
 @pytest.mark.parametrize(
@@ -116,3 +169,43 @@ def test_decode_context():
 
     assert sorted(idx1) == [9, 12, 14, 19, 21, 25, 37, 40, 46, 58, 64, 65, 67, 77]
     assert sorted(idx2) == [9, 20, 29, 36, 47, 49, 64, 69, 70, 79]
+
+
+def test_reproject():
+    gwcs_wcs = create_gwcs()
+    astropy_wcs = fitswcs.WCS(gwcs_wcs.to_fits_sip())
+    xmin, xmax = 100, 500
+    slices = (slice(xmin, xmax), slice(xmin, xmax))
+    sliced_wcs = fitswcs.wcsapi.SlicedLowLevelWCS(gwcs_wcs, slices)
+
+    x = np.arange(150, 200)
+    
+    f = reproject(gwcs_wcs, astropy_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0], atol=0.1, rtol=0)
+    assert_allclose(x, res[1], atol=0.1, rtol=0)
+
+    f = reproject(astropy_wcs, gwcs_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0], atol=0.1, rtol=0)
+    assert_allclose(x, res[1], atol=0.1, rtol=0)
+
+    f = reproject(gwcs_wcs, sliced_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0] + xmin, atol=0.1, rtol=0)
+    assert_allclose(x, res[1] + xmin, atol=0.1, rtol=0)
+
+    f = reproject(sliced_wcs, gwcs_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0] - xmin, atol=0.1, rtol=0)
+    assert_allclose(x, res[1] - xmin, atol=0.1, rtol=0)
+
+    f = reproject(astropy_wcs, sliced_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0] + xmin, atol=0.1, rtol=0)
+    assert_allclose(x, res[1] + xmin, atol=0.1, rtol=0)
+
+    f = reproject(sliced_wcs, astropy_wcs)
+    res = f(x, x)
+    assert_allclose(x, res[0] - xmin, atol=0.1, rtol=0)
+    assert_allclose(x, res[1] - xmin, atol=0.1, rtol=0)
