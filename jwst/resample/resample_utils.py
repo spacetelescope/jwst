a9f22be948109370a3ca69b3457fecaa1073ac4c
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2024-01-05 17:26:03 +0800

    Fix a bug in reproject
    `all_pix2world` and `all_world2pix` require `origin` to be set

--- jwst/resample/resample_utils.py
+++ jwst/resample/resample_utils.py
@@ -146,7 +146,7 @@ def reproject(wcs1, wcs2):
     """
 
     if isinstance(wcs1, fitswcs.WCS):
-        forward_transform = wcs1.all_pix2world
+        forward_transform = lambda x, y: wcs1.all_pix2world(x, y, 0)
     elif isinstance(wcs1, gwcs.WCS):
         forward_transform = wcs1.forward_transform
     elif isinstance(wcs1, fitswcs.wcsapi.SlicedLowLevelWCS):
@@ -158,7 +158,7 @@ def reproject(wcs1, wcs2):
                         "object or astropy.modeling.Model subclass")
 
     if isinstance(wcs2, fitswcs.WCS):
-        backward_transform = wcs2.all_world2pix
+        backward_transform = lambda x, y: wcs2.all_world2pix(x, y, 0)
     elif isinstance(wcs2, gwcs.WCS):
         backward_transform = wcs2.backward_transform
     elif isinstance(wcs2, fitswcs.wcsapi.SlicedLowLevelWCS):
