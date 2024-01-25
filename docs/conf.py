61384b54adef95ba3a7945abdf3ac7f7233754e2
Brett <brettgraham@gmail.com>
2024-01-25 16:07:23 -0500

    turn off sticky navigation

--- docs/conf.py
+++ docs/conf.py
@@ -261,7 +261,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "collapse_navigation": True
+    "collapse_navigation": True,
+    "sticky_navigation": False,
     # "nosidebar": "false",
     # "sidebarbgcolor": "#4db8ff",
     # "sidebartextcolor": "black",
