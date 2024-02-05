13070149c2f90db9dffae4cc84df1ffad490447f
Ned Molter <emolter@stsci.edu>
2024-02-05 10:56:48 -0500

    removed test reliance on fits file per @hbushouse suggestion

--- jwst/extract_1d/tests/test_expected_skips.py
+++ jwst/extract_1d/tests/test_expected_skips.py
@@ -2,20 +2,31 @@ import os
 import stdatamodels.jwst.datamodels as dm
 from jwst.extract_1d import Extract1dStep
 from jwst.photom import PhotomStep
-
-data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
-TEST_FILES = {
-    'SOSS/FULL': os.path.join(data_dir, 'jw01209001001_03101_00001_nis_pathloss_dummy.fits'),
-}
+import pytest
+import numpy as np
 
 
-def test_expected_skip_niriss_soss_full():
+@pytest.fixture(scope='module')
+def mock_niriss_full():
+    model = dm.CubeModel((3, 3, 3))
+    model.meta.instrument.name = 'NIRISS'
+    model.meta.instrument.detector = 'NIS'
+    model.meta.observation.date = '2023-07-22'
+    model.meta.observation.time = '06:24:45.569'
+    model.meta.instrument.name = 'NIRISS'
+    model.meta.instrument.detector = 'NIS'
+    model.meta.instrument.filter = 'CLEAR'
+    model.meta.exposure.type = 'NIS_SOSS'
+    model.meta.subarray.name = 'FULL'
+    model.data = np.arange(27).reshape((3, 3, 3))
+    return model
 
-    infile = TEST_FILES['SOSS/FULL']
-    with dm.open(infile) as model:
+
+def test_expected_skip_niriss_soss_full(mock_niriss_full):
+
+    with mock_niriss_full as model:
         result = Extract1dStep().process(model)
         result2 = PhotomStep().process(result)
         assert result2.meta.cal_step.photom == 'SKIPPED'
         assert result2.meta.cal_step.extract_1d == 'SKIPPED'
-
-    assert result2.data == model.data
+        assert np.all(result2.data == model.data)
