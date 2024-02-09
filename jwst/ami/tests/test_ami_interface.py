4622246d7e4a4ecd5d753b6f6c76f2af7bfb5259
Brett <brettgraham@gmail.com>
2024-02-09 12:26:05 -0500

    refine test error matches

--- jwst/ami/tests/test_ami_interface.py
+++ jwst/ami/tests/test_ami_interface.py
@@ -44,16 +44,8 @@ def example_model(mock_nrm_reference_file):
     model.meta.subarray.name = "SUB80"
     model.meta.observation.date = "2021-12-26"
     model.meta.observation.time = "00:00:00"
-    model.meta.target.proposer_name = ""
-    model.meta.program.pi_name = "someone"
-    model.meta.target.catalog_name = ""
-    model.meta.visit.start_time = "2022-06-05 12:15:41.5020000"
-    model.meta.pointing.pa_v3 = 171.8779402866089
-    model.meta.wcsinfo.v3yangle = 0.56126717
-    model.meta.filename = "test_calints.fits"
-    model.meta.instrument.pupil = "NRM"
-    model.meta.exposure.type = "NIS_AMI"
-    return model
+    with pytest.raises(RuntimeError, match="Input must be a 2D ImageModel."):
+        AmiAnalyzeStep.call(model)
 
 
 @pytest.mark.parametrize("oversample", [2, 4])
@@ -72,8 +64,4 @@ def test_ami_analyze_no_reffile_fail(monkeypatch, example_model):
     monkeypatch.setattr(stpipe.crds_client, 'get_reference_file', mockreturn)
 
     with pytest.raises(RuntimeError, match="No throughput reference file found."):
-        AmiAnalyzeStep.call(example_model)
-
-
-def test_ami_analyze_step(example_model):
-    AmiAnalyzeStep.call(example_model)
+        AmiAnalyzeStep.call(model)
