84d8b3f265f5fee2b373adc2bbece75dc20f243e
Brett <brettgraham@gmail.com>
2024-02-09 13:05:51 -0500

    add test for get_filt_spec

--- jwst/ami/tests/test_ami_utils.py
+++ jwst/ami/tests/test_ami_utils.py
@@ -1,28 +1,16 @@
+from synphot.spectrum import SpectralElement
 
-import numpy as np
 import pytest
 
 import jwst.ami.utils as utils
 
 
-@pytest.mark.parametrize("shape, center", [
-    ((10, 10), (4.5, 4.5)),
-    ((11, 11), (5, 5)),
-])
-def test_centerpoint(shape, center):
-    assert utils.centerpoint(shape) == center
+@pytest.mark.parametrize("filt", ["F277W", "F380M", "F430M", "F480M"])
+def test_get_filt_spec(filt):
+    spec = utils.get_filt_spec(filt)
+    assert isinstance(spec, SpectralElement)
 
 
-def test_find_centroid():
-    arr = np.zeros((30, 30), dtype='f4')
-    arr[15, 15] = 1
-    thresh = 0.02
-    assert np.allclose(utils.find_centroid(arr, thresh), (0.5, 0.5))
-
-
-@pytest.mark.parametrize("mas, rad", [
-    (206264.8062471, 0.001),
-    (103132403.12355, 0.5),
-])
-def test_mas2rad(mas, rad):
-    assert np.isclose(utils.mas2rad(mas), rad)
+def test_get_filt_spec_fail():
+    with pytest.raises(ValueError, match="not a known NIRISS AMI filter"):
+        utils.get_filt_spec("unknown")
