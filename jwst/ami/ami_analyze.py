Correct NaNs in median image used to find rotation. Add 'fudge' factor to avoid singularity in centered psf models as in ImPlaneIA.

jwst/ami/ami_analyze.py
@@ -4,7 +4,6 @@ import numpy as np
 import copy
 import synphot
 
-
 from .find_affine2d_parameters import find_rotation
 from . import instrument_data
 from . import nrm_core
@@ -151,7 +150,22 @@ def apply_LG_plus(input_model,
     if affine2d is None:
         # affine2d object, can be overridden by user input affine.
         # do rotation search on uncropped median image (assuming rotation constant over exposure)
+        # replace remaining NaNs in median image with median of surrounding 8 (non-NaN) pixels
+        # (only used for centroiding in rotation search)
         meddata = np.median(data,axis=0)
+        nan_locations = np.where(np.isnan(meddata))
+        log.info(f'Replacing {len(nan_locations[0])} NaNs in median image with median of surrounding pixels')
+        box_size = 3
+        hbox = int(box_size / 2)
+        for i_pos in range(len(nan_locations[0])):
+            y_box = nan_locations[0][i_pos]
+            x_box = nan_locations[1][i_pos]
+            box = meddata[y_box - hbox:y_box + hbox + 1, x_box - hbox: x_box + hbox + 1]
+            median_fill = np.nanmedian(box)
+            if np.isnan(median_fill):
+                median_fill = 0 # not ideal
+            meddata[y_box, x_box] = median_fill
+
         affine2d = find_rotation(meddata, psf_offset, rotsearch_d,
                                  mx, my, sx, sy, xo, yo,
                                  PIXELSCALE_r, dim, bandpass, oversample, holeshape)

jwst/ami/hextransformee.py
@@ -119,7 +119,7 @@ def hextransform(s=None, c=None, d=None, lam=None, pitch=None, affine2d=None):
                                   pixel=pitch, affine2d=affine2d, minus=False) + \
         np.fromfunction(gfunction, s, d=d, c=c_adjust, lam=lam,
                         pixel=pitch, affine2d=affine2d, minus=True)
-
-    hex_complex[int(c[0]), int(c[1])] = (np.sqrt(3) / 2.0)
+    fudge = np.sqrt(4.0) # this gives the analytic central PSF correctly.
+    hex_complex[int(c[0]),int(c[1])] = fudge * (np.sqrt(3) / 4.0)
 
     return hex_complex