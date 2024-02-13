8efc42e952fc2e0d6809e71f82917360067275d7
Brett <brettgraham@gmail.com>
2024-02-13 17:03:42 -0500

    add a few more unit tests

--- jwst/ami/tests/test_ami_utils.py
+++ jwst/ami/tests/test_ami_utils.py
@@ -44,3 +44,26 @@ def test_get_src_spec_default():
     assert isinstance(spec, SourceSpectrum)
     # check model expression for matching teff
     re.match(r".*T_eff=(?P<teff>[0-9]+)", spec.meta["expr"]).group("teff") == "9500"
+
+
+@pytest.mark.parametrize("shape, center", [
+    ((10, 10), (4.5, 4.5)),
+    ((11, 11), (5, 5)),
+])
+def test_centerpoint(shape, center):
+    assert utils.centerpoint(shape) == center
+
+
+def test_find_centroid():
+    arr = np.zeros((30, 30), dtype='f4')
+    arr[15, 15] = 1
+    thresh = 0.02
+    assert np.allclose(utils.find_centroid(arr, thresh), (0.5, 0.5))
+
+
+@pytest.mark.parametrize("mas, rad", [
+    (206264.8062471, 0.001),
+    (103132403.12355, 0.5),
+])
+def test_mas2rad(mas, rad):
+    assert np.isclose(utils.mas2rad(mas), rad)
