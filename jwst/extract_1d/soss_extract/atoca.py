0352cbf8343bd69b2b31b2d4333ba5f177130f76
Ned Molter <emolter@users.noreply.github.com>
2024-02-16 06:20:39 -0800

    JP-3483: fix atoca failures due to sparse matrix solver (#8273)

--- jwst/extract_1d/soss_extract/atoca.py
+++ jwst/extract_1d/soss_extract/atoca.py
@@ -12,8 +12,9 @@ ATOCA: Algorithm to Treat Order ContAmination (English)
 
 # General imports.
 import numpy as np
+import warnings
 from scipy.sparse import issparse, csr_matrix, diags
-from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import spsolve, lsqr, MatrixRankWarning
 from scipy.interpolate import interp1d
 
 # Local imports.
@@ -1290,7 +1291,15 @@ class _BaseOverlap:
         # Only solve for valid indices, i.e. wavelengths that are
         # covered by the pixels on the detector.
         # It will be a singular matrix otherwise.
-        sln[idx] = spsolve(matrix[idx, :][:, idx], result[idx])
+        warnings.filterwarnings(action='error', category=MatrixRankWarning)
+        try:
+            sln[idx] = spsolve(matrix[idx, :][:, idx], result[idx])
+        except MatrixRankWarning:
+            # on rare occasions spsolve's approximation of the matrix is not appropriate
+            # and fails on good input data. revert to different solver
+            log.info('ATOCA matrix solve failed with spsolve. Retrying with least-squares.')
+            sln[idx] = lsqr(matrix[idx, :][:, idx], result[idx])[0]
+        warnings.resetwarnings()
 
         return sln
 
