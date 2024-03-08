4401a7208e0489dffe7d1358b64e642239685f23
Rachel Cooper <rcooper@stsci.edu>
2024-03-08 14:55:26 -0500

    remove stsynphot dependency and unit tests

--- jwst/ami/tests/test_ami_utils.py
+++ jwst/ami/tests/test_ami_utils.py
@@ -7,26 +7,6 @@ from synphot.spectrum import SourceSpectrum
 import jwst.ami.utils as utils
 
 
-# only testing a few of the catalog entries
-@pytest.mark.parametrize("spectype, teff", [
-    ("O3V", 45000),
-    ("M2I", 3500),
-])
-def test_get_src_spec(spectype, teff):
-    spec = utils.get_src_spec(spectype)
-    assert isinstance(spec, SourceSpectrum)
-    # check model expression for matching teff
-    re.match(r".*T_eff=(?P<teff>[0-9]+)", spec.meta["expr"]).group("teff") == str(teff)
-
-
-def test_get_src_spec_default():
-    with pytest.warns(UserWarning, match="Input spectral type missing did not match"):
-        spec = utils.get_src_spec("missing")
-    assert isinstance(spec, SourceSpectrum)
-    # check model expression for matching teff
-    re.match(r".*T_eff=(?P<teff>[0-9]+)", spec.meta["expr"]).group("teff") == "9500"
-
-
 @pytest.mark.parametrize("shape, center", [
     ((10, 10), (4.5, 4.5)),
     ((11, 11), (5, 5)),
