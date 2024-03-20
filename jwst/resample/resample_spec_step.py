fc8062bd086df56d31b41ec8991e5934724f5287
Maria Pena-Guerrero <pena@stsci.edu>
2024-03-20 15:37:09 -0400

    bug fixes

--- jwst/resample/resample_spec_step.py
+++ jwst/resample/resample_spec_step.py
@@ -7,6 +7,9 @@ from jwst.datamodels import ModelContainer
 from . import resample_spec, ResampleStep
 from ..exp_to_source import multislit_to_container
 from ..assign_wcs.util import update_s_region_spectral
+from jwst.lib.wcs_utils import get_wavelengths
+
+import numpy as np
 
 # Force use of all DQ flagged data except for DO_NOT_USE and NON_SCIENCE
 GOOD_BITS = '~DO_NOT_USE+NON_SCIENCE'
@@ -109,13 +112,13 @@ class ResampleSpecStep(ResampleStep):
         result.meta.asn.table_name = input_models[0].meta.asn.table_name
         result.meta.asn.pool_name = input_models[0].meta.asn.pool_name
 
-        # carry over the result wavelength attribute
-        if model_is_msm:
-            for slit_idx, slit in enumerate(input_new.slits):
-                wl_array = slit.wavelength
-                result.slits[slit_idx].wavelength = wl_array
-        else:
-            result.wavelength = input.wavelength
+        # populate the result wavelength attribute
+        if isinstance(result, MultiSlitModel):
+            for slit_idx, slit in enumerate(result.slits):
+                result_wl = result.slits[slit_idx].wavelength
+                if len(result_wl) == 0 or np.all(result_wl == 0.0):
+                    wl_array = get_wavelengths(result.slits[slit_idx])
+                    result.slits[slit_idx].wavelength = wl_array
 
         return result
 
