9a3e62abea0ddca8e29f6c73f1bf0c2561621354
Brett <brettgraham@gmail.com>
2024-03-28 09:20:20 -0400

    fix some skymatch tests

--- jwst/skymatch/tests/test_skymatch.py
+++ jwst/skymatch/tests/test_skymatch.py
@@ -425,8 +425,6 @@ def test_asn_input(tmp_cwd, nircam_rate, tmp_path):
 
     result = step.run(asn_out_fname)
 
-    assert isinstance(result, str)
-
     ref_levels = np.subtract(levels, min(levels))
     sub_levels = np.subtract(levels, ref_levels)
 
