JP-2075: fixed scaling issues, some loose ends still remaining though

jwst/wfss_contam/disperse.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from scipy.interpolate import interp1d
+import warnings
 
 from ..lib.winclip import get_clipped_pixels
-#from .sens1d import create_1d_sens
+from .sens1d import create_1d_sens
 
 
 def dispersed_pixel(x0, y0, width, height, lams, flxs, order, wmin, wmax,
-                    seg_wcs, grism_wcs, ID, naxis,
+                    sens_waves, sens_resp, seg_wcs, grism_wcs, ID, naxis,
                     oversample_factor=2, extrapolate_sed=False, xoffset=0,
                     yoffset=0):
     """
@@ -155,7 +156,7 @@ def dispersed_pixel(x0, y0, width, height, lams, flxs, order, wmin, wmax,
         return None
 
     # compute 1D sensitivity array corresponding to list of wavelengths
-    #sens, no_cal = create_1d_sens(lams, sens_waves, sens_resp)
+    sens, no_cal = create_1d_sens(lams, sens_waves, sens_resp)
 
     # Compute countrates for dispersed pixels. Note that dispersed pixel
     # values are naturally in units of physical fluxes, so we divide out
@@ -163,7 +164,9 @@ def dispersed_pixel(x0, y0, width, height, lams, flxs, order, wmin, wmax,
     # countrate (DN/s).
     # flux(lams) is either single-valued (for a single direct image) 
     # or an array of the same length as lams (for multiple direct images in different filters)
-    counts = flux(lams) * areas # / sens
-    #counts[no_cal] = 0.  # set to zero where no flux cal info available
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=RuntimeWarning, message="divide by zero")
+        counts = flux(lams) * areas / (sens * oversample_factor)
+    counts[no_cal] = 0.  # set to zero where no flux cal info available
 
     return xs, ys, areas, lams, counts, ID

jwst/wfss_contam/observations.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
-from multiprocessing import Pool
+import multiprocessing as mp
+import concurrent.futures
 
 from scipy import sparse
 
@@ -11,8 +12,51 @@ from .disperse import dispersed_pixel
 import logging
 import warnings
 
+from photutils.background import Background2D, MedianBackground
+from astropy.stats import SigmaClip
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+
+
+def background_subtract(data, box_size=None, filter_size=(3,3), sigma=3.0, exclude_percentile=30.0):
+    """
+    Simple astropy background subtraction
+
+    Parameters
+    ----------
+    data : np.ndarray
+        2D array of pixel values
+    box_size : tuple
+        Size of box in pixels to use for background estimation. 
+        If not set, defaults to 1/5 of the image size.
+    filter_size : tuple
+        Size of filter to use for background estimation
+    sigma : float
+        Sigma threshold for background clipping
+    exclude_percentile : float
+        Percentage of masked pixels above which box is excluded from background estimation
+
+    Returns
+    -------
+    data : np.ndarray
+        2D array of pixel values with background subtracted
+
+    Notes
+    -----
+    Improper background subtraction in input _i2d image leads to extra flux
+    in the simulated dispersed image, and was one cause of flux scaling issues
+    in a previous version.
+    """
+    if box_size is None:
+        box_size = (int(data.shape[0]/5), int(data.shape[1]/5))
+    sigma_clip = SigmaClip(sigma=sigma)
+    bkg_estimator = MedianBackground()
+    bkg = Background2D(data, (500, 500), filter_size=filter_size,
+                   sigma_clip=sigma_clip, bkg_estimator=bkg_estimator, 
+                   exclude_percentile=exclude_percentile)
+
+    return data - bkg.background
 
 
 class Observation:
@@ -121,8 +165,6 @@ class Observation:
                 self.xs.append(xs)
                 self.ys.append(ys)
 
-        print("length of xs and ys", len(self.xs), len(self.ys))
-
         # Populate lists of direct image flux values for the sources.
         self.fluxes = {}
         for dir_image_name in self.dir_image_names:
@@ -130,6 +172,7 @@ class Observation:
             log.info(f"Using direct image {dir_image_name}")
             with datamodels.open(dir_image_name) as model:
                 dimage = model.data
+                dimage = background_subtract(dimage)
 
                 if self.sed_file is None:
                     # Default pipeline will use sed_file=None, so we need to compute
@@ -160,7 +203,7 @@ class Observation:
                     for i in range(len(self.IDs)):
                         self.fluxes["sed"].append(dnew[self.ys[i], self.xs[i]])
 
-    def disperse_all(self, order, wmin, wmax, cache=False):
+    def disperse_all(self, order, wmin, wmax, sens_waves, sens_resp, cache=False):
         """
         Compute dispersed pixel values for all sources identified in
         the segmentation map.
@@ -173,6 +216,10 @@ class Observation:
             Minimum wavelength for dispersed spectra
         wmax : float
             Maximum wavelength for dispersed spectra
+        sens_waves : float array
+            Wavelength array from photom reference file
+        sens_resp : float array
+            Response (flux calibration) array from photom reference file
         """
         if cache:
             log.debug("Object caching ON")
@@ -195,9 +242,10 @@ class Observation:
                 self.cached_object[i]['maxx'] = []
                 self.cached_object[i]['miny'] = []
                 self.cached_object[i]['maxy'] = []
-            disperse_chunk_args = [i, order, wmin, wmax, 
+
+            disperse_chunk_args = [i, order, wmin, wmax, sens_waves, sens_resp,
                                    self.IDs[i], self.xs[i], self.ys[i], 
-                                   self.fluxes, #check shape!
+                                   self.fluxes, 
                                    self.seg_wcs, self.grism_wcs, self.dims, 
                                    self.extrapolate_sed, self.xoffset, self.yoffset]
             pool_args.append(disperse_chunk_args)
@@ -206,11 +254,17 @@ class Observation:
         t0 = time.time()
         if self.max_cpu > 1:
             log.info(f"Using multiprocessing with {self.max_cpu} cores to compute dispersion")
-            with Pool(self.max_cpu) as mypool:
+            ctx = mp.get_context("forkserver")
+            with ctx.Pool(self.max_cpu) as mypool:
                 disperse_chunk_output = mypool.starmap(self.disperse_chunk, pool_args)
+            #with concurrent.futures.ProcessPoolExecutor(max_workers=self.max_cpu) as executor:
+            #    these_futures = [executor.submit(self.disperse_chunk, *args) for args in pool_args]
+            #    concurrent.futures.wait(these_futures)
+            #    disperse_chunk_output = [future.result() for future in these_futures]
         else:
+            disperse_chunk_output = []
             for i in range(len(self.IDs)):
-                disperse_chunk_output = self.disperse_chunk(*pool_args[i])
+                disperse_chunk_output.append(self.disperse_chunk(*pool_args[i]))
         t1 = time.time()
         log.info(f"Wall clock time for disperse_chunk order {order}: {(t1-t0):.1f} sec")
         
@@ -225,7 +279,7 @@ class Observation:
                 self.simul_slits_sid.append(this_sid)
 
     @staticmethod
-    def disperse_chunk(c, order, wmin, wmax, sid, xs, ys, fluxes_dict, seg_wcs, grism_wcs, dims, extrapolate_sed, xoffset, yoffset):
+    def disperse_chunk(c, order, wmin, wmax, sens_waves, sens_resp, sid, xs, ys, fluxes_dict, seg_wcs, grism_wcs, dims, extrapolate_sed, xoffset, yoffset):
         """
         Method that computes dispersion for a single source.
         To be called after create_pixel_list().
@@ -241,6 +295,10 @@ class Observation:
             Minimum wavelength for dispersed spectra
         wmax : float
             Maximum wavelength for dispersed spectra
+        sens_waves : float array
+            Wavelength array from photom reference file
+        sens_resp : float array
+            Response (flux calibration) array from photom reference file        
         sid : int
             Source ID
         xs : np.ndarray
@@ -288,8 +346,8 @@ class Observation:
         log.debug(f"source {sid} contains {len(xs)} pixels")
         all_res = []
         for i in range(len(xs)):
-            # Here "i" indexes the pixel list for the object
-            # being processed, as opposed to the ID number of the object itself
+            # Here "i" indexes the pixel list for the segment
+            # being processed, as opposed to the ID number of the segment
 
             width = 1.0
             height = 1.0
@@ -302,7 +360,7 @@ class Observation:
             ]))
 
             pars_i = (xc, yc, width, height, lams, fluxes, order,
-                      wmin, wmax,
+                      wmin, wmax, sens_waves, sens_resp,
                       seg_wcs, grism_wcs, i, dims[::-1], 2,
                       extrapolate_sed, xoffset, yoffset)
             with warnings.catch_warnings():

jwst/wfss_contam/wfss_contam.py
@@ -1,51 +1,32 @@
-import matplotlib.pyplot as plt
 import logging
 import multiprocessing
 import numpy as np
 
 from stdatamodels.jwst import datamodels
+from astropy.table import Table
 
 from .observations import Observation
+from .sens1d import get_photom_data
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def contam_corr(input_model, waverange, max_cores, n_sources=None, source_0=0):
-    """
-    The main WFSS contamination correction function
+def _determine_multiprocessing_ncores(max_cores):
+
+    """Determine the number of cores to use for multiprocessing.
 
     Parameters
     ----------
-    input_model : `~jwst.datamodels.MultiSlitModel`
-        Input data model containing 2D spectral cutouts
-    waverange : `~jwst.datamodels.WavelengthrangeModel`
-        Wavelength range reference file model
     max_cores : string
-        Number of cores to use for multiprocessing. If set to 'none'
-        (the default), then no multiprocessing will be done. The other
-        allowable values are 'quarter', 'half', and 'all', which indicate
-        the fraction of cores to use for multi-proc. The total number of
-        cores includes the SMT cores (Hyper Threading for Intel).
-    n_sources : int
-        Number of sources to simulate. If None, then all sources in the
-        input model will be simulated. This is primarily useful for testing.
-    source_0 : int
-        Source ID to start with when selecting sources to simulate. This
-        is primarily useful for testing.
+        See docstring of contam_corr
 
     Returns
     -------
-    output_model : `~jwst.datamodels.MultiSlitModel`
-        A copy of the input_model that has been corrected
-    simul_model : `~jwst.datamodels.ImageModel`
-        Full-frame simulated image of the grism exposure
-    contam_model : `~jwst.datamodels.MultiSlitModel`
-        Contamination estimate images for each source slit
+    ncpus : int
+        Number of cores to use for multiprocessing
 
     """
-
-    # Determine number of cpu's to use for multi-processing
     if max_cores == 'none':
         ncpus = 1
     else:
@@ -60,13 +41,50 @@ def contam_corr(input_model, waverange, max_cores, n_sources=None, source_0=0):
             ncpus = 1
         log.debug(f"Found {num_cores} cores; using {ncpus}")
 
+    return ncpus
+
+
+def contam_corr(input_model, waverange, photom, max_cores, brightest_n=None):
+    """
+    The main WFSS contamination correction function
+
+    Parameters
+    ----------
+    input_model : `~jwst.datamodels.MultiSlitModel`
+        Input data model containing 2D spectral cutouts
+    waverange : `~jwst.datamodels.WavelengthrangeModel`
+        Wavelength range reference file model
+    photom : `~jwst.datamodels.NrcWfssPhotomModel` or `~jwst.datamodels.NisWfssPhotomModel`
+        Photom (flux cal) reference file model    
+    max_cores : string
+        Number of cores to use for multiprocessing. If set to 'none'
+        (the default), then no multiprocessing will be done. The other
+        allowable values are 'quarter', 'half', and 'all', which indicate
+        the fraction of cores to use for multi-proc. The total number of
+        cores includes the SMT cores (Hyper Threading for Intel).
+    brightest_n : int
+        Number of sources to simulate. If None, then all sources in the
+        input model will be simulated. Requires loading the source catalog
+        file if not None.
+
+    Returns
+    -------
+    output_model : `~jwst.datamodels.MultiSlitModel`
+        A copy of the input_model that has been corrected
+    simul_model : `~jwst.datamodels.ImageModel`
+        Full-frame simulated image of the grism exposure
+    contam_model : `~jwst.datamodels.MultiSlitModel`
+        Contamination estimate images for each source slit
+
+    """
+
+    ncpus = _determine_multiprocessing_ncores(max_cores)
+
     # Initialize output model
     output_model = input_model.copy()
 
-    # Get the segmentation map for this grism exposure
+    # Get the segmentation map, direct image for this grism exposure
     seg_model = datamodels.open(input_model.meta.segmentation_map)
-
-    # Get the direct image from which the segmentation map was constructed
     direct_file = input_model.meta.direct_image
     image_names = [direct_file]
     log.debug(f"Direct image names={image_names}")
@@ -100,37 +118,37 @@ def contam_corr(input_model, waverange, max_cores, n_sources=None, source_0=0):
     else:
         filter_name = filter_kwd
 
-    # Load lists of wavelength ranges and flux cal info for all orders
-    wmin = {}
-    wmax = {}
-    for order in spec_orders:
-        wavelength_range = waverange.get_wfss_wavelength_range(filter_name, [order])
-        wmin[order] = wavelength_range[order][0]
-        wmax[order] = wavelength_range[order][1]
-    log.debug(f"wmin={wmin}, wmax={wmax}")
+    # select a subset of the brightest sources using source catalog
+    if brightest_n is not None:
+        source_catalog = Table.read(input_model.meta.source_catalog, format='ascii.ecsv')
+        source_catalog.sort("isophotal_abmag", reverse=False) #magnitudes in ascending order, since brighter is smaller mag number
+        selected_IDs = list(source_catalog["label"])[:brightest_n]
+    else:
+        selected_IDs = None
 
-    # for testing, select a subset of the brightest sources, as extracted in extract2d
-    ids_in_extract2d = np.array([slit.source_id for slit in output_model.slits])
-    good = (ids_in_extract2d >= source_0)
-    selected_IDs = list(ids_in_extract2d[good])[:n_sources]
-    simul_all = None
     obs = Observation(image_names, seg_model, grism_wcs, filter_name,
                       boundaries=[0, 2047, 0, 2047], offsets=[xoffset, yoffset], max_cpu=ncpus,
                       ID=selected_IDs)
     
     good_slits = [slit for slit in output_model.slits if slit.source_id in obs.IDs]
-    #output_model.slits = good_slits #not sure why, but this fails to index properly
     output_model = datamodels.MultiSlitModel()
     output_model.slits.extend(good_slits)
-    log.info(f"Simulating only the first {n_sources} sources starting at index {source_0}")
+    log.info(f"Simulating only the first {brightest_n} sources")
 
-    # Create simulated grism image for each order and sum them up
+
+    simul_all = None
     for order in spec_orders:
 
-        log.info(f"Creating full simulated grism image for order {order}")
-        obs.disperse_all(order, wmin[order], wmax[order])
+        # Load lists of wavelength ranges and flux cal info
+        wavelength_range = waverange.get_wfss_wavelength_range(filter_name, [order])
+        wmin = wavelength_range[order][0]
+        wmax = wavelength_range[order][1]
+        log.debug(f"wmin={wmin}, wmax={wmax} for order {order}")
+        sens_waves, sens_response = get_photom_data(photom, filter_kwd, pupil_kwd, order)
 
-        # Accumulate result for this order into the combined image
+        # Create simulated grism image for each order and sum them up
+        log.info(f"Creating full simulated grism image for order {order}")
+        obs.disperse_all(order, wmin, wmax, sens_waves, sens_response)
         if simul_all is None:
             simul_all = obs.simulated_image
         else:
@@ -142,8 +160,6 @@ def contam_corr(input_model, waverange, max_cores, n_sources=None, source_0=0):
 
     # save the simulation multislitmodel
     obs.simul_slits.save("simulated_slits.fits", overwrite=True)
-
-    # need to re-make these now that I changed disperse_chunk
     simul_slit_sids = np.array(obs.simul_slits_sid)
     simul_slit_orders = np.array(obs.simul_slits_order)
 
@@ -159,19 +175,18 @@ def contam_corr(input_model, waverange, max_cores, n_sources=None, source_0=0):
         order = slit.meta.wcsinfo.spectral_order
         good = (simul_slit_sids == sid) * (simul_slit_orders == order)
         if not any(good):
+            log.warning(f"Source {sid} order {order} requested by input slit model \
+                        but not found in simulated slits")
             continue
         else:
             print('Subtracting contamination for source', sid, 'order', order)
-        
         good_idx = np.where(good)[0][0]
         this_simul = obs.simul_slits.slits[good_idx]
 
-
+        # cut out this source's contamination from the full simulated image
         fullframe_sim = np.zeros(obs.dims)
         y0 = this_simul.ystart 
         x0 = this_simul.xstart 
-        #print(obs.dims, this_simul.data.shape, slit.data.shape)
-        #print(y0, x0)
         fullframe_sim[y0:y0 + this_simul.ysize, x0:x0 + this_simul.xsize] = this_simul.data
         contam = simul_all - fullframe_sim
 