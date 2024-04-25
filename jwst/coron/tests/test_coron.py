aa9aa87a484236100fc9524bbcd07417a23c997e
Brett <brettgraham@gmail.com>
2024-04-25 13:11:05 -0400

    adjust test tolerances for scipy 1.13.0

--- jwst/coron/tests/test_coron.py
+++ jwst/coron/tests/test_coron.py
@@ -141,8 +141,8 @@ def test_align_array():
         ]
     )
 
-    npt.assert_allclose(aligned, truth_aligned, atol=1e-6)
-    npt.assert_allclose(shifts, truth_shifts, atol=1e-6)
+    npt.assert_allclose(aligned, truth_aligned, atol=1e-6, rtol=1e-4)
+    npt.assert_allclose(shifts, truth_shifts, atol=1e-6, rtol=1e-3)
 
 
 def test_align_models():
