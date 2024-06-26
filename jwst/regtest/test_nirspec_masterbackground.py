fac514565c5a7d8241a194f49ceabfca8ed0f3a1
Ned Molter <emolter@stsci.edu>
2024-06-26 14:44:58 -0400

    proposed fix for regtest failure

--- jwst/regtest/test_nirspec_masterbackground.py
+++ jwst/regtest/test_nirspec_masterbackground.py
@@ -76,7 +76,6 @@ def test_masterbkg_corrpars(rtdata):
         mbs = MasterBackgroundMosStep()
         corrected = mbs.run(data)
 
-        print(corrected.meta.cal_step.__dict__)
         mbs.use_correction_pars = True
         corrected_corrpars = mbs.run(data)
 
