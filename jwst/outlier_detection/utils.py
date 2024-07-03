cbd87c8b20795c975bea25efa265b635cecbd06b
Brett <brettgraham@gmail.com>
2024-07-03 10:20:17 -0400

    move some outlier_detection utility functions to stcal

--- jwst/outlier_detection/utils.py
+++ jwst/outlier_detection/utils.py
@@ -1,17 +1,14 @@
 """
 The ever-present utils sub-module. A home for all...
 """
-import os
 import warnings
 
 import numpy as np
-from astropy.stats import sigma_clip
-from drizzle.cdrizzle import tblot
-from scipy import ndimage
 
 from jwst.datamodels import ModelContainer
-from jwst.resample.resample_utils import build_driz_weight, calc_gwcs_pixmap
+from jwst.resample.resample_utils import build_driz_weight
 from jwst.resample.resample import compute_image_pixel_area
+from stcal.outlier_detection.utils import compute_weight_threshold, gwcs_blot, flag_cr
 from stdatamodels.jwst import datamodels
 
 import logging
@@ -24,42 +21,7 @@ DO_NOT_USE = datamodels.dqflags.pixel['DO_NOT_USE']
 OUTLIER = datamodels.dqflags.pixel['OUTLIER']
 
 
-# TODO stcal
-def compute_weight_threshold(weight, maskpt):
-    '''
-    Compute the weight threshold for a single image or cube.
-
-    Parameters
-    ----------
-    weight : numpy.ndarray
-        The weight array
-
-    maskpt : float
-        The percentage of the mean weight to use as a threshold for masking.
-
-    Returns
-    -------
-    float
-        The weight threshold for this integration.
-    '''
-
-    # necessary in order to assure that mask gets applied correctly
-    if hasattr(weight, '_mask'):
-        del weight._mask
-    mask_zero_weight = np.equal(weight, 0.)
-    mask_nans = np.isnan(weight)
-    # Combine the masks
-    weight_masked = np.ma.array(weight, mask=np.logical_or(
-        mask_zero_weight, mask_nans))
-    # Sigma-clip the unmasked data
-    weight_masked = sigma_clip(weight_masked, sigma=3, maxiters=5)
-    mean_weight = np.mean(weight_masked)
-    # Mask pixels where weight falls below maskpt percent
-    weight_threshold = mean_weight * maskpt
-    return weight_threshold
-
-
-# TODO stcal as a "median computer"
+# TODO figure out how to move to stcal
 def create_median(resampled_models, maskpt):
     """Create a median image from the singly resampled images.
     """
@@ -101,154 +63,6 @@ def create_median(resampled_models, maskpt):
         del resampled_sci, resampled_weight
 
     return median_image
-
-
-# TODO stcal utils
-def _abs_deriv(array):
-    """Take the absolute derivate of a numpy array."""
-    tmp = np.zeros(array.shape, dtype=np.float64)
-    out = np.zeros(array.shape, dtype=np.float64)
-
-    tmp[1:, :] = array[:-1, :]
-    tmp, out = _absolute_subtract(array, tmp, out)
-    tmp[:-1, :] = array[1:, :]
-    tmp, out = _absolute_subtract(array, tmp, out)
-
-    tmp[:, 1:] = array[:, :-1]
-    tmp, out = _absolute_subtract(array, tmp, out)
-    tmp[:, :-1] = array[:, 1:]
-    tmp, out = _absolute_subtract(array, tmp, out)
-
-    return out
-
-
-# TODO stcal utils
-def _absolute_subtract(array, tmp, out):
-    tmp = np.abs(array - tmp)
-    out = np.maximum(tmp, out)
-    tmp = tmp * 0.
-    return tmp, out
-
-
-# TODO stcal utils
-def flag_cr(
-    sci_data,
-    sci_err,
-    blot_data,
-    snr1,
-    snr2,
-    scale1,
-    scale2,
-    backg,
-    resample_data,
-):
-    """Masks outliers in science image by updating DQ in-place
-
-    Mask blemishes in dithered data by comparing a science image
-    with a model image and the derivative of the model image.
-
-    Parameters
-    ----------
-
-    FIXME: update these
-
-    sci_image : ~jwst.datamodels.ImageModel
-        the science data. Can also accept a CubeModel, but only if
-        resample_data is False
-
-    blot_array : np.ndarray
-        the blotted median image of the dithered science frames.
-
-    snr : str
-        Signal-to-noise ratio
-
-    scale : str
-        scaling factor applied to the derivative
-
-    backg : float
-        Background value (scalar) to subtract
-
-    resample_data : bool
-        Boolean to indicate whether blot_image is created from resampled,
-        dithered data or not
-
-    Notes
-    -----
-    Accepting a CubeModel for sci_image and blot_image with resample_data=True
-    appears to be a relatively simple extension, as the only thing that explicitly
-    relies on the dimensionality is the kernel, which could be generalized.
-    However, this is not currently needed, as CubeModels are only passed in for
-    TSO data, where resampling is always False.
-    """
-    err_data = np.nan_to_num(sci_err)
-
-    # create the outlier mask
-    if resample_data:  # dithered outlier detection
-        blot_deriv = _abs_deriv(blot_data)
-        diff_noise = np.abs(sci_data - blot_data - backg)
-
-        # Create a boolean mask based on a scaled version of
-        # the derivative image (dealing with interpolating issues?)
-        # and the standard n*sigma above the noise
-        threshold1 = scale1 * blot_deriv + snr1 * err_data
-        mask1 = np.greater(diff_noise, threshold1)
-
-        # Smooth the boolean mask with a 3x3 boxcar kernel
-        kernel = np.ones((3, 3), dtype=int)
-        mask1_smoothed = ndimage.convolve(mask1, kernel, mode='nearest')
-
-        # Create a 2nd boolean mask based on the 2nd set of
-        # scale and threshold values
-        threshold2 = scale2 * blot_deriv + snr2 * err_data
-        mask2 = np.greater(diff_noise, threshold2)
-
-        # Final boolean mask
-        cr_mask = mask1_smoothed & mask2
-
-    else:  # stack outlier detection
-        diff_noise = np.abs(sci_data - blot_data)
-
-        # straightforward detection of outliers for non-dithered data since
-        # err_data includes all noise sources (photon, read, and flat for baseline)
-        cr_mask = np.greater(diff_noise, snr1 * err_data)
-
-    return cr_mask
-
-
-# TODO stcal? (would require moving calc_gwcs_pixmap)
-# FIXME (or fixed) interp and sinscl were "options" only when provided
-# as part of the step spec (which becomes outlierpars). As neither was
-# in the spec (and providing unknown arguments causes an error), these
-# were never configurable and always defaulted to linear and 1.0
-def gwcs_blot(median_data, median_wcs, blot_data, blot_wcs, pix_ratio):
-    """
-    Resample the output/resampled image to recreate an input image based on
-    the input image's world coordinate system
-
-    Parameters
-    ----------
-    median_model : `~stdatamodels.jwst.datamodels.JwstDataModel`
-
-    blot_img : datamodel
-        Datamodel containing header and WCS to define the 'blotted' image
-    """
-    # Compute the mapping between the input and output pixel coordinates
-    pixmap = calc_gwcs_pixmap(blot_wcs, median_wcs, blot_data.shape)
-    log.debug("Pixmap shape: {}".format(pixmap[:, :, 0].shape))
-    log.debug("Sci shape: {}".format(blot_data.shape))
-    log.info('Blotting {} <-- {}'.format(blot_data.shape, median_data.shape))
-
-    outsci = np.zeros(blot_data.shape, dtype=np.float32)
-
-    # Currently tblot cannot handle nans in the pixmap, so we need to give some
-    # other value.  -1 is not optimal and may have side effects.  But this is
-    # what we've been doing up until now, so more investigation is needed
-    # before a change is made.  Preferably, fix tblot in drizzle.
-    pixmap[np.isnan(pixmap)] = -1
-    tblot(median_data, pixmap, outsci, scale=pix_ratio, kscale=1.0,
-          interp='linear', exptime=1.0, misval=0.0, sinscl=1.0)
-
-    return outsci
 
 
 def compute_weight_threshold_container(resampled_models, maskpt):
