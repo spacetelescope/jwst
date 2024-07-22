4a2026b33674782dc839a6cec04158cf8e3032ae
Melanie Clarke <mclarke@stsci.edu>
2024-07-22 11:23:10 -0400

    Move iscale computation to helper function

--- jwst/resample/resample.py
+++ jwst/resample/resample.py
@@ -215,6 +215,57 @@ class ResampleData:
             ignore=ignore_list
         )
 
+    def _get_intensity_scale(self, img):
+        """
+        Compute an intensity scale from the input and output pixel area.
+
+        For imaging data, the scaling is used to account for differences
+        between the nominal pixel area and the average pixel area for
+        the input data.
+
+        For spectral data, the scaling is used to account for flux
+        conservation with non-unity pixel scale ratios, when the
+        data units are flux density.
+
+        Parameters
+        ----------
+        img : DataModel
+            The input data model.
+
+        Returns
+        -------
+        iscale : float
+            The scale to apply to the input data before drizzling.
+        """
+        input_pixflux_area = img.meta.photometry.pixelarea_steradians
+        if input_pixflux_area:
+            if 'SPECTRAL' in img.meta.wcs.output_frame.axes_type:
+                # Use the nominal area as is
+                input_pixel_area = input_pixflux_area
+
+                # If input image is in flux density units, correct the
+                # flux for the user-specified change to the spatial dimension
+                if 'sr' not in str(img.meta.bunit_data).lower():
+                    input_pixel_area *= self.pscale_ratio
+            else:
+                img.meta.wcs.array_shape = img.data.shape
+                input_pixel_area = compute_image_pixel_area(img.meta.wcs)
+                if input_pixel_area is None:
+                    raise ValueError(
+                        "Unable to compute input pixel area from WCS of input "
+                        f"image {repr(img.meta.filename)}."
+                    )
+                if self.input_pixscale0 is None:
+                    self.input_pixscale0 = np.rad2deg(
+                        np.sqrt(input_pixel_area)
+                    )
+                    if self._recalc_pscale_ratio:
+                        self.pscale_ratio = self.pscale / self.input_pixscale0
+            iscale = np.sqrt(input_pixflux_area / input_pixel_area)
+        else:
+            iscale = 1.0
+        return iscale
+
     def resample_many_to_many(self):
         """Resample many inputs to many outputs where outputs have a common frame.
 
@@ -244,34 +295,7 @@ class ResampleData:
             log.info(f"{len(exposure)} exposures to drizzle together")
             for img in exposure:
                 img = datamodels.open(img)
-
-                input_pixflux_area = img.meta.photometry.pixelarea_steradians
-                if input_pixflux_area:
-                    if 'SPECTRAL' in img.meta.wcs.output_frame.axes_type:
-                        # Use the nominal area as is
-                        input_pixel_area = input_pixflux_area
-
-                        # If input image is in flux density units, correct the
-                        # flux for the user-specified change to the spatial dimension
-                        if 'sr' not in str(img.meta.bunit_data).lower():
-                            input_pixel_area *= self.pscale_ratio
-                    else:
-                        img.meta.wcs.array_shape = img.data.shape
-                        input_pixel_area = compute_image_pixel_area(img.meta.wcs)
-                        if input_pixel_area is None:
-                            raise ValueError(
-                                "Unable to compute input pixel area from WCS of input "
-                                f"image {repr(img.meta.filename)}."
-                            )
-                        if self.input_pixscale0 is None:
-                            self.input_pixscale0 = np.rad2deg(
-                                np.sqrt(input_pixel_area)
-                            )
-                            if self._recalc_pscale_ratio:
-                                self.pscale_ratio = self.pscale / self.input_pixscale0
-                    iscale = np.sqrt(input_pixflux_area / input_pixel_area)
-                else:
-                    iscale = 1.0
+                iscale = self._get_intensity_scale(img)
                 log.debug(f'Using intensity scale iscale={iscale}')
 
                 inwht = resample_utils.build_driz_weight(
@@ -339,36 +363,8 @@ class ResampleData:
 
         log.info("Resampling science data")
         for img in self.input_models:
-            input_pixflux_area = img.meta.photometry.pixelarea_steradians
-            if input_pixflux_area:
-                if 'SPECTRAL' in img.meta.wcs.output_frame.axes_type:
-                    # Use the nominal area as is
-                    input_pixel_area = input_pixflux_area
-
-                    # If input image is in flux density units, correct the
-                    # flux for the user-specified change to the spatial dimension
-                    if 'sr' not in str(img.meta.bunit_data).lower():
-                        input_pixel_area *= self.pscale_ratio
-                else:
-                    img.meta.wcs.array_shape = img.data.shape
-                    input_pixel_area = compute_image_pixel_area(img.meta.wcs)
-
-                    if input_pixel_area is None:
-                        raise ValueError(
-                            "Unable to compute input pixel area from WCS of input "
-                            f"image {repr(img.meta.filename)}."
-                        )
-                    if self.input_pixscale0 is None:
-                        self.input_pixscale0 = np.rad2deg(
-                            np.sqrt(input_pixel_area)
-                        )
-                        if self._recalc_pscale_ratio:
-                            self.pscale_ratio = self.pscale / self.input_pixscale0
-                iscale = np.sqrt(input_pixflux_area / input_pixel_area)
-            else:
-                iscale = 1.0
+            iscale = self._get_intensity_scale(img)
             log.debug(f'Using intensity scale iscale={iscale}')
-
             img.meta.iscale = iscale
 
             inwht = resample_utils.build_driz_weight(img,
