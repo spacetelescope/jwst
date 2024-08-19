8f902b789aed33cdfb04ac123b69d2d6a5e128da
Ned Molter <emolter@stsci.edu>
2024-08-19 08:53:27 -0400

    call img.area

--- jwst/resample/resample.py
+++ jwst/resample/resample.py
@@ -307,7 +307,10 @@ class ResampleData:
                 log.info(f"{len(indices)} exposures to drizzle together")
                 for index in indices:
                     img = input_models.borrow(index)
-                    img = datamodels.open(img) # must call this explicitly to get area reference file
+                    if isinstance(img, datamodels.SlitModel):
+                        # must call this explicitly to populate area extension
+                        # although the existence of this extension may not be necessary
+                        img.area = img.area 
                     iscale = self._get_intensity_scale(img)
                     log.debug(f'Using intensity scale iscale={iscale}')
 
