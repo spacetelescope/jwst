e353f1f8f81412b696784b5925cfa8c51267f644
Brett <brettgraham@gmail.com>
2024-08-27 11:39:48 -0400

    require pinned crds context on build

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -1,6 +1,8 @@
+import os
 import re
 
 from importlib.metadata import version
+
 
 __version__ = version(__name__)
 
@@ -10,3 +12,10 @@ if "+" in __version__:
     commit = _regex_git_hash.match(__version__).groups()
     if commit:
         __version_commit__ = commit[0]
+
+if "CRDS_CONTEXT" not in os.environ:
+    try:
+        from ._crds_context import crds_context
+        os.environ["CRDS_CONTEXT"] = crds_context
+    except ImportError:
+        pass
