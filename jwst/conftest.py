29210e849a653f04569959f14e2b16657d8bfe7f
Brett <brettgraham@gmail.com>
2024-09-23 11:31:38 -0400

    pyinstrument profiling

--- jwst/conftest.py
+++ jwst/conftest.py
@@ -5,8 +5,31 @@ import pytest
 import inspect
 from pathlib import Path
 
+import pytest
+
 from jwst.associations import (AssociationRegistry, AssociationPool)
 from jwst.associations.tests.helpers import t_path
+
+try:
+    from pyinstrument import Profiler
+
+    TESTS_ROOT = Path.cwd()
+
+    @pytest.fixture(autouse=True)
+    def auto_profile(request):
+        PROFILE_ROOT = (TESTS_ROOT / ".profiles")
+        # Turn profiling on
+        profiler = Profiler()
+        profiler.start()
+
+        yield  # Run test
+
+        profiler.stop()
+        PROFILE_ROOT.mkdir(exist_ok=True)
+        results_file = PROFILE_ROOT / f"{request.node.name}.html"
+        profiler.write_html(results_file)
+except ImportError:
+    pass
 
 
 @pytest.fixture
