15fa0befecbce65cc526927f9b4076644c7ee975
Ned Molter <emolter@users.noreply.github.com>
2024-09-27 07:31:17 -0700

    JP-3741: Faster temporary file I/O for outlier detection in on-disk mode (#8782)

--- jwst/regtest/test_nirspec_fs_spec3.py
+++ jwst/regtest/test_nirspec_fs_spec3.py
@@ -1,6 +1,7 @@
 from astropy.io.fits.diff import FITSDiff
 import pytest
 import numpy as np
+import os
 from gwcs import wcstools
 
 from jwst.stpipe import Step
@@ -20,20 +21,27 @@ def run_pipeline(rtdata_module):
     # Run the calwebb_spec3 pipeline; save results from intermediate steps
     args = ["calwebb_spec3", rtdata.input,
             "--steps.outlier_detection.save_results=true",
+            "--steps.outlier_detection.save_intermediate_results=true",
             "--steps.resample_spec.save_results=true",
             "--steps.extract_1d.save_results=true"]
     Step.from_cmdline(args)
 
 
 @pytest.mark.bigdata
-@pytest.mark.parametrize("suffix", ["cal", "crf", "s2d", "x1d"])
+@pytest.mark.parametrize("suffix", ["cal", "crf", "s2d", "x1d", "median"])
 @pytest.mark.parametrize("source_id,slit_name", [("s000000001","s200a2"), ("s000000021","s200a1"),
     ("s000000023","s400a1"), ("s000000024","s1600a1"), ("s000000025","s200b1")])
 def test_nirspec_fs_spec3(run_pipeline, rtdata_module, fitsdiff_default_kwargs, suffix, source_id, slit_name):
     """Test spec3 pipeline on a set of NIRSpec FS exposures."""
     rtdata = rtdata_module
 
-    output = f"jw01309-o022_{source_id}_nirspec_f290lp-g395h-{slit_name}-allslits_{suffix}.fits"
+    if suffix == "median":
+        output = f"jw01309022001_04102_00002_nrs2_{slit_name}_{suffix}.fits"
+        # also ensure drizzled and blot models were created with the correct names
+        assert os.path.isfile(output.replace("median", "outlier_s2d"))
+        assert os.path.isfile(output.replace("median", "blot"))
+    else:
+        output = f"jw01309-o022_{source_id}_nirspec_f290lp-g395h-{slit_name}-allslits_{suffix}.fits"
     rtdata.output = output
     rtdata.get_truth(f"truth/test_nirspec_fs_spec3/{output}")
 
