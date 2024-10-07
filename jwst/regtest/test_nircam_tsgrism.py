d2dfb6a1325d71174f893e3ee83434b27fa0d1e8
Melanie Clarke <mclarke@stsci.edu>
2024-10-07 17:33:37 -0400

    Remove non-functional setpointing test

--- jwst/regtest/test_nircam_tsgrism.py
+++ jwst/regtest/test_nircam_tsgrism.py
@@ -2,7 +2,6 @@ import pytest
 from astropy.io.fits.diff import FITSDiff
 from astropy.table import Table, setdiff
 
-from jwst.lib.set_telescope_pointing import add_wcs
 from jwst.stpipe import Step
 
 
@@ -33,7 +32,6 @@ def run_pipelines(rtdata_module):
                 params=["jw01185015001_03104_00001-seg004_nrcalong_rate.fits",
                         "jw01185013001_04103_00001-seg003_nrcalong_rate.fits"])
 def run_pipeline_offsetSR(request, rtdata_module):
-    ''''''
     rtdata = rtdata_module
     rtdata.get_data("nircam/tsgrism/" + request.param)
     args = ["calwebb_spec2", rtdata.input,
@@ -44,12 +42,13 @@ def run_pipeline_offsetSR(request, rtdata_module):
 
 @pytest.mark.bigdata
 def test_nircam_tsgrism_stage2_offsetSR(run_pipeline_offsetSR, fitsdiff_default_kwargs):
-    '''
+    """
     Test coverage for offset special requirement specifying nonzero offset in X.
+
     Test data are two observations of Gliese 436, one with offset specified and one without.
     Quantitatively we just ensure that the outputs are identical to the inputs, but qualitatively
     can check that the spectral lines fall at the same wavelengths in both cases,
-    which is why the zero-offset case is also included here.'''
+    which is why the zero-offset case is also included here."""
     rtdata = run_pipeline_offsetSR
     rtdata.output = rtdata.input.replace("rate", "x1d")
     rtdata.get_truth("truth/test_nircam_tsgrism_stages/" + rtdata.output.split('/')[-1])
@@ -92,33 +91,8 @@ def test_nircam_tsgrism_stage3_whtlt(run_pipelines):
     rtdata.output = "jw01366-o002_t001_nircam_f322w2-grismr-subgrism256_whtlt.ecsv"
     rtdata.get_truth("truth/test_nircam_tsgrism_stages/jw01366-o002_t001_nircam_f322w2-grismr-subgrism256_whtlt.ecsv")
 
-
     table = Table.read(rtdata.output)
     table_truth = Table.read(rtdata.truth)
 
     # setdiff returns a table of length zero if there is no difference
     assert len(setdiff(table, table_truth)) == 0
-
-
-@pytest.mark.bigdata
-def test_nircam_setpointing_tsgrism(rtdata, fitsdiff_default_kwargs):
-    """
-    Regression test of the set_telescope_pointing script on a level-1b NIRCam file.
-    """
-    # Get SIAF PRD database file
-    siaf_path = rtdata.get_data("common/prd.db")
-    rtdata.get_data("nircam/tsgrism/jw00721012001_03103_00001-seg001_nrcalong_uncal.fits")
-    # The add_wcs function overwrites its input
-    rtdata.output = rtdata.input
-
-    # Call the WCS routine, using the ENGDB_Service
-    try:
-        add_wcs(rtdata.input, siaf_path=siaf_path)
-    except ValueError:
-        pytest.skip('Engineering Database not available.')
-
-    rtdata.get_truth("truth/test_nircam_setpointing/jw00721012001_03103_00001-seg001_nrcalong_uncal.fits")
-
-    fitsdiff_default_kwargs['rtol'] = 1e-6
-    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
-    assert diff.identical, diff.report()
