a84347b64cacb71044485c9e6f30c2216c3c1328
Melanie Clarke <mclarke@stsci.edu>
2024-10-09 10:58:41 -0400

    Use nrca3 data

--- jwst/regtest/test_nircam_wfs.py
+++ jwst/regtest/test_nircam_wfs.py
@@ -17,8 +17,8 @@ def run_pipelines(rtdata_module):
 
     # Get the rate files and run the image2 pipeline on each
     rate_files = [
-        "nircam/wavefront/jw02586173001_03104_00001_nrcalong_rate.fits",
-        "nircam/wavefront/jw02586173001_03104_00002_nrcalong_rate.fits"
+        "nircam/wavefront/jw02586173001_03104_00001_nrca3_rate.fits",
+        "nircam/wavefront/jw02586173001_03104_00002_nrca3_rate.fits"
     ]
     for rate_file in rate_files:
         rtdata.get_data(rate_file)
@@ -27,7 +27,7 @@ def run_pipelines(rtdata_module):
 
     # Get the image3 ASN file only, not the members. The members were just
     # produced by the calwebb_image2 pipeline run.
-    rtdata.get_data("nircam/wavefront/jw02586-o173_20240923t165435_wfs-image3_00006_asn.json")
+    rtdata.get_data("nircam/wavefront/jw02586-o173_20240923t165435_wfs-image3_00008_asn.json")
 
     # Run the calwebb_wfs-image3 pipeline with no refinement (default)
     args = ["calwebb_wfs-image3", rtdata.input]
@@ -35,7 +35,7 @@ def run_pipelines(rtdata_module):
 
     # Get the second version of the image3 ASN, which has a unique output
     # product name. Only get the ASN file, not the members.
-    rtdata.get_data("nircam/wavefront/jw02586-o173_20240923t165435_wfs-image3_00006_asn_v2.json")
+    rtdata.get_data("nircam/wavefront/jw02586-o173_20240923t165435_wfs-image3_00008_asn_v2.json")
 
     # Run the calwebb_wfs-image3 pipeline with refinement
     args = ["calwebb_wfs-image3", rtdata.input, "--do_refine=True"]
@@ -54,10 +54,10 @@ def test_nicam_wfsimage_noextras(run_pipelines):
 
 @pytest.mark.bigdata
 @pytest.mark.parametrize("output",[
-    "jw02586173001_03104_00001_nrcalong_cal.fits",
-    "jw02586173001_03104_00002_nrcalong_cal.fits",
-    "jw02586-o173_t035_nircam_clear-f356w-nrcalong_wfscmb-04.fits",
-    "jw02586-o173_t035_nircam_clear-f356w-nrcalong_wfscmb-04_refine.fits"],
+    "jw02586173001_03104_00001_nrca3_cal.fits",
+    "jw02586173001_03104_00002_nrca3_cal.fits",
+    "jw02586-o173_t035_nircam_f212n-wlm8-nrca3_wfscmb-04.fits",
+    "jw02586-o173_t035_nircam_f212n-wlm8-nrca3_wfscmb-04_refine.fits"],
     ids=["cal1", "cal2", "wfscmb1", "wfscmb2"])
 def test_nircam_wfsimage(run_pipelines, fitsdiff_default_kwargs, output):
     """Regression test of the calwebb_wfs-image2 and calwebb_wfs-image3
