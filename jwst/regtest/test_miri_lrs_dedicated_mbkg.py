86b767ce78a392abc5cfeaef7d1985b5f11794f3
Melanie Clarke <mclarke@stsci.edu>
2024-10-25 17:19:53 -0400

    Update data and test background products

--- jwst/regtest/test_miri_lrs_dedicated_mbkg.py
+++ jwst/regtest/test_miri_lrs_dedicated_mbkg.py
@@ -12,22 +12,24 @@ def run_pipeline(rtdata_module):
 
     rtdata.get_asn("miri/lrs/jw01529-o003_spec3_with_bg_00001_asn.json")
 
-    MasterBackgroundStep.call(rtdata.input, save_results=True, suffix='mbsub')
+    MasterBackgroundStep.call(rtdata.input, save_results=True, suffix='mbsub',
+                              save_background=True)
 
     return rtdata
 
 
 @pytest.mark.bigdata
-def test_miri_lrs_dedicated_mbkg(run_pipeline, fitsdiff_default_kwargs):
+@pytest.mark.parametrize('output', ['jw01529-o004_t002_miri_p750l_o003_masterbg1d.fits',
+                                    'jw01529003001_03103_00011_mirimage_o003_masterbg2d.fits',
+                                    'jw01529003001_03103_00011_mirimage_mbsub.fits'])
+def test_miri_lrs_dedicated_mbkg(run_pipeline, fitsdiff_default_kwargs, output):
     """Run a test for MIRI LRS data with dedicated background exposures."""
 
     rtdata = run_pipeline
-    rtdata.output = "jw01529003001_03103_00011_mirimage_mbsub.fits"
+    rtdata.output = output
 
     # Get the truth file
-    rtdata.get_truth(os.path.join(
-        "truth/test_miri_lrs_dedicated_mbkg",
-        "jw01529003001_03103_00011_mirimage_mbsub.fits"))
+    rtdata.get_truth(os.path.join("truth/test_miri_lrs_dedicated_mbkg", output))
 
     # Compare the results
     diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
