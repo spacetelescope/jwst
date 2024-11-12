a30eaac82119a2c65d22c32b547b0fd0fc59b7e5
Melanie Clarke <mclarke@stsci.edu>
2024-11-12 09:57:15 -0500

    Update unit tests for new extract1d

--- jwst/extract_1d/tests/test_extract_src_flux.py
+++ jwst/extract_1d/tests/test_extract_src_flux.py
@@ -13,105 +13,77 @@ from jwst.extract_1d import extract1d
 def inputs_constant():
     shape = (9, 5)
     image = np.arange(shape[0] * shape[1], dtype=np.float32).reshape(shape)
-    var_poisson = image.copy()
     var_rnoise = image.copy()
+    var_poisson = image.copy()
     var_rflat = image.copy()
-    x = 2
-    j = 2
-    lam = 1.234                 # an arbitrary value (not actually used)
     weights = None
-    bkgmodels = [None, None, None, None]
-    lower = np.zeros(shape[1], dtype=np.float64) + 3.   # middle of pixel 3
-    upper = np.zeros(shape[1], dtype=np.float64) + 7.   # middle of pixel 7
-    srclim = [[lower, upper]]
+    profile_bg = None
 
-    return (image, var_poisson, var_rnoise, var_rflat,
-            x, j, lam, srclim, weights, bkgmodels)
+    profile = np.zeros_like(image)
+    profile[3] = 0.5    # lower limit: middle of pixel 3
+    profile[4:7] = 1.0  # center of aperture
+    profile[7] = 0.5    # upper limit: middle of pixel 7
+
+    return (image, var_rnoise, var_poisson, var_rflat,
+            profile, weights, profile_bg)
 
 
 def test_extract_src_flux(inputs_constant):
-    (image, var_poisson, var_rnoise, var_rflat,
-     x, j, lam, srclim, weights, bkgmodels) = inputs_constant
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg) = inputs_constant
 
-    (total_flux, f_var_poisson, f_var_rnoise, f_var_flat,
-     bkg_flux, b_var_poisson, b_var_rnoise, b_var_flat,
-     tarea, twht) = extract1d._extract_src_flux(
-        image, var_poisson, var_rnoise, var_rflat,
-        x, j, lam, srclim, weights, bkgmodels)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg)
 
+    # check the value at column 2
     # 0.5 * 17. + 22. + 27. + 32. + 0.5 * 37.
-    assert math.isclose(total_flux, 108., rel_tol=1.e-8, abs_tol=1.e-8)
+    assert math.isclose(total_flux[0][2], 108., rel_tol=1.e-8, abs_tol=1.e-8)
+    assert bkg_flux[0][2] == 0.
+    assert math.isclose(npixels[0][2], 4., rel_tol=1.e-8, abs_tol=1.e-8)
 
-    assert bkg_flux == 0.
-
-    assert math.isclose(tarea, 4., rel_tol=1.e-8, abs_tol=1.e-8)
-
+    # set a NaN value in the column of interest
     image[5, 2] = np.nan
 
-    (total_flux, f_var_poisson, f_var_rnoise, f_var_flat,
-     bkg_flux, b_var_poisson, b_var_rnoise, b_var_flat,
-     tarea, twht) = extract1d._extract_src_flux(
-        image, var_poisson, var_rnoise, var_rflat,
-        x, j, lam, srclim, weights, bkgmodels)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg)
 
     # 0.5 * 17. + 22. + 32. + 0.5 * 37.
-    assert math.isclose(total_flux, 81., rel_tol=1.e-8, abs_tol=1.e-8)
+    assert math.isclose(total_flux[0][2], 81., rel_tol=1.e-8, abs_tol=1.e-8)
+    assert math.isclose(npixels[0][2], 3., rel_tol=1.e-8, abs_tol=1.e-8)
 
-    assert math.isclose(tarea, 3., rel_tol=1.e-8, abs_tol=1.e-8)
-
+    # set the whole column to NaN
     image[:, 2] = np.nan
 
-    (total_flux, f_var_poisson, f_var_rnoise, f_var_flat,
-     bkg_flux, b_var_poisson, b_var_rnoise, b_var_flat,
-     tarea, twht) = extract1d._extract_src_flux(
-        image, var_poisson, var_rnoise, var_rflat,
-        x, j, lam, srclim, weights, bkgmodels)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg)
 
-    assert np.isnan(total_flux)
+    assert np.isnan(total_flux[0][2])
+    assert npixels[0][2] == 0.
 
-    assert tarea == 0.
 
+def test_extract_src_flux_empty_interval(inputs_constant):
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg) = inputs_constant
 
-@pytest.mark.parametrize('test_type', ['all_empty', 'all_equal'])
-def test_extract_src_flux_empty_interval(inputs_constant, test_type):
-    (image, var_poisson, var_rnoise, var_rflat,
-     x, j, lam, srclim, weights, bkgmodels) = inputs_constant
+    # empty extraction range
+    profile[:] = 0.0
 
-    if test_type == 'all_empty':
-        # no limits provided
-        srclim = []
-    else:
-        # empty extraction range: upper equals lower
-        srclim[0][1] = srclim[0][0].copy()
-
-    (total_flux, f_var_poisson, f_var_rnoise, f_var_flat,
-     bkg_flux, b_var_poisson, b_var_rnoise, b_var_flat,
-     tarea, twht) = extract1d._extract_src_flux(
-        image, var_poisson, var_rnoise, var_rflat,
-        x, j, lam, srclim, weights, bkgmodels)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg)
 
     # empty interval, so no flux returned
-    assert np.isnan(total_flux)
-    assert bkg_flux == 0.
-    assert tarea == 0.
-
-
-@pytest.mark.parametrize('offset', [-100, 100])
-def test_extract_src_flux_interval_out_of_range(inputs_constant, offset):
-    (image, var_poisson, var_rnoise, var_rflat,
-     x, j, lam, srclim, weights, bkgmodels) = inputs_constant
-
-    # extraction limits out of range
-    srclim[0][0] += offset
-    srclim[0][1] += offset
-
-    (total_flux, f_var_poisson, f_var_rnoise, f_var_flat,
-     bkg_flux, b_var_poisson, b_var_rnoise, b_var_flat,
-     tarea, twht) = extract1d._extract_src_flux(
-        image, var_poisson, var_rnoise, var_rflat,
-        x, j, lam, srclim, weights, bkgmodels)
-
-    # empty interval, so no flux returned
-    assert np.isnan(total_flux)
-    assert bkg_flux == 0.
-    assert tarea == 0.
+    assert np.all(np.isnan(total_flux))
+    assert np.all(bkg_flux == 0.)
+    assert np.all(npixels == 0.)
--- jwst/extract_1d/tests/test_fit_background_model.py
+++ jwst/extract_1d/tests/test_fit_background_model.py
@@ -17,212 +17,101 @@ def inputs_constant():
     var_poisson = image.copy()
     var_rnoise = image.copy()
     var_rflat = image.copy()
-    x = 2
-    j = 2
 
-    b_lower = np.zeros(shape[1], dtype=np.float64) + 3.5    # 4, inclusive
-    b_upper = np.zeros(shape[1], dtype=np.float64) + 4.5    # 4, inclusive
-    bkglim = [[b_lower, b_upper]]
+    profile = np.zeros_like(image)
+    profile[1] = 1.0 # one pixel aperture
+    weights = None
+
+    profile_bg = np.zeros_like(image)
+    profile_bg[4:8] = 1.0  # background region
     bkg_order = 0
     bkg_fit = "poly"
 
-    return image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order
+    return (image, var_rnoise, var_poisson, var_rflat,
+            profile, weights, profile_bg, bkg_fit, bkg_order)
 
 
-def test_fit_background_model(inputs_constant):
+@pytest.mark.parametrize('bkg_fit_type', ['poly', 'median'])
+def test_fit_background(inputs_constant, bkg_fit_type):
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg, bkg_fit, bkg_order) = inputs_constant
 
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(*inputs_constant)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, fit_bkg=True,
+        bkg_fit_type=bkg_fit_type, bkg_order=bkg_order)
 
-    assert math.isclose(bkg_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
+    if bkg_fit_type == 'median':
+        extra_factor = 1.2 ** 2
+    else:
+        extra_factor = 1.0
 
-    assert math.isclose(b_var_poisson_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 2
-
-
-def test_fit_background_mean(inputs_constant):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-    bkg_fit = "mean"
-
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
-
-    assert math.isclose(bkg_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 2
-
-
-def test_fit_background_median(inputs_constant):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-    bkg_fit = "median"
-
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
-
-    assert math.isclose(bkg_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 2
+    assert np.allclose(bkg_flux[0], np.sum(image[4:8], axis=0) / 4)
+    assert np.allclose(b_var_rnoise[0], extra_factor * np.sum(image[4:8], axis=0) / 16)
+    assert np.allclose(b_var_poisson[0], extra_factor * np.sum(image[4:8], axis=0) / 16)
+    assert np.allclose(b_var_flat[0], extra_factor * np.sum(image[4:8], axis=0) / 16)
 
 
 def test_handles_nan(inputs_constant):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg, bkg_fit, bkg_order) = inputs_constant
     image[:, 2] = np.nan
 
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, fit_bkg=True,
+        bkg_fit_type=bkg_fit, bkg_order=bkg_order)
 
-    assert math.isclose(bkg_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 0
+    assert np.allclose(bkg_flux[0], np.nansum(image[4:8], axis=0) / 4)
+    assert np.allclose(b_var_rnoise[0], np.nansum(image[4:8], axis=0) / 16)
+    assert np.allclose(b_var_poisson[0], np.nansum(image[4:8], axis=0) / 16)
+    assert np.allclose(b_var_flat[0], np.nansum(image[4:8], axis=0) / 16)
 
 
-def test_handles_one_value(inputs_constant):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-    image[np.where(image == 22)] = np.nan   # During extraction, only two pixels are returned "normally"; set one to Nan
+@pytest.mark.parametrize('bkg_order_val', [0, 1, 2])
+def test_handles_one_value(inputs_constant, bkg_order_val):
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg, bkg_fit, bkg_order) = inputs_constant
+    profile_bg[:] = 0.0
+    profile_bg[4:6] = 1.0
+    image[4] = np.nan
 
-    # If only one data point is available, the polynomial fit is forced to 0
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, fit_bkg=True,
+        bkg_fit_type=bkg_fit, bkg_order=bkg_order_val)
 
-    assert math.isclose(bkg_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 0
-
-
-@pytest.mark.parametrize('test_type', ['all_empty', 'all_equal'])
-def test_handles_empty_interval(inputs_constant, test_type):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-
-    if test_type == 'all_empty':
-        # no limits provided
-        bkglim = []
+    if bkg_order_val == 0:
+        expected = image[5]
     else:
-        # empty extraction range: upper equals lower
-        bkglim[0][1] = bkglim[0][0].copy()
+        # not enough input for fit: value is set to 0.0
+        expected = 0.0
 
-    # No data available: background model is 0
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
-
-    assert math.isclose(bkg_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 0
+    assert np.allclose(bkg_flux[0], expected)
+    assert np.allclose(b_var_rnoise[0], expected)
+    assert np.allclose(b_var_poisson[0], expected)
+    assert np.allclose(b_var_flat[0], expected)
 
 
-@pytest.mark.parametrize('offset', [-100, 100])
-def test_handles_interval_out_of_range(inputs_constant, offset):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-    bkglim[0][0] += offset
-    bkglim[0][1] += offset
+def test_handles_empty_interval(inputs_constant):
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg, bkg_fit, bkg_order) = inputs_constant
+    profile_bg[:] = 0.0
 
-    # No data available: background model is 0
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, fit_bkg=True,
+        bkg_fit_type=bkg_fit, bkg_order=bkg_order)
 
-    assert math.isclose(bkg_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 0.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 0
-
-
-def test_handles_one_empty_interval(inputs_constant):
-    image, var_poisson, var_rnoise, var_rflat, x, j, bkglim, bkg_fit, bkg_order = inputs_constant
-
-    # add an extra interval that is empty
-    bkglim.append(deepcopy(bkglim[0]))
-    bkglim[1][0] += 2.0
-    bkglim[1][1] = bkglim[1][0].copy()
-    print(bkglim)
-
-    # should ignore the second interval and return a valid answer for the first
-    (bkg_model, b_var_poisson_model, b_var_rnoise_model, b_var_flat_model, npts) = \
-        extract1d._fit_background_model(image, var_poisson, var_rnoise, var_rflat,
-                                        x, j, bkglim, bkg_fit, bkg_order)
-
-    assert math.isclose(bkg_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(bkg_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_poisson_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_poisson_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_rnoise_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_rnoise_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert math.isclose(b_var_flat_model(0.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-    assert math.isclose(b_var_flat_model(8.), 22.0, rel_tol=1.e-8, abs_tol=1.e-8)
-
-    assert npts == 2
+    assert np.allclose(bkg_flux[0], 0.0)
+    assert np.allclose(b_var_rnoise[0], 0.0)
+    assert np.allclose(b_var_poisson[0], 0.0)
+    assert np.allclose(b_var_flat[0], 0.0)
