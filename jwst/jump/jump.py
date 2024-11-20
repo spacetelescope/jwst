36b1fddb58c5040e89d4278e3acc6ae807079e83
Ken MacDonald <kmacdonald@stsci.edu>
2024-11-19 22:32:29 -0500

    Adding white space to make the code easier to read.
    Adding JumpData class to running jump.
    
    Prepping for JumpData class.
    
    Updating import functions.
    
    Removing the dqflags parameter.
    
    The file jump.py is no longer needed, but there is a test suite that still uses the deprectaed run_detect_jumps function directly.
    
    Refactoring out run_detect_jumps in favor of using JumpData.
    
    Refactored jump step.
    
    Updating setting the parameters of the JumpData instance.

--- jwst/jump/jump.py
+++ jwst/jump/jump.py
@@ -1,6 +1,17 @@
+# XXX
+"""
+The function run_detect_jumps is no longer used by the jump step.
+
+This file is no longer needed, but it cannot simply be deleted because
+there are CI tests thet call run_detect_jumps directly.  Those tests are
+in test_detect_jumps.py.  The tests in that test suite should be refactored
+and moved to test_jump_step.py now that run_detect_jumps is no longer used.
+"""
+
 import logging
 import numpy as np
-from stcal.jump.jump import detect_jumps
+from stcal.jump.jump import detect_jumps_data
+from stcal.jump.jump_class import JumpData
 from stdatamodels.jwst.datamodels import dqflags
 
 from ..lib import reffile_utils
@@ -9,43 +20,45 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def run_detect_jumps(output_model, gain_model, readnoise_model,
-                     rejection_thresh, three_grp_thresh, four_grp_thresh,
-                     max_cores, max_jump_to_flag_neighbors,
-                     min_jump_to_flag_neighbors, flag_4_neighbors,
-                     after_jump_flag_dn1=0.0,
-                     after_jump_flag_time1=0.0,
-                     after_jump_flag_dn2=0.0,
-                     after_jump_flag_time2=0.0,
-                     min_sat_area=1.0, min_jump_area=5.0, min_sat_radius_extend=2.5,
-                     expand_factor=2.0, use_ellipses=False,
-                     sat_required_snowball=True, sat_expand=2,
-                     expand_large_events=False, find_showers=False, edge_size=25, extend_snr_threshold=1.1,
-                     extend_min_area=90, extend_inner_radius=1, extend_outer_radius=2.6, extend_ellipse_expand_ratio=1.1,
-                     time_masked_after_shower=30, min_diffs_single_pass=10,
-                     max_extended_radius=200,
-                     minimum_groups=3,
-                     minimum_sigclip_groups=100,
-                     only_use_ints=True,
-                     mask_snowball_persist_next_int=True,
-                     snowball_time_masked_next_int=250,
-                     max_shower_amplitude=4
-                     ):
+def run_detect_jumps(
+        output_model, gain_model, readnoise_model,
 
-    # Runs `detect_jumps` in stcal
+        rejection_thresh, three_grp_thresh, four_grp_thresh,
+        max_cores, max_jump_to_flag_neighbors,
+        min_jump_to_flag_neighbors, flag_4_neighbors,
 
-    # extract data and info from input_model to pass to detect_jumps
-    frames_per_group = output_model.meta.exposure.nframes
+        after_jump_flag_dn1=0.0,
+        after_jump_flag_time1=0.0,
+        after_jump_flag_dn2=0.0,
+        after_jump_flag_time2=0.0,
 
+        min_sat_area=1.0, min_jump_area=5.0, min_sat_radius_extend=2.5,
+        expand_factor=2.0, use_ellipses=False,
+        sat_required_snowball=True, sat_expand=2,
+
+        expand_large_events=False, find_showers=False, edge_size=25, extend_snr_threshold=1.1,
+        extend_min_area=90, extend_inner_radius=1, extend_outer_radius=2.6, extend_ellipse_expand_ratio=1.1,
+
+        time_masked_after_shower=30, min_diffs_single_pass=10,
+        max_extended_radius=200,
+        minimum_groups=3,
+        minimum_sigclip_groups=100,
+
+        only_use_ints=True,
+        mask_snowball_persist_next_int=True,
+        snowball_time_masked_next_int=250
+):
     # determine the number of groups that correspond to the after_jump times
-    # needed because the group time is not passed to detect_jumps
+    # needed because the group time is not passed to detect_jumps_data
     gtime = output_model.meta.exposure.group_time
     after_jump_flag_n1 = int(after_jump_flag_time1 // gtime)
     after_jump_flag_n2 = int(after_jump_flag_time2 // gtime)
     grps_masked_after_shower = int(time_masked_after_shower // gtime)
     snowball_grps_masked_next_int = int(snowball_time_masked_next_int // gtime)
+
     # Likewise, convert a max MIRI shower amplitude in DN/s to DN/group
     max_shower_amplitude = max_shower_amplitude * gtime
+
     # Get 2D gain and read noise values from their respective models
     if reffile_utils.ref_matches_sci(output_model, gain_model):
         gain_2d = gain_model.data
@@ -57,45 +70,35 @@ def run_detect_jumps(output_model, gain_model, readnoise_model,
         readnoise_2d = readnoise_model.data
     else:
         log.info('Extracting readnoise subarray to match science data')
-        readnoise_2d = reffile_utils.get_subarray_data(output_model,
-                                                       readnoise_model)
-    new_gdq, new_pdq, number_crs, number_extended_events, stddev\
-        = detect_jumps(frames_per_group, output_model.data, output_model.groupdq,
-                                    output_model.pixeldq, output_model.err,
-                                    gain_2d, readnoise_2d,
-                                    rejection_thresh, three_grp_thresh,
-                                    four_grp_thresh, max_cores,
-                                    max_jump_to_flag_neighbors,
-                                    min_jump_to_flag_neighbors,
-                                    flag_4_neighbors, dqflags.pixel,
-                                    after_jump_flag_dn1,
-                                    after_jump_flag_n1,
-                                    after_jump_flag_dn2,
-                                    after_jump_flag_n2,
-                                    min_sat_area=min_sat_area, min_jump_area=min_jump_area,
-                                    expand_factor=expand_factor, use_ellipses=use_ellipses,
-                                    min_sat_radius_extend=min_sat_radius_extend,
-                                    sat_required_snowball=sat_required_snowball, sat_expand=sat_expand,
-                                    expand_large_events=expand_large_events, find_showers=find_showers,
-                                    edge_size=edge_size, extend_snr_threshold=extend_snr_threshold,
-                                    extend_min_area=extend_min_area, extend_inner_radius=extend_inner_radius,
-                                    extend_outer_radius=extend_outer_radius,
-                                    extend_ellipse_expand_ratio=extend_ellipse_expand_ratio,
-                                    grps_masked_after_shower=grps_masked_after_shower,
-                                    min_diffs_single_pass=min_diffs_single_pass,
-                                    max_extended_radius=max_extended_radius,
-                                    minimum_groups=minimum_groups,
-                                    minimum_sigclip_groups=minimum_sigclip_groups,
-                                    only_use_ints=only_use_ints,
-                                    mask_persist_grps_next_int = mask_snowball_persist_next_int,
-                                    persist_grps_flagged = snowball_grps_masked_next_int,
-                                    max_shower_amplitude = max_shower_amplitude
-                                    )
+        readnoise_2d = reffile_utils.get_subarray_data(output_model, readnoise_model)
 
+    # Create and populate the RampData class
+    jump_data = JumpData(output_model, gain_2d, readnoise_2d, dqflags.pixel)
+    jump_data.set_detection_settings(
+        rejection_thresh, three_grp_thresh, four_grp_thresh, max_jump_to_flag_neighbors,
+        min_jump_to_flag_neighbors, flag_4_neighbors)
+    jump_data.set_after_jump(
+        after_jump_flag_dn1, after_jump_flag_n1, after_jump_flag_dn2, after_jump_flag_n2)
+    jump_data.set_snowball_info(
+        expand_large_events, min_jump_area, min_sat_area, expand_factor, sat_required_snowball,
+        min_sat_radius_extend, sat_expand, edge_size)
+    jump_data.set_shower_info(
+        find_showers, extend_snr_threshold, extend_min_area, extend_inner_radius,
+        extend_outer_radius, extend_ellipse_expand_ratio, min_diffs_single_pass, max_extended_radius)
+    jump_data.set_sigma_clipping_info(
+        minimum_groups, minimum_sigclip_groups, only_use_ints)
+    jump_data.max_cores = max_cores
+    jump_data.grps_masked_after_shower = int(time_masked_after_shower // gtime)
+    jump_data.mask_persist_grps_next_int = mask_snowball_persist_next_int
+    jump_data.persist_grps_flagged = int(snowball_time_masked_next_int // gtime)
 
+    # Detect jumps
+    new_gdq, new_pdq, number_crs, number_extended_events, stddev = detect_jumps_data(jump_data)
+        
     # Update the DQ arrays of the output model with the jump detection results
     output_model.groupdq = new_gdq
     output_model.pixeldq = new_pdq
+
     # determine the number of groups with all pixels set to DO_NOT_USE
     dnu_flag = 1
     num_flagged_grps = 0
