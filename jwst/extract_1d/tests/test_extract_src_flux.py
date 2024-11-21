20fb4cb84e35a9159b5d81925a65d6d25ab156c2
Melanie Clarke <mclarke@stsci.edu>
2024-11-21 17:11:18 -0500

    Adding unit tests for extraction

--- jwst/extract_1d/tests/test_extract_src_flux.py
+++ jwst/extract_1d/tests/test_extract_src_flux.py
@@ -28,6 +28,28 @@ def inputs_constant():
             profile, weights, profile_bg)
 
 
+@pytest.fixture
+def inputs_with_source():
+    shape = (9, 5)
+    image = np.full(shape, 0.0)
+    image[3] = 5.0
+    image[4] = 10.0
+    image[5] = 5.0
+    var_rnoise = image * 0.05
+    var_poisson = image * 0.05
+    var_rflat = image * 0.05
+    weights = None
+    profile_bg = None
+
+    profile = np.zeros_like(image)
+    profile[3] = 0.25
+    profile[4] = 0.5
+    profile[5] = 0.25
+
+    return (image, var_rnoise, var_poisson, var_rflat,
+            profile, weights, profile_bg)
+
+
 def test_extract_src_flux(inputs_constant):
     (image, var_rnoise, var_poisson, var_rflat,
      profile, weights, profile_bg) = inputs_constant
@@ -87,3 +109,45 @@ def test_extract_src_flux_empty_interval(inputs_constant):
     assert np.all(np.isnan(total_flux))
     assert np.all(bkg_flux == 0.)
     assert np.all(npixels == 0.)
+
+
+def test_extract_optimal(inputs_with_source):
+    (image, var_rnoise, var_poisson, var_rflat,
+     profile, weights, profile_bg) = inputs_with_source
+
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, extraction_type='optimal')
+
+    # Total flux should be well modeled
+    assert np.allclose(total_flux[0], 20)
+    assert np.allclose(bkg_flux[0], 0)
+    assert np.allclose(npixels[0], 3)
+
+    # set a NaN value in a column of interest
+    image[4, 2] = np.nan
+
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg, extraction_type='optimal')
+
+    # Total flux is still well modeled from 2 pixels
+    assert np.allclose(total_flux[0], 20)
+    assert npixels[0, 2] == 2
+
+    # set the whole column to NaN
+    image[:, 2] = np.nan
+
+    (total_flux, f_var_rnoise, f_var_poisson, f_var_flat,
+     bkg_flux, b_var_rnoise, b_var_poisson, b_var_flat,
+     npixels, model) = extract1d.extract1d(
+        image, [profile], var_rnoise, var_poisson, var_rflat,
+        weights=weights, profile_bg=profile_bg)
+
+    # Now the flux can no longer be estimated in that column
+    assert np.isnan(total_flux[0][2])
+    assert npixels[0][2] == 0.
