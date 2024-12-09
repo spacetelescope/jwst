fixed dispersion axis calculation and zero-valued variance problem

jwst/background/background_sub_wfss.py
@@ -87,7 +87,7 @@ def subtract_wfss_bkg(
     result.data = input_model.data - subtract_this
     result.dq = np.bitwise_or(input_model.dq, bkg_ref.dq)
 
-    log.info(f"Average of background image subtracted = {np.nanmean(subtract_this):.5f}")
+    log.info(f"Average of background image subtracted = {np.nanmean(subtract_this):.3e}")
 
     bkg_ref.close()
 
@@ -115,7 +115,7 @@ class _ScalingFactorComputer:
         """
         if rms_thresh is None:
             rms_thresh = -1
-        if (rms_thresh > 0) and (dispersion_axis not in [0,1]):
+        if (rms_thresh > 0) and (dispersion_axis not in [1,2]):
             msg = (f"Unrecognized dispersion axis {dispersion_axis}. "
                     "Dispersion axis must be specified if rms_thresh "
                     "is used as a stopping criterion.")
@@ -177,6 +177,9 @@ class _ScalingFactorComputer:
 
 
     def err_weighted_mean(self, sci, bkg, var):
+        """Remove any var=0 values, which can happen for real data"""
+        mask = (var == 0)
+        self._update_nans(sci, bkg, var, mask)
         return np.nansum(sci*bkg/var) / np.nansum(bkg*bkg/var)
 
 
@@ -187,11 +190,14 @@ class _ScalingFactorComputer:
 
 
     def _compute_rms_residual(self, sci_sub):
-        """Calculate the RMS of the cross-dispersion background, which is found by taking
-        the median along the dispersion axis"""
-        sci_sub_profile = np.nanmedian(sci_sub, axis=self.dispersion_axis)
+        """
+        Calculate the RMS of the cross-dispersion background, which is found by taking
+        the median along the dispersion axis.
+        Note meta.wcsinfo.dispersion_axis is 1-indexed coming out of assign_wcs, i.e., in [1,2].
+        So we need to """
+        collapsing_axis = int(not bool(self.dispersion_axis - 1))
+        sci_sub_profile = np.nanmedian(sci_sub, axis=collapsing_axis)
         return np.sqrt(np.nanmean(sci_sub_profile**2))
-
 
 
 def _sufficient_background_pixels(dq_array, bkg_mask, min_pixels=100):

jwst/background/tests/test_background_wfss.py
@@ -233,6 +233,13 @@ def test_weighted_mean(make_wfss_datamodel, bkg_file):
     var = make_wfss_datamodel.err**2
     with datamodels.open(bkg_file) as bkg_model:
         bkg = bkg_model.data
+    
+    # put 0.1% zero values in variance to ensure coverage of previous bug where zero-valued
+    # variances in real data caused factor = 1/np.inf = 0
+    rng = np.random.default_rng(seed=42)
+    n_bad = int(var.size / 1000)
+    bad_i = rng.choice(var.size-1, n_bad)
+    var[np.unravel_index(bad_i, var.shape)] = 0.0
 
     # instantiate scaling factor computer
     rescaler = _ScalingFactorComputer()
@@ -262,7 +269,7 @@ def test_weighted_mean(make_wfss_datamodel, bkg_file):
     # need lots of significant digits here because iterating makes little difference
     # for this test case
     maxiter = 10
-    rms_thresh = 0.0215 
+    rms_thresh = 0.0217 
     rescaler = _ScalingFactorComputer(p=0.5, dispersion_axis=1, rms_thresh=rms_thresh, maxiter=maxiter)
     factor, mask_out = rescaler(sci, bkg, var)
     assert rescaler._iters_run_last_call < maxiter