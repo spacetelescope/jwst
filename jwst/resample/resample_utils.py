548955fb28b8e753848ba76d5ebe51c27fd3f735
Mihai Cara <mihail.cara@gmail.com>
2024-12-19 16:08:13 -0500

    do not deepcopy wcs

--- jwst/resample/resample_utils.py
+++ jwst/resample/resample_utils.py
@@ -178,12 +178,9 @@ def reproject(wcs1, wcs2):
         # keyword arguments and `with_bounding_box=False` cannot be passsed.
         # We delete the bounding box on a copy of the WCS - yes, inefficient.
         forward_transform = wcs1.pixel_to_world_values
-        wcs_no_bbox = deepcopy(wcs2)
-        wcs_no_bbox.bounding_box = None
-        backward_transform = wcs_no_bbox.world_to_pixel_values
+        backward_transform = wcs1.world_to_pixel_values
     except AttributeError as err:
         raise TypeError("Input should be a WCS") from err
-
 
     def _reproject(x, y):
         sky = forward_transform(x, y)
