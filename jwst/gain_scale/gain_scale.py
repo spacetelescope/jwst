c0d9b538ceacb8d7fffe3b7632460d95722a4600
Ken MacDonald <kmacdonald@stsci.edu>
2025-02-10 07:42:23 -0500

    Updating pre-commit ruff configuration and files pre-commit fixed.

--- jwst/gain_scale/gain_scale.py
+++ jwst/gain_scale/gain_scale.py
@@ -24,24 +24,21 @@ def do_correction(output_model, gain_factor):
         Output datamodel with rescaled data
 
     """
-
     # Apply the gain factor to the SCI and ERR arrays
-    log.info('Rescaling by {0}'.format(gain_factor))
+    log.info(f"Rescaling by {gain_factor}")
     output_model.data *= gain_factor
     output_model.err *= gain_factor
 
     # Apply the square of the gain factor to the variance arrays,
     # if they exist
-    if (output_model.var_poisson is not None
-            and np.size(output_model.var_poisson)) > 0:
+    if (output_model.var_poisson is not None and np.size(output_model.var_poisson)) > 0:
         output_model.var_poisson *= gain_factor**2
 
-    if (output_model.var_rnoise is not None
-            and np.size(output_model.var_rnoise)) > 0:
+    if (output_model.var_rnoise is not None and np.size(output_model.var_rnoise)) > 0:
         output_model.var_rnoise *= gain_factor**2
 
     # Set step status info
     output_model.meta.exposure.gain_factor = gain_factor
-    output_model.meta.cal_step.gain_scale = 'COMPLETE'
+    output_model.meta.cal_step.gain_scale = "COMPLETE"
 
     return output_model
