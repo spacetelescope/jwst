a0c6fa6810977f7519463c17941968d83dd3c309
jemorrison <morrison@as.arizona.edu>
2025-02-10 11:14:35 -0700

    Update codestyle for first frame step

--- jwst/firstframe/firstframe_step.py
+++ jwst/firstframe/firstframe_step.py
@@ -8,22 +8,34 @@ __all__ = ["FirstFrameStep"]
 
 class FirstFrameStep(Step):
     """
-    FirstFrameStep: This is a MIRI specific task.  If the number of groups
-    is greater than 3, the DO_NOT_USE group data quality flag is added to
-    first group.
+    Set data quality flags for the first group in MIRI ramps.
+
+    A MIRI specific task.  If the number of groups is > than 3,
+    the DO_NOT_USE group data quality flag is added to first group.
     """
 
     class_alias = "firstframe"
 
     spec = """
         bright_use_group1 = boolean(default=False) # do not flag group1 if group3 is saturated
-    """ # noqa: E501
+    """  # noqa: E501
 
     def process(self, step_input):
+        """
+        For MIRI data with more than 3 groups, set first group dq to DO_NOT_USE.
 
+        Parameters
+        ----------
+        step_input : DataModel
+            Input datamodel to be corrected
+
+        Returns
+        -------
+        output_model : DataModel
+            Firstframe corrected datamodel
+        """
         # Open the input data model
         with datamodels.open(step_input) as input_model:
-
             # check the data is MIRI data
             detector = input_model.meta.instrument.detector.upper()
             if detector[:3] != "MIR":
@@ -36,8 +48,6 @@ class FirstFrameStep(Step):
             result = input_model.copy()
 
             # Do the firstframe correction subtraction
-            result = firstframe_sub.do_correction(
-                result, bright_use_group1=self.bright_use_group1
-            )
+            result = firstframe_sub.do_correction(result, bright_use_group1=self.bright_use_group1)
 
         return result
