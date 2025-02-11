88bf08a7eeb139e21fef4402ef6b75be27983306
Melanie Clarke <mclarke@stsci.edu>
2025-02-11 16:35:15 -0500

    Match imprints for science and background separately

--- jwst/imprint/imprint_step.py
+++ jwst/imprint/imprint_step.py
@@ -23,43 +23,56 @@ class ImprintStep(Step):
         input_model = datamodels.open(input)
         pos_no = input_model.meta.dither.position_number
         obs_no = input_model.meta.observation.observation_number
+        is_bkgd = input_model.meta.observation.bkgdtarg
 
-        # If there is only one imprint image listed in the association,
-        # use it for all science images.
-        # If there is more than one imprint image in the association,
-        # then select the imprint image to be used by matching the dither pattern position
-        # number and observation number to the science image.
-        num_imprint = len(imprint)
-        if num_imprint == 1:
-            match = 0  # use the first, and only, imprint image
-        else:
-            match = None
-            for i in range(num_imprint):
-                # open imprint image and read in pos_no to find match
-                imprint_model = datamodels.open(imprint[i])
-                imprint_pos_no = imprint_model.meta.dither.position_number
-                imprint_obs_no = imprint_model.meta.observation.observation_number
+        # Check all the imprints to see if there is a direct match for the input
+        imprint_models = []
+        match_model = None
+        for imprint_exp in imprint:
+            imprint_model = datamodels.open(imprint_exp)
+            imprint_pos_no = imprint_model.meta.dither.position_number
+            imprint_obs_no = imprint_model.meta.observation.observation_number
+            imprint_bkg = imprint_model.meta.observation.bkgdtarg
+
+            if is_bkgd != imprint_bkg:
+                # Imprint does not match input's background status.
+                # Don't consider it further.
                 imprint_model.close()
+            else:
+                # Keep the open model for further checks
+                imprint_models.append(imprint_model)
+
+                # Check if there is a direct match to the current
+                # observation and dither position.
                 if pos_no == imprint_pos_no and obs_no == imprint_obs_no:
-                    match = i
+                    match_model = imprint_model
                     break
 
-        # Copy the input image to the output (just in case no matching imprint image was found)
+        if match_model is None and len(imprint_models) == 1:
+            # No direct match found.
+            # Check for a single imprint intended for use with all dither positions.
+            match_model = imprint_models[0]
+
+        # Copy the input image to the output
         result = input_model.copy()
 
-        if match is not None:
+        if match_model is not None:
             # Subtract the matching imprint image
-            imprint_model = datamodels.open(imprint[match])
-            self.log.info(f"Subtracting imprint image {imprint_model.meta.filename}")
-            result = subtract_images.subtract(input_model, imprint_model)
+            self.log.info(f"Subtracting imprint image {match_model.meta.filename} "
+                          f"from {input_model.meta.filename}")
+            result = subtract_images.subtract(input_model, match_model)
 
             # Update the step status and close the imprint model
             result.meta.cal_step.imprint = 'COMPLETE'
-            imprint_model.close()
         else:
             self.log.warning(f'No matching imprint image was found for {input}')
             self.log.warning('Step will be skipped')
             result.meta.cal_step.imprint = 'SKIPPED'
 
+        # Close any open imprint models
+        for model in imprint_models:
+            model.close()
+
+        # Close the input model
         input_model.close()
         return result
