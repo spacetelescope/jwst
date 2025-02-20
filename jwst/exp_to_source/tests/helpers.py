f8a46cd51b3c8a1042e287a407596b9e1dc3cfb4
Maria Pena-Guerrero <pena@stsci.edu>
2025-02-20 15:41:19 -0500

    style changes

--- jwst/exp_to_source/tests/helpers.py
+++ jwst/exp_to_source/tests/helpers.py
@@ -2,14 +2,14 @@
 from pathlib import Path
 
 
-INPUT_FILES_GLOB = "data/jwst_nod?_cal.fits"
+INPUT_FILES_GLOB = "data/"
 
 
 def t_path(partial_path):
     """Construct the full path for test files."""
-    test_dir = Path(__file__)
+    test_dir = Path(__file__).parent
     return Path(test_dir) / partial_path
 
 
 # Calculate some extra constants
-INPUT_FILES = list(t_path(INPUT_FILES_GLOB).glob("*"))
+INPUT_FILES = list(t_path("data").glob("jwst_nod*_cal.fits"))
