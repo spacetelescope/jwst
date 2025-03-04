e7f6aaf439ea7f1858cca190aacfb819e2554ef2
Melanie Clarke <mclarke@stsci.edu>
2025-03-04 15:17:32 -0500

    Code style fixes and cleanup

--- jwst/residual_fringe/fitter.py
+++ jwst/residual_fringe/fitter.py
@@ -9,6 +9,34 @@ def _lsq_spline(x, y, weights, knots, degree):
 
 
 def spline_fitter(x, y, weights, knots, degree, reject_outliers=False, domain=10, tolerance=0.0001):
+    """
+    Fit a spline function to 1D data.
+
+    Parameters
+    ----------
+    x : ndarray
+        Independent variable. Must be increasing.
+    y : ndarray
+        Dependent variable, matching dimensions of `x`.
+    weights : ndarray
+        Weights for spline fitting.  Must be positive.
+    knots : ndarray
+        Interior knots for the spline.
+    degree : int
+        Degree of the spline to fit, >= 1 and <= 5.
+    reject_outliers : bool, optional
+        If True, iteratively fit the data with outlier rejection.
+    domain : int, optional
+        Factor controlling the outlier threshold when `reject_outliers`
+        is True.
+    tolerance : float, optional
+        Fit convergence tolerance when reject_outliers` is True.
+
+    Returns
+    -------
+    callable
+        The spline function fit to the data.
+    """
     if not reject_outliers:
         return _lsq_spline(x, y, weights, knots, degree)
 
@@ -32,7 +60,7 @@ def spline_fitter(x, y, weights, knots, degree, reject_outliers=False, domain=10
 
         # Calculate new weights
         resid = (y - spline(x)) / (scale * domain)
-        new_w = (np.where(resid**2 <= 1, 1 - resid**2, 0.))**2 * weights
+        new_w = (np.where(resid**2 <= 1, 1 - resid**2, 0.0)) ** 2 * weights
 
         # Fit new model and find chi
         spline = _lsq_spline(x, y, new_w, knots, degree)
--- jwst/residual_fringe/tests/test_utils.py
+++ jwst/residual_fringe/tests/test_utils.py
@@ -3,6 +3,7 @@ import pytest
 
 from jwst.residual_fringe import utils
 
+
 @pytest.fixture()
 def slice_map():
     """
@@ -21,8 +22,8 @@ def slice_map():
 
 def test_slice_info_ch1(slice_map):
     result = utils.slice_info(slice_map, 1)
-    slices_in_band, xrange_channel, slice_x_ranges, all_slice_masks = result
-    assert np.all(slices_in_band == [101, 102])
+    slices_in_channel, xrange_channel, slice_x_ranges, all_slice_masks = result
+    assert np.all(slices_in_channel == [101, 102])
     assert np.all(xrange_channel == [9, 40])
     assert np.all(slice_x_ranges == [[101, 9, 20], [102, 29, 40]])
     assert np.sum(all_slice_masks[0]) == 200
@@ -31,8 +32,8 @@ def test_slice_info_ch1(slice_map):
 
 def test_slice_info_ch2(slice_map):
     result = utils.slice_info(slice_map, 2)
-    slices_in_band, xrange_channel, slice_x_ranges, all_slice_masks = result
-    assert np.all(slices_in_band == [201, 202])
+    slices_in_channel, xrange_channel, slice_x_ranges, all_slice_masks = result
+    assert np.all(slices_in_channel == [201, 202])
     assert np.all(xrange_channel == [49, 80])
     assert np.all(slice_x_ranges == [[201, 49, 60], [202, 69, 80]])
     assert np.sum(all_slice_masks[0]) == 200
