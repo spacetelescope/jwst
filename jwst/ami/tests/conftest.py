c4cc9f35e200eb62fff6b803b024c88b13dea77e
Ned Molter <emolter@stsci.edu>
2025-03-05 15:32:47 -0500

    Add unt tests for the rest of the utils modules except affine2d

--- jwst/ami/tests/conftest.py
+++ jwst/ami/tests/conftest.py
@@ -14,7 +14,14 @@ PXSC_MAS = PXSC_DEG * 3600 * 1000
 
 @pytest.fixture(scope="package")
 def example_model():
-    """Create a simple CubeModel simulating input to the ami3 pipeline."""
+    """
+    Create a simple CubeModel simulating input to the ami3 pipeline.
+
+    Returns
+    -------
+    CubeModel
+        A simple CubeModel simulating NIRISS AMI data.
+    """
     model = dm.CubeModel((2, 81, 81))
     # some non-zero data is required as this step will center
     # the image and find the centroid (both fail with all zeros)
@@ -39,7 +46,14 @@ def example_model():
 
 @pytest.fixture(scope="package")
 def circular_pupil():
-    """Make a simple circular pupil mask."""
+    """
+    Make a simple circular pupil mask.
+
+    Returns
+    -------
+    np.ndarray
+        A 1024x1024 array with a circular pupil of radius 0.2x the array shape.
+    """
     shape = (1024, 1024)
     r = 0.2
     x = np.linspace(-1, 1, shape[0])
@@ -53,26 +67,80 @@ def circular_pupil():
 
 @pytest.fixture(scope="package")
 def nrm_model_circular(circular_pupil):
-    """Make a simple NRMModel with a circular pupil."""
+    """
+    Make a simple NRMModel with a circular pupil.
+
+    Returns
+    -------
+    NRMModel
+        A simple NRMModel with a circular pupil.
+    """
     return dm.NRMModel(nrm=circular_pupil)
 
 
 @pytest.fixture(scope="package")
 def nrm_model(example_model):
-    """Retrieve a real NRMModel reference file from CRDS."""
+    """
+    Retrieve a real NRMModel reference file from CRDS.
+
+    Returns
+    -------
+    NRMModel
+        The NRMModel reference file for the filter defined by example_model
+    """
     nrm_reffile = Step().get_reference_file(example_model, "nrm")
-    nrm_model = dm.NRMModel(nrm_reffile)
-    return nrm_model
+    return dm.NRMModel(nrm_reffile)
 
 
 @pytest.fixture(scope="package")
 def bandpass(example_model):
-    """Simulate the bandpass of the example_model with a top-hat function."""
-    filt = example_model.meta.instrument.filter
-    wl_low, wl_high = filthp_d[filt]
+    """
+    Simulate the bandpass of the example_model with a top-hat function.
 
-    wls = np.linspace(wl_low, wl_high, 9)
+    Returns
+    -------
+    np.ndarray
+        An Nx2 array with the first column being the throughput and the second
+        column being the wavelength in meters.
+    """
+    sz = 99
+    fractional_pad = 0.25
+    filt = example_model.meta.instrument.filter
+    wlhp_l, wlhp_h = filthp_d[filt]
+
+    # wlhp_l and wlhp_h are the cut-on and cut-off points.
+    # outside of this range, throughput should be low. pad with 0.001 out to 0.5x
+    # the range of the filter
+    wl_low = wlhp_l - fractional_pad * (wlhp_h - wlhp_l)
+    wl_high = wlhp_h + fractional_pad * (wlhp_h - wlhp_l)
+    wls = np.linspace(wl_low, wl_high, sz).astype(np.float32)
     weights = np.ones_like(wls)
-    weights[0] = 0.01
-    weights[-1] = 0.01
+    weights[wls < wlhp_l] = 0.001
+    weights[wls > wlhp_h] = 0.001
     return np.array(list(zip(weights, wls, strict=True)))
+
+
+@pytest.fixture(scope="package")
+def throughput_model(example_model, bandpass):
+    """
+    Mock a datamodels.ThroughputModel.
+
+    Returns
+    -------
+    ThroughputModel
+        A ThroughputModel with the same filter as the example_model and a top-hat
+        bandpass defined by the bandpass parameter.
+    """
+    model = dm.ThroughputModel()
+    model.meta.instrument.filter = example_model.meta.instrument.filter
+
+    # build filter table from bandpass we already have
+    # wavelengths are assumed to be angstrom by synphot, so convert them here
+    wl_angstrom = bandpass[:, 1] * 1e10
+    filter_table = np.rec.array(
+        list(zip(wl_angstrom, bandpass[:, 0], strict=True)),
+        names="wavelength,throughput",
+        formats="f4,f4",
+    )
+    model.filter_table = filter_table
+    return model
--- jwst/ami/tests/test_utils.py
+++ jwst/ami/tests/test_utils.py
@@ -6,6 +6,41 @@ from numpy.testing import assert_allclose
 import stdatamodels.jwst.datamodels as dm
 
 from jwst.ami import utils
+from jwst.ami.bp_fix import filthp_d, filtwl_d
+
+
+def test_affine2d():
+    pass
+
+
+@pytest.mark.parametrize("n, ctr",
+    [
+        (20, (-0.5, -0.5)),
+        (21, (0.0, 0.0)),
+    ]
+)
+def test_makedisk(n, ctr):
+    r = 4
+    arr = utils.makedisk(n, r, ctr=ctr)
+
+    # test disk is symmetric about center in both directions
+    assert_allclose(arr[:10,:], arr[-10:,:][::-1,:])
+    assert_allclose(arr[:,:10], arr[:,-10:][:,::-1])
+
+    # test disk is all 0 or 1
+    assert np.all(np.logical_or(arr == 0, arr == 1))
+
+
+@pytest.mark.parametrize("dtype", [int, float])
+def test_avoidhexsingularity(dtype):
+    rotation = np.arange(-91, 92, 1, dtype=dtype)
+    for rot in rotation:
+        is_multiple_of_15 = rot % 15 == 0
+        rot_fixed = utils.avoidhexsingularity(rot)
+        if is_multiple_of_15:
+            assert rot_fixed != rot
+        else:
+            assert rot_fixed == rot
 
 
 @pytest.mark.parametrize(
@@ -19,35 +54,87 @@ def test_centerpoint(shape, center):
     assert utils.centerpoint(shape) == center
 
 
+def test_min_distance_to_edge():
+    """
+    cntrimg=True case still needs testing.
+    But this will not be used if center_imgpeak can be deleted.
+    """
+    sz = 21
+    shp = (sz, sz)
+    img = np.zeros(shp)
+    img[10, 10] = 1
+    
+    # basic test
+    x_out, y_out, dist = utils.min_distance_to_edge(img, cntrimg=False)
+    assert dist == 10.0
+    assert x_out == 10
+    assert y_out == 10
+
+    # add an equally bright source closer to the edge
+    img[5, 8] = 1
+    x_out, y_out, dist = utils.min_distance_to_edge(img, cntrimg=False)
+    assert dist == 5.0
+    assert x_out == 5
+    assert y_out == 8
+
+    # add a fainter source closer to the edge
+    img[4, 8] = 0.5
+    x_out, y_out, dist = utils.min_distance_to_edge(img, cntrimg=False)
+    assert dist == 5.0
+    assert x_out == 5
+    assert y_out == 8
+
+    # No failure when no sources, just dist goes to zero
+    img = np.zeros(shp)
+    x_out, y_out, dist = utils.min_distance_to_edge(img, cntrimg=False)
+    assert dist == 0.0
+
+
 def test_find_centroid():
+    """Tests of find_centroid and findslope functions."""
+
     arr = np.zeros((30, 30), dtype="f4")
     arr[15, 15] = 1
     assert_allclose(utils.find_centroid(arr), (0.5, 0.5))
 
 
-@pytest.mark.parametrize(
-    "mas, rad",
-    [
-        (206264.8062471, 0.001),
-        (103132403.12355, 0.5),
-    ],
-)
-def test_mas2rad(mas, rad):
-    assert np.isclose(utils.mas2rad(mas), rad)
+def test_quadratic_extremum():
+    """Tests of quadratic_extremum and findpeak_1d functions"""
+
+    def quadratic(p, x):
+        """Ordering of p is consistent with np.polyfit"""
+        return p[0]*x**2 + p[1] * x + p[2]
+    
+    x = np.linspace(-5, 5, 100)
+
+    # Test of quadratic_extremum with a maximum
+    p = np.array([-2.0, 3.0, 7.0])
+    maxx, maxy = utils.quadratic_extremum(p)
+    true_maxx = 0.75
+    true_maxy = 8.125
+    assert_allclose([maxx, maxy], [true_maxx, true_maxy])
+
+    # test findpeak_1d will find the same maximum
+    # TODO: these currently fail because of bug in findpeak_1d
+    # y = quadratic(p, x)
+    # maxx, maxy = utils.findpeak_1d(x, y)
+    # assert_allclose([maxx, maxy], [true_maxx, true_maxy])
+
+    # Test of quadratic_extremum with a minimum
+    p = np.array([2.0, 3.0, 7.0])
+    minx, miny = utils.quadratic_extremum(p)
+    true_minx = -0.75
+    true_miny = 5.875
+    assert_allclose([minx, miny], [true_minx, true_miny])
+
+    # test findpeak_1d will find the same maximum
+    # TODO: these currently fail because of bug in findpeak_1d
+    # y = quadratic(p, x)
+    # minx, miny = utils.findpeak_1d(x, y)
+    # assert_allclose([minx, miny], [true_minx, true_miny])
 
 
-def test_utils_rebin():
-    """Test of rebin() and krebin() in utils module"""
-    arr = np.arange(24).reshape((3, 8)) / 10.0
-    rc = tuple((2, 2))
-
-    binned_arr = utils.rebin(arr, rc)
-
-    true_arr = np.array([[5.1, 6.3, 7.5, 8.7]])
-    assert_allclose(binned_arr, true_arr)
-
-
-def test_utils_quadratic():
+def test_quadratic():
     """Test of quadratic in utils module"""
     x = np.array([0.5, 0.55, 0.55, 0.65, 0.70, 0.8, 0.85, 1.0, 1.01, 1.02, 1.03, 1.04, 1.05])
     p = np.array([-2.0, 3.0, 7.0])
@@ -63,31 +150,8 @@ def test_utils_quadratic():
     )
     assert_allclose(fit_vals, true_fit_vals)
 
-
-def test_utils_findmax():
-    """Test of findmax in utils module"""
-    mag = np.arange(9) + 1.0
-    delt = 1.0e-7
-    mag[2] += delt
-    mag[5] += delt  # Add a bit of deterministic noise
-    mag[1] -= delt
-    mag[7] -= delt
-
-    vals = (mag - 3.0) ** 2 + 5  # Is quadratic ...
-    vals[1] += delt
-    vals[6] += delt  # ... with a bit more noise
-    vals[4] -= delt
-    vals[3] -= delt
-
-    maxx, maxy = utils.findmax(mag, vals)
-
-    true_maxx = 3.0
-    true_maxy = 5.0
-    assert_allclose([maxx, maxy], [true_maxx, true_maxy])
-
-
-def test_utils_makeA():
-    """Test of makeA in utils module"""
+def test_make_a():
+    """Test of make_a in utils module"""
     nh = 4  # number of holes
     arr = utils.make_a(nh)
 
@@ -103,8 +167,7 @@ def test_utils_makeA():
     )
     assert_allclose(arr, true_arr)
 
-
-def test_utils_fringes2pistons():
+def test_fringes2pistons():
     """Test of fringes2pistons in utils module"""
     fringephases = np.array([0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1])
     nholes = 5
@@ -114,8 +177,18 @@ def test_utils_fringes2pistons():
     true_result = np.array([-0.02, -0.034, -0.02, 0.014, 0.06])
     assert_allclose(result, true_result)
 
+def test_rebin():
+    """Test of rebin() and krebin() in utils module"""
+    arr = np.arange(24).reshape((3, 8)) / 10.0
+    rc = tuple((2, 2))
 
-def test_utils_rcrosscorrelate():
+    binned_arr = utils.rebin(arr, rc)
+
+    true_arr = np.array([[5.1, 6.3, 7.5, 8.7]])
+    assert_allclose(binned_arr, true_arr)
+
+
+def test_rcrosscorrelate():
     """Test of rcrosscorrelate() in utils module"""
     a = np.array(
         [
@@ -148,7 +221,7 @@ def test_utils_rcrosscorrelate():
     assert_allclose(result, true_result)
 
 
-def test_utils_crosscorrelate():
+def test_crosscorrelate():
     """Test of crosscorrelate() in utils module"""
     a = np.array(
         [
@@ -180,8 +253,53 @@ def test_utils_crosscorrelate():
     )
     assert_allclose(result, true_result)
 
+def test_rotate2dccw():
 
-def test_utils_imgmedian():
+    # test 90 degree rotation
+    vectors = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
+    rotated = utils.rotate2dccw(vectors, np.pi/2)
+    # very near zero so default rtol
+    assert_allclose(rotated, np.array([[0, 1], [-1, 0], [0, -1], [1, 0]]), atol=1e-10)
+
+    # test that small positive rotation makes x smaller and y larger
+    vector = np.array([[1, 1],])
+    rotated = utils.rotate2dccw(vector, np.pi/30)[0]
+    assert rotated[0] < vector[0][0]
+    assert rotated[1] > vector[0][1]
+
+
+def test_findmax():
+    """Test of findmax in utils module"""
+    mag = np.arange(9) + 1.0
+    delt = 1.0e-7
+    mag[2] += delt
+    mag[5] += delt  # Add a bit of deterministic noise
+    mag[1] -= delt
+    mag[7] -= delt
+
+    vals = (mag - 3.0) ** 2 + 5  # Is quadratic ...
+    vals[1] += delt
+    vals[6] += delt  # ... with a bit more noise
+    vals[4] -= delt
+    vals[3] -= delt
+
+    maxx, maxy = utils.findmax(mag, vals)
+
+    true_maxx = 3.0
+    true_maxy = 5.0
+    assert_allclose([maxx, maxy], [true_maxx, true_maxy])
+
+@pytest.mark.parametrize(
+    "mas, rad",
+    [
+        (206264.8062471, 0.001),
+        (103132403.12355, 0.5),
+    ],
+)
+def test_mas2rad(mas, rad):
+    assert np.isclose(utils.mas2rad(mas), rad)
+
+def test_img_median_replace():
     """Test of img_median_replace() in utils module"""
     # create input image model containing NaN's and DO_NOT_USE flags
     data = np.array(
@@ -223,3 +341,86 @@ def test_utils_imgmedian():
     )
 
     assert_allclose(input_model.data, expected_result)
+
+
+def test_get_cw_beta(example_model, bandpass):
+
+    filt = example_model.meta.instrument.filter
+    cw, beta = utils.get_cw_beta(bandpass)
+
+    # we constructed the bandpass to be symmetric around the input filter central wave
+    # so the central wavelength we get here should equal that input
+    assert np.isclose(cw, filtwl_d[filt])
+
+    # similarly, we should get back the same fractional width as we would get from
+    # the input half-power points, which are where the top-hat function cuts
+    # on and cuts off. This is affected by the wavelength sampling so the rtol
+    # being relatively larger (1e-3) is not a concern.
+    wl_min, wl_max = filthp_d[filt]
+    assert np.isclose(beta, (wl_max - wl_min) / cw, rtol=1e-3)
+
+
+def test_handle_bandpass(bandpass, throughput_model):
+    """Tests for get_filt_spec, get_flat_spec, combine_src_filt, and handle_bandpass functions"""
+
+    # test case where user-defined bandpass is None and we are relying on the throughput model
+    bandpass0 = utils.handle_bandpass(None, throughput_model)
+    wls = bandpass0[:, 1]
+    thrus = bandpass0[:, 0]
+
+    # check data type
+    for arr in [wls, thrus]:
+        assert isinstance(arr, np.ndarray)
+        # this gets upcast to float64 for some reason, even though it's specified to be
+        # float32 by the throughput_model
+        # assert arr.dtype == np.dtype("float32")
+        assert arr.size > 1
+
+    # ensure the wavelengths are in meters and all within the half-power points
+    # cutoff to half-power points is enforced by the fact that this is where throughput model
+    # switches from equaling unity to equaling 1e-3
+    filt = throughput_model.meta.instrument.filter
+    wlhp_l, wlhp_h = filthp_d[filt]
+    assert np.all(wls >= wlhp_l)
+    assert np.all(wls <= wlhp_h)
+
+    # ensure the throughput is flat and normalized
+    assert np.allclose(thrus/np.mean(thrus), 1.0, rtol=1e-5)
+    assert np.isclose(np.sum(thrus), 1.0)
+
+    # test case where user-defined bandpass is provided as an array
+    # In this case, the bandpass just gets returned without modification
+    bandpass1 = utils.handle_bandpass(bandpass, throughput_model)
+    assert np.all(bandpass1 == bandpass)
+
+    # test case where user-defined bandpass is provided as a synphot object
+    # Again the bandpass just gets converted to array then returned without modification
+    # or at least, that's what is supposed to happen but there is a bug where it remains
+    # a synphot object
+    bandpass2 = utils.get_filt_spec(throughput_model)
+    # print(type(bandpass2))
+    # assert np.all(bandpass2 == bandpass)
+
+
+def test_degrees_per_pixel(example_model):
+
+    # example_model has no wcsinfo to start with
+    pixscale_x, pixscale_y = utils.degrees_per_pixel(example_model)
+    assert pixscale_x == pixscale_y == 65.6 / (60.0 * 60.0 * 1000)
+
+    # add cdelt to example_model
+    example_model.meta.wcsinfo.cdelt1 = 0.1
+    example_model.meta.wcsinfo.cdelt2 = 0.05
+    pixscale_x, pixscale_y = utils.degrees_per_pixel(example_model)
+    assert pixscale_x == 0.1
+    assert pixscale_y == 0.05
+
+    # add CD matrix, which should take precedence over cdelt
+    cd = np.array([[0.2, 0.0], [0.0, 0.1]])
+    example_model.meta.wcsinfo.cd1_1 = cd[0, 0]
+    example_model.meta.wcsinfo.cd1_2 = cd[0, 1]
+    example_model.meta.wcsinfo.cd2_1 = cd[1, 0]
+    example_model.meta.wcsinfo.cd2_2 = cd[1, 1]
+    pixscale_x, pixscale_y = utils.degrees_per_pixel(example_model)
+    assert pixscale_x == 0.2
+    assert pixscale_y == 0.1
