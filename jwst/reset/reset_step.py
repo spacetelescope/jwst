229bda5736443e79e489a092776fbc41128cad5b
jemorrison <morrison@as.arizona.edu>
2025-03-10 15:15:58 -0700

    update reset step to new code style rules

--- jwst/reset/reset_step.py
+++ jwst/reset/reset_step.py
@@ -7,40 +7,48 @@ __all__ = ["ResetStep"]
 
 
 class ResetStep(Step):
-    """
-    ResetStep: Performs a reset  correction by subtracting
-    the reset correction reference data from the input science data model.
-    """
+    """Subtracting the reset correction reference data from the MIRI input science data model."""
 
     class_alias = "reset"
 
     spec = """
-    """ # noqa: E501
+    """  # noqa: E501
 
-    reference_file_types = ['reset']
+    reference_file_types = ["reset"]
 
     def process(self, step_input):
+        """
+        Open reset data model and subtracted the reset correction from the MIRI ramp model.
 
+        Parameters
+        ----------
+        step_input : DataModel
+           Input datamodel to be corrected
+
+        Returns
+        -------
+        reset: DataModel
+           The reset corrected ramp model.
+        """
         # Open the input data model
         with datamodels.open(step_input) as input_model:
-
             # check the data is MIRI data
             detector = input_model.meta.instrument.detector
-            if not detector.startswith('MIR'):
-                self.log.warning('Reset Correction is only for MIRI data')
-                self.log.warning('Reset step will be skipped')
-                input_model.meta.cal_step.reset = 'SKIPPED'
+            if not detector.startswith("MIR"):
+                self.log.warning("Reset Correction is only for MIRI data")
+                self.log.warning("Reset step will be skipped")
+                input_model.meta.cal_step.reset = "SKIPPED"
                 return input_model
 
             # Get the name of the reset reference file to use
-            self.reset_name = self.get_reference_file(input_model, 'reset')
-            self.log.info('Using RESET reference file %s', self.reset_name)
+            self.reset_name = self.get_reference_file(input_model, "reset")
+            self.log.info("Using RESET reference file %s", self.reset_name)
 
             # Check for a valid reference file
-            if self.reset_name == 'N/A':
-                self.log.warning('No RESET reference file found')
-                self.log.warning('Reset step will be skipped')
-                input_model.meta.cal_step.reset = 'SKIPPED'
+            if self.reset_name == "N/A":
+                self.log.warning("No RESET reference file found")
+                self.log.warning("Reset step will be skipped")
+                input_model.meta.cal_step.reset = "SKIPPED"
                 return input_model
 
             # Open the reset ref file data model
@@ -51,7 +59,7 @@ class ResetStep(Step):
 
             # Do the reset correction subtraction
             result = reset_sub.do_correction(result, reset_model)
-            result.meta.cal_step.reset = 'COMPLETE'
+            result.meta.cal_step.reset = "COMPLETE"
 
             # Cleanup
             del reset_model
