0fc9a9b62f7dd1d6f682792138ae9b16093d5bdb
Maria Pena-Guerrero <pena@stsci.edu>
2025-03-20 12:29:57 -0400

    style changes

--- jwst/wfs_combine/wfs_combine_step.py
+++ jwst/wfs_combine/wfs_combine_step.py
@@ -9,10 +9,7 @@ __all__ = ["WfsCombineStep"]
 
 
 class WfsCombineStep(Step):
-
-    """
-    This step combines pairs of dithered PSF images
-    """
+    """Combine pairs of dithered PSF images."""
 
     class_alias = "calwebb_wfs-image3"
 
@@ -23,42 +20,59 @@ class WfsCombineStep(Step):
         blur_size = integer(default=10)
         n_size = integer(default=2)
         suffix = string(default="wfscmb")
-    """ # noqa: E501
+    """  # noqa: E501
 
-    def make_output_path(self, basepath, *args, **kwargs):
-        # bypass all stpipe filename formatting
+    def make_output_path(self, basepath, *args, **kwargs):  # noqa:  ARG002
+        """
+        Ensure to bypass all stpipe filename formatting.
+
+        Returns
+        -------
+        basepath : str
+            Basename of the file
+        """
         return basepath
 
     def process(self, input_table):
+        """
+        Do combination of image pairs.
 
-        self.suffix = 'wfscmb'
+        Returns
+        -------
+        output_container : model container
+            Model container with all combined data
+        """
+        self.suffix = "wfscmb"
         self.output_use_model = True
 
         # Load the input ASN table
         asn_table = self.load_as_level3_asn(input_table)
 
-        self.log.info('Using input table: %s', input_table)
-        self.log.info('The number of pairs of input files: %g', len(asn_table['products']))
+        self.log.info("Using input table: %s", input_table)
+        self.log.info("The number of pairs of input files: %g", len(asn_table["products"]))
 
         output_container = ModelContainer()
 
         # Process each pair of input images listed in the association table
-        for which_set in asn_table['products']:
-
+        for which_set in asn_table["products"]:
             # Get the list of science members in this pair
             science_members = [
-                member
-                for member in which_set['members']
-                if member['exptype'].lower() == 'science'
+                member for member in which_set["members"] if member["exptype"].lower() == "science"
             ]
-            infile_1 = science_members[0]['expname']
-            infile_2 = science_members[1]['expname']
-            outfile = which_set['name']
+            infile_1 = science_members[0]["expname"]
+            infile_2 = science_members[1]["expname"]
+            outfile = which_set["name"]
 
             # Create the step instance
             wfs = wfs_combine.DataSet(
-                infile_1, infile_2, outfile, self.do_refine, self.flip_dithers, self.psf_size,
-                self.blur_size, self.n_size
+                infile_1,
+                infile_2,
+                outfile,
+                self.do_refine,
+                self.flip_dithers,
+                self.psf_size,
+                self.blur_size,
+                self.n_size,
             )
 
             # Do the processing
@@ -69,11 +83,13 @@ class WfsCombineStep(Step):
             wfs.input_2.close()
 
             # Update necessary meta info in the output
-            output_model.meta.cal_step.wfs_combine = 'COMPLETE'
-            output_model.meta.asn.pool_name = asn_table['asn_pool']
-            output_model.meta.asn.table_name = os.path.basename(input_table)
+            output_model.meta.cal_step.wfs_combine = "COMPLETE"
+            output_model.meta.asn.pool_name = asn_table["asn_pool"]
+            output_model.meta.asn.table_name = os.path.basename(input_table)  # noqa: PTH119
             # format the filename here
-            output_model.meta.filename = which_set['name'].format(suffix=self.suffix) + self.output_ext
+            output_model.meta.filename = (
+                which_set["name"].format(suffix=self.suffix) + self.output_ext
+            )
 
             output_container.append(output_model)
 
