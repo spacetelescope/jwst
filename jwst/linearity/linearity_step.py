f823a46b47aefda67ca52357b0deb963cb32b317
jemorrison <morrison@as.arizona.edu>
2025-04-07 15:08:53 -0700

    add code style changes

--- jwst/linearity/linearity_step.py
+++ jwst/linearity/linearity_step.py
@@ -7,32 +7,40 @@ __all__ = ["LinearityStep"]
 
 
 class LinearityStep(Step):
-    """
-    LinearityStep: This step performs a correction for non-linear
-    detector response, using the "classic" polynomial method.
-    """
+    """Performs a correction for non-linear detector response, using the polynomial method."""
 
     class_alias = "linearity"
 
     spec = """
-    """ # noqa: E501
+    """  # noqa: E501
 
-    reference_file_types = ['linearity']
+    reference_file_types = ["linearity"]
 
     def process(self, step_input):
+        """
+        Read in linearity correction and apply it to science data.
 
+        Parameters
+        ----------
+        step_input : RampModel
+            The input ramp model.
+
+        Returns
+        -------
+        result : RampModel
+            The output ramp model with linearity correction applied.
+        """
         # Open the input data model
         with datamodels.RampModel(step_input) as input_model:
-
             # Get the name of the linearity reference file to use
-            self.lin_name = self.get_reference_file(input_model, 'linearity')
-            self.log.info('Using Linearity reference file %s', self.lin_name)
+            self.lin_name = self.get_reference_file(input_model, "linearity")
+            self.log.info("Using Linearity reference file %s", self.lin_name)
 
             # Check for a valid reference file
-            if self.lin_name == 'N/A':
-                self.log.warning('No Linearity reference file found')
-                self.log.warning('Linearity step will be skipped')
-                input_model.meta.cal_step.linearity = 'SKIPPED'
+            if self.lin_name == "N/A":
+                self.log.warning("No Linearity reference file found")
+                self.log.warning("Linearity step will be skipped")
+                input_model.meta.cal_step.linearity = "SKIPPED"
                 return input_model
 
             # Open the linearity reference file data model
@@ -43,7 +51,7 @@ class LinearityStep(Step):
 
             # Do the linearity correction
             result = linearity.do_correction(result, lin_model)
-            result.meta.cal_step.linearity = 'COMPLETE'
+            result.meta.cal_step.linearity = "COMPLETE"
 
             # Cleanup
             del lin_model
