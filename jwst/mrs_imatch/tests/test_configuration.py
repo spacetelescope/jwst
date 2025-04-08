b25a00eb9d940a9819893f6f845d2039b192dd9a
Robert Jedrzejewski <rij@stsci.edu>
2025-04-08 11:28:23 -0400

    Only check mrs_imatch step is skipped by default, don't run it.

--- jwst/mrs_imatch/tests/test_configuration.py
+++ jwst/mrs_imatch/tests/test_configuration.py
@@ -76,14 +76,9 @@ def test_imatch_background_subtracted(tmp_cwd, miri_dither_ch12):
 def test_imatch_default_run(tmp_cwd, miri_dither_ch12):
     """ Test mrs_imatch test is skipped by default """
 
-    all_models = ModelContainer(miri_dither_ch12)
-
     # test if default running results in skipping step
     step = MRSIMatchStep()
-    results = step.call(all_models)
-    n = len(all_models)
-    for i  in range(n):
-        assert results[i].meta.cal_step.mrs_imatch =='SKIPPED'
+    assert step.skip == True
 
 
 def test_imatch_background_reset(tmp_cwd, miri_dither_ch12):
