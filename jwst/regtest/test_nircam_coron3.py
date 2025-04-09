f1b3910f5391f6e4a5aaddce3f986f11518ea4ce
Tyler Pauly <tapastro@gmail.com>
2025-04-09 12:06:57 -0400

    JP-3775: Test ci_watson resource_tracker implementation (#9357)

--- jwst/regtest/test_nircam_coron3.py
+++ jwst/regtest/test_nircam_coron3.py
@@ -4,9 +4,12 @@ from astropy.io.fits.diff import FITSDiff
 from jwst.stpipe import Step
 import warnings
 
+# Mark all tests in this module
+pytestmark = [pytest.mark.bigdata]
+
 
 @pytest.fixture(scope="module")
-def run_pipeline(rtdata_module):
+def run_pipeline(rtdata_module, resource_tracker):
     """Run calwebb_coron3 on coronographic data."""
     rtdata = rtdata_module
     rtdata.get_asn("nircam/coron/jw01386-c1020_20220909t073458_coron3_002a_asn.json")
@@ -16,12 +19,15 @@ def run_pipeline(rtdata_module):
     with warnings.catch_warnings():
         # warning is explicitly raised by the pipeline
         warnings.filterwarnings("ignore", category=RuntimeWarning, message="'var_rnoise' array not available")
-        Step.from_cmdline(args)
+        with resource_tracker.track():
+            Step.from_cmdline(args)
 
     return rtdata
 
 
-@pytest.mark.bigdata
+def test_log_tracked_resources(log_tracked_resources, run_pipeline):
+    log_tracked_resources()
+
 @pytest.mark.parametrize("suffix", ["psfalign", "psfsub", "crfints"])
 @pytest.mark.parametrize("obs", ["002", "003"])
 def test_nircam_coron3_sci_exp(run_pipeline, suffix, obs, fitsdiff_default_kwargs):
@@ -37,7 +43,6 @@ def test_nircam_coron3_sci_exp(run_pipeline, suffix, obs, fitsdiff_default_kwarg
     assert diff.identical, diff.report()
 
 
-@pytest.mark.bigdata
 @pytest.mark.parametrize("suffix", ["crfints"])
 @pytest.mark.parametrize("exposure", ["00001", "00002", "00003", "00004"])
 def test_nircam_coron3_psf_exp(run_pipeline, suffix, exposure, fitsdiff_default_kwargs):
@@ -53,7 +58,6 @@ def test_nircam_coron3_psf_exp(run_pipeline, suffix, exposure, fitsdiff_default_
     assert diff.identical, diff.report()
 
 
-@pytest.mark.bigdata
 @pytest.mark.parametrize("suffix", ["psfstack", "i2d"])
 def test_nircam_coron3_product(run_pipeline, suffix, fitsdiff_default_kwargs):
     """Check final products of calwebb_coron3"""
