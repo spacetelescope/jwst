ab8019a33ed19658ae6755b35b341bbc7917b1f3
jemorrison <morrison@as.arizona.edu>
2025-04-16 09:09:14 -0700

    update reg test

--- jwst/regtest/test_miri_mrs_straylight.py
+++ jwst/regtest/test_miri_mrs_straylight.py
@@ -9,22 +9,33 @@ INPUT_PATH = 'miri/mrs'
 TRUTH_PATH = 'truth/test_miri_mrs_straylight'
 
 
-@pytest.mark.bigdata
-@pytest.mark.parametrize(
-    'suffix',
-    ["straylightstep", "shower_model"],
-)
-def test_miri_mrs_straylight_clean_showers(rtdata, fitsdiff_default_kwargs,suffix):
-    """Test running straylight with clean shower= True on an rate file."""
+@pytest.fixture(scope="module")
+def run_straylight(rtdata_module):
+    """Run straylight step turning on clean shower and save_shower model."""
+    rtdata = rtdata_module
 
+    # Get the MIRI MRS rate file 
     filename = "jw01024001001_04101_00001_mirifulong_rate.fits"
     rtdata.get_data(INPUT_PATH + '/' + filename) 
 
     args = ['jwst.straylight.StraylightStep', rtdata.input,
             '--clean_showers=True',
             '--save_shower_model=True']
+
     Step.from_cmdline(args)
 
+    return rtdata
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize(
+    'suffix',
+    ["straylightstep", "shower_model"],
+)
+def test_miri_mrs_straylight_clean_showers(run_straylight, fitsdiff_default_kwargs,suffix):
+    """Test running straylight with clean shower= True on an rate file."""
+
+    rtdata = run_straylight
     output = "jw01024001001_04101_00001_mirifulong_" + suffix + ".fits"
     rtdata.output = output
     # Get the truth file
