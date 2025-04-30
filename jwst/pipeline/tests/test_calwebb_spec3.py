aa9a0e6587e1684e1d716fd287abee064ba68e42
Ned Molter <emolter@stsci.edu>
2025-04-30 14:44:23 -0400

    Add basic unit test for c1d save

--- jwst/pipeline/tests/test_calwebb_spec3.py
+++ jwst/pipeline/tests/test_calwebb_spec3.py
@@ -6,7 +6,7 @@ from jwst.pipeline.calwebb_spec3 import _save_wfss_x1d, _save_wfss_c1d
 
 
 def test_save_wfss_x1d(tmp_cwd):
-    """Test flat file format saving of WFSS data."""
+    """Test flat file format saving of WFSS x1d data."""
 
     # set up a list of MultiSlitModel objects that look like outputs from extract_1d
     n_sources = 5
@@ -51,4 +51,34 @@ def test_save_wfss_x1d(tmp_cwd):
     hdul.close()
 
 
-    #_save_wfss_c1d(results_list, "test_c1d.fits")
+def test_save_wfss_c1d(tmp_cwd):
+    """Test flat file format saving of WFSS c1d data."""
+
+    # Set up a list of MultiSlitModel objects that look like outputs from combine_1d
+    n_sources = 5
+    n_rows = 3
+
+    results_list = []
+    for i in range(n_sources):
+        multi = dm.MultiCombinedSpecModel()
+        # Only one spec per model in this case
+        spec = dm.CombinedSpecModel()
+        spectable_dtype = spec.schema["properties"]["spec_table"]["datatype"]
+        recarray_dtype = [(d["name"], d["datatype"]) for d in spectable_dtype]
+        spec.source_id = i
+        spec_table = np.recarray((n_rows,), dtype=recarray_dtype)
+        spec_table["WAVELENGTH"] = [1.0, 2.0, 3.0]
+        spec_table["FLUX"] = [1.0, 2.0, 3.0]
+        spec.spec_table = spec_table
+        multi.spec.append(spec)
+        results_list.append(multi)
+
+    # Save
+    _save_wfss_c1d(results_list, "test_c1d.fits")
+
+    # Re-open as FITS and check the data
+    hdul = fits.open("test_c1d.fits")
+    assert len(hdul) == 3  # one primary HDU, one BinTableHDU, one ASDF extension
+    bintable = hdul[1].data
+    assert bintable.shape == (n_sources,)
+    hdul.close()
