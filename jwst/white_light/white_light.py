7eb47484209bca470d1a741cbf6e220f64745f1e
Melanie Clarke <mclarke@stsci.edu>
2025-05-05 16:45:12 -0400

    Update white light for new spectral format

--- jwst/white_light/white_light.py
+++ jwst/white_light/white_light.py
@@ -16,8 +16,8 @@ def white_light(input_model, min_wave=None, max_wave=None):
 
     Parameters
     ----------
-    input_model : MultiSpecModel
-        Datamodel containing the multi-integration data
+    input_model : TSOMultiSpecModel
+        Datamodel containing the multi-integration data.
 
     min_wave : float, optional
         Default wavelength minimum for integration.
@@ -35,57 +35,60 @@ def white_light(input_model, min_wave=None, max_wave=None):
     if max_wave is None:
         max_wave = 1.0e10
 
-    # The input should contain one row per integration for each spectral
+    # The input should contain one spectrum for each spectral
     # order.  NIRISS SOSS data can contain up to three orders.
-    n_spec = len(input_model.spec)
+    # Each row in the table is an integration.
     sporders = []  # list of spectral orders available
-    order_list = np.empty((n_spec,))  # order for each spectrum, length nspectra
-    mid_times = np.empty((n_spec,))
-    mid_tdbs = np.empty((n_spec,))
-    fluxsums = np.empty((n_spec,))
+    order_list = []
+    mid_times = None
+    mid_tdbs = []
+    flux_sums = None
 
     # Loop over the spectra in the input model and find mid times and fluxes
-    problems = 0
-    for i, spec in enumerate(input_model.spec):
+    for spec in input_model.spec:
+        n_spec = len(spec.spec_table)
+
         # Figure out the spectral order for this spectrum
         spectral_order = getattr(spec, "spectral_order", None)
-        if spectral_order not in sporders:
-            sporders.append(spectral_order)
-        order_list[i] = spectral_order
+        sporders.append(spectral_order)
+        order_list.extend([spectral_order] * n_spec)
 
-        # Figure out mid times for this spectrum
-        mid_time = getattr(spec, "mid_time_mjd", None)
-        mid_tdb = getattr(spec, "mid_tdb", None)
-        if (mid_time is None) and (mid_tdb is None):
-            problems += 1
-            mid_times[i] = np.nan
-            mid_tdbs[i] = np.nan
-            continue
+        # Get mid times for all integrations in this order
+        mid_time = spec.spec_table["MID_TIME_MJD"]
+        mid_tdb = spec.spec_table["TDB-MID"]
+        good = ~np.isnan(mid_time)
+        if mid_times is None:
+            mid_times = mid_time
+            mid_tdbs = mid_tdb
         else:
-            mid_times[i] = mid_time
-            mid_tdbs[i] = mid_tdb
+            mid_times = np.hstack([mid_times, mid_time])
+            mid_tdbs = np.hstack([mid_tdbs, mid_tdb])
 
         # Create a wavelength mask, using cutoffs if specified, then
         # compute the flux sum for each integration in the input.
-        wave_mask = np.where(
-            (spec.spec_table["WAVELENGTH"] >= min_wave)
-            & (spec.spec_table["WAVELENGTH"] <= max_wave)
-        )[0]
-        fluxsums[i] = np.nansum(spec.spec_table["FLUX"][wave_mask])
+        wave_array = spec.spec_table["WAVELENGTH"]
+        wave_mask = (wave_array >= min_wave) & (wave_array <= max_wave) & good[:, None]
+        masked_flux = spec.spec_table["FLUX"].copy()
+        masked_flux[~wave_mask] = np.nan
+        flux_sum = np.nansum(masked_flux, axis=1)
+        if flux_sums is None:
+            flux_sums = flux_sum
+        else:
+            flux_sums = np.hstack([flux_sums, flux_sum])
 
-    if problems > 0:
-        log.warning(
-            f"There were {problems} spectra with no mid time (%d percent of spectra). "
-            "These spectra will be ignored in the output table." % (100.0 * problems / n_spec)
-        )
+        problems = np.sum(~good)
+        if problems > 0:
+            log.warning(
+                f"There were {problems} spectra in order {spectral_order} "
+                f"with no mid time ({100.0 * problems / n_spec} percent of spectra). "
+                "These spectra will be ignored in the output table."
+            )
 
     # Set up output table, removing problems
     tbl = _make_empty_output_table(input_model)
     good = ~np.isnan(mid_times)
     mid_times = mid_times[good]
     mid_tdbs = mid_tdbs[good]
-    fluxsums = fluxsums[good]
-    order_list = order_list[good]
     unique_mid_times, unq_indices = np.unique(mid_times, return_index=True)
     bjd_times = mid_tdbs[unq_indices]
     tbl["int_mid_MJD_UTC"] = unique_mid_times
@@ -100,7 +103,7 @@ def white_light(input_model, min_wave=None, max_wave=None):
 
         # NaN-pad fluxes for times not represented in this order
         fluxes = np.full(len(unique_mid_times), np.nan)
-        fluxes[time_is_in_this_order] = fluxsums[is_this_order]
+        fluxes[time_is_in_this_order] = flux_sums[is_this_order]
         if len(sporders) > 1:
             colname = f"whitelight_flux_order_{order}"
         else:
