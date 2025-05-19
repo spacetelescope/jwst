9a082dd2dbc836c1bfd2b737cea8d32f567b9250
jemorrison <morrison@as.arizona.edu>
2025-05-19 08:07:55 -0700

    first set of  changes

--- jwst/assign_wcs/assign_wcs.py
+++ jwst/assign_wcs/assign_wcs.py
@@ -56,7 +56,7 @@ def load_wcs(input_model, reference_files=None, nrs_slit_y_range=None, nrs_ifu_s
     instrument = input_model.meta.instrument.name.lower()
     mod = importlib.import_module("." + instrument, "jwst.assign_wcs")
 
-    print('SPEC_TYPES', SPEC_TYPES, type(SPEC_TYPES))
+    print("SPEC_TYPES", SPEC_TYPES, type(SPEC_TYPES))
     if (
         input_model.meta.exposure.type.lower() in SPEC_TYPES
         or input_model.meta.instrument.lamp_mode.lower() in NRS_LAMP_MODE_SPEC_TYPES
