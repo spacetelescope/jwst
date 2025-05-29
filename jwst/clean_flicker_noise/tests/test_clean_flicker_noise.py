264681c5e74b8e1e9374d5453d975c6269763b8c
Melanie Clarke <mclarke@stsci.edu>
2025-05-29 14:59:44 -0400

    Catch a few more warnings

--- jwst/clean_flicker_noise/tests/test_clean_flicker_noise.py
+++ jwst/clean_flicker_noise/tests/test_clean_flicker_noise.py
@@ -267,12 +267,8 @@ def test_clip_to_background_fit_fails(log_watcher):
     image = np.full(shape, np.nan)
     mask = np.full(shape, True)
     watcher.message = "Histogram failed"
-    with warnings.catch_warnings():
-        # These warnings only appear for certain deps combo.
-        # RuntimeWarning: Mean of empty slice
-        # RuntimeWarning: Degrees of freedom <= 0 for slice
-        warnings.filterwarnings("ignore", category=RuntimeWarning, message=".* slice")
-        cfn.clip_to_background(image, mask, fit_histogram=True, verbose=True)
+
+    cfn.clip_to_background(image, mask, fit_histogram=True, verbose=True)
     assert np.all(mask)
     watcher.assert_seen()
 
