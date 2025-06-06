1051883a26c960e251ca1640c3cdcd584745f3c1
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-06-06 11:41:04 -0400

    TST: Inflight pool for standards for pool_002_image_miri
    TST: Inflight pool for standards for pool_004_wfs
    
    TST: Inflight pool for standards for pool_005_spec_niriss split into 3 pools
    
    TST: Inflight pool for standards for pool_007_spec_miri split into 3 pools.
    
    [ci skip]

--- jwst/regtest/test_associations_standards.py
+++ jwst/regtest/test_associations_standards.py
@@ -59,11 +59,6 @@ class MakePars:
 
 
 standards = [
-    MakePars("pool_002_image_miri", main_args=LV3_ONLY_ARGS),
-    MakePars("pool_004_wfs", slow=True),
-    MakePars("pool_005_spec_niriss"),
-    MakePars("pool_006_spec_nirspec"),
-    MakePars("pool_007_spec_miri"),
     MakePars("pool_009_spec_miri_lv2bkg"),
     MakePars("pool_010_spec_nirspec_lv2bkg"),
     MakePars("pool_011_spec_miri_lv2bkg_lrs"),
