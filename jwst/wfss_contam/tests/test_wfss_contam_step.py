1f2bfc609582e9eae87aa552254b5f0636e5a7db
Ned Molter <emolter@stsci.edu>
2025-06-13 16:26:22 -0400

    update unit test to also save intermediates

--- jwst/wfss_contam/tests/test_wfss_contam_step.py
+++ jwst/wfss_contam/tests/test_wfss_contam_step.py
@@ -62,6 +62,6 @@ def test_wfss_contam_step(multislitmodel, tmp_cwd_module):
     between a WCS taken from a random real image and the mock data.
     This could be fixed in the future by mocking the WCS object.
     """
-    result = WfssContamStep.call(multislitmodel)
+    result = WfssContamStep.call(multislitmodel, save_simulated_image=True, save_contam_images=True)
     assert isinstance(result, dm.MultiSlitModel)
     assert result.meta.cal_step.wfss_contam == "COMPLETE"
