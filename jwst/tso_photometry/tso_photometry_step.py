4c25e4ad0472d7756967ffdbc8c51f1d980f693c
Melanie Clarke <mclarke@stsci.edu>
2025-06-18 16:53:32 -0400

    Move TSO photometry radii to parameters; add test coverage

--- jwst/tso_photometry/tso_photometry_step.py
+++ jwst/tso_photometry/tso_photometry_step.py
@@ -1,6 +1,6 @@
 import logging
 
-from stdatamodels.jwst.datamodels import CubeModel, GainModel, TsoPhotModel
+from stdatamodels.jwst.datamodels import CubeModel, GainModel
 
 from jwst.lib import reffile_utils
 from jwst.lib.catalog_utils import replace_suffix_ext
@@ -18,6 +18,9 @@ class TSOPhotometryStep(Step):
     class_alias = "tso_photometry"
 
     spec = """
+        radius = float(default=3.0) # Aperture radius in pixels
+        radius_inner = float(default=4.0) # Background annulus inner radius in pixels
+        radius_outer = float(default=5.0) # Background annulus outer radius in pixels
         save_catalog = boolean(default=False)  # save exposure-level catalog
     """  # noqa: E501
 
@@ -53,14 +56,6 @@ class TSOPhotometryStep(Step):
             xcenter = model.meta.wcsinfo.siaf_xref_sci - 1  # 1-based origin
             ycenter = model.meta.wcsinfo.siaf_yref_sci - 1  # 1-based origin
 
-            # Get the tsophot reference file
-            tsophot_filename = self.get_reference_file(model, "tsophot")
-            log.debug(f"Reference file name = {tsophot_filename}")
-            if tsophot_filename == "N/A":
-                log.warning("No TSOPHOT reference file found;")
-                log.warning("the tso_photometry step will be skipped.")
-                return None
-
             # Get the gain reference file
             gain_filename = self.get_reference_file(model, "gain")
             gain_m = GainModel(gain_filename)
@@ -71,22 +66,15 @@ class TSOPhotometryStep(Step):
                 log.info("Extracting gain subarray to match science data")
                 gain_2d = reffile_utils.get_subarray_model(model, gain_m).data
 
-            # Retrieve aperture info from the reference file
-            pupil_name = "ANY"
-            if model.meta.instrument.pupil is not None:
-                pupil_name = model.meta.instrument.pupil
-
-            (radius, radius_inner, radius_outer) = get_ref_data(tsophot_filename, pupil=pupil_name)
-
-            log.debug(f"radius = {radius}")
-            log.debug(f"radius_inner = {radius_inner}")
-            log.debug(f"radius_outer = {radius_outer}")
+            log.debug(f"radius = {self.radius}")
+            log.debug(f"radius_inner = {self.radius_inner}")
+            log.debug(f"radius_outer = {self.radius_outer}")
             log.debug(f"xcenter = {xcenter}")
             log.debug(f"ycenter = {ycenter}")
 
             # Compute the aperture photometry
             catalog = tso_aperture_photometry(
-                model, xcenter, ycenter, radius, radius_inner, radius_outer, gain_2d
+                model, xcenter, ycenter, self.radius, self.radius_inner, self.radius_outer, gain_2d
             )
 
             # Save the photometry in an output catalog
@@ -104,29 +92,3 @@ class TSOPhotometryStep(Step):
                 log.info(f"Wrote TSO photometry catalog: {cat_filepath}")
 
         return catalog
-
-
-def get_ref_data(reffile, pupil="ANY"):
-    ref_model = TsoPhotModel(reffile)
-    radii = ref_model.radii
-    value = None
-    val_any_pupil = None
-    for item in radii:
-        if item.pupil == pupil.upper():
-            value = (item.radius, item.radius_inner, item.radius_outer)
-            break
-        elif item.pupil == "ANY" and val_any_pupil is None:
-            # Save this value as a fallback, in case we don't find a match
-            # to an actual pupil name.
-            val_any_pupil = (item.radius, item.radius_inner, item.radius_outer)
-
-    if value is not None:
-        (radius, radius_inner, radius_outer) = value
-    elif val_any_pupil is not None:
-        (radius, radius_inner, radius_outer) = val_any_pupil
-    else:
-        (radius, radius_inner, radius_outer) = (0.0, 0.0, 0.0)
-
-    ref_model.close()
-
-    return radius, radius_inner, radius_outer
