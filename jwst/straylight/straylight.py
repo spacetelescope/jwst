ea67790460fe24eff10b33c1fa2445fff225fdee
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-07-17 14:03:09 -0400

    Add __all__ to spectral_leak, srctype, straylight, superbias, tso_photometry, wavecorr, wfs_combine, and white_light

--- jwst/straylight/straylight.py
+++ jwst/straylight/straylight.py
@@ -12,6 +12,8 @@ from jwst.straylight.calc_xart import xart_wrapper  # c extension
 
 log = logging.getLogger(__name__)
 
+__all__ = ["makemodel_ccode", "makemodel_composite", "correct_xartifact", "clean_showers"]
+
 
 def makemodel_ccode(fimg, xvec, imin, imax, lor_fwhm, lor_amp, g_fwhm, g_dx, g1_amp, g2_amp):
     """
