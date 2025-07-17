a68c250b9765398b09cd38da2db45c11b8895c4e
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-07-16 21:37:45 -0400

    In-flight pool for pool_013_coron_nircam maybe [ci skip]

--- jwst/regtest/test_associations_standards.py
+++ jwst/regtest/test_associations_standards.py
@@ -59,7 +59,6 @@ class MakePars:
 
 
 standards = [
-    MakePars("pool_013_coron_nircam", slow=True),
     MakePars("pool_014_ami_niriss"),
     MakePars("pool_015_spec_nirspec_lv2bkg_reversed", main_args=LV2_ONLY_ARGS),
     MakePars("pool_016_spec_nirspec_lv2bkg_double", main_args=LV2_ONLY_ARGS),
