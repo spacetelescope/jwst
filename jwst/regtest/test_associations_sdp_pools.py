6b791336d01f33e13f6572c0a8f7c70c1b6ff7d5
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-07-22 14:43:10 -0400

    TST: Inflight pool for pool_017_spec_nirspec_lv2imprint maybe [ci skip]

--- jwst/regtest/test_associations_sdp_pools.py
+++ jwst/regtest/test_associations_sdp_pools.py
@@ -75,6 +75,7 @@ def _assoc_sdp_against_standard(rtdata, resource_tracker, request, pool_args):
             ("jw01863_20250316t002754_pool", []), id="pool_010_spec_nirspec_lv2bkg_IFU_BG_DEDICATED"
         ),
         pytest.param(("jw04611_20250308t142406_pool", []), id="pool_014_ami_niriss"),
+        pytest.param(("jw01970_20250317t180031_pool", []), id="pool_017_spec_nirspec_lv2imprint"),
     ],
     ids=parfunc,
 )
