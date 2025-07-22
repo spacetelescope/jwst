e0841e2f2082a3bf02e3907132595b7d8488ad47
jemorrison <morrison@as.arizona.edu>
2025-07-22 16:28:22 -0700

    update miri.py in assign_wcs

--- jwst/assign_wcs/assign_wcs_step.py
+++ jwst/assign_wcs/assign_wcs_step.py
@@ -109,6 +109,7 @@ class AssignWcsStep(Step):
                 reffile = self.get_reference_file(input_model, reftype)
                 reference_file_names[reftype] = reffile if reffile else ""
             log.debug(f"reference files used in assign_wcs: {reference_file_names}")
+            print("*****************************************")
             print(f"reference files used in assign_wcs: {reference_file_names}")
 
             # Get the MSA metadata file if needed and add to reffiles
@@ -122,6 +123,8 @@ class AssignWcsStep(Step):
                     log.error(message)
                     raise MSAFileError(message)
             slit_y_range = [self.slit_y_low, self.slit_y_high]
+
+            print("in assign_wcs_step", self.slit_y_low, self.slit_y_high)
             result = load_wcs(
                 input_model,
                 reference_file_names,
