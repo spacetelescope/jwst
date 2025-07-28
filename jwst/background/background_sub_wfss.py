2af17fb54cceeb523f49b3fb7ab3aa1412959cd6
Maria Pena-Guerrero <pena@stsci.edu>
2025-07-28 10:14:10 -0400

    generalizing background extension for future use

--- jwst/background/background_sub_wfss.py
+++ jwst/background/background_sub_wfss.py
@@ -110,7 +110,7 @@ def subtract_wfss_bkg(
     result.data = input_model.data - subtract_this
     result.dq = np.bitwise_or(input_model.dq, bkg_ref.dq)
     result.meta.background.scaling_factor = factor
-    result.bkgdmask = bkg_mask
+    result.background = bkg_mask
 
     log.info(f"Average of scaled background image = {np.nanmean(subtract_this):.3e}")
     log.info(f"Scaling factor = {factor:.5e}")
