45919d6457590a2a36b61bc295d018ab211b7889
Melanie Clarke <mclarke@stsci.edu>
2025-07-30 17:11:20 -0400

    Reorganize ami test data to avoid importing conftest

---
+++ jwst/ami/tests/helpers.py
@@ -0,0 +1,7 @@
+"""Useful constants and functions for AMI tests."""
+
+import numpy as np
+
+PXSC_DEG = 65.6 / (60.0 * 60.0 * 1000)
+PXSC_RAD = PXSC_DEG * np.pi / (180)
+PXSC_MAS = PXSC_DEG * 3600 * 1000
