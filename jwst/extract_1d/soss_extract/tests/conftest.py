e242def12974dffc16905aa448e59316d1f15db0
Melanie Clarke <mclarke@stsci.edu>
2025-07-30 16:58:28 -0400

    Reorganize extract_1d test data

--- jwst/extract_1d/soss_extract/tests/conftest.py
+++ jwst/extract_1d/soss_extract/tests/conftest.py
@@ -25,16 +25,15 @@ from stdatamodels.jwst.datamodels import PastasossModel
 
 from jwst.extract_1d.soss_extract import atoca
 from jwst.extract_1d.soss_extract import atoca_utils as au
-
-PWCPOS = 245.85932900002442
-DATA_SHAPE = (25, 200)
-WAVE_BNDS_O1 = [2.8, 0.8]
-WAVE_BNDS_O2 = [1.4, 0.5]
-WAVE_BNDS_GRID = [0.7, 2.7]
-ORDER1_SCALING = 20.0
-ORDER2_SCALING = 2.0
-TRACE_END_IDX = [DATA_SHAPE[1], 180]
-SPECTRAL_SLOPE = 2
+from jwst.extract_1d.soss_extract.tests.helpers import (
+    DATA_SHAPE,
+    PWCPOS,
+    TRACE_END_IDX,
+    WAVE_BNDS_GRID,
+    WAVE_BNDS_O1,
+    WAVE_BNDS_O2,
+    f_lam,
+)
 
 
 @pytest.fixture(scope="package")
@@ -311,27 +310,6 @@ def engine(
         mask_trace_profile,
         global_mask=detector_mask,
     )
-
-
-def f_lam(wl, m=SPECTRAL_SLOPE, b=0):
-    """
-    Return a linear model of flux as function of wavelength.
-
-    Parameters
-    ----------
-    wl : array[float]
-        Wavelength
-    m : float
-        Slope of linear model
-    b : float
-        Intercept of linear model
-
-    Returns
-    -------
-    array[float]
-        Flux
-    """
-    return m * wl + b
 
 
 @pytest.fixture(scope="package")
