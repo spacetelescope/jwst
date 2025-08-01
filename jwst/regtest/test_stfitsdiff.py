28fc29265ad8edb7dde72b4e14735a0a3895abd7
Maria Pena-Guerrero <pena@stsci.edu>
2025-08-01 15:01:37 -0400

    added line to add report line to march astropy

--- jwst/regtest/test_stfitsdiff.py
+++ jwst/regtest/test_stfitsdiff.py
@@ -132,7 +132,7 @@ def report_to_list(report, from_line=11, report_pixel_loc_diffs=False):
             elif "Values in" in line or "Reporting percentages" in line:
                 stidx = True
                 pixidx = False
-            elif "differs" in line or "differ:" in line:
+            elif "differs" in line or "differ:" in line or "Extra column" in line:
                 stidx = False
                 pixidx = True
                 if diffsline:
