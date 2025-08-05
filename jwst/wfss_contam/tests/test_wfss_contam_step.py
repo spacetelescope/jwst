e4084bbe0be11bce3e440f05a18c468388942d58
Melanie Clarke <mclarke@stsci.edu>
2025-08-05 15:28:37 -0400

    Fix shallow copy for wfss_contam; add smoke test

--- jwst/wfss_contam/tests/test_wfss_contam_step.py
+++ jwst/wfss_contam/tests/test_wfss_contam_step.py
@@ -5,7 +5,7 @@ import stdatamodels.jwst.datamodels as dm
 from jwst.wfss_contam.wfss_contam_step import WfssContamStep
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def multislitmodel(
     tmp_cwd_module, direct_image_with_gradient, segmentation_map, source_catalog, grism_wcs
 ):
@@ -66,3 +66,32 @@ def test_wfss_contam_step(multislitmodel, tmp_cwd_module):
     result = WfssContamStep.call(multislitmodel, save_simulated_image=True, save_contam_images=True)
     assert isinstance(result, dm.MultiSlitModel)
     assert result.meta.cal_step.wfss_contam == "COMPLETE"
+    assert (tmp_cwd_module / "multislit_model_simul.fits").exists()
+    assert (tmp_cwd_module / "multislit_model_simul_slits.fits").exists()
+    assert (tmp_cwd_module / "multislit_model_contam.fits").exists()
+
+
+def test_output_is_not_input(multislitmodel, tmp_cwd_module):
+    """Check that input is not modified by the step."""
+    datamodel = dm.open(multislitmodel)
+    input_copy = datamodel.copy()
+
+    result = WfssContamStep.call(datamodel)
+    assert isinstance(result, dm.MultiSlitModel)
+    assert result.meta.cal_step.wfss_contam == "COMPLETE"
+
+    # input is not modified
+    assert result is not datamodel
+    # TODO: add wfss_contam to core schema for cal_step
+    # assert datamodel.meta.cal_step.wfss_contam is None
+    any_modified = False
+    for i in range(len(datamodel.slits)):
+        # Input data is not modified
+        np.testing.assert_allclose(datamodel.slits[i].data, input_copy.slits[i].data)
+
+        # Output data may have been modified
+        if not np.allclose(result.slits[i].data, datamodel.slits[i].data):
+            any_modified = True
+
+    # There was at least one slit updated in the output and not modified in the input
+    assert any_modified
