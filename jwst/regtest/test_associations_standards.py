20767a6523157944cf6be999038d0f021ce4852f
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-08-13 16:46:06 -0400

    TST: Inflight pool for pool_028_mir_lrsfs_nods [ci skip]

--- jwst/regtest/test_associations_standards.py
+++ jwst/regtest/test_associations_standards.py
@@ -59,7 +59,6 @@ class MakePars:
 
 
 standards = [
-    MakePars("pool_028_mir_lrsfs_nods"),
     MakePars("pool_029_mir_lrsfs_nonod"),
     MakePars("pool_030_mir_lrs_nods_bkg"),
     MakePars("pool_031_mir_lrs_nonod_bkg"),
