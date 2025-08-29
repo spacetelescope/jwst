57697bd9af072417fb134b74af51e163c41b5ea6
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2025-08-29 14:23:43 -0400

    DOC: Fix more broken refs
    Co-authored-by: Melanie Clarke <mclarke@stsci.edu>

--- jwst/datamodels/source_container.py
+++ jwst/datamodels/source_container.py
@@ -100,7 +100,7 @@ class SourceModelContainer(ModelContainer):
 
         Returns
         -------
-        `~jwst.datamodels.SourceModelContainer`
+        `~jwst.datamodels.source_container.SourceModelContainer`
             A deep copy of the container and all the models in it.
         """
         multi_exposure = self.multiexposure.copy(memo=memo)
--- jwst/resample/resample_utils.py
+++ jwst/resample/resample_utils.py
@@ -36,7 +36,7 @@ def resampled_wcs_from_models(
 
     Parameters
     ----------
-    input_models : `~jwst.datamodels.ModelLibrary`
+    input_models : `~jwst.datamodels.library.ModelLibrary`
         Each datamodel must have a ``model.meta.wcs`` set to a `~gwcs.wcs.WCS` object.
     pixel_scale_ratio : float, optional
         Desired pixel scale ratio defined as the ratio of the desired output
