f76fe124a5f3f62016015750f08dfbb83c02df21
Melanie Clarke <mclarke@stsci.edu>
2025-09-16 13:13:16 -0400

    JP-3930: Step function for opening models (#9723)

--- jwst/assign_mtwcs/tests/test_mtwcs.py
+++ jwst/assign_mtwcs/tests/test_mtwcs.py
@@ -3,7 +3,7 @@ from astropy.utils.data import get_pkg_data_filename
 from stdatamodels.jwst import datamodels
 
 from jwst.assign_mtwcs import AssignMTWcsStep
-from jwst.datamodels import ModelLibrary
+from jwst.datamodels import ModelContainer, ModelLibrary
 
 
 @pytest.mark.parametrize("errtype", ["ra", "dec", "both", "none"])
@@ -70,3 +70,47 @@ def test_mt_slitmodel(errtype):
         zero = result.borrow(0)
         assert zero.meta.wcs.output_frame.name == expected_frame
         result.shelve(zero, 0, modify=False)
+
+
+@pytest.mark.parametrize("success", [True, False])
+def test_output_is_not_input(monkeypatch, success):
+    """
+    Test that input is not modified by the step.
+
+    This is specific to the use case of calling the step on non-library
+    model input.  When the input is already a ModelLibrary, it's assumed
+    that performance is the most important thing and extra copies are
+    not desired.
+    """
+    # Mock a failure in the ModelLibrary init, to exercise the "skipped" condition
+    if not success:
+
+        def raise_error(*args, **kwargs):
+            raise ValueError("test")
+
+        monkeypatch.setattr(ModelLibrary, "__init__", raise_error)
+
+    file_path = get_pkg_data_filename("data/test_mt_asn.json", package="jwst.assign_mtwcs.tests")
+    with datamodels.open(file_path) as container:
+        result = AssignMTWcsStep.call(container)
+        if success:
+            assert isinstance(result, ModelLibrary)
+        else:
+            assert isinstance(result, ModelContainer)
+        with result:
+            for im, input_im in zip(result, container):
+                if success:
+                    assert im.meta.cal_step.assign_mtwcs == "COMPLETE"
+                else:
+                    assert im.meta.cal_step.assign_mtwcs == "SKIPPED"
+                assert im is not input_im
+                assert input_im.meta.cal_step.assign_mtwcs is None
+
+                if success:
+                    result.shelve(im, modify=False)
+
+
+def test_input_not_supported(caplog):
+    input_data = datamodels.ImageModel()
+    AssignMTWcsStep.call(input_data)
+    assert "Input data type is not supported" in caplog.text
