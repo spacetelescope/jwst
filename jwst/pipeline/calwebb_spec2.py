b218cbd69f6d86c526a829cb66bcb3179031f41d
Ned Molter <emolter@users.noreply.github.com>
2025-09-19 08:17:18 -0700

    Make DeprecationWarnings show up in log (#9860)

--- jwst/pipeline/calwebb_spec2.py
+++ jwst/pipeline/calwebb_spec2.py
@@ -117,13 +117,13 @@ class Spec2Pipeline(Pipeline):
         log.info("Starting calwebb_spec2 ...")
 
         if self.save_bsub:
-            warnings.warn(
+            deprecation_message = (
                 "The --save_bsub parameter is deprecated and will be removed in a future release. "
                 "To toggle saving background-subtracted data, use the background step's "
-                "--save_results parameter instead.",
-                DeprecationWarning,
-                stacklevel=2,
+                "--save_results parameter instead."
             )
+            warnings.warn(deprecation_message, DeprecationWarning, stacklevel=2)
+            log.warning(deprecation_message)
 
         # Setup step parameters required by the pipeline.
         self.resample_spec.save_results = self.save_results
