56670c22f005df9e3b79d15082099b37f5d2cc27
Melanie Clarke <mclarke@stsci.edu>
2025-09-30 11:53:27 -0400

    Prepare output for dq_init and clean up unnecessary copies and functions

--- jwst/dq_init/dq_initialization.py
+++ jwst/dq_init/dq_initialization.py
@@ -12,28 +12,7 @@ log = logging.getLogger(__name__)
 # FGS guide star mode exposure types
 guider_list = ["FGS_ID-IMAGE", "FGS_ID-STACK", "FGS_ACQ1", "FGS_ACQ2", "FGS_TRACK", "FGS_FINEGUIDE"]
 
-__all__ = ["correct_model", "do_dqinit", "check_dimensions"]
-
-
-def correct_model(input_model, mask_model):
-    """
-    Perform the dq_init step on a JWST datamodel.
-
-    Parameters
-    ----------
-    input_model : input JWST datamodel
-        The jwst datamodel to be corrected
-    mask_model : mask datamodel
-        The mask model to use in the correction
-
-    Returns
-    -------
-    output_model : JWST datamodel
-        The corrected JWST datamodel
-    """
-    output_model = do_dqinit(input_model, mask_model)
-
-    return output_model
+__all__ = ["do_dqinit", "check_dimensions"]
 
 
 def do_dqinit(output_model, mask_model):
@@ -42,15 +21,17 @@ def do_dqinit(output_model, mask_model):
 
     Parameters
     ----------
-    output_model : input JWST datamodel
-        The jwst datamodel to be corrected
-    mask_model : mask datamodel
-        The mask model to use in the correction
+    output_model : `~stdatamodels.jwst.datamodels.RampModel` \
+                   or `~stdatamodels.jwst.datamodels.GuiderRawModel`
+        The JWST datamodel to be corrected.
+    mask_model : `~stdatamodels.jwst.datamodels.MaskModel`
+        The mask model to use in the correction.
 
     Returns
     -------
-    output_model : JWST datamodel
-        The corrected JWST datamodel
+    output_model : `~stdatamodels.jwst.datamodels.RampModel` \
+                   or `~stdatamodels.jwst.datamodels.GuiderRawModel`
+        The corrected JWST datamodel, updated in place.
     """
     # Inflate empty DQ array, if necessary
     check_dimensions(output_model)
@@ -61,21 +42,18 @@ def do_dqinit(output_model, mask_model):
     else:
         log.info("Extracting mask subarray to match science data")
         mask_sub_model = reffile_utils.get_subarray_model(output_model, mask_model)
-        mask_array = mask_sub_model.dq.copy()
-        mask_sub_model.close()
+        mask_array = mask_sub_model.dq
+        del mask_sub_model
 
     # Set model-specific data quality in output
     if output_model.meta.exposure.type in guider_list:
-        dq = np.bitwise_or(output_model.dq, mask_array)
-        output_model.dq = dq
+        output_model.dq |= mask_array
     else:
-        dq = np.bitwise_or(output_model.pixeldq, mask_array)
-        output_model.pixeldq = dq
+        output_model.pixeldq |= mask_array
+
         # Additionally, propagate mask DO_NOT_USE flags to groupdq to
         # ensure no ramps are fit to bad pixels.
-        output_model.groupdq = np.bitwise_or(
-            output_model.groupdq, mask_array & dqflags.group["DO_NOT_USE"]
-        )
+        output_model.groupdq |= mask_array & dqflags.group["DO_NOT_USE"]
 
     output_model.meta.cal_step.dq_init = "COMPLETE"
 
@@ -94,7 +72,8 @@ def check_dimensions(input_model):
 
     Parameters
     ----------
-    input_model : JWST datamodel
+    input_model : `~stdatamodels.jwst.datamodels.RampModel` \
+                  or `~stdatamodels.jwst.datamodels.GuiderRawModel`
         Input datamodel.
     """
     input_shape = input_model.data.shape
@@ -105,7 +84,7 @@ def check_dimensions(input_model):
             # a shape of (0,0).
             # If that's the case, create the array
             if input_model.dq.shape == (0, 0):
-                input_model.dq = np.zeros(input_shape[-2:]).astype("uint32")
+                input_model.dq = np.zeros(input_shape[-2:], dtype=np.uint32)
             else:
                 log.error(f"DQ array has the wrong shape: {input_model.dq.shape}")
 
@@ -115,13 +94,13 @@ def check_dimensions(input_model):
             # a shape of (0,0).
             # If that's the case, create the array
             if input_model.pixeldq.shape == (0, 0):
-                input_model.pixeldq = np.zeros(input_shape[-2:]).astype("uint32")
+                input_model.pixeldq = np.zeros(input_shape[-2:], dtype=np.uint32)
             else:
                 log.error(f"Pixeldq array has the wrong shape: {input_model.pixeldq.shape}")
 
         # Perform the same check for the input model groupdq array
         if input_model.groupdq.shape != input_shape:
             if input_model.groupdq.shape == (0, 0, 0, 0):
-                input_model.groupdq = np.zeros(input_shape).astype("uint8")
+                input_model.groupdq = np.zeros(input_shape, dtype=np.uint8)
             else:
                 log.error(f"Groupdq array has wrong shape: {input_model.groupdq.shape}")
