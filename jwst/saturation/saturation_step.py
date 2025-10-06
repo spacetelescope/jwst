cd6c3829dcf1bef300d239ad66a2f908f9ffeff0
Melanie Clarke <mclarke@stsci.edu>
2025-10-06 10:11:52 -0400

    Make sure input is opened as a RampModel

--- jwst/saturation/saturation_step.py
+++ jwst/saturation/saturation_step.py
@@ -38,7 +38,7 @@ class SaturationStep(Step):
             Output datamodel with saturation flags set for saturated pixels.
         """
         # Open the input data model
-        result = self.prepare_output(step_input)
+        result = self.prepare_output(step_input, open_as_type=datamodels.RampModel)
 
         # Get the name of the saturation reference file
         ref_name = self.get_reference_file(result, "saturation")
