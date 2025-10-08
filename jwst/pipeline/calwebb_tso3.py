8c0052111ab5f6e8394af9c0e376225d67dbb118
Ned Molter <emolter@stsci.edu>
2025-10-08 16:57:31 -0400

    Intermediate save, wrote code but wcs is wrong

--- jwst/pipeline/calwebb_tso3.py
+++ jwst/pipeline/calwebb_tso3.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 import numpy as np
 from astropy.io.fits import FITS_rec
+from astropy.modeling.models import Mapping
 from astropy.table import vstack
+from stcal.alignment import combine_sregions
 from stdatamodels.jwst import datamodels
 
 from jwst.extract_1d import extract_1d_step
@@ -166,6 +168,10 @@ class Tso3Pipeline(Pipeline):
             x1d_result.meta.asn.pool_name = input_models.asn_table["asn_pool"]
             x1d_result.meta.asn.table_name = Path(input_data).name
 
+            # Combine S_REGION values from all members to allow MAST to do spatial queries
+            # on this type of product
+            self._populate_tso_sregion(x1d_result, input_models)
+
             # Save the final x1d Multispec model
             x1d_result.meta.cal_step.pixel_replace = state
             if len(x1d_result.spec) == 0:
@@ -192,3 +198,60 @@ class Tso3Pipeline(Pipeline):
         # All done. Nothing to return, because all products have
         # been created here.
         return
+
+    def _populate_tso_sregion(self, model, cal_model_list):
+        """
+        Generate cumulative S_REGION footprint from input images.
+
+        Take the input S_REGION values from all input models, and combine
+        them using a polygon union to create a cumulative footprint for the output
+        TSO spectral product.
+        The union is performed in pixel coordinates to avoid distortion,
+        so the WCS of the first input model is used to convert to and from sky coordinates.
+
+        Parameters
+        ----------
+        model : `~stdatamodels.jwst.datamodels.TSOMultiSpecModel`
+            The newly generated TSOMultiSpecModel made as part of
+            the save operation for spec3 processing of TSO data.
+
+        cal_model_list : `~jwst.datamodels.ModelContainer`
+            The input models provided to Tso3Pipeline by the
+            input association.
+        """
+        # WCS of any slit should be ok - internally this does a round-trip, so any offsets
+        # introduced for a specific slit won't matter
+
+        # currently this gives all NaNs.
+        # problem is likely the offset for SlitModels causing NaNs in detector position
+        # can this be avoided by e.g. allowing bbox to be anything?
+        wcs = cal_model_list[0].meta.wcs
+        try:
+            input_sregions = [w.meta.wcsinfo.s_region for w in cal_model_list]
+        except AttributeError:
+            log.warning(
+                "One or more input model(s) are missing an `s_region` attribute; "
+                "output S_REGION will not be set."
+            )
+            return
+
+        # Modify the det2world transform to ignore extra inputs/outputs (wavelength and order)
+        if "moving_target" in wcs.available_frames:
+            # This should never be hit for WFSS data but is here just in case
+            det2world = wcs.get_transform("detector", "moving_target")
+        else:
+            det2world = wcs.get_transform("detector", "world")
+
+        mapping1 = Mapping((0, 1, 0, 1))  # last two are placeholders and don't do anything
+        mapping1.inverse = Mapping((0, 1), n_inputs=4)
+        mapping2 = Mapping((0, 1), n_inputs=3)
+        mapping2.inverse = Mapping((0, 1, 0))
+        det2world = det2world | mapping2
+
+        try:
+            sregion = combine_sregions(input_sregions, det2world)
+        except ValueError as e:
+            log.warning(f"Could not combine S_REGIONs: {e}. Output S_REGION will not be set.")
+            return
+        log.info(f"Setting S_REGION for combined footprint to: {sregion}")
+        model.spec[0].s_region = sregion
