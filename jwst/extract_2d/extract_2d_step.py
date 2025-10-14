e87cd6fd5c72ac41d0e58bf57ced2b1d50c11e55
jemorrison <morrison@as.arizona.edu>
2025-10-14 14:47:34 -0700

    remove blank line

--- jwst/extract_2d/extract_2d_step.py
+++ jwst/extract_2d/extract_2d_step.py
@@ -39,7 +39,6 @@ class Extract2dStep(Step):
         output_model : DataModel
             The resulting DataModel of the extract_2d step
         """
-
         with datamodels.open(input_model) as dm:
             reference_file_names = {}
             if dm.meta.exposure.type in extract_2d.slitless_modes:
