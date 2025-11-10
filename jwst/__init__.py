23c139b9a609c01efb6cd57b251784328fd822b5
zacharyburnett <zburnett@stsci.edu>
2025-11-10 15:55:29 -0500

    update if statement to specify group flag

--- jwst/__init__.py
+++ jwst/__init__.py
@@ -9,7 +9,7 @@ __version__ = version(__name__)
 
 _regex_git_hash = re.compile(r".*\+g(\w+)")
 __version_commit__ = ""
-if "+" in __version__:
+if "+g" in __version__:
     commit = _regex_git_hash.match(__version__).groups()  # type: ignore[union-attr]
     if commit:
         __version_commit__ = commit[0]
