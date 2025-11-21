0d4c5327f79b2a496a79a6d0421087b8149a6d9e
Nadia Dencheva <nadia.dencheva@gmail.com>
2025-11-21 12:03:36 -0500

    test with stcal branch

--- jwst/resample/resample_spec.py
+++ jwst/resample/resample_spec.py
@@ -19,11 +19,9 @@ from astropy.utils.exceptions import AstropyUserWarning
 from gwcs import WCS, wcstools
 from gwcs import coordinate_frames as cf
 from stcal.alignment.util import compute_scale, wcs_bbox_from_shape
-
-# from stcal.alignment.util import wcs_bbox_from_shape
 from stdatamodels.jwst import datamodels
 
-from jwst.assign_wcs.util import wrap_ra  # , compute_scale
+from jwst.assign_wcs.util import wrap_ra
 from jwst.datamodels import ModelLibrary
 from jwst.resample import resample_utils
 from jwst.resample.resample import ResampleImage
@@ -1073,6 +1071,6 @@ def compute_spectral_pixel_scale(wcs, fiducial=None, disp_axis=1):
     if fiducial is None:
         center_x, center_y = np.mean(wcs.bounding_box, axis=1)
         fiducial = wcs(center_x, center_y)
-    log.info(f"fiducial {fiducial}")
+
     pixel_scale = compute_scale(wcs, fiducial, disp_axis=disp_axis)
     return float(pixel_scale)
