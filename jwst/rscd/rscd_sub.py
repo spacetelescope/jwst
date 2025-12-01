1ec8d70e85c8c0a5aa57c7101dfb4364f10d1044
jemorrison <morrison@as.arizona.edu>
2025-12-01 14:52:50 -0700

    skip first integration groups

--- jwst/rscd/rscd_sub.py
+++ jwst/rscd/rscd_sub.py
@@ -14,10 +14,12 @@ __all__ = ["do_correction", "correction_skip_groups", "get_rscd_parameters"]
 
 def do_correction(output_model, rscd_model):
     """
-    Set the initial groups of MIRI data to 'DO_NOT_USE' for integrations 2 and higher.
+    Set the initial groups of an integration for  MIRI data to 'DO_NOT_USE'.
 
     The number of initial groups to set to 'DO_NOT_USE' is read in from the RSCD reference
-    file. No flagging is done for the first integration.
+    file. The number of groups to skip is integration dependent. The first integration has
+    a value defined in the reference file and the second and higher integrations have a 
+    seperate value in the reference file. 
 
     Parameters
     ----------
@@ -42,12 +44,13 @@ def do_correction(output_model, rscd_model):
         output_model.meta.cal_step.rscd = "SKIPPED"
         return output_model
 
-    group_skip = param["skip"]
-    output_model = correction_skip_groups(output_model, group_skip)
+    group_skip_int1 = param["skip_int1"] # integration 1
+    group_skip_int2p = param["skip_int2p"]    # integration 2,  plus higher integrations 
+    output_model = correction_skip_groups(output_model, group_skip_int1, group_skip_int2p)
     return output_model
 
 
-def correction_skip_groups(output, group_skip):
+def correction_skip_groups(output, group_skip_int1, group_skip_int2p):
     """
     Set the initial groups in integration to DO_NOT_USE to skip groups affected by RSCD effect.
 
@@ -56,8 +59,11 @@ def correction_skip_groups(output, group_skip):
     output : RampModel
         Science data to be flagged
 
-    group_skip : int
-        Number of groups to skip at the beginning of the ramp
+    group_skip_int1 : int
+        Number of groups to skip at the beginning of the ramp for integration 1
+
+    group_skip_int2p : int
+        Number of groups to skip at the beginning of the ramp for integration 2 and higher
 
     Returns
     -------
@@ -76,24 +82,46 @@ def correction_skip_groups(output, group_skip):
 
     log.debug(f"RSCD correction using: nints={sci_nints}, ngroups={sci_ngroups}")
     log.debug(f"The first integration in the data is integration: {sci_int_start}")
-    log.info(f"Number of groups to skip for integrations 2 and higher: {group_skip}")
+    log.info(f"Number of groups to skip for integration 1: {group_skip_int1}")
+    log.info(f"Number of groups to skip for integrations 2 and higher: {group_skip_int2p}")
 
+    # Check that we have enough groups for all valid integrations
     # If ngroups <= group_skip+3, skip the flagging
     # the +3 is to ensure there is a slope to be fit including the flagging for
     # the last frame correction
-    if sci_ngroups <= (group_skip + 3):
+    # Chkec integration 1
+    # Skip RSCD
+    skip = False
+    if sci_ngroups <= (group_skip_int1 + 3):
+        skip = True
+
+    if sci_ints> 1:
+        if sci_ngroups <= (group_skip_int2p + 3):
+            skip = True
+    if skip:
         log.warning("Too few groups to apply RSCD correction")
         log.warning("RSCD step will be skipped")
         output.meta.cal_step.rscd = "SKIPPED"
         return output
 
-    # The RSCD correction is applied to integrations 2 and higher.
     # For segmented data the first integration in the file may not be the first
     # integration in the exposure. The value in meta.exposure.integration_start
     # holds the value of the first integration in the file.
     # If a correction is to be done and if ngroups > group_skip+3, then set all
     # of the GROUPDQ flags from groups 0 to group_skip to 'DO_NOT_USE'.
 
+    # drop groups in integration 1
+    drop_int1 = True
+    if sci_int_start != 1:
+        drop_int1 = False
+
+    if drop_int1: 
+        output.groupdq[0, 0:group_skip_int1, :, :] = np.bitwise_or(
+            output.groupdq[0:, 0:group_skip_int1, :, :], dqflags.group["DO_NOT_USE"]
+    )
+    log.debug(f"RSCD Sub: adding DO_NOT_USE to GROUPDQ for the first {group_skip_int1} groups of integration1")
+
+    
     int_start = 1
     if sci_int_start != 1:  # we have segmented data
         int_start = 0
@@ -140,9 +168,12 @@ def get_rscd_parameters(input_model, rscd_model):
     for tabdata in rscd_model.rscd_group_skip_table:
         subarray_table = tabdata["subarray"]
         readpatt_table = tabdata["readpatt"]
-        group_skip_table = tabdata["group_skip"]
+        group_skip_table_int2p = tabdata["group_skip"] # integration 2 and higher (+)
+        group_skip_table_int1 = tabdata["group_skip1"]
+        
         if subarray_table == subarray and readpatt_table == readpatt:
-            param["skip"] = group_skip_table
+            param["skip_int1"] = group_skip_table
+            param["skip_int2p"] = group_skip_table  # integration 2 and higher
             break
 
     return param
