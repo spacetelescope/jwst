2a063d02394842fcd416914e822c5beb45a242a7
Melanie Clarke <mclarke@stsci.edu>
2025-12-02 14:43:03 -0500

    Add fit_profile to spec pipelines, before pixel replace

--- jwst/fit_profile/fit_profile_step.py
+++ jwst/fit_profile/fit_profile_step.py
@@ -2,6 +2,7 @@ import logging
 
 from stdatamodels.jwst import datamodels
 
+from jwst.datamodels import ModelContainer
 from jwst.fit_profile.fit_profile import fit_and_oversample_ifu
 from jwst.stpipe import Step
 
@@ -19,7 +20,7 @@ class FitProfileStep(Step):
     threshsig = float(default=10) # Limiting sigma for fitting splines
     slopelim = float(default=0.1) # Slope limit for using splines in oversample
     oversample = float(default=1.0) # Use the profile fit to oversample the data by this factor
-    # psfoptimal = boolean(default=False)
+    skip = boolean(default=True) # By default, skip the step.
     """  # noqa: E501
 
     def process(self, input_data):
@@ -37,19 +38,26 @@ class FitProfileStep(Step):
             The input model, updated with the fit profile.
         """
         output_model = self.prepare_output(input_data)
+        if isinstance(output_model, ModelContainer):
+            models = output_model
+        else:
+            models = [output_model]
 
-        if not isinstance(output_model, datamodels.IFUImageModel):
-            log.warning("The fit_profile step is only implemented for IFU data.")
-            log.warning("Skipping processing for datamodel type %s.", str(output_model))
-            output_model.meta.cal_step.fit_profile = "SKIPPED"
-            return output_model
+        for model in models:
+            if not isinstance(model, datamodels.IFUImageModel):
+                log.warning("The fit_profile step is only implemented for IFU data.")
+                log.warning("Skipping processing for datamodel type %s.", str(output_model))
+                model.meta.cal_step.fit_profile = "SKIPPED"
+                continue
 
-        output_model = fit_and_oversample_ifu(
-            output_model,
-            threshsig=self.threshsig,
-            slopelim=self.slopelim,
-            oversample_factor=self.oversample,
-        )
+            log.info("Fitting profile for %s", model.meta.filename)
+            fit_and_oversample_ifu(
+                model,
+                threshsig=self.threshsig,
+                slopelim=self.slopelim,
+                oversample_factor=self.oversample,
+            )
 
-        output_model.meta.cal_step.fit_profile = "COMPLETE"
+            model.meta.cal_step.fit_profile = "COMPLETE"
+
         return output_model
