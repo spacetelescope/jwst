c84bf0af2472bf92343f46461847e0fda5b895d7
Melanie Clarke <mclarke@stsci.edu>
2025-12-30 11:20:59 -0500

    Docs for step.process

--- jwst/adaptive_trace_model/adaptive_trace_model_step.py
+++ jwst/adaptive_trace_model/adaptive_trace_model_step.py
@@ -18,23 +18,54 @@ class AdaptiveTraceModelStep(Step):
 
     spec = """
     threshsig = float(default=10) # Limiting sigma for fitting splines
-    slopelim = float(default=0.1) # Slope limit for using splines in oversample
     oversample = float(default=1.0) # Use the trace model to oversample the data by this factor
+    slopelim = float(default=0.1) # Slope limit for using splines in oversample
     skip = boolean(default=True) # By default, skip the step.
     """  # noqa: E501
 
     def process(self, input_data):
         """
-        Execute the step.
+        Fit an adaptive trace model to a spectral image.
+
+        For each spectral region in the input (i.e. a slice or a slit), the algorithm
+        fits a univariate basis spline to the spatial data within a window of each
+        dispersion element. A spectral region may be ignored if its signal-to-noise
+        ratio is too low. This decision is controlled by the ``threshsig`` parameter:
+        lower values will create spline models for more slices.
+
+        After fitting, the set of spline models is then evaluated at each pixel to
+        create a model of the spectral trace at all valid data points.
+
+        If no oversample factor is specified, the only change to the datamodel
+        is to attach the resulting spectral trace image, in the ``trace_model``
+        attribute.
+
+        If an oversample factor is specified, the input flux, error, and variance
+        images are replaced with interpolated data scaled to a new pixel grid. The DQ
+        and wavelength images are also oversampled and the trace model image is
+        attached, but any additional images (e.g. pathloss corrections) are not
+        propagated.
+
+        To perform this oversampling, the trace model is used in combination with a
+        linear interpolation at each dispersion element.  Which model is used at each
+        pixel depends on some heuristic decisions derived from the data: bright, compact
+        source regions generally use the spline model; faint, diffuse regions use the linear
+        interpolation.  The ``slopelim`` parameter can be modified to impact the decision
+        on whether sources are bright and compact enough to use the spline models. Lower
+        values will use the spline model for fainter sources.
 
         Parameters
         ----------
-        input_data : str or `~stdatamodels.jwst.datamodels.JwstDataModel`
-            The input datamodel or filename containing spectral data.
+        input_data : str or `~stdatamodels.jwst.datamodels.IFUImageModel` \
+                     or `~jwst.datamodels.container.ModelContainer`
+            The input datamodel, container, or filename containing spectral data.
+            If the input is a container, each contained model is separately
+            processed.
 
         Returns
         -------
-        `~stdatamodels.jwst.datamodels.JwstDataModel`
+        output_model : `~stdatamodels.jwst.datamodels.IFUImageModel` \
+                       or `~jwst.datamodels.container.ModelContainer`
             The input model, updated with the fit trace model.
         """
         output_model = self.prepare_output(input_data)
