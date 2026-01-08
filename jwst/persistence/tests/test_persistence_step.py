e71403734856d035682b91291cfee153c018ee58
Ken MacDonald <kmacdonald@stsci.edu>
2026-01-08 10:54:11 -0500

    Added tests for persistence flagging.

--- jwst/persistence/tests/test_persistence_step.py
+++ jwst/persistence/tests/test_persistence_step.py
@@ -19,7 +19,7 @@ def test_persistence_time_none_keeps_groupdq_unchanged(create_sci_model):
     assert np.array_equal(res.groupdq, model.groupdq)
 
 
-def test_persistence_time_10_sec(create_sci_model):
+def test_persistence_time_nonneg_sec(create_sci_model):
     """Test persitence flag gets set inside of persistence window"""
     nints, ngroups, nrows, ncols = 2, 7, 1, 2
     model = create_sci_model(nints=nints, ngroups=ngroups, nrows=nrows, ncols=ncols)
@@ -28,7 +28,7 @@ def test_persistence_time_10_sec(create_sci_model):
     step = PersistenceStep(persistence_time=70)
     res, _ = step.run(model)
 
-    # With a persistenc window of 70 seconds and group time of 21.47354 seconds, the 5th
+    # With a persistence window of 70 seconds and group time of 21.47354 seconds, the 5th
     # group of the first integration for pixel (0, 1) and the following three groups
     # (crossing the integration) will be flagged as persistent.
 
@@ -41,7 +41,95 @@ def test_persistence_time_10_sec(create_sci_model):
     np.testing.assert_equal(res.groupdq[0, :, 0, 1], check3)
     np.testing.assert_equal(res.groupdq[1, :, 0, 1], check4)
 
-# XXX Continue creating CI tests for new persistence flagging.
+
+def test_persistence_time_0_sec(create_sci_model):
+    """Test persitence flag gets set inside of persistence window of 0 seconds"""
+    nints, ngroups, nrows, ncols = 2, 7, 1, 2
+    model = create_sci_model(nints=nints, ngroups=ngroups, nrows=nrows, ncols=ncols)
+    model.groupdq[0, 5:, 0, 1] |= dqflags.group["SATURATED"]
+
+    step = PersistenceStep(persistence_time=0)
+    res, _ = step.run(model)
+
+    # With a zero persistence window, no groups should be flagged as persistent and
+    # persistence_time gets set to None.
+    assert step.persistence_time is None
+    check1 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check2 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check3 = np.array([0, 0, 0, 0, 0, 2, 2], dtype=np.uint8)
+    check4 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 0], check1)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 0], check2)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 1], check3)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 1], check4)
+
+
+def test_persistence_time_neg_sec(create_sci_model):
+    """Test persitence flag gets set inside of negative persistence window."""
+    nints, ngroups, nrows, ncols = 2, 7, 1, 2
+    model = create_sci_model(nints=nints, ngroups=ngroups, nrows=nrows, ncols=ncols)
+    model.groupdq[0, 5:, 0, 1] |= dqflags.group["SATURATED"]
+
+    step = PersistenceStep(persistence_time=-100)
+
+    res, _ = step.run(model)
+
+    # With a negative persistence window, no groups should be flagged as persistent and
+    # persistence_time gets set to None.
+    assert step.persistence_time is None
+    check1 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check2 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check3 = np.array([0, 0, 0, 0, 0, 2, 2], dtype=np.uint8)
+    check4 = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 0], check1)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 0], check2)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 1], check3)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 1], check4)
+
+
+def test_persistence_time_with_array(create_sci_model):
+    """Test persitence flag gets set using a persistence_array"""
+    nints, ngroups, nrows, ncols = 2, 7, 1, 3
+    model = create_sci_model(nints=nints, ngroups=ngroups, nrows=nrows, ncols=ncols)
+    model.groupdq[0, 5:, 0, 1] |= dqflags.group["SATURATED"]
+
+    persistence_array = [[0.0, 0.0, 1648999345.9470801]]
+    parr = np.array(persistence_array)
+
+    step = PersistenceStep(persistence_array=persistence_array, persistence_time=70)
+    res, _ = step.run(model)
+
+    # With a non-zero persistence_array entry for pixel (0, 2), that pixel should have
+    # persistence flagging applied based on the time in the array.
+    checkz = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check1 = np.array([0, 0, 0, 0, 0, 34, 34], dtype=np.uint8)
+    check2 = np.array([32, 32, 0, 0, 0, 0, 0], dtype=np.uint8)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 0], checkz)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 0], checkz)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 1], check1)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 1], check2)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 2], check2)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 2], checkz)
+
+
+def test_persistence_time_dnu(create_sci_model):
+    """Test persitence flag gets set with DO_NOT_USE"""
+    nints, ngroups, nrows, ncols = 2, 7, 1, 2
+    model = create_sci_model(nints=nints, ngroups=ngroups, nrows=nrows, ncols=ncols)
+    model.groupdq[0, 5:, 0, 1] |= dqflags.group["SATURATED"]
+
+    step = PersistenceStep(persistence_dnu=True, persistence_time=70)
+    res, _ = step.run(model)
+
+    # With persistence_dnu=True, the DO_NOT_USE flag should be set along with PERSISTENCE
+    checkz = np.array([0, 0, 0, 0, 0, 0, 0], dtype=np.uint8)
+    check1 = np.array([0, 0, 0, 0, 0, 35, 35], dtype=np.uint8)
+    check2 = np.array([33, 33, 0, 0, 0, 0, 0], dtype=np.uint8)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 0], checkz)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 0], checkz)
+    np.testing.assert_equal(res.groupdq[0, :, 0, 1], check1)
+    np.testing.assert_equal(res.groupdq[1, :, 0, 1], check2)
+
 
 def test_step_no_trapsfilled(create_sci_model, tmp_path):
     model = create_sci_model()
