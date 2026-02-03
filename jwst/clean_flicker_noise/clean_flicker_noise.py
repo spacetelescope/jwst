98812308b6c6f1bdef91cdaae389c09e3bf69777
Melanie Clarke <mclarke@stsci.edu>
2026-02-03 10:38:11 -0500

    Fix hasattr for wcs in clean_flicker_noise

--- jwst/clean_flicker_noise/clean_flicker_noise.py
+++ jwst/clean_flicker_noise/clean_flicker_noise.py
@@ -182,7 +182,7 @@ def post_process_rate(
     output_model = input_model
 
     # If needed, assign a WCS
-    if (assign_wcs or msaflagopen) and not hasattr(output_model.meta, "wcs"):
+    if (assign_wcs or msaflagopen) and not output_model.meta.hasattr("wcs"):
         log.info("Assigning a WCS for scene masking")
         with disable_logging(level=logging.WARNING):
             step = AssignWcsStep()
