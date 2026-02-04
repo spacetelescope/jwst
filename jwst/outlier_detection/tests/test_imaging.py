eda9f615d1f7d9f8b5ce56a5c89e88d015ef5a59
Mihai Cara <mihail.cara@gmail.com>
2026-02-04 17:44:19 -0500

    Fix pixel scale of test images and add test for detecting CR

--- jwst/outlier_detection/tests/test_imaging.py
+++ jwst/outlier_detection/tests/test_imaging.py
@@ -186,6 +186,71 @@ def test_outlier_step_with_source_no_outliers(mirimage_three_sci, tmp_cwd, src_t
             result.shelve(corrected, modify=False)
 
 
+@pytest.mark.parametrize("weight", ["exptime", "ivm"])
+@pytest.mark.parametrize("src_type", ["gaussian", "square", "strip"])
+@pytest.mark.parametrize("idx", [0, 1, 2])
+def test_outlier_step_with_outliers(mirimage_three_sci, tmp_cwd, src_type, weight, idx):
+    """Test whole step with no outlier and an artificial source: "gaussian" source
+    or a constant square source with sharp edges, no outliers"""
+    container = ModelLibrary(list(mirimage_three_sci))
+
+    atol = 2.0 * np.finfo(np.float32).eps
+
+    # Create artificial source
+    if src_type == "strip":
+        sl = np.s_[9:12, 5:16]
+        src = np.full((3, 11), 100 * helpers.SIGMA, dtype=np.float32)
+    elif src_type == "square":
+        sl = np.s_[5:16, 5:16]
+        src = np.full((11, 11), 100 * helpers.SIGMA, dtype=np.float32)
+    else:  # gaussian
+        sl = np.s_[5:16, 5:16]
+        y, x = np.indices((11, 11)) - 5
+        fwhm = 1.5
+        src = 100 * helpers.SIGMA * np.exp(-4.0 * np.log(2.0) * (x**2 + y**2) / fwhm**2)
+
+    cr_mask = src > 5 * helpers.SIGMA
+
+    # put a Gaussian source in all three exposures
+    with container:
+        for i, ccont in enumerate(container):
+            if i == idx:
+                ccont.data[sl] += src
+                # ccont.err[5:16, 5:16] = np.sqrt(ccont.err[5:16, 5:16]**2 + src)
+            container.shelve(ccont)
+
+    # Save all the data into a separate array before passing into step
+    data_as_cube = []
+    dq_as_cube = []
+    non_nan_mask_as_cube = []
+    with container:
+        for model in container:
+            data_as_cube.append(model.data.copy())
+            dq_as_cube.append(model.dq.copy())
+            non_nan_mask_as_cube.append(np.isfinite(model.data))
+            container.shelve(model, modify=False)
+
+    result = OutlierDetectionStep.call(container, in_memory=True, weight_type=weight)
+
+    with result:
+        for i, corrected in enumerate(result):
+            m = non_nan_mask_as_cube[i]
+            if i == idx:
+                # Make sure CR pixels are now NaN in SCI and flagged in DQ
+                m[sl] = ~cr_mask
+                m2 = np.isfinite(corrected.data)
+
+                if src_type == "square":
+                    pytest.xfail("Square CR fails with pixels in the interior of the square not flagged as outliers.")
+
+                assert np.all(m == m2)
+                assert np.all((corrected.dq[~m] & helpers.OUTLIER_DO_NOT_USE) == helpers.OUTLIER_DO_NOT_USE)
+
+            np.testing.assert_allclose(data_as_cube[i][m], corrected.data[m], rtol=0.0, atol=atol)
+            np.testing.assert_array_equal(dq_as_cube[i][m], corrected.dq[m])
+            result.shelve(corrected, modify=False)
+
+
 def test_skip_one_exposure_imaging():
     model = datamodels.ImageModel()
     input_models = [model]
