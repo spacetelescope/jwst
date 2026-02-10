08626a41483ac7e9f761f9cd6466438c3495d2c5
Ned Molter <emolter@stsci.edu>
2026-02-10 10:50:31 -0500

    Use get_dtype in test_dq_init

--- jwst/dq_init/tests/test_dq_init.py
+++ jwst/dq_init/tests/test_dq_init.py
@@ -458,6 +458,6 @@ def make_maskmodel(ysize, xsize):
         (5, 32, "REFERENCE_PIXEL", "Reference Pixel"),
     ]
 
-    dq_def = np.array((dqdef), dtype=mask.dq_def.dtype)
+    dq_def = np.array((dqdef), dtype=mask.get_dtype("dq_def"))
 
     return dq, dq_def
