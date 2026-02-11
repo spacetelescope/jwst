820d874dd6cf14bbb7c19eedee1a23202423ec6a
Tyler Pauly <tapastro@gmail.com>
2026-02-11 09:30:10 -0500

    more rebase issues

--- jwst/extract_2d/grisms.py
+++ jwst/extract_2d/grisms.py
@@ -140,12 +140,18 @@ def extract_tso_object(
     output_model.update(input_model)
     subwcs = copy.deepcopy(input_model.meta.wcs)
 
+    # Some NIRCam wavelengthrange entries have a fieldpoint entry, while NIRISS does not.
+    if len(wavelengthrange[0]) == 5:
+        filter_idx = 2
+    else:
+        filter_idx = 1
+
     # Loop over spectral orders
     for order in available_orders:
         range_select = [
             (x[-2], x[-1])
             for x in wavelengthrange
-            if (x[0] == order and x[2] == input_model.meta.instrument.filter)
+            if (x[0] == order and x[filter_idx] == input_model.meta.instrument.filter)
         ]
 
         # Use the filter that was in front of the grism for translation
