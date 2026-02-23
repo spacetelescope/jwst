a64b01f35c2a627fe22567cb69d02ae2dd09a6ab
Tyler Pauly <tapastro@gmail.com>
2026-02-23 13:35:08 -0500

    update RTs

--- jwst/regtest/test_nircam_dhs.py
+++ jwst/regtest/test_nircam_dhs.py
@@ -36,6 +36,34 @@ def test_log_tracked_resources_det1(log_tracked_resources, run_detector1pipeline
     log_tracked_resources()
 
 
+@pytest.fixture(scope="module")
+def run_spec2pipeline(rtdata_module, resource_tracker):
+    """Run calwebb_detector1 on NIRCam imaging long data"""
+    rtdata = rtdata_module
+    rtdata.get_data("nircam/dhs/tpauly_nrca1_regions.asdf")
+    rtdata.get_data("nircam/dhs/nircam_nrca1_specwcs.asdf")
+    rtdata.get_data("nircam/dhs/jw04453010001_02106_00001_nrca1_genheader_rateints.fits")
+
+    # Run spec2 pipeline on rateints file
+    args = [
+        "calwebb_spec2",
+        rtdata.input,
+        "--steps.assign_wcs.save_results=True",
+        "--steps.assign_wcs.override_regions=tpauly_nrca1_regions.asdf",
+        "--steps.assign_wcs.override_specwcs=nircam_nrca1_specwcs.asdf",
+        "--steps.extract_2d.save_results=True",
+        "--steps.srctype.save_results=True",
+        "--steps.photom.save_results=True",
+        "--steps.extract_1d.save_results=True",
+    ]
+    with resource_tracker.track():
+        Step.from_cmdline(args)
+
+
+def test_log_tracked_resources_spec2(log_tracked_resources, run_spec2pipeline):
+    log_tracked_resources()
+
+
 @pytest.mark.parametrize(
     "suffix",
     [
@@ -62,5 +90,28 @@ def test_nircam_dhs_detector1(
 
     # Ignore the custom dark because it contains a full path.
     fitsdiff_default_kwargs["ignore_keywords"].append("R_DARK")
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
+
+
+@pytest.mark.parametrize(
+    "suffix",
+    [
+        "assign_wcs",
+        "extract_2d",
+        "srctype",
+        "photom",
+        "calints",
+        "x1dints",
+    ],
+)
+def test_nircam_dhs_spec2(run_spec2pipeline, rtdata_module, fitsdiff_default_kwargs, suffix):
+    """Regression test of detector1 pipeline performed on NIRCam DHS mock data."""
+    rtdata = rtdata_module
+    rtdata.input = "jw04453010001_02106_00001_nrca1_genheader_rateints.fits"
+    output = "jw04453010001_02106_00001_nrca1_genheader_" + suffix + ".fits"
+    rtdata.output = output
+    rtdata.get_truth(f"truth/test_nircam_dhs/{output}")
+
     diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
     assert diff.identical, diff.report()
