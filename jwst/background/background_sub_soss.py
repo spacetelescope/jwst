58306b6f7384375b89be5d11fc956ccdec47b9db
AarynnCarter <23636747+AarynnCarter@users.noreply.github.com>
2026-04-10 15:11:08 -0400

    Tweak for masking, adding changelog

--- jwst/background/background_sub_soss.py
+++ jwst/background/background_sub_soss.py
@@ -205,7 +205,7 @@ def subtract_soss_bkg(
                 continue
 
             scales[i] = np.nanmedian(ratio[valid_pixels])
-            rmse[i] = _rms_error( data[mask][valid_pixels] - (template[mask][valid_pixels] * scales[i]))
+            rmse[i] = _rms_error(data[mask][finite] - (template[mask][finite] * scales[i]))
 
         if np.sum(rmse) < best_rmse:
             best_rmse = np.sum(rmse)
