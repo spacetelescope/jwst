3d7b724f17f6ffff7a1c55bf2a8b204aee02d6a4
Mihai Cara <mihail.cara@gmail.com>
2026-05-07 15:18:16 -0400

    Update resampled output image's background level and status

--- jwst/resample/resample.py
+++ jwst/resample/resample.py
@@ -460,6 +460,8 @@ class ResampleImage(Resample):
         model.meta.exposure.measurement_time = info_dict["measurement_time"]
         model.meta.exposure.effective_exposure_time = info_dict["exposure_time"]
         model.meta.exposure.elapsed_exposure_time = info_dict["elapsed_exposure_time"]
+        model.meta.background.level = info_dict.get("level", 0.0)
+        model.meta.background.subtracted = info_dict.get("subtracted", True)
 
     def add_model(self, model):
         """
