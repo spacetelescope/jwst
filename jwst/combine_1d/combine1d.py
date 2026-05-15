bde344507e8f61892f4e853ed1b8b7aa3e7eb6c4
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-05-15 17:22:36 -0400

    WIP: Attempt to fix tests and local strun

--- jwst/combine_1d/combine1d.py
+++ jwst/combine_1d/combine1d.py
@@ -861,7 +861,7 @@ def check_monotonic(wavelength):
     if len(wavelength) < 2:
         return True
 
-    is_increasing = all(np.diff(wavelength) > 0)
-    is_decreasing = all(np.diff(wavelength) < 0)
+    is_increasing = bool((np.diff(wavelength) > 0).all())
+    is_decreasing = bool((np.diff(wavelength) < 0).all())
 
     return is_increasing or is_decreasing
