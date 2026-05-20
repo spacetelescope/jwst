a85c48099e75e006576a93f18b068a13bd684560
Brett <brettgraham@gmail.com>
2026-05-20 15:49:04 -0400

    update integration test to not expect jwst are the only registered steps

--- jwst/stpipe/tests/test_entry_points.py
+++ jwst/stpipe/tests/test_entry_points.py
@@ -1,25 +1,21 @@
-from stpipe.entry_points import get_steps
-
 import jwst
 import jwst.pipeline
 import jwst.step
+from jwst.stpipe.integration import get_steps
 
 
 def test_get_steps():
-    step_info_by_class = {s.class_name: s for s in get_steps()}
+    step_info_by_class = {s[0]: s for s in get_steps()}
 
     for class_name in jwst.pipeline.__all__:
         pipeline_class = getattr(jwst.pipeline, class_name)
-        step_info = step_info_by_class[f"jwst.pipeline.{class_name}"]
-        assert step_info.class_alias == pipeline_class.class_alias
-        assert step_info.is_pipeline is True
-        assert step_info.package_name == "jwst"
-        assert step_info.package_version == jwst.__version__
+        _, class_alias, is_pipeline = step_info_by_class[f"jwst.pipeline.{class_name}"]
+
+        assert class_alias == pipeline_class.class_alias
+        assert is_pipeline is True
 
     for class_name in jwst.step.__all__:
         step_class = getattr(jwst.step, class_name)
-        step_info = step_info_by_class[f"jwst.step.{class_name}"]
-        assert step_info.class_alias == step_class.class_alias
-        assert step_info.is_pipeline is False
-        assert step_info.package_name == "jwst"
-        assert step_info.package_version == jwst.__version__
+        _, class_alias, is_pipeline = step_info_by_class[f"jwst.step.{class_name}"]
+        assert class_alias == step_class.class_alias
+        assert is_pipeline is False
