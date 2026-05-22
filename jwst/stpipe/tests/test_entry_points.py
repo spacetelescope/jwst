5937652de4a53bb16150fde01e8380d8bb174fdd
Brett Graham <brettgraham@gmail.com>
2026-05-22 10:59:45 -0400

    Move stpipe unit tests to stpipe (#10568)

--- jwst/stpipe/tests/test_entry_points.py
+++ jwst/stpipe/tests/test_entry_points.py
@@ -1,25 +1,32 @@
-from stpipe.entry_points import get_steps
-
 import jwst
 import jwst.pipeline
 import jwst.step
+from jwst.stpipe import Pipeline, Step
+from jwst.stpipe.integration import get_steps
 
 
 def test_get_steps():
-    step_info_by_class = {s.class_name: s for s in get_steps()}
+    step_info_by_class = {s[0]: s for s in get_steps()}
+    class_paths = set()
 
     for class_name in jwst.pipeline.__all__:
         pipeline_class = getattr(jwst.pipeline, class_name)
-        step_info = step_info_by_class[f"jwst.pipeline.{class_name}"]
-        assert step_info.class_alias == pipeline_class.class_alias
-        assert step_info.is_pipeline is True
-        assert step_info.package_name == "jwst"
-        assert step_info.package_version == jwst.__version__
+        class_path = f"jwst.pipeline.{class_name}"
+        _, class_alias, is_pipeline = step_info_by_class[class_path]
+
+        assert class_alias == pipeline_class.class_alias
+        assert is_pipeline is True
+        assert issubclass(pipeline_class, Step)
+        assert issubclass(pipeline_class, Pipeline)
+        class_paths.add(class_path)
 
     for class_name in jwst.step.__all__:
         step_class = getattr(jwst.step, class_name)
-        step_info = step_info_by_class[f"jwst.step.{class_name}"]
-        assert step_info.class_alias == step_class.class_alias
-        assert step_info.is_pipeline is False
-        assert step_info.package_name == "jwst"
-        assert step_info.package_version == jwst.__version__
+        class_path = f"jwst.step.{class_name}"
+        _, class_alias, is_pipeline = step_info_by_class[class_path]
+        assert class_alias == step_class.class_alias
+        assert is_pipeline is False
+        assert issubclass(pipeline_class, Step)
+        class_paths.add(class_path)
+
+    assert class_paths == set(step_info_by_class.keys())
