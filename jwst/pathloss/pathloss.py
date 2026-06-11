9d730d435989a8fe59c86a811e65c695a0e56c83
Ian Wong <iwong@mit.edu>
2026-06-11 19:08:34 -0400

    Read cross-slit offset for LRS slit
    Added code to propagate cross-slit dither offsets to the ycenter. Until now, the wcs solution placed the target at the nominal pointing position at the center of the slit, regardless of whether a cross-slit dither pattern was executed. This update will be critical for utilizing the improved LRS slit pathloss correction.

--- jwst/pathloss/pathloss.py
+++ jwst/pathloss/pathloss.py
@@ -119,6 +119,16 @@ def get_center(exp_type, input_model, offsets=False):
             input_model.meta.target.ra, input_model.meta.target.dec, ref_wave
         )
         log.info(f"LRS target location from RA/Dec = {xcenter, ycenter}")
+
+        # compute cross-slit dither offset
+        location = (_ref_ra, _ref_dec, ref_wave)
+        scale_degrees = util.compute_scale(
+                input_model.meta.wcs, location, disp_axis=input_model.meta.wcsinfo.dispersion_direction
+            )
+        scale_arcsec = scale_degrees * 3600.0
+        yoffset = input_model.meta.dither.y_offset / scale_arcsec
+        ycenter += yoffset
+        log.info(f"Cross-slit dither offset = {yoffset} px") 
     else:
         log.info(f"LRS target location from source_xpos, source_ypos = {xcenter, ycenter}")
 
