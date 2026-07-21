9b6ebfbb744b2acecfdf7284557f11527a7c0601
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-07-21 17:57:58 -0400

    Extend functionality to calwebb_dark

--- jwst/pipeline/tests/test_calwebb_dark.py
+++ jwst/pipeline/tests/test_calwebb_dark.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import numpy as np
 
 from jwst.pipeline import DarkPipeline
 from jwst.pipeline.tests.helpers import make_miri_ramp_model
 
 
-def test_dark_pipeline(tmp_path):
+def test_dark_pipeline_model(tmp_path):
     input_model = make_miri_ramp_model()
     input_model_copy = input_model.copy()
     DarkPipeline.call(input_model, save_results=True, output_dir=str(tmp_path))
@@ -18,3 +20,22 @@ def test_dark_pipeline(tmp_path):
 
     input_model.close()
     input_model_copy.close()
+
+
+def test_dark_pipeline_asn(tmp_cwd):
+    prodname = "test_dark"
+    modname = f"{prodname}_target_1.fits"
+    input_model = make_miri_ramp_model()
+    input_model.save(modname)
+    asn = {
+        "asn_id": "c1000",
+        "target": "t001",
+        "asn_pool": f"{prodname}_pool.csv",
+        "products": [{"name": prodname, "members": [{"expname": modname, "exptype": "science"}]}],
+    }
+    DarkPipeline.call(asn, save_results=True, output_file="test_miri")
+
+    # Check for expected output
+    assert Path("test_miri_dark.fits").exists()
+
+    input_model.close()
