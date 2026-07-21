bab72275f41938e10a555a511d7723cef64e22be
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-07-21 18:07:08 -0400

    Extend functionality to calwebb_guider

--- jwst/pipeline/tests/test_calwebb_guider.py
+++ jwst/pipeline/tests/test_calwebb_guider.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import numpy as np
 
 from jwst.guider_cds.tests.helpers import make_guider_image
 from jwst.pipeline import GuiderPipeline
 
 
-def test_guider_pipeline(tmp_path):
+def test_guider_pipeline_model(tmp_path):
     input_model = make_guider_image()
     input_model_copy = input_model.copy()
     GuiderPipeline.call(input_model, save_results=True, output_dir=str(tmp_path))
@@ -18,3 +20,22 @@ def test_guider_pipeline(tmp_path):
 
     input_model.close()
     input_model_copy.close()
+
+
+def test_guider_pipeline_asn(tmp_cwd):
+    prodname = "test_guider"
+    modname = f"{prodname}_target_1.fits"
+    input_model = make_guider_image()
+    input_model.save(modname)
+    asn = {
+        "asn_id": "c1000",
+        "target": "t001",
+        "asn_pool": f"{prodname}_pool.csv",
+        "products": [{"name": prodname, "members": [{"expname": modname, "exptype": "science"}]}],
+    }
+    GuiderPipeline.call(asn, save_results=True, output_file="test_guider")
+
+    # Check for expected output
+    assert Path("test_guider_cal.fits").exists()
+
+    input_model.close()
