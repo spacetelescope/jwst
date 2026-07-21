273c7135ac73016bdf64691127227660e90d05ae
Ned Molter <emolter@stsci.edu>
2026-07-21 14:43:54 -0400

    Update niriss regtest too

--- jwst/regtest/test_niriss_wfss_contam.py
+++ jwst/regtest/test_niriss_wfss_contam.py
@@ -3,6 +3,7 @@
 import pytest
 
 from jwst.regtest import regtestdata as rt
+from jwst.regtest.st_fitsdiff import STFITSDiff as FITSDiff
 
 # Mark all tests in this module
 pytestmark = [pytest.mark.bigdata]
@@ -17,15 +18,18 @@ def run_wfss_contam(rtdata_module, resource_tracker):
     rtdata.get_data("niriss/wfss/jw02738-o001_t001_niriss_clear-f200w_i2d.fits")
     rtdata.get_data("niriss/wfss/jw02738-o001_t001_niriss_clear-f200w_segm.fits")
     rtdata.get_data("niriss/wfss/jw02738-o001_t001_niriss_clear-f200w_cat.ecsv")
-    rtdata.get_data("niriss/wfss/jw02738001001_04201_00004_nis_srctype.fits")
+    rtdata.get_data("niriss/wfss/jw02738001001_04201_00004_nis_rate.fits")
+    rtdata.get_data("niriss/wfss/jw02738-o001_20260704t072252_spec2_00013_asn.json")
 
     # Run the step
     step_params = {
-        "step": "jwst.wfss_contam.WfssContamStep",
+        "step": "jwst.pipeline.Spec2Pipeline",
         "args": [
-            "--save_simulated_image=True",
-            "--save_contam_images=True",
-            "--skip=False",
+            "--steps.wfss_contam.save_simulated_image=True",
+            "--steps.wfss_contam.save_contam_images=True",
+            "--steps.wfss_contam.save_results=true",
+            "--steps.wfss_contam.skip=False",
+            "--steps.wfss_contam.maximum_cores=none",
         ],
     }
     with resource_tracker.track():
@@ -37,9 +41,17 @@ def test_log_tracked_resources_nis_wfss_contam(log_tracked_resources, run_wfss_c
     log_tracked_resources()
 
 
-@pytest.mark.parametrize("suffix", ["simul", "simul_slits", "contam", "wfsscontamstep"])
+@pytest.mark.parametrize("suffix", ["simul", "simul_slits", "contam", "wfss_contam"])
 def test_niriss_wfss_contam(run_wfss_contam, fitsdiff_default_kwargs, suffix):
     """Regression test for wfss_contam applied to NIRISS WFSS data"""
-    rt.is_like_truth(
-        run_wfss_contam, fitsdiff_default_kwargs, suffix, "truth/test_niriss_wfss_contam"
-    )
+
+    rtdata = run_wfss_contam
+    output = f"jw02738001001_04201_00004_nis_{suffix}.fits"
+    rtdata.output = output
+
+    # Get the truth files
+    rtdata.get_truth("truth/test_niriss_wfss_contam/" + output)
+
+    # Compare the results
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
