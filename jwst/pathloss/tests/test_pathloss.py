e27927b97338610fac6d1b1ef5321cb7a142d656
Ian Wong <iwong@mit.edu>
2026-09-16 16:15:41 -0400

    Separated unit tests
    Created new unit test for dither offsets, separate from the source position test.

--- jwst/pathloss/tests/test_pathloss.py
+++ jwst/pathloss/tests/test_pathloss.py
@@ -112,17 +112,17 @@ def test_get_center_lrs_from_ra_dec():
     assert y_pos == 53.0 + offset_2_val
 
 
-def test_get_center_lrs_with_offsets():
+def test_get_center_lrs_with_dither_offsets():
     """Test MIR_LRS-FIXEDSLIT with offsets=True"""
     datmod = ImageModel()
     datmod.meta.exposure.type = "MIR_LRS-FIXEDSLIT"
 
-    # Create WCS with offsets
+    # Create WCS with offsets and detector position
     offset_1_val = -100.0
     offset_2_val = -50.0
-    datmod.meta.wcs = mock_lrs_wcs(offset_1=offset_1_val, offset_2=offset_2_val)
-    datmod.source_xpos = 105.0
-    datmod.source_ypos = 53.0
+    datmod.meta.wcs = mock_lrs_wcs(
+        offset_1=offset_1_val, offset_2=offset_2_val, det_pos=(105.0, 53.0)
+    )
 
     # Add dither offsets (in arcsecs)
     nod_1_xoffset = -0.945
@@ -136,6 +136,28 @@ def test_get_center_lrs_with_offsets():
     # Should return source position minus aperture ref, plus the aperture ref separately
     assert x_pos == 105.0 + offset_1_val + nod_1_xoffset
     assert y_pos == 53.0 + offset_2_val + nod_1_yoffset
+    assert imx == -offset_1_val
+    assert imy == -offset_2_val
+
+
+def test_get_center_lrs_with_source_position_and_offsets():
+    """Test MIR_LRS-FIXEDSLIT with offsets=True"""
+    datmod = ImageModel()
+    datmod.meta.exposure.type = "MIR_LRS-FIXEDSLIT"
+
+    # Create WCS with offsets
+    offset_1_val = -100.0
+    offset_2_val = -50.0
+    datmod.meta.wcs = mock_lrs_wcs(offset_1=offset_1_val, offset_2=offset_2_val)
+    datmod.source_xpos = 105.0
+    datmod.source_ypos = 53.0
+
+    # Test with offsets=True
+    x_pos, y_pos, imx, imy = pl.get_center("MIR_LRS-FIXEDSLIT", datmod, offsets=True)
+
+    # Should return source position minus aperture ref, plus the aperture ref separately
+    assert x_pos == 105.0 + offset_1_val
+    assert y_pos == 53.0 + offset_2_val
     assert imx == -offset_1_val
     assert imy == -offset_2_val
 
