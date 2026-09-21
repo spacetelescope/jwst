7cec0f8027937e0b4ac904086aed5d5eac35ba2c
Melanie Clarke <mclarke@stsci.edu>
2026-09-21 14:44:30 -0400

    Move wavecorr is_point_source to lib

--- jwst/lib/tests/test_exposure_types.py
+++ jwst/lib/tests/test_exposure_types.py
@@ -1,7 +1,7 @@
 import pytest
-from stdatamodels.jwst.datamodels import JwstDataModel
+from stdatamodels.jwst.datamodels import JwstDataModel, SlitModel
 
-from jwst.lib.exposure_types import is_moving_target
+from jwst.lib.exposure_types import is_moving_target, is_point_source
 
 
 @pytest.mark.parametrize("target_type", ["FIXED", "MOVING", None])
@@ -22,3 +22,30 @@ def test_is_moving_target(model_type, target_type):
 def test_moving_target_typeerror():
     with pytest.raises(TypeError):
         is_moving_target("not a datamodel or dict")
+
+
+@pytest.mark.parametrize("override", ["POINT", "EXTENDED", None])
+@pytest.mark.parametrize("level", ["top", "meta", None])
+@pytest.mark.parametrize("value", ["POINT", "EXTENDED", None])
+def test_is_point_source(override, level, value):
+    model = SlitModel()
+    if level == "top":
+        model.source_type = value
+        model.meta.target.source_type = None
+    elif level == "meta":
+        model.source_type = None
+        model.meta.target.source_type = value
+    else:
+        model.source_type = None
+        model.meta.target.source_type = None
+
+    check = is_point_source(model, override_srctype=override)
+    if override is not None:
+        if override == "POINT":
+            assert check is True
+        else:
+            assert check is False
+    elif level is not None and value == "POINT":
+        assert check is True
+    else:
+        assert check is False
